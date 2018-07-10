5014e8c64f18080dc345288b2d199bedcae8bce9
mbutrovich <mbutrovich@gmail.com>
2018-07-10 16:09:53 -0400

    Minor cleanup of Google Test CMake stuff.

--- cmake/GoogleTest.cmake
+++ cmake/GoogleTest.cmake
@@ -1,5 +1,7 @@
+# This was taken from Google Test's readme.md for CMake integration
+
 # Download and unpack googletest at configure time
-configure_file(cmake/Templates/CMakeLists.txt.in googletest-download/CMakeLists.txt)
+configure_file(cmake/Templates/GoogleTestConfig.cmake.in googletest-download/CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
   RESULT_VARIABLE result
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
@@ -22,15 +24,3 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
                  ${CMAKE_BINARY_DIR}/googletest-build
                  EXCLUDE_FROM_ALL)
-
-# The gtest/gtest_main targets carry header search path
-# dependencies automatically when using CMake 2.8.11 or
-# later. Otherwise we have to add them here ourselves.
-if (CMAKE_VERSION VERSION_LESS 2.8.11)
-  include_directories("${gtest_SOURCE_DIR}/include")
-endif()
-
-# Now simply link against gtest or gtest_main as needed. Eg
-#add_executable(example example.cpp)
-#target_link_libraries(example gtest_main)
-#add_test(NAME example_test COMMAND example)
