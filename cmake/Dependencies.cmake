ce150a5bc7dc34f8b5884597efb026f3133a0d06
mbutrovich <mbutrovich@gmail.com>
2018-07-11 13:44:32 -0400

    Add LLVM 6.0 dependency to CMake.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -22,6 +22,16 @@ list(APPEND TERRIER_LINKER_LIBS ${TBB_LIBRARIES})
 # ---[  Google Test
 list(APPEND TERRIER_LINKER_LIBS gtest_main)
 
+# --[ LLVM 6.0+
+find_package(LLVM REQUIRED CONFIG)
+message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+if (${LLVM_PACKAGE_VERSION} VERSION_LESS "6.0")
+    message( FATAL_ERROR "LLVM 6.0 or newer is required." )
+endif()
+llvm_map_components_to_libnames(LLVM_LIBRARIES core mcjit nativecodegen native)
+include_directories(SYSTEM ${LLVM_INCLUDE_DIRS})
+list(APPEND TERRIER_LINKER_LIBS ${LLVM_LIBRARIES})
+
 # ---[ Doxygen
 if(BUILD_docs)
     find_package(Doxygen)
