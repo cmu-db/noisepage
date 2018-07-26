95f0199c31b6d875534b48570c9b6745afcea2c9
mbutrovich <mbutrovich@gmail.com>
2018-07-26 15:22:46 -0400

    Formatting pass.

--- src/include/storage/tuple_access_strategy.h
+++ src/include/storage/tuple_access_strategy.h
@@ -42,7 +42,7 @@ struct BlockLayout {
   // header size is smaller than block size which is 1MB, so this cast is safe
   uint32_t HeaderSize() {
     return static_cast<uint32_t>(sizeof(uint32_t) * 3  // layout_version, num_records, num_slots
-           + num_cols_ * sizeof(uint32_t) + sizeof(uint16_t) + num_cols_ * sizeof(uint8_t));
+                                 + num_cols_ * sizeof(uint32_t) + sizeof(uint16_t) + num_cols_ * sizeof(uint8_t));
   }
 
   uint32_t NumSlots() {
