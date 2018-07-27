1bb8eeb95835544c6691008e5a04fd22d89e20ff
Aaron Tian <aatt@cmu.edu>
2018-07-27 15:12:59 -0400

    Add JsonSerializable class for printing state information

---
+++ src/include/common/json_serializable.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef __APPLE__
+#include <json/json.h>
+#else
+#include <jsoncpp/json/json.h>
+#endif
+
+namespace terrier {
+
+//===--------------------------------------------------------------------===//
+// Json Serializable Object
+//===--------------------------------------------------------------------===//
+
+/**
+ * @brief Interface to allow printing of debug information in Json foramt
+ *
+ * Most stateful classes should implement this interface and return useful
+ * information about its state for debugging purposes.
+ */
+class JsonSerializable {
+ public:
+  virtual ~JsonSerializable(){};
+
+  /** @brief Get the Json value about the state information. */
+  virtual Json::Value GetJson() = 0;
+};
+
+}  // namespace terrier
