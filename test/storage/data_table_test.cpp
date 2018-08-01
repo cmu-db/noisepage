5ef6e83580ee9669bf7b0d450101f48c3259d2a8
mbutrovich <mbutrovich@gmail.com>
2018-08-01 19:55:29 -0400

    Remove the float distribution for null_bias since that seems to be breaking stuff right now. Will revisit in AM.

--- test/storage/data_table_test.cpp
+++ test/storage/data_table_test.cpp
@@ -17,11 +17,11 @@ TEST_F(DataTableTests, SimpleInsertSelectTest) {
   const uint32_t num_inserts = 100;
   const uint16_t max_columns = 100;
 
-  std::uniform_real_distribution<> distribution(0.0, 1.0);
+//  std::uniform_real_distribution<> distribution(0.0, 1.0);
 
   for (uint16_t iteration = 0; iteration < num_iterations; ++iteration) {
 
-    double null_bias = distribution(generator_);
+//    double null_bias = distribution(generator_);
 
     storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
     storage::DataTable table(block_store_, layout);
@@ -41,7 +41,7 @@ TEST_F(DataTableTests, SimpleInsertSelectTest) {
       byte *redo_buffer = new byte[redo_size];
       redo_buffers[i] = redo_buffer;
       storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
-      testutil::GenerateRandomRow(redo, layout, generator_, null_bias);
+      testutil::GenerateRandomRow(redo, layout, generator_, 0);
 
       // generate an undo DeltaRecord to populate on Insert
       byte *undo_buffer = new byte[undo_size];
@@ -85,11 +85,11 @@ TEST_F(DataTableTests, SimpleVersionChainTest) {
   const uint32_t num_inserts = 10;
   const uint16_t max_columns = 100;
 
-  std::uniform_real_distribution<> distribution(0.0, 1.0);
+//  std::uniform_real_distribution<> distribution(0.0, 1.0);
 
   for (uint16_t iteration = 0; iteration < num_iterations; ++iteration) {
 
-    double null_bias = distribution(generator_);
+//    double null_bias = distribution(generator_);
 
     storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
     storage::DataTable table(block_store_, layout);
@@ -109,7 +109,7 @@ TEST_F(DataTableTests, SimpleVersionChainTest) {
       byte *redo_buffer = new byte[redo_size];
       redo_buffers[i] = redo_buffer;
       storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
-      testutil::GenerateRandomRow(redo, layout, generator_, null_bias);
+      testutil::GenerateRandomRow(redo, layout, generator_, 0);
 
       // generate an undo DeltaRecord to populate on Insert
       byte *undo_buffer = new byte[undo_size];
