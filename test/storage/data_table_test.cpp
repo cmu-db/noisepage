0a6c27b875d5f43b777b8eaa7981cc766b0b2584
mbutrovich <mbutrovich@gmail.com>
2018-08-01 17:33:53 -0400

    Minor refactor, added more repetitions for column layouts.

--- test/storage/data_table_test.cpp
+++ test/storage/data_table_test.cpp
@@ -12,62 +12,64 @@ struct DataTableTests : public ::testing::Test {
 TEST_F(DataTableTests, SimpleInsertTest) {
   uint32_t num_inserts = 1000;
   uint16_t max_columns = 100;
+  uint16_t num_repetitions = 10;
 
-  storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
-  storage::DataTable table(block_store_, layout);
+  for (uint16_t iteration = 0; iteration < num_repetitions; ++iteration) {
 
-  std::vector<byte *> insert_redos(num_inserts);
-  std::vector<byte *> insert_undos(num_inserts);
-  std::vector<std::pair<storage::TupleSlot, storage::ProjectedRow *>> inserted_tuples;
+    storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
+    std::cout << layout.num_cols_ << std::endl;
+    storage::DataTable table(block_store_, layout);
 
-  std::vector<uint16_t> col_ids = testutil::ProjectionListAllColumns(layout);
+    std::vector<byte *> insert_redos(num_inserts);
+    std::vector<byte *> insert_undos(num_inserts);
+    std::vector<std::pair<storage::TupleSlot, storage::ProjectedRow *>> inserted_tuples;
 
-  uint32_t redo_size = storage::ProjectedRow::Size(layout, col_ids);
-  uint32_t undo_size = storage::DeltaRecord::Size(layout, col_ids);
+    std::vector<uint16_t> col_ids = testutil::ProjectionListAllColumns(layout);
 
-  for (uint32_t i = 0; i < num_inserts; ++i) {
+    uint32_t redo_size = storage::ProjectedRow::Size(layout, col_ids);
+    uint32_t undo_size = storage::DeltaRecord::Size(layout, col_ids);
 
-    // generate a random redo ProjectedRow to Insert
-    byte *redo_buffer = new byte[redo_size];
-    insert_redos[i] = redo_buffer;
-    storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
-    testutil::GenerateRandomRow(redo, layout, generator_, 0);
+    for (uint32_t i = 0; i < num_inserts; ++i) {
 
-    // generate an undo DeltaRecord to populate on Insert
-    byte *undo_buffer = new byte[undo_size];
-    insert_undos[i] = undo_buffer;
-    storage::DeltaRecord *undo =
-        storage::DeltaRecord::InitializeDeltaRecord(nullptr, VALUE_OF(timestamp_t, 0ull), layout, col_ids, undo_buffer);
+      // generate a random redo ProjectedRow to Insert
+      byte *redo_buffer = new byte[redo_size];
+      insert_redos[i] = redo_buffer;
+      storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
+      testutil::GenerateRandomRow(redo, layout, generator_, 0);
 
-    storage::TupleSlot tuple = table.Insert(*redo, undo);
+      // generate an undo DeltaRecord to populate on Insert
+      byte *undo_buffer = new byte[undo_size];
+      insert_undos[i] = undo_buffer;
+      storage::DeltaRecord *undo =
+          storage::DeltaRecord::InitializeDeltaRecord(nullptr, VALUE_OF(timestamp_t, 0ull), layout, col_ids, undo_buffer);
 
-    inserted_tuples.emplace_back(tuple, redo);
-  }
+      storage::TupleSlot tuple = table.Insert(*redo, undo);
 
-  EXPECT_EQ(num_inserts, inserted_tuples.size());
+      inserted_tuples.emplace_back(tuple, redo);
+    }
 
+    EXPECT_EQ(num_inserts, inserted_tuples.size());
 
-  std::vector<byte *> select_buffers(num_inserts);
+    std::vector<byte *> select_buffers(num_inserts);
 
-  for (uint32_t i = 0; i < num_inserts; ++i) {
-    // generate a redo ProjectedRow for Select
-    byte *select_buffer = new byte[redo_size];
-    select_buffers[i] = select_buffer;
-    storage::ProjectedRow *select_row = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, select_buffer);
+    for (uint32_t i = 0; i < num_inserts; ++i) {
+      // generate a redo ProjectedRow for Select
+      byte *select_buffer = new byte[redo_size];
+      select_buffers[i] = select_buffer;
+      storage::ProjectedRow *select_row = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, select_buffer);
 
-    table.Select(VALUE_OF(timestamp_t, 1ull), inserted_tuples[i].first, select_row);
+      table.Select(VALUE_OF(timestamp_t, 1ull), inserted_tuples[i].first, select_row);
 
-    storage::ProjectedRow *inserted_row = inserted_tuples[i].second;
+      storage::ProjectedRow *inserted_row = inserted_tuples[i].second;
 
-    EXPECT_EQ(0, std::memcmp(inserted_row, select_row, redo_size));
-  }
+      EXPECT_EQ(0, std::memcmp(inserted_row, select_row, redo_size));
+    }
 
-  for (auto i : insert_redos) {
-    delete[] i;
-  }
-
-  for (auto i : insert_undos) {
-    delete[] i;
+    for (uint32_t i = 0; i < num_inserts; i++) {
+      delete[] insert_redos[i];
+      delete[] insert_undos[i];
+      delete[] select_buffers[i];
+    }
   }
 }
 
