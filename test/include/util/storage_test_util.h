384f2e5d0934dc5147d9ef97db15806b4cebdf9c
Tianyu Li <tli2@andrew.cmu.edu>
2018-08-02 14:50:24 -0400

    Add equality check for projected row.

--- test/include/util/storage_test_util.h
+++ test/include/util/storage_test_util.h
@@ -3,11 +3,12 @@
 #include <vector>
 #include "common/test_util.h"
 #include "storage/storage_defs.h"
+#include "storage/storage_utils.h"
 
 namespace terrier::testutil {
 
 // Returns a random layout that is guaranteed to be valid.
-template <typename Random>
+template<typename Random>
 storage::BlockLayout RandomLayout(Random &generator, uint16_t max_cols = UINT16_MAX) {
   PELOTON_ASSERT(max_cols > 1);
   // We probably won't allow tables with fewer than 2 columns
@@ -21,13 +22,13 @@ storage::BlockLayout RandomLayout(Random &generator, uint16_t max_cols = UINT16_
 
 // Fill the given location with the specified amount of random bytes, using the
 // given generator as a source of randomness.
-template <typename Random>
+template<typename Random>
 void FillWithRandomBytes(uint32_t num_bytes, byte *out, Random &generator) {
   std::uniform_int_distribution<uint8_t> dist(0, UINT8_MAX);
   for (uint32_t i = 0; i < num_bytes; i++) out[i] = static_cast<byte>(dist(generator));
 }
 
-template <typename Random>
+template<typename Random>
 void GenerateRandomRow(storage::ProjectedRow *row, const storage::BlockLayout &layout, Random &generator,
                        const double null_bias = 0.1) {
   // For every column in the project list, populate its attribute with random bytes or set to null based on coin flip
@@ -51,7 +52,7 @@ std::vector<uint16_t> ProjectionListAllColumns(const storage::BlockLayout &layou
   return col_ids;
 }
 
-template <typename Random>
+template<typename Random>
 std::vector<uint16_t> ProjectionListRandomColumns(const storage::BlockLayout &layout, Random &generator) {
   // randomly select a number of columns for this delta to contain. Must be at least 1, but shouldn't be num_cols since
   // we exclude the version vector column
@@ -71,5 +72,26 @@ std::vector<uint16_t> ProjectionListRandomColumns(const storage::BlockLayout &la
   col_ids.resize(num_cols);
 
   return col_ids;
+}
+
+bool ProjectionListEqual(const storage::BlockLayout &layout,
+                         const storage::ProjectedRow *one,
+                         const storage::ProjectedRow *other) {
+  if (one->NumColumns() != other->NumColumns()) return false;
+  for (uint16_t projection_list_index = 0; projection_list_index < one->NumColumns(); projection_list_index++) {
+    if (one->ColumnIds()[projection_list_index] != other->ColumnIds()[projection_list_index]) return false;
+  }
+
+  for (uint16_t projection_list_index = 0; projection_list_index < one->NumColumns(); projection_list_index++) {
+    uint8_t attr_size = layout.attr_sizes_[one->ColumnIds()[projection_list_index]];
+    const byte *one_content = one->AccessWithNullCheck(projection_list_index);
+    const byte *other_content = other->AccessWithNullCheck(projection_list_index);
+
+    if (!((one_content == nullptr && other_content == nullptr)
+        || storage::ReadBytes(attr_size, one_content) == storage::ReadBytes(attr_size, other_content)))
+      return false;
+  }
+
+  return true;
 }
 }  // namespace terrier::testutil
--- test/storage/data_table_test.cpp
+++ test/storage/data_table_test.cpp
@@ -7,21 +7,22 @@ namespace terrier {
 struct DataTableTests : public ::testing::Test {
   storage::BlockStore block_store_{100};
   std::default_random_engine generator_;
+
 };
 
 // Generates a random table layout and coin flip bias for an attribute being null, inserts num_inserts random tuples
 // into an empty DataTable. Then, Selects the inserted TupleSlots and compares the results to the original inserted
 // random tuple. Repeats for num_iterations.
 TEST_F(DataTableTests, SimpleInsertSelectTest) {
-  const uint16_t num_iterations = 100;
-  const uint32_t num_inserts = 100;
+  const uint32_t num_iterations = 10;
+  const uint32_t num_inserts = 100000;
   const uint16_t max_columns = 100;
 
-//  std::uniform_real_distribution<> distribution(0.0, 1.0);
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-  for (uint16_t iteration = 0; iteration < num_iterations; ++iteration) {
+  for (uint32_t iteration = 0; iteration < num_iterations; ++iteration) {
 
-//    double null_bias = distribution(generator_);
+    double null_bias = distribution(generator_);
 
     storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
     storage::DataTable table(block_store_, layout);
@@ -41,7 +42,7 @@ TEST_F(DataTableTests, SimpleInsertSelectTest) {
       byte *redo_buffer = new byte[redo_size];
       redo_buffers[i] = redo_buffer;
       storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
-      testutil::GenerateRandomRow(redo, layout, generator_, 0);
+      testutil::GenerateRandomRow(redo, layout, generator_, null_bias);
 
       // generate an undo DeltaRecord to populate on Insert
       byte *undo_buffer = new byte[undo_size];
@@ -66,7 +67,7 @@ TEST_F(DataTableTests, SimpleInsertSelectTest) {
 
       table.Select(timestamp_t(1), inserted_tuples[i].first, select_row);
 
-      EXPECT_EQ(0, std::memcmp(inserted_tuples[i].second, select_row, redo_size));
+      EXPECT_TRUE(testutil::ProjectionListEqual(layout, select_row, inserted_tuples[i].second));
     }
 
     for (uint32_t i = 0; i < num_inserts; i++) {
@@ -77,74 +78,17 @@ TEST_F(DataTableTests, SimpleInsertSelectTest) {
   }
 }
 
-// Generates a random table layout and coin flip bias for an attribute being null, inserts num_inserts random tuples
-// into an empty DataTable with timestamp 1. Then, Selects the inserted TupleSlots with timestamp 0, and ensures that
-// all NULL attributes are returned. Repeats for num_iterations.
-TEST_F(DataTableTests, SimpleVersionChainTest) {
-  const uint16_t num_iterations = 1000;
-  const uint32_t num_inserts = 10;
-  const uint16_t max_columns = 100;
 
-//  std::uniform_real_distribution<> distribution(0.0, 1.0);
-
-  for (uint16_t iteration = 0; iteration < num_iterations; ++iteration) {
-
-//    double null_bias = distribution(generator_);
-
-    storage::BlockLayout layout = testutil::RandomLayout(generator_, max_columns);
-    storage::DataTable table(block_store_, layout);
-
-    std::vector<byte *> redo_buffers(num_inserts);
-    std::vector<byte *> undo_buffers(num_inserts);
-    std::vector<std::pair<storage::TupleSlot, storage::ProjectedRow *>> inserted_tuples;
-
-    std::vector<uint16_t> col_ids = testutil::ProjectionListAllColumns(layout);
-
-    uint32_t redo_size = storage::ProjectedRow::Size(layout, col_ids);
-    uint32_t undo_size = storage::DeltaRecord::Size(layout, col_ids);
-
-    for (uint32_t i = 0; i < num_inserts; ++i) {
-
-      // generate a random redo ProjectedRow to Insert
-      byte *redo_buffer = new byte[redo_size];
-      redo_buffers[i] = redo_buffer;
-      storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, redo_buffer);
-      testutil::GenerateRandomRow(redo, layout, generator_, 0);
-
-      // generate an undo DeltaRecord to populate on Insert
-      byte *undo_buffer = new byte[undo_size];
-      undo_buffers[i] = undo_buffer;
-      storage::DeltaRecord *undo =
-          storage::DeltaRecord::InitializeDeltaRecord(nullptr, timestamp_t(1), layout, col_ids, undo_buffer);
-
-      storage::TupleSlot tuple = table.Insert(*redo, undo);
-
-      inserted_tuples.emplace_back(tuple, redo);
-    }
-
-    EXPECT_EQ(num_inserts, inserted_tuples.size());
-
-    std::vector<byte *> select_buffers(num_inserts);
-
-    for (uint32_t i = 0; i < num_inserts; ++i) {
-      // generate a redo ProjectedRow for Select
-      byte *select_buffer = new byte[redo_size];
-      select_buffers[i] = select_buffer;
-      storage::ProjectedRow *select_row = storage::ProjectedRow::InitializeProjectedRow(layout, col_ids, select_buffer);
-
-      table.Select(timestamp_t(0), inserted_tuples[i].first, select_row);
-
-      for (uint16_t j = 0; j < select_row->NumColumns(); j++) {
-        EXPECT_EQ(nullptr, select_row->AccessWithNullCheck(j));
-      }
-    }
-
-    for (uint32_t i = 0; i < num_inserts; i++) {
-      delete[] redo_buffers[i];
-      delete[] undo_buffers[i];
-      delete[] select_buffers[i];
-    }
-  }
-}
+//TEST_F(DataTableTests, VersionChainTest) {
+//  const uint32_t num_iterations = 100;
+//  const uint32_t num_tuples = 100;
+//  const uint32_t num_versions = 5;
+//  const uint32_t max_col = 100;
+//  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//  for (uint32_t iter = 0; iter < num_iterations; iter++) {
+//     storage::BlockLayout layout = testutil::RandomLayout(generator_, max_col);
+//
+//  }
+//}
 
 }  // namespace terrier
