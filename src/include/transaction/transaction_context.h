03599588070c9429b949f676e4383c7e6d6926de
Tianyu Li <tli2@andrew.cmu.edu>
2018-08-09 17:53:34 -0400

    Some documentation

--- src/include/transaction/transaction_context.h
+++ src/include/transaction/transaction_context.h
@@ -9,19 +9,43 @@ namespace terrier::storage {
 class DataTable;
 }
 namespace terrier::transaction {
-// TODO(Tianyu): Change to be significant larger than a single record could be
+// TODO(Tianyu): Size of this buffer can probably be optimized
+// TODO(Tianyu): Put this into common::Constants?
 #define UNDO_BUFFER_SEGMENT_SIZE (1 << 15)
 
+/**
+ * An UndoBufferSegment is a piece of (reusable) memory used to hold undo records. The segment internally keeps track
+ * of its memory usage.
+ *
+ * This class should not be used by itself. @see UndoBuffer.
+ *
+ * Not thread-safe.
+ */
 class UndoBufferSegment {
  public:
+  /**
+   * @param size the amount of bytes to check for
+   * @return Whether this segment have enough space left for size many bytes
+   */
   bool HasBytesLeft(uint32_t size) { return end_ + size <= UNDO_BUFFER_SEGMENT_SIZE; }
 
+  /**
+   * Reserve space for a delta record of given size to be written in this segment. The segment must have
+   * enough space for that record.
+   *
+   * @param size the amount of bytes to reserve
+   * @return pointer to the head of the allocated undo record
+   */
   storage::DeltaRecord *Reserve(uint32_t size) {
+    PELOTON_ASSERT(HasBytesLeft(size), "buffer segment allocation out of bounds");
     auto *result = reinterpret_cast<storage::DeltaRecord *>(bytes_ + end_);
     end_ += size;
     return result;
   }
 
+  /**
+   * Clears the buffer segment.
+   */
   void Reset() { end_ = 0; }
 
  private:
@@ -30,20 +54,48 @@ class UndoBufferSegment {
   byte bytes_[UNDO_BUFFER_SEGMENT_SIZE];
 };
 
-// TODO(Tianyu): Not thread-safe. We can probably just allocate thread-local buffers if we ever want
+// TODO(Tianyu): Not thread-safe. We can probably just allocate thread-local buffers (or segments) if we ever want
 // multiple workers on the same transaction.
+/**
+ * An UndoBuffer is a resizable buffer to hold UndoRecords.
+ *
+ * Pointers to UndoRecords and Iterators handed out from the UndoBuffer is guaranteed to be persistent. Adding new
+ * entries to the buffer does not interfere with existing pointers and iterators.
+ *
+ * UndoRecords should not be removed from UndoBuffer until the transaction's end (only in the destructor)
+ *
+ * Not thread-safe
+ */
 class UndoBuffer {
  public:
+  /**
+   * Iterator to access DeltaRecords inside the undo buffer.
+   *
+   * Iterators are always guaranteed to be valid once handed out, provided that the UndoBuffer is not
+   * destructed, regardless of modifications to the UndoBuffer.
+   *
+   * Not thread-safe
+   */
   class Iterator {
    public:
+    /**
+     * @return reference to the underlying UndoRecord
+     */
     storage::DeltaRecord &operator*() const {
       return *reinterpret_cast<storage::DeltaRecord *>((*curr_segment_)->bytes_ + segment_offset_);
     }
 
+    /**
+     * @return pointer to the underlying UndoRecord
+     */
     storage::DeltaRecord *operator->() const {
       return reinterpret_cast<storage::DeltaRecord *>((*curr_segment_)->bytes_ + segment_offset_);
     }
 
+    /**
+     * prefix-increment
+     * @return self-reference
+     */
     Iterator &operator++() {
       storage::DeltaRecord &me = this->operator*();
       segment_offset_ += me.Size();
@@ -56,16 +108,30 @@ class UndoBuffer {
       return *this;
     }
 
+    /**
+     * postfix-increment
+     * @return iterator equal to this iterator before increment
+     */
     const Iterator operator++(int) {
       Iterator copy = *this;
       operator++();
       return copy;
     }
 
+    /**
+     * Equality test
+     * @param other iterator to compare to
+     * @return if this is equal to other
+     */
     bool operator==(const Iterator &other) const {
       return segment_offset_ == other.segment_offset_ && curr_segment_ == other.curr_segment_;
     }
 
+    /**
+     * Inequality test
+     * @param other iterator to compare to
+     * @return if this is not equal to other
+     */
     bool operator!=(const Iterator &other) const { return !(*this == other); }
 
    private:
@@ -76,14 +142,27 @@ class UndoBuffer {
     uint32_t segment_offset_;
   };
 
+  /**
+   * Constructs a new undo buffer, drawing its segments from the given buffer pool.
+   * @param buffer_pool buffer pool to draw segments from
+   */
   explicit UndoBuffer(common::ObjectPool<UndoBufferSegment> *buffer_pool) : buffer_pool_(buffer_pool) {}
 
+  /**
+   * Destructs this buffer, releases all its segments back to the buffer pool it draws from.
+   */
   ~UndoBuffer() {
     for (auto *segment : buffers_) buffer_pool_->Release(segment);
   }
 
+  /**
+   * @return Iterator to the first element
+   */
   Iterator Begin() { return {buffers_.begin(), 0}; }
 
+  /**
+   * @return Iterator to the element following the last element
+   */
   Iterator End() { return {buffers_.end(), 0}; }
 
  private:
