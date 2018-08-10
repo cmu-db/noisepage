09a553d0946081810a4762e797b78dc6edf06cea
Tianyu Li <tli2@andrew.cmu.edu>
2018-08-10 12:50:32 -0400

    More documentation.

--- src/include/transaction/transaction_context.h
+++ src/include/transaction/transaction_context.h
@@ -181,17 +181,42 @@ class UndoBuffer {
   std::vector<UndoBufferSegment *> buffers_;
 };
 
+/**
+ * A transaction context encapsulates the information kept while the transaction is runnning
+ */
 class TransactionContext {
  public:
+  /**
+   * Constructs a new transaction context
+   * @param start the start timestamp of the transaction
+   * @param txn_id the id of the transaction, should be larger than all start time and commit time
+   * @param buffer_pool the buffer pool to draw this transaction's undo buffer from
+   */
   TransactionContext(timestamp_t start, timestamp_t txn_id, common::ObjectPool<UndoBufferSegment> *buffer_pool)
       : start_time_(start), txn_id_(txn_id), undo_buffer_(buffer_pool) {}
 
+  /**
+   * @return start time of this transaction
+   */
   timestamp_t StartTime() const { return start_time_; }
 
+  /**
+   * @return id of this tranaction
+   */
   timestamp_t TxnId() const { return txn_id_; }
 
+  /**
+   * @return the undo buffer of this transaction
+   */
   UndoBuffer &GetUndoBuffer() { return undo_buffer_; }
 
+  /**
+   * Reserve space on this transaction's undo buffer for a record to log the update given
+   * @param table pointer to the updated DataTable object
+   * @param slot the TupleSlot being updated
+   * @param redo the content of the update
+   * @return a persistent pointer to the head of a memory chunk large enough to hold the undo record
+   */
   storage::DeltaRecord *UndoRecordForUpdate(storage::DataTable *table, storage::TupleSlot slot,
                                             const storage::ProjectedRow &redo) {
     uint32_t size = storage::DeltaRecord::Size(redo);
@@ -199,8 +224,13 @@ class TransactionContext {
     return storage::DeltaRecord::InitializeDeltaRecord(result, txn_id_, slot, table, redo);
   }
 
-  // TODO(Tianyu): Whether this flips a slot back to being unallocated,
-  // or logically deleted (and GC deallocate it) is up for debate
+  /**
+   * Reserve space on this transaction's undo buffer for a record to log the insert given
+   * @param table pointer to the updated DataTable object
+   * @param layout the layout of the insert target
+   * @param slot the TupleSlot being updated
+   * @return a persistent pointer to the head of a memory chunk large enough to hold the undo record
+   */
   storage::DeltaRecord *UndoRecordForInsert(storage::DataTable *table, const storage::BlockLayout &layout,
                                             storage::TupleSlot slot) {
     // TODO(Tianyu): Remove magic constant
--- src/include/transaction/transaction_manager.h
+++ src/include/transaction/transaction_manager.h
@@ -6,16 +6,33 @@
 #include "transaction/transaction_context.h"
 
 namespace terrier::transaction {
+/**
+ * A transaction manager maintains global state about all running transactions, and is responsible for creating,
+ * committing and aborting transactions
+ */
 class TransactionManager {
   // TODO(Tianyu): Implement the global transaction tables
  public:
+  /**
+   * Initializes a new transaction manager. Transactions will use the given object pool as source of their undo
+   * buffers.
+   * @param buffer_pool the buffer pool to use for transaction undo buffers
+   */
   explicit TransactionManager(common::ObjectPool<UndoBufferSegment> *buffer_pool) : buffer_pool_(buffer_pool) {}
 
+  /**
+   * Begins a transaction.
+   * @return transaction context for the newly begun transaction
+   */
   TransactionContext BeginTransaction() {
     tbb::reader_writer_lock::scoped_lock_read guard(commit_latch_);
     return {time_++, txn_id_++, buffer_pool_};
   }
 
+  /**
+   * Commits a transaction, making all of its changes visible to others.
+   * @param txn the transaction to commit
+   */
   void Commit(TransactionContext *txn) {
     tbb::reader_writer_lock::scoped_lock guard(commit_latch_);
     timestamp_t commit_time = time_++;
@@ -24,6 +41,10 @@ class TransactionManager {
     for (auto it = undos.Begin(); it != undos.End(); ++it) it->Timestamp().store(commit_time);
   }
 
+  /**
+   * Aborts a transaction, rolling back its changes (if any).
+   * @param txn the transaction to abort.
+   */
   void Abort(TransactionContext *txn) {
     // no latch required on undo since all operations are transaction-local
     UndoBuffer &undos = txn->GetUndoBuffer();
