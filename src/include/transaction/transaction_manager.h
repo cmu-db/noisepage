05ef66cd43b5beb5f97fb733e24bc76caa780e3f
Tianyu Li <tli2@andrew.cmu.edu>
2018-08-12 12:56:05 -0400

    Add a retarded global transactions table.

--- src/include/transaction/transaction_manager.h
+++ src/include/transaction/transaction_manager.h
@@ -4,6 +4,7 @@
 #include "common/typedefs.h"
 #include "storage/data_table.h"
 #include "transaction/transaction_context.h"
+#include "common/rw_latch.h"
 
 namespace terrier::transaction {
 /**
@@ -25,8 +26,18 @@ class TransactionManager {
    * @return transaction context for the newly begun transaction
    */
   TransactionContext *BeginTransaction() {
-    tbb::reader_writer_lock::scoped_lock_read guard(commit_latch_);
-    return new TransactionContext{time_++, txn_id_++, buffer_pool_};
+    common::ReaderWriterLatch::ScopedReaderLatch guard(&commit_latch_);
+    // TODO(Tianyu):
+    // Maybe embed this into the data structure, or use an object pool?
+    // Doing this with std::map or other data structure is risky though, as they may not
+    // guarantee that the iterator or underlying pointer is stable across operations.
+    // (That is, they may change as concurrent inserts and deletes happen)
+    auto *result = new TransactionContext(time_++, txn_id_++, buffer_pool_);
+    table_latch_.Lock();
+    auto ret UNUSED_ATTRIBUTE = curr_running_txns_.emplace(result->StartTime(), result);
+    PELOTON_ASSERT(ret.second, "commit start time should be globally unique");
+    table_latch_.Unlock();
+    return result;
   }
 
   /**
@@ -34,11 +45,15 @@ class TransactionManager {
    * @param txn the transaction to commit
    */
   void Commit(TransactionContext *txn) {
-    tbb::reader_writer_lock::scoped_lock guard(commit_latch_);
+    common::ReaderWriterLatch::ScopedWriterLatch guard(&commit_latch_);
     timestamp_t commit_time = time_++;
     // Flip all timestamps to be committed
     UndoBuffer &undos = txn->GetUndoBuffer();
     for (auto it = undos.Begin(); it != undos.End(); ++it) it->Timestamp().store(commit_time);
+    table_latch_.Lock();
+    auto ret UNUSED_ATTRIBUTE = curr_running_txns_.erase(txn->StartTime());
+    PELOTON_ASSERT(ret == 1, "committed transaction did not exist in global transactions table");
+    table_latch_.Unlock();
   }
 
   /**
@@ -49,16 +64,42 @@ class TransactionManager {
     // no latch required on undo since all operations are transaction-local
     UndoBuffer &undos = txn->GetUndoBuffer();
     for (auto it = undos.Begin(); it != undos.End(); ++it) it->Table()->Rollback(txn->TxnId(), it->Slot());
+    table_latch_.Lock();
+    auto ret UNUSED_ATTRIBUTE = curr_running_txns_.erase(txn->StartTime());
+    PELOTON_ASSERT(ret == 1, "aborted transaction did not exist in global transactions table");
+    table_latch_.Unlock();
+  }
+
+  /**
+   * Get the oldest transaction alive in the system at this time. Because of concurrent operations, it
+   * is not guaranteed that upon return the txn is still alive. However, it is guaranteed that the return
+   * timestamp is older than any transactions live.
+   * @return timestamp that is older than any transactions alive
+   */
+  timestamp_t OldestTransactionStartTime() const {
+    table_latch_.Lock();
+    timestamp_t result = curr_running_txns_.begin()->second->StartTime();
+    table_latch_.Unlock();
+    return result;
   }
 
  private:
   common::ObjectPool<UndoBufferSegment> *buffer_pool_;
   // TODO(Tianyu): Timestamp generation needs to be more efficient
   std::atomic<timestamp_t> time_{timestamp_t(0)};
+  // TODO(Tianyu): Actually, maybe we can just set txn_id to be start time with the first bit flipped? Start time
+  // is globally unique in our setup anyway...
   std::atomic<timestamp_t> txn_id_{timestamp_t(static_cast<uint64_t>(INT64_MIN))};  // start from "negative" value
 
   // TODO(Tianyu): Maybe don't use tbb?
   // TODO(Tianyu): This is the famed HyPer Latch. We will need to re-evaluate performance later.
-  tbb::reader_writer_lock commit_latch_;
+  mutable common::ReaderWriterLatch commit_latch_;
+
+  // TODO(Tianyu): Get a better data structure for this.
+  // TODO(Tianyu): Also, we are leveraging off the fact that we know start time to be globally unique, so we should
+  // think about this when refactoring the txn id thing.
+  mutable common::SpinLatch table_latch_;
+  std::map<timestamp_t, TransactionContext *> curr_running_txns_;
+
 };
 }  // namespace terrier::transaction
