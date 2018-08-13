af992bc4432d3292efa4ce88ee7aa7e811664e7b
Aaron Tian <aatt@cmu.edu>
2018-08-13 00:41:17 -0400

    Update comments for LogManager

--- src/include/logging/log_manager.h
+++ src/include/logging/log_manager.h
@@ -7,21 +7,21 @@ namespace terrier::logging {
 
 /**
  * A LogManager allocates free buffers to work threads, and writes a buffer to
- * a log file after the buffer is returned by a work thread, where the buffer
- * either gets full or the transaction commits.
+ * a log file after the buffer is returned by a work thread, where either the
+ * buffer gets full or a transaction commits.
  */
 class LogManager {
  public:
   /**
-   * A LogManager is only initiated with parameters when the system starts,
-   * and should never be copied or moved.
+   * @brief A LogManager is only initiated with parameters when the system
+   * starts, and should never be copied or moved.
    */
   LogManager() = delete;
   DISALLOW_COPY_AND_MOVE(LogManager);
 
   /**
-   * Constructs a LogManager with the given logging switch, using the given
-   * directory to store log files if logging is enabled.
+   * @brief Constructs a LogManager with the given logging switch, using the
+   * given directory to store log files if logging is enabled.
    *
    * @param enabled whether the LogManager is enabled
    * @param dir the directory where the log file is stored
@@ -34,26 +34,26 @@ class LogManager {
   }
 
   /**
-   * Destructs a LogManager, closes the log file.
+   * @brief Destructs a LogManager, closes the log file.
    */
   ~LogManager() { file_.close(); }
 
   /**
-   * Get the log file path.
+   * @brief Gets the log file path.
    *
    * @return the log file path
    */
   bool IsEnabled() { return enabled_; }
 
   /**
-   * Get the log file path.
+   * @brief Gets the log file path.
    *
    * @return the log file path
    */
   std::string GetPath() { return dir_ + "/" + file_name_; }
 
   /**
-   * Get the log file stream.
+   * @brief Gets the log file stream.
    *
    * @return the log file stream
    */
