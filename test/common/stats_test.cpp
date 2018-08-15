fdbd35ea4bc1dc334dd438ca54834903e2a221b0
ksaito7 <new.wahiro@gmail.com>
2018-08-15 19:48:46 -0400

    Add destructor test

--- test/common/stats_test.cpp
+++ test/common/stats_test.cpp
@@ -9,14 +9,14 @@ namespace terrier {
 // Block store performance counter Tests
 //===--------------------------------------------------------------------===//
 
-TEST(BlockStorePCTests, BlockCountTest) {
+TEST(StatsTests, ObjectPoolStatsTest) {
   const uint64_t reuse_limit = 1;
   common::StatsCollector stats_collector;
-  storage::BlockStore tested(reuse_limit, &stats_collector);
+  storage::BlockStore *tested = new storage::BlockStore(reuse_limit, &stats_collector);
 
   // Create RawBlock
-  storage::RawBlock *block_ptr1 = tested.Get();
-  storage::RawBlock *block_ptr2 = tested.Get();
+  storage::RawBlock *block_ptr1 = tested->Get();
+  storage::RawBlock *block_ptr2 = tested->Get();
   EXPECT_EQ(0, stats_collector.GetCounter("create block"));
   EXPECT_EQ(0, stats_collector.GetCounter("reuse block"));
   stats_collector.ColloectCounters();
@@ -25,13 +25,13 @@ TEST(BlockStorePCTests, BlockCountTest) {
   EXPECT_EQ(0, stats_collector.GetCounter("reuse block"));
 
   // Release RawBlock
-  tested.Release(block_ptr1);
-  tested.Release(block_ptr2);
+  tested->Release(block_ptr1);
+  tested->Release(block_ptr2);
 
   // Create again
-  storage::RawBlock *block_ptr3 = tested.Get();
-  storage::RawBlock *block_ptr4 = tested.Get();
-  storage::RawBlock *block_ptr5 = tested.Get();
+  storage::RawBlock *block_ptr3 = tested->Get();
+  storage::RawBlock *block_ptr4 = tested->Get();
+  storage::RawBlock *block_ptr5 = tested->Get();
   EXPECT_EQ(2, stats_collector.GetCounter("create block"));
   EXPECT_EQ(0, stats_collector.GetCounter("reuse block"));
   stats_collector.ColloectCounters();
@@ -40,9 +40,17 @@ TEST(BlockStorePCTests, BlockCountTest) {
   EXPECT_EQ(2, stats_collector.GetCounter("reuse block"));
 
   // Release RawBlock
-  tested.Release(block_ptr3);
-  tested.Release(block_ptr4);
-  tested.Release(block_ptr5);
+  tested->Release(block_ptr3);
+  tested->Release(block_ptr4);
+  tested->Release(block_ptr5);
+
+  // Destructor test
+  storage::RawBlock *block_ptr6 = tested->Get();
+  tested->Release(block_ptr6);
+  delete tested;
+  stats_collector.PrintStats();
+  EXPECT_EQ(3, stats_collector.GetCounter("create block"));
+  EXPECT_EQ(3, stats_collector.GetCounter("reuse block"));
 }
 
 }  // namespace terrier
