7d81629a1d415157c13a4a9784a5cd604fbaacc6
Matt Butrovich <mbutrovich@users.noreply.github.com>
2018-08-17 13:14:05 -0400

    Benchmarks refactor (#104)

--- benchmark/storage/data_table_benchmark.cpp
+++ benchmark/storage/data_table_benchmark.cpp
@@ -1,6 +1,5 @@
-#include <unordered_map>
-#include <utility>
 #include <vector>
+
 #include "benchmark/benchmark.h"
 #include "common/typedefs.h"
 #include "storage/data_table.h"
@@ -13,96 +12,86 @@
 
 namespace terrier {
 
-// Test raw DataTable insert time. Generate a fixed layout allocate undo and redo buffers, and then reuse them to
-// repeatedly insert the same garbage tuple over and over into the DataTable to test throughput.
-// NOLINTNEXTLINE
-static void BM_SimpleInsert(benchmark::State &state) {
-  std::default_random_engine generator;
-  const uint32_t num_inserts = 100000;
+// This benchmark simulates a key-value store inserting a large number of tuples. This provides a good baseline and
+// reference to other fast data structures (indexes) to compare against. We are interested in the DataTable's raw
+// performance, so the tuple's contents are intentionally left garbage and we don't verify correctness. That's the job
+// of the Google Tests.
 
-  storage::BlockStore block_store{1000};
-  common::ObjectPool<transaction::UndoBufferSegment> buffer_pool{num_inserts};
-  transaction::TransactionManager txn_manager(&buffer_pool);
+class DataTableBenchmark : public benchmark::Fixture {
+ public:
+  void SetUp(const benchmark::State &state) final {
+    // generate a random redo ProjectedRow to Insert
+    redo_buffer_ = new byte[redo_size_];
+    redo_ = storage::ProjectedRow::InitializeProjectedRow(redo_buffer_, all_col_ids_, layout_);
+    StorageTestUtil::PopulateRandomRow(redo_, layout_, 0, &generator_);
+  }
+  void TearDown(const benchmark::State &state) final {
+    delete[] redo_buffer_;
+  }
 
   // Tuple layout
-  uint16_t num_columns = 2;
-  uint8_t column_size = 8;
-  storage::BlockLayout layout(num_columns, {column_size, column_size});
+  const uint16_t num_columns_ = 2;
+  const uint8_t column_size_ = 8;
+  const storage::BlockLayout layout_{num_columns_, {column_size_, column_size_}};
 
-  std::vector<uint16_t> all_col_ids_{StorageTestUtil::ProjectionListAllColumns(layout)};
-  uint32_t redo_size_ = storage::ProjectedRow::Size(layout, all_col_ids_);
-  uint32_t undo_size_ = storage::DeltaRecord::Size(layout, all_col_ids_);
+  // Tuple properties
+  const std::vector<uint16_t> all_col_ids_{StorageTestUtil::ProjectionListAllColumns(layout_)};
+  const uint32_t redo_size_ = storage::ProjectedRow::Size(layout_, all_col_ids_);
 
-  // generate a random redo ProjectedRow to Insert
-  auto *redo_buffer = new byte[redo_size_];
-  storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(redo_buffer, all_col_ids_, layout);
-  StorageTestUtil::PopulateRandomRow(redo, layout, 0, &generator);
+  // Workload
+  const uint32_t num_inserts_ = 10000000;
+  const uint32_t num_threads_ = 8;
 
-  // Populate the table with tuples
-  while (state.KeepRunning()) {
-    storage::DataTable table(&block_store, layout);
-    transaction::TransactionContext txn(timestamp_t(0), timestamp_t(0), &buffer_pool);
-    for (uint32_t i = 0; i < num_inserts; ++i) {
-      table.Insert(&txn, *redo);
+  // Test infrastructure
+  std::default_random_engine generator_;
+  storage::BlockStore block_store_{1000};
+  common::ObjectPool<transaction::UndoBufferSegment> buffer_pool_{num_inserts_};
+
+  // Insert buffer pointers
+  byte *redo_buffer_;
+  storage::ProjectedRow *redo_;
+};
+
+// Insert the num_inserts_ of tuples into a DataTable in a single thread
+// NOLINTNEXTLINE
+BENCHMARK_DEFINE_F(DataTableBenchmark, SimpleInsert)(benchmark::State &state) {
+  // NOLINTNEXTLINE
+  for (auto _ : state) {
+    storage::DataTable table(&block_store_, layout_);
+    // We can use dummy timestamps here since we're not invoking concurrency control
+    transaction::TransactionContext txn(timestamp_t(0), timestamp_t(0), &buffer_pool_);
+    for (uint32_t i = 0; i < num_inserts_; ++i) {
+      table.Insert(&txn, *redo_);
     }
   }
 
-  delete[] redo_buffer;
-
-  state.SetBytesProcessed(state.iterations() * num_inserts * (redo_size_ + undo_size_));
-  state.SetItemsProcessed(state.iterations() * num_inserts);
+  state.SetItemsProcessed(state.iterations() * num_inserts_);
 }
 
-
-// Test raw DataTable insert time concurrently. Generate a fixed layout allocate undo and redo buffers, and then reuse
-// them to repeatedly insert the same garbage tuple over and over into the DataTable to test throughput. Expect high
-// contention on this benchmark right now due to inserting into a single block in the DataTable.
+// Insert the num_inserts_ of tuples into a DataTable concurrently
 // NOLINTNEXTLINE
-static void BM_ConcurrentInsert(benchmark::State &state) {
-  std::default_random_engine generator;
-  const uint32_t num_inserts = 100000;
-
-  storage::BlockStore block_store{1000};
-  common::ObjectPool<transaction::UndoBufferSegment> buffer_pool{num_inserts};
-
-  // Tuple layout
-  uint16_t num_columns = 2;
-  uint8_t column_size = 8;
-  storage::BlockLayout layout(num_columns, {column_size, column_size});
-
-  std::vector<uint16_t> all_col_ids_{StorageTestUtil::ProjectionListAllColumns(layout)};
-  uint32_t redo_size_ = storage::ProjectedRow::Size(layout, all_col_ids_);
-  uint32_t undo_size_ = storage::DeltaRecord::Size(layout, all_col_ids_);
-
-  const uint32_t num_threads = 8;
-
-  // generate a random redo ProjectedRow to Insert
-  auto *redo_buffer = new byte[redo_size_];
-  storage::ProjectedRow *redo = storage::ProjectedRow::InitializeProjectedRow(redo_buffer, all_col_ids_, layout);
-  StorageTestUtil::PopulateRandomRow(redo, layout, 0, &generator);
-
-  while (state.KeepRunning()) {
-    storage::DataTable table(&block_store, layout);
+BENCHMARK_DEFINE_F(DataTableBenchmark, ConcurrentInsert)(benchmark::State &state) {
+  // NOLINTNEXTLINE
+  for (auto _ : state) {
+    storage::DataTable table(&block_store_, layout_);
     auto workload = [&](uint32_t id) {
-      transaction::TransactionContext txn(timestamp_t(0), timestamp_t(0), &buffer_pool);
-      for (uint32_t i = 0; i < num_inserts / num_threads; i++)
-        table.Insert(&txn, *redo);
+      // We can use dummy timestamps here since we're not invoking concurrency control
+      transaction::TransactionContext txn(timestamp_t(0), timestamp_t(0), &buffer_pool_);
+      for (uint32_t i = 0; i < num_inserts_ / num_threads_; i++)
+        table.Insert(&txn, *redo_);
     };
-    MultiThreadedTestUtil::RunThreadsUntilFinish(num_threads, workload);
+    MultiThreadedTestUtil::RunThreadsUntilFinish(num_threads_, workload);
   }
 
-  delete[] redo_buffer;
-
-  state.SetBytesProcessed(state.iterations() * num_inserts * (redo_size_ + undo_size_));
-  state.SetItemsProcessed(state.iterations() * num_inserts);
+  state.SetItemsProcessed(state.iterations() * num_inserts_);
 }
 
-BENCHMARK(BM_SimpleInsert)
+BENCHMARK_REGISTER_F(DataTableBenchmark, SimpleInsert)
     ->Repetitions(10)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 
-BENCHMARK(BM_ConcurrentInsert)
+BENCHMARK_REGISTER_F(DataTableBenchmark, ConcurrentInsert)
     ->Repetitions(10)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
