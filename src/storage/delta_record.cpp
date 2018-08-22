12c6106e1aace4e6ad68292fb3274944d24685d4
Tianyu Li <tli2@andrew.cmu.edu>
2018-08-22 15:29:01 -0400

    fix dumb std::move bug

--- src/storage/delta_record.cpp
+++ src/storage/delta_record.cpp
@@ -17,13 +17,13 @@ ProjectedRow *ProjectedRow::CopyProjectedRowLayout(void *head, const ProjectedRo
 ProjectedRowInitializer::ProjectedRowInitializer(const terrier::storage::BlockLayout &layout,
                                                  std::vector<uint16_t> col_ids)
     : col_ids_(std::move(col_ids)),
-      offsets_(col_ids.size()) {
+      offsets_(col_ids_.size()) {
   TERRIER_ASSERT(!col_ids_.empty(), "cannot initialize an empty ProjectedRow");
   size_ = sizeof(ProjectedRow);  // size and num_col size
   // space needed to store col_ids, must be padded up so that the following offsets are aligned
   size_ = StorageUtil::PadUpToSize(sizeof(uint32_t), size_ + static_cast<uint32_t>(col_ids_.size() * sizeof(uint16_t)));
   // space needed to store value offsets, TODO(Tianyu): I don't think bitmaps need to be padded up to anything?
-  size_ += static_cast<uint32_t>(col_ids.size() * sizeof(uint32_t));
+  size_ += static_cast<uint32_t>(col_ids_.size() * sizeof(uint32_t));
   // space needed to store the bitmap, padded up to the size of the first value in this projected row
   size_ = StorageUtil::PadUpToSize(layout.attr_sizes_[col_ids_[0]],
                                    size_ + common::RawBitmap::SizeInBytes(static_cast<uint32_t>(col_ids_.size())));
