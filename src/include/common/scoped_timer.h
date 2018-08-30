2bbb7386a8dd411779830c569dd3ced7872b6b66
mbutrovich <mbutrovich@gmail.com>
2018-08-30 14:19:35 -0400

    PR feedback and added more documentation about acceptable use cases.

--- src/include/common/scoped_timer.h
+++ src/include/common/scoped_timer.h
@@ -13,11 +13,13 @@ namespace terrier::common {
 /**
  * The ScopedTimer provided an easy way to collect the elapsed time for a block of code. It stores the current time when
  * it is instantiated, and then when it is destructed it updates the elapsed milliseconds time in the value pointed to
- * by the constructor argument.
+ * by the constructor argument. This is meant for debugging the performance impact of components that don't have
+ * dedicate benchmarks, and possibly for use with performance counters in the future. Its output shouldn't be used as
+ * pass/fail criteria in any Google Benchmarks (use the framework for that) or Google Tests (performance is variable
+ * from machine to machine).
  */
 class ScopedTimer {
  public:
-  ScopedTimer() = delete;
   /**
    * Constucts a ScopedTimer. This marks the beginning of the timer's elapsed time.
    * @param elapsed_ms pointer to write the elapsed time (milliseconds) on destruction
