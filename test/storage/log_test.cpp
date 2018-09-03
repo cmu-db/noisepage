3a1d707d87d27feaa86cc1fac44d4f7a46510c60
Tianyu Li <tli2@andrew.cmu.edu>
2018-09-03 12:33:27 -0400

    memory leak

--- test/storage/log_test.cpp
+++ test/storage/log_test.cpp
@@ -3,27 +3,60 @@
 #include "storage/log_manager.h"
 #include "transaction/transaction_manager.h"
 #include "util/storage_test_util.h"
+#include "util/test_harness.h"
+#include "util/transaction_test_util.h"
+#include "gtest/gtest.h"
 
 namespace terrier {
+class LargeLoggingTests : public TerrierTest {
+ public:
+  void StartLogging(uint32_t log_period_milli) {
+    logging_ = true;
+    log_thread_ = std::thread([log_period_milli, this] { LogThreadLoop(log_period_milli); });
+  }
+
+  void EndLogging() {
+    logging_ = false;
+    log_thread_.join();
+    log_manager_.Process();
+    log_manager_.Flush();
+  }
+
+  std::default_random_engine generator_;
+  storage::RecordBufferSegmentPool pool_{1000, 100};
+  storage::BlockStore block_store_{100, 100};
+  storage::LogManager log_manager_{"test.txt", &pool_};
+  transaction::TransactionManager txn_manager_{&pool_, false, &log_manager_};
+  std::thread log_thread_;
+  bool logging_;
+ private:
+  void LogThreadLoop(uint32_t log_period_milli) {
+    while (logging_) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(log_period_milli));
+      log_manager_.Process();
+    }
+  }
+};
+
 // NOLINTNEXTLINE
-TEST(LogTest, SimpleLogTest) {
-  std::default_random_engine engine;
-  storage::RecordBufferSegmentPool pool(100, 100);
-  storage::BlockStore block_store(100, 100);
-  storage::LogManager log_manager("test.txt", &pool);
-  storage::BlockLayout layout = StorageTestUtil::RandomLayout(2, &engine);
-  storage::DataTable table(&block_store, layout, layout_version_t(0));
-  transaction::TransactionManager txn_manager(&pool, false, &log_manager);
-  transaction::TransactionContext *txn = txn_manager.BeginTransaction();
-  execution::SqlTable fake_table;
-
-  const storage::ProjectedRowInitializer initializer(layout, StorageTestUtil::ProjectionListAllColumns(layout));
-
-  auto *redo = txn->StageWrite(&fake_table, tuple_id_t(0), initializer);
-  StorageTestUtil::PopulateRandomRow(redo->Delta(), layout, 0, &engine);
-  table.Insert(txn, *redo->Delta());
-  txn_manager.Commit(txn, [] {});
-  log_manager.Process();
-  log_manager.Flush();
+TEST_F(LargeLoggingTests, LargeLogTest) {
+//  LargeTransactionTestObject
+//      tested(2, 10, 1, {1.0, 0.0}, &block_store_, &pool_, &generator_, false, false, &log_manager_);
+//  StartLogging(1);
+//  tested.SimulateOltp(100, 4);
+//  EndLogging();
+  storage::BlockLayout layout(4, {8, 8, 8, 4});
+  storage::DataTable table(&block_store_, layout, layout_version_t(0));
+  auto *txn = txn_manager_.BeginTransaction();
+  storage::ProjectedRowInitializer init(layout, {col_id_t(1), col_id_t(2), col_id_t(3)});
+  auto *record = txn->StageWrite(nullptr, tuple_id_t(0), init);
+  *reinterpret_cast<uint64_t *>(record->Delta()->AccessForceNotNull(0)) = 15;
+  *reinterpret_cast<uint64_t *>(record->Delta()->AccessForceNotNull(1)) = 721;
+  record->Delta()->SetNull(2);
+  table.Insert(txn, *record->Delta());
+  txn_manager_.Commit(txn, [] {});
+  log_manager_.Process();
+  log_manager_.Flush();
+  delete txn;
 }
 }  // namespace terrier
