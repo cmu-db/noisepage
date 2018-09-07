53c16646cd750c6bcc45f0c70b864cd110d4cd71
Matt Butrovich <mbutrovich@users.noreply.github.com>
2018-09-07 13:38:44 -0400

    TypeId from Peloton. (#160)

---
+++ src/include/type/type_id.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace terrier::type {
+/**
+ * All of our possible SQL types
+ */
+enum class TypeId : uint8_t {
+  INVALID = 0,
+  PARAMETER_OFFSET,
+  BOOLEAN,
+  TINYINT,
+  SMALLINT,
+  INTEGER,
+  BIGINT,
+  DECIMAL,
+  TIMESTAMP,
+  DATE,
+  VARCHAR,
+  VARBINARY,
+  ARRAY,
+  UDT
+};
+
+}  // namespace terrier::type
