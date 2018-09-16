6a351a89a1ca9fb22e45b6fe45cf8165954d46c0
wenxuanq <wenxuanq@andrew.cmu.edu>
2018-09-16 06:46:59 -0400

    #141 - Add test cases for low abort high throughput scenarios

--- test/transaction/large_transaction_test.cpp
+++ test/transaction/large_transaction_test.cpp
@@ -32,4 +32,44 @@ TEST_F(LargeTransactionTests, MixedReadWrite) {
     for (auto w : result.second) delete w;
   }
 }
+
+// This test targets the scenario of low abort rate (~1% of num_txns) and high throughput of statements
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, LowAbortHighThroughput) {
+  const uint32_t num_iterations = 100;
+  const uint16_t max_columns = 20;
+  const uint32_t initial_table_size = 1000;
+  const uint32_t txn_length = 1;
+  const uint32_t num_txns = 100;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
+
+// This test is a duplicate of LowAbortHighThroughput but with higher number of thread swapouts
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, LowAbortHighThroughputHighThread) {
+  const uint32_t num_iterations = 100;
+  const uint16_t max_columns = 20;
+  const uint32_t initial_table_size = 1000;
+  const uint32_t txn_length = 1;
+  const uint32_t num_txns = 100;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = 2 * TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
 }  // namespace terrier
