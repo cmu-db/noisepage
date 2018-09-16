628c34b712380bee74b1c47c0781c7da49a31346
wenxuanq <wenxuanq@andrew.cmu.edu>
2018-09-16 10:49:53 -0400

    #141 - Add test cases for TPC-c scenario

--- test/transaction/large_transaction_test.cpp
+++ test/transaction/large_transaction_test.cpp
@@ -72,4 +72,44 @@ TEST_F(LargeTransactionTests, LowAbortHighThroughputHighThread) {
     for (auto w : result.second) delete w;
   }
 }
+
+// This test aims to behave like a TPC-C benchmark
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, TPCC) {
+  const uint32_t num_iterations = 100;
+  const uint16_t max_columns = 20;
+  const uint32_t initial_table_size = 1000;
+  const uint32_t txn_length = 5;
+  const uint32_t num_txns = 100;
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
+  const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
+
+// This test is a duplicate of TPCC but with higher number of thread swapouts
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, TPCCHighThread) {
+  const uint32_t num_iterations = 100;
+  const uint16_t max_columns = 20;
+  const uint32_t initial_table_size = 1000;
+  const uint32_t txn_length = 5;
+  const uint32_t num_txns = 100;
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
+  const uint32_t num_concurrent_txns = 2 * TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
 }  // namespace terrier
