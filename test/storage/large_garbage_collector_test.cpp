c58a1c7d8ee76859015aa5fac5db0aa653212ad0
Matt Butrovich <mbutrovich@gmail.com>
2018-09-20 14:48:53 -0400

    Cleanup after merging master.

--- test/storage/large_garbage_collector_test.cpp
+++ test/storage/large_garbage_collector_test.cpp
@@ -6,10 +6,10 @@
 namespace terrier {
 class LargeGCTests : public TerrierTest {
  public:
-  void StartGC(transaction::TransactionManager *txn_manager, uint32_t gc_period_milli) {
+  void StartGC(transaction::TransactionManager *const txn_manager) {
     gc_ = new storage::GarbageCollector(txn_manager);
     run_gc_ = true;
-    gc_thread_ = std::thread([gc_period_milli, this] { GCThreadLoop(gc_period_milli); });
+    gc_thread_ = std::thread([this] { GCThreadLoop(); });
   }
 
   void EndGC() {
@@ -21,48 +21,72 @@ class LargeGCTests : public TerrierTest {
     delete gc_;
   }
 
-  const uint32_t num_iterations = 100;
+  const uint32_t num_iterations = 10;
   const uint16_t max_columns = 20;
   const uint32_t initial_table_size = 1000;
-  const uint32_t num_txns = 1000;
-  const uint32_t batch_size = 100;
+  const uint32_t num_txns = 10000;
+  const uint32_t batch_size = 1000;
   storage::BlockStore block_store_{1000, 1000};
-  common::ObjectPool<storage::BufferSegment> buffer_pool_{1000, 1000};
+  common::ObjectPool<storage::BufferSegment> buffer_pool_{10000, 10000};
   std::default_random_engine generator_;
   volatile bool run_gc_ = false;
-  volatile bool paused_ = false;
+  volatile bool gc_paused_ = false;
   std::thread gc_thread_;
   storage::GarbageCollector *gc_ = nullptr;
 
  private:
-  void GCThreadLoop(uint32_t gc_period_milli) {
+  const std::chrono::milliseconds gc_period_{10};
+
+  void GCThreadLoop() {
     while (run_gc_) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(gc_period_milli));
-      if (!paused_) gc_->PerformGarbageCollection();
+      std::this_thread::sleep_for(gc_period_);
+      if (!gc_paused_) gc_->PerformGarbageCollection();
     }
   }
 };
 
-// This test case generates random update-selects in concurrent transactions on a pre-populated database.
+// These test cases generates random update-selects in concurrent transactions on a pre-populated database.
 // Each transaction logs their operations locally. At the end of the run, we can reconstruct the snapshot of
 // a database using the updates at every timestamp, and compares the reads with the reconstructed snapshot versions
 // to make sure they are the same.
 // NOLINTNEXTLINE
 TEST_F(LargeGCTests, MixedReadWriteWithGC) {
   const uint32_t txn_length = 10;
-  const std::vector<double> update_select_ratio = {0.3, 0.7};
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
   const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
+    }
+    EndGC();
+  }
+}
+
+// Double the thread count to force more thread swapping and try to capture unexpected races
+// NOLINTNEXTLINE
+TEST_F(LargeGCTests, MixedReadWriteHighThreadWithGC) {
+  const uint32_t txn_length = 10;
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
+  const uint32_t num_concurrent_txns = 2 * TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, true, true);
+    StartGC(tested.GetTxnManager());
+    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
+      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
+      gc_paused_ = true;
+      tested.CheckReadsCorrect(&result.first);
+      for (auto w : result.first) delete w;
+      for (auto w : result.second) delete w;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -77,14 +101,14 @@ TEST_F(LargeGCTests, LowAbortHighThroughputWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -99,14 +123,14 @@ TEST_F(LargeGCTests, LowAbortHighThroughputHighThreadWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -122,14 +146,14 @@ TEST_F(LargeGCTests, HighAbortRateWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -144,14 +168,14 @@ TEST_F(LargeGCTests, HighAbortRateHighThreadWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -166,14 +190,14 @@ TEST_F(LargeGCTests, TPCCWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
@@ -188,14 +212,14 @@ TEST_F(LargeGCTests, TPCCHighThreadWithGC) {
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, true, true);
-    StartGC(tested.GetTxnManager(), 10);
+    StartGC(tested.GetTxnManager());
     for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
       auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-      paused_ = true;
+      gc_paused_ = true;
       tested.CheckReadsCorrect(&result.first);
       for (auto w : result.first) delete w;
       for (auto w : result.second) delete w;
-      paused_ = false;
+      gc_paused_ = false;
     }
     EndGC();
   }
--- test/transaction/large_transaction_test.cpp
+++ test/transaction/large_transaction_test.cpp
@@ -5,24 +5,40 @@
 namespace terrier {
 class LargeTransactionTests : public TerrierTest {
  public:
-  const uint32_t num_iterations = 100;
+  const uint32_t num_iterations = 10;
   const uint16_t max_columns = 20;
   const uint32_t initial_table_size = 1000;
-  const uint32_t num_txns = 100;
+  const uint32_t num_txns = 10000;
   storage::BlockStore block_store_{1000, 1000};
-  common::ObjectPool<storage::BufferSegment> buffer_pool_{1000, 1000};
+  common::ObjectPool<storage::BufferSegment> buffer_pool_{10000, 10000};
   std::default_random_engine generator_;
 };
 
-// This test case generates random update-selects in concurrent transactions on a pre-populated database.
+// These test cases generates random update-selects in concurrent transactions on a pre-populated database.
 // Each transaction logs their operations locally. At the end of the run, we can reconstruct the snapshot of
 // a database using the updates at every timestamp, and compares the reads with the reconstructed snapshot versions
 // to make sure they are the same.
 // NOLINTNEXTLINE
 TEST_F(LargeTransactionTests, MixedReadWrite) {
-  const uint32_t txn_length = 20;
+  const uint32_t txn_length = 10;
   const std::vector<double> update_select_ratio = {0.4, 0.6};
   const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
+
+// Double the thread count to force more thread swapping and try to capture unexpected races
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, MixedReadWriteHighThread) {
+  const uint32_t txn_length = 10;
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
+  const uint32_t num_concurrent_txns = 2 * TestThreadPool::HardwareConcurrency();
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, false, true);
@@ -65,22 +81,6 @@ TEST_F(LargeTransactionTests, LowAbortHighThroughputHighThread) {
   }
 }
 
-// This test aims to behave like a TPC-C benchmark
-// NOLINTNEXTLINE
-TEST_F(LargeTransactionTests, TPCC) {
-  const uint32_t txn_length = 5;
-  const std::vector<double> update_select_ratio = {0.4, 0.6};
-  const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
-  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
-    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
-                                      &buffer_pool_, &generator_, false, true);
-    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
-    tested.CheckReadsCorrect(&result.first);
-    for (auto w : result.first) delete w;
-    for (auto w : result.second) delete w;
-  }
-}
-
 // This test is similar to the previous one, but with a higher ratio of updates
 // and longer transactions leading to more aborts.
 // NOLINTNEXTLINE
@@ -104,6 +104,22 @@ TEST_F(LargeTransactionTests, HighAbortRateHighThread) {
   const uint32_t txn_length = 40;
   const std::vector<double> update_select_ratio = {0.7, 0.3};
   const uint32_t num_concurrent_txns = 2 * TestThreadPool::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, false, true);
+    auto result = tested.SimulateOltp(num_txns, num_concurrent_txns);
+    tested.CheckReadsCorrect(&result.first);
+    for (auto w : result.first) delete w;
+    for (auto w : result.second) delete w;
+  }
+}
+
+// This test aims to behave like a TPC-C benchmark
+// NOLINTNEXTLINE
+TEST_F(LargeTransactionTests, TPCC) {
+  const uint32_t txn_length = 5;
+  const std::vector<double> update_select_ratio = {0.4, 0.6};
+  const uint32_t num_concurrent_txns = TestThreadPool::HardwareConcurrency();
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
                                       &buffer_pool_, &generator_, false, true);
