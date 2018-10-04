74cfed13064f13c2b11d312b945a71ea0b35f53d
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2018-10-04 17:08:30 -0400

    Suppress some stdoutput and comments

--- test/index/bwtree_test.cpp
+++ test/index/bwtree_test.cpp
@@ -7,22 +7,12 @@
 //#define BWTREE_DEBUG
 
 namespace terrier {
-using TEST_TYPE = char;
-//using BwTree = ::wangziqi2013::bwtree::BwTree<TEST_TYPE, TEST_TYPE, std::less<>, std::equal_to<>, std::hash<TEST_TYPE>,
-//                                              std::equal_to<>, std::hash<TEST_TYPE>>;
-
-struct BwTreeTests : public TerrierTest {
-  //BwTree tree_;
-};
 
 // NOLINTNEXTLINE
-TEST_F(BwTreeTests, IteratorTest) {
-  /////////////////////////////////////////////////////////////////
-  // Test iterator
-  /////////////////////////////////////////////////////////////////
+TEST(BwTreeTests, IteratorTest) {
   TreeType *t1 = nullptr;
-  // This could print
-  t1 = GetEmptyTree();
+
+  t1 = GetEmptyTree(true);
 
   const int key_num = 1024 * 1024;
 
@@ -30,27 +20,19 @@ TEST_F(BwTreeTests, IteratorTest) {
   for(int i = 0;i < key_num;i++) {
     t1->Insert(i, i);
   }
-  //TODO{yangjuns}: The following line makes RandomInsert pass. I have no idea why.
-  //t1->GetValue(0);
 
   ForwardIteratorTest(t1, key_num);
   BackwardIteratorTest(t1, key_num);
 
-  PrintStat(t1);
-
-  printf("Finised testing iterator\n");
+  //PrintStat(t1);
 
   // Do not forget to deletet the tree here
   DestroyTree(t1, true);
 }
 
 // NOLINTNEXTLINE
-TEST_F(BwTreeTests, RandomInsert) {
-  /////////////////////////////////////////////////////////////////
-  // Test random insert
-  /////////////////////////////////////////////////////////////////
+TEST(BwTreeTests, RandomInsert) {
   TreeType *t1 = nullptr;
-  printf("Testing random insert...\n");
 
   // Do not print here otherwise we could not see result
   t1 = GetEmptyTree(true);
@@ -58,25 +40,19 @@ TEST_F(BwTreeTests, RandomInsert) {
   LaunchParallelTestID(t1, 8, RandomInsertTest, t1);
   RandomInsertVerify(t1);
 
-  printf("Finished random insert testing. Delete the tree.\n");
-
   // no print
   DestroyTree(t1, true);
 }
 
 // NOLINTNEXTLINE
-TEST_F(BwTreeTests, MixedTest) {
-  /////////////////////////////////////////////////////////////////
-  // Test mixed insert/delete
-  /////////////////////////////////////////////////////////////////
+TEST(BwTreeTests, MixedTest) {
   TreeType *t1 = nullptr;
   // no print
   t1 = GetEmptyTree(true);
 
   LaunchParallelTestID(t1, basic_test_thread_num, MixedTest1, t1);
-  printf("Finished mixed testing\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   MixedGetValueTest(t1);
   // no print
@@ -84,78 +60,79 @@ TEST_F(BwTreeTests, MixedTest) {
 }
 
 // NOLINTNEXTLINE
-TEST_F(BwTreeTests, MultiThread) {
-/////////////////////////////////////////////////////////////////
-  // Test Basic Insert/Delete/GetValue
-  //   with different patterns and multi thread
-  /////////////////////////////////////////////////////////////////
+TEST(BwTreeTests, MultiThread) {
   TreeType *t1 = nullptr;
   t1 = GetEmptyTree(true);
   LaunchParallelTestID(t1, basic_test_thread_num, InsertTest2, t1);
-  printf("Finished inserting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   InsertGetValueTest(t1);
-  printf("Finished verifying all inserted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, DeleteTest1, t1);
-  printf("Finished deleting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   DeleteGetValueTest(t1);
-  printf("Finished verifying all deleted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, InsertTest1, t1);
-  printf("Finished inserting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   InsertGetValueTest(t1);
-  printf("Finished verifying all inserted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, DeleteTest2, t1);
-  printf("Finished deleting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   DeleteGetValueTest(t1);
-  printf("Finished verifying all deleted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, InsertTest1, t1);
-  printf("Finished inserting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   InsertGetValueTest(t1);
-  printf("Finished verifying all inserted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, DeleteTest1, t1);
-  printf("Finished deleting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   DeleteGetValueTest(t1);
-  printf("Finished verifying all deleted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, InsertTest2, t1);
-  printf("Finished inserting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   InsertGetValueTest(t1);
-  printf("Finished verifying all inserted values\n");
 
   LaunchParallelTestID(t1, basic_test_thread_num, DeleteTest2, t1);
-  printf("Finished deleting all keys\n");
 
-  PrintStat(t1);
+  //PrintStat(t1);
 
   DeleteGetValueTest(t1);
-  printf("Finished verifying all deleted values\n");
 
-  DestroyTree(t1);
+  DestroyTree(t1, true);
 }
 
+// The following test is disabled because it has infinite loop
+// NOLINTNEXTLINE
+TEST(BwTreeTests, DISABLED_StressTest){
+  TreeType *t1 = nullptr;
+  t1 = GetEmptyTree(true);
+
+  LaunchParallelTestID(t1, 8, StressTest, t1);
+
+  DestroyTree(t1, true);
+}
+
+// The following test is disabled because it takes too long (10 mins)
+// NOLINTNEXTLINE
+TEST(BwTreeTests, DISABLED_MiscTest){
+  TreeType *t1 = nullptr;
+  t1 = GetEmptyTree(true);
+
+  TestEpochManager(t1);
+
+  DestroyTree(t1, true);
+}
 }  // namespace terrier
