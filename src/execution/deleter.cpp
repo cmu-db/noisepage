a75d5ee5bdb27dcf76e23dc1e60669362c23963a
Matt Butrovich <mbutrovich@gmail.com>
2018-10-05 20:23:02 -0400

    Deleter and removed query cache.

--- src/execution/deleter.cpp
+++ src/execution/deleter.cpp
@@ -1,105 +1,35 @@
-//===----------------------------------------------------------------------===//
-//
-//                         Peloton
-//
-// deleter.cpp
-//
-// Identification: src/execution/deleter.cpp
-//
-// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
-//
-//===----------------------------------------------------------------------===//
-
 #include "execution/deleter.h"
 
-#include "concurrency/transaction_manager_factory.h"
+#include "common/macros.h"
+#include "execution/execution_context.h"
 #include "execution/transaction_runtime.h"
-#include "executor/executor_context.h"
-#include "storage/data_table.h"
+#include "loggers/execution_logger.h"
+#include "storage/sql_table.h"
+#include "storage/storage_defs.h"
+#include "transaction/transaction_context.h"
+#include "transaction/transaction_defs.h"
 
 namespace terrier::execution {
 
-Deleter::Deleter(storage::DataTable *table, executor::ExecutionContext *executor_context)
+Deleter::Deleter(storage::SqlTable *const table, execution::ExecutionContext *const executor_context)
     : table_(table), executor_context_(executor_context) {
-  PELOTON_ASSERT(table != nullptr && executor_context != nullptr);
+  TERRIER_ASSERT(table != nullptr && executor_context != nullptr, "SqlTable and ExecutionConext must be non-null!");
 }
 
-void Deleter::Init(Deleter &deleter, storage::DataTable *table, executor::ExecutionContext *executor_context) {
+void Deleter::Init(Deleter &deleter, storage::SqlTable *const table, execution::ExecutionContext *const executor_context) {
   new (&deleter) Deleter(table, executor_context);
 }
 
-void Deleter::Delete(uint32_t tile_group_id, uint32_t tuple_offset) {
-  LOG_TRACE("Deleting tuple <%u, %u> from table '%s' (db ID: %u, table ID: %u)", tile_group_id, tuple_offset,
-            table_->GetName().c_str(), table_->GetDatabaseOid(), table_->GetOid());
+void Deleter::Delete(const storage::TupleSlot slot) {
+  EXECUTION_LOG_TRACE("Deleting tuple {}, {} from SqlTable oid {} ", slot.GetBlock(), slot.GetOffset(),
+                      table_->GetOid());
 
-  auto *txn = executor_context_->GetTransaction();
-  auto tile_group = table_->GetTileGroupById(tile_group_id);
-  auto *tile_group_header = tile_group->GetHeader();
+  auto *const txn = executor_context_->GetTransaction();
 
-  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
-
-  bool is_owner = txn_manager.IsOwner(txn, tile_group_header, tuple_offset);
-  bool is_written = txn_manager.IsWritten(txn, tile_group_header, tuple_offset);
-
-  ItemPointer old_location{tile_group_id, tuple_offset};
-
-  // Did the current transaction create this version we're deleting? If so, we
-  // can perform the deletion without inserting a new empty version.
-
-  if (is_owner && is_written) {
-    LOG_TRACE("The current transaction is the owner of the tuple");
-    txn_manager.PerformDelete(txn, old_location);
-    executor_context_->num_processed++;
+  if (!table_->Delete(txn, slot)) {
+    txn->SetTransactionResult(ResultType::FAILURE);
     return;
   }
-
-  // We didn't create this version. In order to perform the delete, we need to
-  // acquire ownership of the version. Let's check if we can do so.
-
-  bool is_ownable = is_owner || txn_manager.IsOwnable(txn, tile_group_header, tuple_offset);
-  if (!is_ownable) {
-    // Version is not own-able. The transaction should be aborted as we cannot
-    // update the latest version.
-    LOG_TRACE("Tuple [%u-%u] isn't own-able. Failing transaction.", tile_group_id, tuple_offset);
-    txn_manager.SetTransactionResult(txn, ResultType::FAILURE);
-    return;
-  }
-
-  // Version is own-able. Let's grab ownership of the version.
-
-  bool acquired_ownership = is_owner || txn_manager.AcquireOwnership(txn, tile_group_header, tuple_offset);
-  if (!acquired_ownership) {
-    LOG_TRACE("Failed acquiring ownership of tuple [%u-%u]. Failing transaction.", tile_group_id, tuple_offset);
-    txn_manager.SetTransactionResult(txn, ResultType::FAILURE);
-    return;
-  }
-
-  // We've acquired ownership of the latest version, and it isn't locked by any
-  // other threads. Now, let's insert an empty version.
-
-  ItemPointer new_location = table_->InsertEmptyVersion();
-
-  // Insertion into the table may fail. PerformDelete() should not be called if
-  // the insertion fails. At this point, we've acquired a write lock on the
-  // version, but since it is not in the write set (since we haven't yet put it
-  // into the write set), the acquired lock can't be released when the
-  // transaction is aborted. The YieldOwnership() function helps us release the
-  // acquired write lock.
-
-  if (new_location.IsNull()) {
-    LOG_TRACE("Failed to insert new tuple. Failing transaction.");
-    if (!is_owner) {
-      // If ownership was acquired, we release it here, thus releasing the
-      // write lock.
-      txn_manager.YieldOwnership(txn, tile_group_header, tuple_offset);
-    }
-    txn_manager.SetTransactionResult(txn, ResultType::FAILURE);
-    return;
-  }
-
-  // All is well
-  txn_manager.PerformDelete(txn, old_location, new_location);
-  executor_context_->num_processed++;
 }
 
 }  // namespace terrier::execution
