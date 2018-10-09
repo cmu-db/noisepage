8fb9ba7ba1692b1b242befc3c41a9150509a5160
Matt Butrovich <mbutrovich@gmail.com>
2018-10-09 17:35:32 -0400

    Add single statement insert benchmark.

--- benchmark/transaction/large_transaction_benchmark.cpp
+++ benchmark/transaction/large_transaction_benchmark.cpp
@@ -63,6 +63,46 @@ BENCHMARK_DEFINE_F(LargeTransactionBenchmark, HighAbortRate)(benchmark::State &s
 }
 
 /**
+ * Single statement insert throughput. Should have no aborts.
+ */
+// NOLINTNEXTLINE
+BENCHMARK_DEFINE_F(LargeTransactionBenchmark, SingleStatementInsert)(benchmark::State &state) {
+  TestThreadPool thread_pool;
+  transaction::TransactionManager txn_manager(&buffer_pool_, false, LOGGING_DISABLED);
+  const storage::BlockLayout layout{attr_sizes};
+  const storage::ProjectedRowInitializer initializer_{layout, StorageTestUtil::ProjectionListAllColumns(layout)};
+  // NOLINTNEXTLINE
+  for (auto _ : state) {
+    storage::DataTable table(&block_store_, layout, layout_version_t(0));
+
+    auto workload = [&](uint32_t id) {
+      // generate a random redo ProjectedRow to Insert
+      byte *redo_buffer = common::AllocationUtil::AllocateAligned(initializer_.ProjectedRowSize());
+      storage::ProjectedRow *redo = initializer_.InitializeRow(redo_buffer);
+      StorageTestUtil::PopulateRandomRow(redo, layout, 0, &generator_);
+
+      for (uint32_t i = 0; i < num_txns / num_concurrent_txns_; i++) {
+        auto *txn = txn_manager.BeginTransaction();
+        table.Insert(txn, *redo);
+        txn_manager.Commit(txn, [] {});
+        delete txn;
+      }
+
+      delete[] redo_buffer;
+    };
+
+    uint64_t elapsed_ms;
+    {
+      common::ScopedTimer timer(&elapsed_ms);
+      thread_pool.RunThreadsUntilFinish(num_concurrent_txns_, workload);
+    }
+
+    state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
+  }
+  state.SetItemsProcessed(state.iterations() * num_txns);
+}
+
+/**
  * Single statement update throughput. Should have low abort rates.
  */
 // NOLINTNEXTLINE
@@ -112,6 +152,11 @@ BENCHMARK_REGISTER_F(LargeTransactionBenchmark, HighAbortRate)
     ->Unit(benchmark::kMillisecond)
     ->UseManualTime()
     ->MinTime(10);
+
+BENCHMARK_REGISTER_F(LargeTransactionBenchmark, SingleStatementInsert)
+    ->Unit(benchmark::kMillisecond)
+    ->UseManualTime()
+    ->MinTime(5);
 
 BENCHMARK_REGISTER_F(LargeTransactionBenchmark, SingleStatementUpdate)
     ->Unit(benchmark::kMillisecond)
