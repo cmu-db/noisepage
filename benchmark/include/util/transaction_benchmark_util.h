0d5f044b8775bd6bd28f01234041a69604f05a11
Matt Butrovich <mbutrovich@users.noreply.github.com>
2018-10-12 16:24:20 -0400

    Large transaction benchmarks (#200)

---
+++ benchmark/include/util/transaction_benchmark_util.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <algorithm>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include "gtest/gtest.h"
+#include "storage/data_table.h"
+#include "transaction/transaction_context.h"
+#include "transaction/transaction_manager.h"
+#include "util/storage_test_util.h"
+#include "util/test_harness.h"
+
+namespace terrier {
+class LargeTransactionBenchmarkObject;
+class RandomWorkloadTransaction;
+using TupleEntry = std::pair<storage::TupleSlot, storage::ProjectedRow *>;
+
+/**
+ * A RandomWorkloadTransaction class provides a simple interface to simulate a transaction running in the system.
+ */
+class RandomWorkloadTransaction {
+ public:
+  /**
+   * Initializes a new RandomWorkloadTransaction to work on the given test object
+   * @param test_object the test object that runs this transaction
+   */
+  explicit RandomWorkloadTransaction(LargeTransactionBenchmarkObject *test_object);
+
+  /**
+   * Destructs a random workload transaction
+   */
+  ~RandomWorkloadTransaction();
+
+  /**
+   * Randomly updates a tuple, using the given generator as source of randomness.
+   *
+   * @tparam Random the type of random generator to use
+   * @param generator the random generator to use
+   */
+  template <class Random>
+  void RandomUpdate(Random *generator);
+
+  /**
+   * Randomly inserts a tuple, using the given generator as source of randomness.
+   *
+   * @tparam Random the type of random generator to use
+   * @param generator the random generator to use
+   */
+  template <class Random>
+  void RandomInsert(Random *generator);
+
+  /**
+   * Randomly selects a tuple, using the given generator as source of randomness.
+   *
+   * @tparam Random the type of random generator to use
+   * @param generator the random generator to use
+   */
+  template <class Random>
+  void RandomSelect(Random *generator);
+
+  /**
+   * Finish the simulation of this transaction. The underlying transaction will either commit or abort.
+   */
+  void Finish();
+
+  timestamp_t BeginTimestamp() const { return start_time_; }
+
+  timestamp_t CommitTimestamp() const {
+    if (aborted_) return timestamp_t(static_cast<uint64_t>(-1));
+    return commit_time_;
+  }
+
+  std::unordered_map<storage::TupleSlot, storage::ProjectedRow *> *Updates() { return &updates_; }
+
+ private:
+  friend class LargeTransactionBenchmarkObject;
+  LargeTransactionBenchmarkObject *test_object_;
+  transaction::TransactionContext *txn_;
+  // extra bookkeeping for correctness checks
+  bool aborted_;
+  timestamp_t start_time_, commit_time_;
+  std::unordered_map<storage::TupleSlot, storage::ProjectedRow *> updates_;
+  byte *buffer_;
+};
+
+/**
+ * A LargeTransactionTest bootstraps a table, and runs randomly generated workloads concurrently against the table to
+ * simulate a real run of the system. This works with or without gc.
+ *
+ * So far we only do updates and selects, as inserts and deletes are not given much special meaning without the index.
+ */
+class LargeTransactionBenchmarkObject {
+ public:
+  /**
+   * Initializes a test object with the given configuration
+   * @param max_columns the max number of columns in the generated test table
+   * @param initial_table_size number of tuples the table should have
+   * @param txn_length length of every simulated transaction, in number of operations (select or update)
+   * @param update_select_ratio the ratio of inserts vs. updates vs. select in the generated transaction
+   *                             (e.g. {0.0, 0.3, 0.7} will be 0% inserts, 30% updates, and 70% reads)
+   * @param block_store the block store to use for the underlying data table
+   * @param buffer_pool the buffer pool to use for simulated transactions
+   * @param generator the random generator to use for the test
+   * @param gc_on whether gc is enabled
+   * @param log_manager pointer to the LogManager if enabled
+   */
+  LargeTransactionBenchmarkObject(const std::vector<uint8_t> &attr_sizes, uint32_t initial_table_size,
+                                  uint32_t txn_length, std::vector<double> operation_ratio,
+                                  storage::BlockStore *block_store, storage::RecordBufferSegmentPool *buffer_pool,
+                                  std::default_random_engine *generator, bool gc_on,
+                                  storage::LogManager *log_manager = LOGGING_DISABLED);
+
+  /**
+   * Destructs a LargeTransactionBenchmarkObject
+   */
+  ~LargeTransactionBenchmarkObject();
+
+  /**
+   * @return the transaction manager used by this test
+   */
+  transaction::TransactionManager *GetTxnManager() { return &txn_manager_; }
+
+  /**
+   * Simulate an oltp workload, running the specified number of total transactions while allowing the specified number
+   * of transactions to run concurrently. Transactions are generated using the configuration provided on construction.
+   *
+   * @param num_transactions total number of transactions to run
+   * @param num_concurrent_txns number of transactions allowed to run concurrently
+   * @return abort count
+   */
+  uint64_t SimulateOltp(uint32_t num_transactions, uint32_t num_concurrent_txns);
+
+  /**
+   * @return layout of the randomly generated table
+   */
+  const storage::BlockLayout &Layout() const { return layout_; }
+
+ private:
+  void SimulateOneTransaction(RandomWorkloadTransaction *txn, uint32_t txn_id);
+
+  template <class Random>
+  void PopulateInitialTable(uint32_t num_tuples, Random *generator);
+
+  friend class RandomWorkloadTransaction;
+  uint32_t txn_length_;
+  std::vector<double> operation_ratio_;
+  std::default_random_engine *generator_;
+  storage::BlockLayout layout_;
+  storage::DataTable table_;
+  transaction::TransactionManager txn_manager_;
+  transaction::TransactionContext *initial_txn_;
+  bool gc_on_, wal_on_;
+  uint64_t abort_count_;
+
+  // tuple content is meaningless if bookkeeping is off.
+  std::vector<TupleEntry> last_checked_version_;
+  // so we don't have to calculate these over and over again
+  storage::ProjectedRowInitializer row_initializer_{layout_, StorageTestUtil::ProjectionListAllColumns(layout_)};
+};
+}  // namespace terrier
---
+++ benchmark/util/transaction_benchmark_util.cpp
@@ -0,0 +1,172 @@
+#include "util/transaction_benchmark_util.h"
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include "common/allocator.h"
+#include "transaction/transaction_util.h"
+
+namespace terrier {
+RandomWorkloadTransaction::RandomWorkloadTransaction(LargeTransactionBenchmarkObject *test_object)
+    : test_object_(test_object),
+      txn_(test_object->txn_manager_.BeginTransaction()),
+      aborted_(false),
+      start_time_(txn_->StartTime()),
+      commit_time_(UINT64_MAX),
+      buffer_(common::AllocationUtil::AllocateAligned(test_object->row_initializer_.ProjectedRowSize())) {}
+
+RandomWorkloadTransaction::~RandomWorkloadTransaction() {
+  if (!test_object_->gc_on_) delete txn_;
+  delete[] buffer_;
+  for (auto &entry : updates_) delete[] reinterpret_cast<byte *>(entry.second);
+}
+
+template <class Random>
+void RandomWorkloadTransaction::RandomUpdate(Random *generator) {
+  if (aborted_) return;
+  storage::TupleSlot updated =
+      RandomTestUtil::UniformRandomElement(test_object_->last_checked_version_, generator)->first;
+  std::vector<col_id_t> update_col_ids = StorageTestUtil::ProjectionListRandomColumns(test_object_->layout_, generator);
+  storage::ProjectedRowInitializer initializer(test_object_->layout_, update_col_ids);
+  auto *update_buffer = buffer_;
+  storage::ProjectedRow *update = initializer.InitializeRow(update_buffer);
+
+  StorageTestUtil::PopulateRandomRow(update, test_object_->layout_, 0.0, generator);
+  // TODO(Tianyu): Hardly efficient, but will do for testing.
+  if (test_object_->wal_on_) {
+    auto *record = txn_->StageWrite(nullptr, updated, initializer);
+    TERRIER_MEMCPY(record->Delta(), update, update->Size());
+  }
+  auto result = test_object_->table_.Update(txn_, updated, *update);
+  aborted_ = !result;
+}
+
+template <class Random>
+void RandomWorkloadTransaction::RandomInsert(Random *generator) {
+  if (aborted_) return;
+  std::vector<col_id_t> insert_col_ids = StorageTestUtil::ProjectionListAllColumns(test_object_->layout_);
+  storage::ProjectedRowInitializer initializer(test_object_->layout_, insert_col_ids);
+  auto *insert_buffer = buffer_;
+  storage::ProjectedRow *insert = initializer.InitializeRow(insert_buffer);
+
+  StorageTestUtil::PopulateRandomRow(insert, test_object_->layout_, 0.0, generator);
+  storage::TupleSlot inserted = test_object_->table_.Insert(txn_, *insert);
+  // TODO(Tianyu): Hardly efficient, but will do for testing.
+  if (test_object_->wal_on_) {
+    auto *record = txn_->StageWrite(nullptr, inserted, initializer);
+    TERRIER_MEMCPY(record->Delta(), insert, insert->Size());
+  }
+}
+
+template <class Random>
+void RandomWorkloadTransaction::RandomSelect(Random *generator) {
+  if (aborted_) return;
+  storage::TupleSlot selected =
+      RandomTestUtil::UniformRandomElement(test_object_->last_checked_version_, generator)->first;
+  auto *select_buffer = buffer_;
+  storage::ProjectedRow *select = test_object_->row_initializer_.InitializeRow(select_buffer);
+  test_object_->table_.Select(txn_, selected, select);
+}
+
+void RandomWorkloadTransaction::Finish() {
+  if (aborted_)
+    test_object_->txn_manager_.Abort(txn_);
+  else
+    commit_time_ = test_object_->txn_manager_.Commit(txn_, [] {});
+}
+
+LargeTransactionBenchmarkObject::LargeTransactionBenchmarkObject(const std::vector<uint8_t> &attr_sizes,
+                                                                 uint32_t initial_table_size, uint32_t txn_length,
+                                                                 std::vector<double> operation_ratio,
+                                                                 storage::BlockStore *block_store,
+                                                                 storage::RecordBufferSegmentPool *buffer_pool,
+                                                                 std::default_random_engine *generator, bool gc_on,
+                                                                 storage::LogManager *log_manager)
+    : txn_length_(txn_length),
+      operation_ratio_(std::move(operation_ratio)),
+      generator_(generator),
+      layout_({attr_sizes}),
+      table_(block_store, layout_, layout_version_t(0)),
+      txn_manager_(buffer_pool, gc_on, log_manager),
+      gc_on_(gc_on),
+      wal_on_(log_manager != LOGGING_DISABLED),
+      abort_count_(0) {
+  // Bootstrap the table to have the specified number of tuples
+  PopulateInitialTable(initial_table_size, generator_);
+}
+
+LargeTransactionBenchmarkObject::~LargeTransactionBenchmarkObject() {
+  if (!gc_on_) delete initial_txn_;
+}
+
+// Caller is responsible for freeing the returned results if bookkeeping is on.
+uint64_t LargeTransactionBenchmarkObject::SimulateOltp(uint32_t num_transactions, uint32_t num_concurrent_txns) {
+  TestThreadPool thread_pool;
+  std::vector<RandomWorkloadTransaction *> txns;
+  std::function<void(uint32_t)> workload;
+  std::atomic<uint32_t> txns_run = 0;
+  if (gc_on_) {
+    // Then there is no need to keep track of RandomWorkloadTransaction objects
+    workload = [&](uint32_t) {
+      for (uint32_t txn_id = txns_run++; txn_id < num_transactions; txn_id = txns_run++) {
+        RandomWorkloadTransaction txn(this);
+        SimulateOneTransaction(&txn, txn_id);
+      }
+    };
+  } else {
+    txns.resize(num_transactions);
+    // Either for correctness checking, or to cleanup memory afterwards, we need to retain these
+    // test objects
+    workload = [&](uint32_t) {
+      for (uint32_t txn_id = txns_run++; txn_id < num_transactions; txn_id = txns_run++) {
+        txns[txn_id] = new RandomWorkloadTransaction(this);
+        SimulateOneTransaction(txns[txn_id], txn_id);
+      }
+    };
+  }
+
+  thread_pool.RunThreadsUntilFinish(num_concurrent_txns, workload);
+
+  // We only need to deallocate, and return, if gc is on, this loop is a no-op
+  for (RandomWorkloadTransaction *txn : txns) {
+    if (txn->aborted_) abort_count_++;
+    delete txn;
+  }
+  // This result is meaningless if bookkeeping is not turned on.
+  return abort_count_;
+}
+
+void LargeTransactionBenchmarkObject::SimulateOneTransaction(terrier::RandomWorkloadTransaction *txn, uint32_t txn_id) {
+  std::default_random_engine thread_generator(txn_id);
+
+  auto insert = [&] { txn->RandomInsert(&thread_generator); };
+  auto update = [&] { txn->RandomUpdate(&thread_generator); };
+  auto select = [&] { txn->RandomSelect(&thread_generator); };
+  RandomTestUtil::InvokeWorkloadWithDistribution({insert, update, select}, operation_ratio_, &thread_generator,
+                                                 txn_length_);
+  txn->Finish();
+}
+
+template <class Random>
+void LargeTransactionBenchmarkObject::PopulateInitialTable(uint32_t num_tuples, Random *generator) {
+  initial_txn_ = txn_manager_.BeginTransaction();
+  byte *redo_buffer = nullptr;
+
+  redo_buffer = common::AllocationUtil::AllocateAligned(row_initializer_.ProjectedRowSize());
+  row_initializer_.InitializeRow(redo_buffer);
+
+  for (uint32_t i = 0; i < num_tuples; i++) {
+    auto *const redo = reinterpret_cast<storage::ProjectedRow *>(redo_buffer);
+    StorageTestUtil::PopulateRandomRow(redo, layout_, 0.0, generator);
+    storage::TupleSlot inserted = table_.Insert(initial_txn_, *redo);
+    // TODO(Tianyu): Hardly efficient, but will do for testing.
+    if (wal_on_) {
+      auto *record = initial_txn_->StageWrite(nullptr, inserted, row_initializer_);
+      TERRIER_MEMCPY(record->Delta(), redo, redo->Size());
+    }
+    last_checked_version_.emplace_back(inserted, nullptr);
+  }
+  txn_manager_.Commit(initial_txn_, [] {});
+  // cleanup if not keeping track of all the inserts.
+  delete[] redo_buffer;
+}
+}  // namespace terrier
