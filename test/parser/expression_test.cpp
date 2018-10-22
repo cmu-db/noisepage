80a6d7f747c61be4bfcee77a8351c1a42fcd89b1
Tianyu Li <tli2@andrew.cmu.edu>
2018-10-22 11:26:02 -0400

    remove duplicate includes

--- test/parser/expression_test.cpp
+++ test/parser/expression_test.cpp
@@ -3,12 +3,14 @@
 #include <vector>
 
 #include "gtest/gtest.h"
+
+#include "parser/expression/conjunction_expression.h"
+#include "parser/expression/constant_value_expression.h"
+// TODO(Tianyu): They are included here so they will get compiled and statically analyzed despite not being used
 #include "parser/expression/abstract_expression.h"
 #include "parser/expression/aggregate_expression.h"
 #include "parser/expression/case_expression.h"
 #include "parser/expression/comparison_expression.h"
-#include "parser/expression/conjunction_expression.h"
-#include "parser/expression/constant_value_expression.h"
 #include "parser/expression/function_expression.h"
 #include "parser/expression/operator_expression.h"
 #include "parser/expression/parameter_value_expression.h"
