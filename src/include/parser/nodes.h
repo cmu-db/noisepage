a57093aa000b86b3dcc14d83b83955e94d051c29
pakhtar <PelotonDB17>
2018-11-01 11:13:13 -0400

    Documentation updates
    - exclude parsenodes.h from doxygen builds
    - document items in nodes.h

--- src/include/parser/nodes.h
+++ src/include/parser/nodes.h
@@ -2,10 +2,16 @@
 
 #include "libpg_query/nodes.h"
 
+/**
+ * A value parsenode as produced by the Postgres parser
+ */
 typedef struct value {
-  NodeTag type; /* tag appropriately (eg. T_String) */
+  NodeTag type;     /**< tag appropriately (eg. T_String) */
+  /**
+   * value, as specified via tag
+   */
   union ValUnion {
-    int32_t ival; /* machine integer */
-    char *str;    /* string */
-  } val;
+    int32_t ival;  /**< A machine integer */
+    char *str;     /**< string */
+  } val;           /**< value */
 } value;
