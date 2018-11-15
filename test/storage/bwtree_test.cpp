1ca70032c23553abd1722f83cb090d2f396fef77
Matt Butrovich <mbutrovich@gmail.com>
2018-11-15 17:21:27 -0500

    BwTreeTestUtil

--- test/storage/bwtree_test.cpp
+++ test/storage/bwtree_test.cpp
@@ -1,14 +1,18 @@
-#include "bwtree/bwtree.h"
 #include <algorithm>
 #include <random>
 #include <vector>
 #include "bwtree/bloom_filter.h"
 #include "bwtree/sorted_small_set.h"
+#include "util/bwtree_test_util.h"
 #include "util/test_harness.h"
 
 namespace terrier {
 
-struct BwTreeTests : public TerrierTest {};
+struct BwTreeTests : public TerrierTest {
+  void SetUp() final {}
+
+  void TearDown() final {}
+};
 
 /**
  * Adapted from https://github.com/wangziqi2013/BwTree/blob/master/stl_test/bloom_filter.cpp
@@ -16,24 +20,24 @@ struct BwTreeTests : public TerrierTest {};
  */
 // NOLINTNEXTLINE
 TEST_F(BwTreeTests, BloomFilterTest) {
-  std::vector<int *> loose_pointers;
-  const int *buffer[256];
+  std::vector<uint32_t *> loose_pointers;
+  const uint32_t *buffer[256];
 
-  third_party::bwtree::BloomFilter<int> bf{buffer};
+  third_party::bwtree::BloomFilter<uint32_t> bf{buffer};
 
   // Generate input and verify that it's not present in the BloomFilter
-  for (int i = 0; i < 256; i++) {
-    loose_pointers.emplace_back(new int{i});
+  for (uint32_t i = 0; i < 256; i++) {
+    loose_pointers.emplace_back(new uint32_t{i});
     EXPECT_FALSE(bf.Exists(i));
   }
 
   // Insert into BloomFilter
-  for (int i = 0; i < 256; i++) {
+  for (uint32_t i = 0; i < 256; i++) {
     bf.Insert(*loose_pointers[i]);
   }
 
   // Verify present in BloomFilter
-  for (int i = 0; i < 256; i++) {
+  for (uint32_t i = 0; i < 256; i++) {
     EXPECT_TRUE(bf.Exists(i));
   }
 
@@ -49,10 +53,10 @@ TEST_F(BwTreeTests, BloomFilterTest) {
 // NOLINTNEXTLINE
 TEST_F(BwTreeTests, SortedSmallSetTest) {
   const uint32_t num_inserts = 100;
-  int *buffer = new int[num_inserts];
+  uint32_t *buffer = new uint32_t[num_inserts];
 
   // Generate randomly permuted input
-  std::vector<int> inserts;
+  std::vector<uint32_t> inserts;
   for (uint32_t i = 0; i < num_inserts; i++) {
     inserts.push_back(i);
   }
@@ -60,12 +64,13 @@ TEST_F(BwTreeTests, SortedSmallSetTest) {
   std::shuffle(inserts.begin(), inserts.end(), rd);
 
   // Insert into a SortedSmallSet and verify that it's sorted at all times
-  third_party::bwtree::SortedSmallSet<int> sss{buffer};
+  third_party::bwtree::SortedSmallSet<uint32_t> sss{buffer};
   for (const auto i : inserts) {
     sss.Insert(i);
     EXPECT_TRUE(std::is_sorted(sss.GetBegin(), sss.GetEnd()));
   }
 
+  // Clean up
   delete[] buffer;
 }
 }  // namespace terrier
