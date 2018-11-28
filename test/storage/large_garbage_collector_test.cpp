7746864fccf056f1da0c531744d7a6c555c308c9
Matt Butrovich <mbutrovich@gmail.com>
2018-11-28 13:24:25 -0500

    Reenable disabled tests.

--- test/storage/large_garbage_collector_test.cpp
+++ test/storage/large_garbage_collector_test.cpp
@@ -45,96 +45,96 @@ class LargeGCTests : public TerrierTest {
   }
 };
 
-//// These test cases generates random update-selects in concurrent transactions on a pre-populated database.
-//// Each transaction logs their operations locally. At the end of the run, we can reconstruct the snapshot of
-//// a database using the updates at every timestamp, and compares the reads with the reconstructed snapshot versions
-//// to make sure they are the same.
-//// NOLINTNEXTLINE
-// TEST_F(LargeGCTests, MixedReadWriteWithGC) {
-//  const uint32_t txn_length = 10;
-//  const std::vector<double> update_select_ratio = {0.5, 0.5};
-//  const uint32_t num_concurrent_txns = MultiThreadTestUtil::HardwareConcurrency();
-//  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
-//    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
-//                                      &buffer_pool_, &generator_, true, true);
-//    StartGC(tested.GetTxnManager());
-//    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
-//      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-//      gc_paused_ = true;
-//      tested.CheckReadsCorrect(&result.first);
-//      for (auto w : result.first) delete w;
-//      for (auto w : result.second) delete w;
-//      gc_paused_ = false;
-//    }
-//    EndGC();
-//  }
-//}
-//
-//// Double the thread count to force more thread swapping and try to capture unexpected races
-//// NOLINTNEXTLINE
-// TEST_F(LargeGCTests, MixedReadWriteHighThreadWithGC) {
-//  const uint32_t txn_length = 10;
-//  const std::vector<double> update_select_ratio = {0.5, 0.5};
-//  const uint32_t num_concurrent_txns = 2 * MultiThreadTestUtil::HardwareConcurrency();
-//  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
-//    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
-//                                      &buffer_pool_, &generator_, true, true);
-//    StartGC(tested.GetTxnManager());
-//    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
-//      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-//      gc_paused_ = true;
-//      tested.CheckReadsCorrect(&result.first);
-//      for (auto w : result.first) delete w;
-//      for (auto w : result.second) delete w;
-//      gc_paused_ = false;
-//    }
-//    EndGC();
-//  }
-//}
-//
-//// This test targets the scenario of low abort rate (~1% of num_txns) and high throughput of statements
-//// NOLINTNEXTLINE
-// TEST_F(LargeGCTests, LowAbortHighThroughputWithGC) {
-//  const uint32_t txn_length = 1;
-//  const std::vector<double> update_select_ratio = {0.5, 0.5};
-//  const uint32_t num_concurrent_txns = MultiThreadTestUtil::HardwareConcurrency();
-//  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
-//    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
-//                                      &buffer_pool_, &generator_, true, true);
-//    StartGC(tested.GetTxnManager());
-//    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
-//      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-//      gc_paused_ = true;
-//      tested.CheckReadsCorrect(&result.first);
-//      for (auto w : result.first) delete w;
-//      for (auto w : result.second) delete w;
-//      gc_paused_ = false;
-//    }
-//    EndGC();
-//  }
-//}
-//
-//// This test is a duplicate of LowAbortHighThroughputWithGC but with higher number of thread swapouts
-//// NOLINTNEXTLINE
-// TEST_F(LargeGCTests, LowAbortHighThroughputHighThreadWithGC) {
-//  const uint32_t txn_length = 1;
-//  const std::vector<double> update_select_ratio = {0.5, 0.5};
-//  const uint32_t num_concurrent_txns = 2 * MultiThreadTestUtil::HardwareConcurrency();
-//  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
-//    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
-//                                      &buffer_pool_, &generator_, true, true);
-//    StartGC(tested.GetTxnManager());
-//    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
-//      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
-//      gc_paused_ = true;
-//      tested.CheckReadsCorrect(&result.first);
-//      for (auto w : result.first) delete w;
-//      for (auto w : result.second) delete w;
-//      gc_paused_ = false;
-//    }
-//    EndGC();
-//  }
-//}
+// These test cases generates random update-selects in concurrent transactions on a pre-populated database.
+// Each transaction logs their operations locally. At the end of the run, we can reconstruct the snapshot of
+// a database using the updates at every timestamp, and compares the reads with the reconstructed snapshot versions
+// to make sure they are the same.
+// NOLINTNEXTLINE
+TEST_F(LargeGCTests, MixedReadWriteWithGC) {
+  const uint32_t txn_length = 10;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = MultiThreadTestUtil::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, true, true);
+    StartGC(tested.GetTxnManager());
+    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
+      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
+      gc_paused_ = true;
+      tested.CheckReadsCorrect(&result.first);
+      for (auto w : result.first) delete w;
+      for (auto w : result.second) delete w;
+      gc_paused_ = false;
+    }
+    EndGC();
+  }
+}
+
+// Double the thread count to force more thread swapping and try to capture unexpected races
+// NOLINTNEXTLINE
+TEST_F(LargeGCTests, MixedReadWriteHighThreadWithGC) {
+  const uint32_t txn_length = 10;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = 2 * MultiThreadTestUtil::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, true, true);
+    StartGC(tested.GetTxnManager());
+    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
+      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
+      gc_paused_ = true;
+      tested.CheckReadsCorrect(&result.first);
+      for (auto w : result.first) delete w;
+      for (auto w : result.second) delete w;
+      gc_paused_ = false;
+    }
+    EndGC();
+  }
+}
+
+// This test targets the scenario of low abort rate (~1% of num_txns) and high throughput of statements
+// NOLINTNEXTLINE
+TEST_F(LargeGCTests, LowAbortHighThroughputWithGC) {
+  const uint32_t txn_length = 1;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = MultiThreadTestUtil::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, true, true);
+    StartGC(tested.GetTxnManager());
+    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
+      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
+      gc_paused_ = true;
+      tested.CheckReadsCorrect(&result.first);
+      for (auto w : result.first) delete w;
+      for (auto w : result.second) delete w;
+      gc_paused_ = false;
+    }
+    EndGC();
+  }
+}
+
+// This test is a duplicate of LowAbortHighThroughputWithGC but with higher number of thread swapouts
+// NOLINTNEXTLINE
+TEST_F(LargeGCTests, LowAbortHighThroughputHighThreadWithGC) {
+  const uint32_t txn_length = 1;
+  const std::vector<double> update_select_ratio = {0.5, 0.5};
+  const uint32_t num_concurrent_txns = 2 * MultiThreadTestUtil::HardwareConcurrency();
+  for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
+    LargeTransactionTestObject tested(max_columns, initial_table_size, txn_length, update_select_ratio, &block_store_,
+                                      &buffer_pool_, &generator_, true, true);
+    StartGC(tested.GetTxnManager());
+    for (uint32_t batch = 0; batch * batch_size < num_txns; batch++) {
+      auto result = tested.SimulateOltp(batch_size, num_concurrent_txns);
+      gc_paused_ = true;
+      tested.CheckReadsCorrect(&result.first);
+      for (auto w : result.first) delete w;
+      for (auto w : result.second) delete w;
+      gc_paused_ = false;
+    }
+    EndGC();
+  }
+}
 
 // This test is similar to the previous one, but with a higher ratio of updates
 // and longer transactions leading to more aborts.
