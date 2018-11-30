f4faa3ca5f4424bdcfd287f446a036d5954c0a47
Matt Butrovich <mbutrovich@gmail.com>
2018-11-30 15:09:01 -0500

    lint stuffs.

--- test/storage/bwtree_index_test.cpp
+++ test/storage/bwtree_index_test.cpp
@@ -8,7 +8,7 @@ struct BwTreeIndexTests : public ::terrier::TerrierTest {};
 
 // NOLINTNEXTLINE
 TEST_F(BwTreeIndexTests, BuilderTest) {
-  storage::index::BwTreeIndex<int64_t, std::less<int64_t>, std::equal_to<int64_t>, std::hash<int64_t>>::Builder builder;
+  storage::index::BwTreeIndex<int64_t, std::less<>, std::equal_to<>, std::hash<int64_t>>::Builder builder;
   auto bwtree = builder.Build();
   delete bwtree;
 }
