86831e878201069aa60b9934b5dbe68d09af67ed
Matt Butrovich <mbutrovich@gmail.com>
2018-11-30 11:30:16 -0500

    Checkpoint.

--- src/include/storage/index/bwtree_index.h
+++ src/include/storage/index/bwtree_index.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include "bwtree/bwtree.h"
 #include "catalog/catalog_defs.h"
 #include "common/performance_counter.h"
 #include "storage/index/index_defs.h"
@@ -20,12 +21,51 @@ namespace terrier::storage::index {
 DEFINE_PERFORMANCE_CLASS(IndexCounter, IndexCounterMembers)
 #undef IndexCounterMembers
 
+template <typename KeyType, typename KeyComparator, typename KeyEqualityChecker, typename KeyHashFunc>
 class BwTreeIndex {
  private:
   BwTreeIndex(const catalog::index_oid_t oid, const ConstraintType constraint_type)
       : oid_{oid}, constraint_type_{constraint_type} {}
   const catalog::index_oid_t oid_;
   const ConstraintType constraint_type_;
+  third_party::bwtree::BwTree<KeyType, TupleSlot, KeyComparator, KeyEqualityChecker, KeyHashFunc> *const bwtree_;
+
+  bool Insert(const ProjectedRow &tuple, const TupleSlot location) {
+    KeyType index_key;
+    index_key.SetFromKey(tuple);
+    return bwtree_->Insert(index_key, location, HasUniqueKeys());
+  }
+
+  bool Delete(const ProjectedRow &tuple, const TupleSlot location) {
+    KeyType index_key;
+    index_key.SetFromKey(tuple);
+    return bwtree_->Delete(index_key, location);
+  }
+
+  bool ConditionalInsert(const ProjectedRow &tuple, const TupleSlot location,
+                         std::function<bool(const void *)> predicate) {
+    KeyType index_key;
+    index_key.SetFromKey(tuple);
+
+    bool predicate_satisfied = false;
+
+    // This function will complete them in one step predicate will be set to nullptr if the predicate returns true for
+    // some value
+    const bool ret = bwtree_->ConditionalInsert(index_key, location, predicate, &predicate_satisfied);
+
+    // If predicate is not satisfied then we know insertion succeeds
+    if (!predicate_satisfied) {
+      TERRIER_ASSERT(ret, "Insertion should always succeed. (Ziqi)");
+    } else {
+      TERRIER_ASSERT(!ret, "Insertion should always fail. (Ziqi)");
+    }
+
+    return ret;
+  }
+
+  bool HasUniqueKeys() const {
+    return constraint_type_ == ConstraintType::PRIMARY_KEY || constraint_type_ == ConstraintType::UNIQUE;
+  }
 
  public:
   class Builder {
