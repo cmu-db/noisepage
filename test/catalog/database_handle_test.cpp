7d016a01aceab4dafcbc045f114b3e59fd002511
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2018-12-04 20:36:36 -0500

    Add basic database handle test

---
+++ test/catalog/database_handle_test.cpp
@@ -0,0 +1,45 @@
+#include "catalog/database_handle.h"
+#include <algorithm>
+#include <random>
+#include <vector>
+#include "catalog/catalog.h"
+#include "transaction/transaction_manager.h"
+#include "util/test_harness.h"
+namespace terrier {
+
+struct DatabaseHandleTests : public TerrierTest {
+  void SetUp() override {
+    TerrierTest::SetUp();
+    catalog_ = new catalog::Catalog();
+    // bootstrap catalog
+    catalog_->Bootstrap();
+    // create a fake transaction
+    txn_manager_ = new transaction::TransactionManager(&buffer_pool_, true, nullptr);
+  }
+
+  void TearDown() override {
+    TerrierTest::TearDown();
+    delete txn_manager_;
+    delete catalog_;
+  }
+
+  catalog::Catalog* catalog_;
+  storage::RecordBufferSegmentPool buffer_pool_{100, 100};
+
+  transaction::TransactionContext *txn_;
+  transaction::TransactionManager *txn_manager_;
+};
+
+// NOLINTNEXTLINE
+TEST_F(DatabaseHandleTests, DatabaseEntryTest) {
+  txn_ = txn_manager_->BeginTransaction();
+  // terrier has db_oid_t 0
+  const catalog::oid_t terrier_oid = catalog::oid_t(0);
+  catalog::DatabaseHandle db_handle = catalog_->GetDatabase(terrier_oid);
+  auto db_entry_ptr = db_handle.GetDatabaseEntry(txn_, terrier_oid);
+  EXPECT_TRUE(db_entry_ptr != nullptr);
+
+  // db_entry_ptr->
+}
+
+}  // namespace terrier
