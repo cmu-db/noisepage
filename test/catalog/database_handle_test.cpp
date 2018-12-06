898142719634b6f8cf998d747a477ab975144c8d
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2018-12-05 22:59:53 -0500

    Clean up databse_handle_test

--- test/catalog/database_handle_test.cpp
+++ test/catalog/database_handle_test.cpp
@@ -10,7 +10,6 @@ namespace terrier {
 struct DatabaseHandleTests : public TerrierTest {
   void SetUp() override {
     TerrierTest::SetUp();
-    // create a fake transaction
     txn_manager_ = new transaction::TransactionManager(&buffer_pool_, true, LOGGING_DISABLED);
 
     catalog_ = new catalog::Catalog(txn_manager_);
@@ -20,6 +19,7 @@ struct DatabaseHandleTests : public TerrierTest {
     TerrierTest::TearDown();
     delete txn_manager_;
     delete catalog_;
+    if (txn_) delete txn_;
   }
 
   catalog::Catalog *catalog_;
@@ -30,7 +30,7 @@ struct DatabaseHandleTests : public TerrierTest {
 };
 
 // NOLINTNEXTLINE
-TEST_F(DatabaseHandleTests, DatabaseEntryTest) {
+TEST_F(DatabaseHandleTests, BasicCorrectnessTest) {
   txn_ = txn_manager_->BeginTransaction();
   // terrier has db_oid_t 0
   const catalog::oid_t terrier_oid = catalog::oid_t(0);
@@ -38,12 +38,9 @@ TEST_F(DatabaseHandleTests, DatabaseEntryTest) {
   auto db_entry_ptr = db_handle.GetDatabaseEntry(txn_, terrier_oid);
   EXPECT_TRUE(db_entry_ptr != nullptr);
   // test if we are getting the correct value
-
   // oid has col_oid_t = 1
   EXPECT_TRUE(*reinterpret_cast<uint32_t *>(db_entry_ptr->GetValue(catalog::col_oid_t(1))) == 0);
   // datname has col_oid_t = 2
   EXPECT_TRUE(*reinterpret_cast<uint32_t *>(db_entry_ptr->GetValue(catalog::col_oid_t(2))) == 15721);
-  delete txn_;
 }
-
 }  // namespace terrier
