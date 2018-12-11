479986b4668a7446afd5fc4598824a1b5c958fdd
Tanuj Nayak <tanujnay112@live.com>
2018-12-10 19:42:13 -0500

    commented out lock_free_queue stuff

--- src/include/common/container/lock_free_queue.h
+++ src/include/common/container/lock_free_queue.h
@@ -10,55 +10,55 @@
 //
 //===----------------------------------------------------------------------===//
 
-#pragma once
-
-#include "common/macros.h"
-#include "concurrentqueue/concurrentqueue.h"
-
-namespace peloton {
-
-//===--------------------------------------------------------------------===//
+//#pragma once
 //
-// Lock-free Queue -- Supports multiple consumers and multiple producers.
+//#include "common/macros.h"
+//#include "concurrentqueue/concurrentqueue.h"
 //
-//===--------------------------------------------------------------------===//
-template <typename T>
-class LockFreeQueue {
- public:
-  /// Constructor
-  explicit LockFreeQueue(size_t size) : queue_(size) {}
-
-  /// This class cannot be copied
-  DISALLOW_COPY(LockFreeQueue);
-
-  /**
-   * @brief Move-enqueues the given item.
-   * @param item To be enqueued
-   */
-  void Enqueue(T &&item) { queue_.enqueue(std::move(item)); }
-
-  /**
-   * @brief Enqueues the given item by copying it into this queue
-   * @param item To be enqueued
-   */
-  void Enqueue(const T &item) { queue_.enqueue(item); }
-
-  /**
-   * @brief Tries to dequeue one item
-   * @param[out] item That is dequeued
-   * @return True is an item was removed, false if the queue was empty
-   */
-  bool Dequeue(T &item) { return queue_.try_dequeue(item); }
-
-  /**
-   * @brief Determines if the queue is empty
-   * @return True if the queue is empty
-   */
-  bool IsEmpty() const { return queue_.size_approx() == 0; }
-
- private:
-  // Underlying moodycamel concurrent queue
-  moodycamel::ConcurrentQueue<T> queue_;
-};
-
-}  // namespace peloton
+//namespace terrier {
+//
+////===--------------------------------------------------------------------===//
+////
+//// Lock-free Queue -- Supports multiple consumers and multiple producers.
+////
+////===--------------------------------------------------------------------===//
+//template <typename T>
+//class LockFreeQueue {
+// public:
+//  /// Constructor
+//  explicit LockFreeQueue(size_t size) : queue_(size) {}
+//
+//  /// This class cannot be copied
+//  DISALLOW_COPY(LockFreeQueue);
+//
+//  /**
+//   * @brief Move-enqueues the given item.
+//   * @param item To be enqueued
+//   */
+//  void Enqueue(T &&item) { queue_.enqueue(std::move(item)); }
+//
+//  /**
+//   * @brief Enqueues the given item by copying it into this queue
+//   * @param item To be enqueued
+//   */
+//  void Enqueue(const T &item) { queue_.enqueue(item); }
+//
+//  /**
+//   * @brief Tries to dequeue one item
+//   * @param[out] item That is dequeued
+//   * @return True is an item was removed, false if the queue was empty
+//   */
+//  bool Dequeue(T &item) { return queue_.try_dequeue(item); }
+//
+//  /**
+//   * @brief Determines if the queue is empty
+//   * @return True if the queue is empty
+//   */
+//  bool IsEmpty() const { return queue_.size_approx() == 0; }
+//
+// private:
+//  // Underlying moodycamel concurrent queue
+//  moodycamel::ConcurrentQueue<T> queue_;
+//};
+//
+//}  // namespace peloton
