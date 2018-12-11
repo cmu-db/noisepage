fb306abe1cf7e1d186e166cdde3d471016057aeb
Tanuj Nayak <tanujnay112@live.com>
2018-12-10 20:27:45 -0500

    fixed bad reference to eigen3

--- src/include/common/internal_types.h
+++ src/include/common/internal_types.h
@@ -47,7 +47,7 @@
 
 // Impose Row-major to avoid confusion
 #define EIGEN_DEFAULT_TO_ROW_MAJOR
-#include "eigen3/Eigen/Dense"
+//#include "eigen3/Eigen/Dense"
 
 namespace terrier {
 
@@ -1468,8 +1468,8 @@ enum class PostgresNetworkObjectType : uchar {
 // TODO(saatvik): Generalize Eigen utilities across all types
 typedef std::vector<float> vector_t;
 typedef std::vector<std::vector<float>> matrix_t;
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+/*typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
     matrix_eig;
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::RowMajor> vector_eig;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::RowMajor> vector_eig;*/
 
 }  // namespace terrier
