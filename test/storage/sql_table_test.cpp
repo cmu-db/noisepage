ee3604c0c3c6780196e4bccb826d58361474508b
Pervaze Akhtar <pakhtar@andrew.cmu.edu>
2019-01-16 16:19:36 -0500

    Updates to sql_table_test

--- test/storage/sql_table_test.cpp
+++ test/storage/sql_table_test.cpp
@@ -9,36 +9,58 @@
 #include "util/transaction_test_util.h"
 namespace terrier {
 
+/**
+ * Help class to simplify operations on a SqlTable
+ */
 class SqlTableRW {
  public:
   explicit SqlTableRW(catalog::table_oid_t table_oid) : table_oid_(table_oid) {}
+  ~SqlTableRW() {
+    delete pri_;
+    delete pr_map_;
+  }
 
+  /**
+   * Append a column definition to the internal list. The list will be
+   * used when creating the SqlTable.
+   * @param name of the column
+   * @param type of the column
+   * @param nullable
+   * @param oid for the column
+   */
   void DefineColumn(std::string name, type::TypeId type, bool nullable, catalog::col_oid_t oid) {
     cols_.emplace_back(name, type, nullable, oid);
   }
 
   /**
-   * Create the SQL table
+   * Create the SQL table.
    */
   void Create() {
-    if (schema_ == nullptr) {
-      schema_ = new catalog::Schema(cols_);
-    }
-    if (table_ == nullptr) {
-      table_ = new storage::SqlTable(&block_store_, *schema_, table_oid_);
-    }
+    schema_ = new catalog::Schema(cols_);
+    table_ = new storage::SqlTable(&block_store_, *schema_, table_oid_);
 
     for (const auto &c : cols_) {
       col_oids_.emplace_back(c.GetOid());
     }
-  }
 
-  void StartRow() {
+    // save information needed for (later) reading and writing
     auto row_pair = table_->InitializerForProjectedRow(col_oids_);
-    insert_buffer_ = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-    insert_ = row_pair.first.InitializeRow(insert_buffer_);
+    pri_ = new storage::ProjectedRowInitializer(std::get<0>(row_pair));
+    pr_map_ = new storage::ProjectionMap(std::get<1>(row_pair));
   }
 
+  /**
+   * First step in writing a row.
+   */
+  void StartRow() {
+    insert_buffer_ = common::AllocationUtil::AllocateAligned(pri_->ProjectedRowSize());
+    insert_ = pri_->InitializeRow(insert_buffer_);
+  }
+
+  /**
+   * Insert the row into the table
+   * @return slot where the row was created
+   */
   storage::TupleSlot EndRowAndInsert() {
     auto txn = txn_manager_.BeginTransaction();
     auto slot = table_->Insert(txn, *insert_);
@@ -50,30 +72,45 @@ class SqlTableRW {
     return storage::TupleSlot(slot.GetBlock(), slot.GetOffset());
   }
 
+  /**
+   * Read an integer from a row
+   * @param col_num column number in the schema
+   * @param slot - tuple to read from
+   * @return integer value
+   */
   uint32_t GetIntColInRow(int32_t col_num, storage::TupleSlot slot) {
     auto txn = txn_manager_.BeginTransaction();
-    auto row_pair = table_->InitializerForProjectedRow(col_oids_);
-    auto read_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-    storage::ProjectedRow *read = row_pair.first.InitializeRow(read_buffer);
+    auto read_buffer = common::AllocationUtil::AllocateAligned(pri_->ProjectedRowSize());
+    storage::ProjectedRow *read = pri_->InitializeRow(read_buffer);
     table_->Select(txn, slot, read);
-    byte *col_p = read->AccessForceNotNull(row_pair.second[col_oids_[col_num]]);
+    byte *col_p = read->AccessForceNotNull(pr_map_->at(col_oids_[col_num]));
     txn_manager_.Commit(txn, TestCallbacks::EmptyCallback, nullptr);
     return *(reinterpret_cast<uint32_t *>(col_p));
   }
 
+  /**
+   * Save an integer, for insertion by EndRowAndInsert
+   * @param col_num column number in the schema
+   * @param value to save
+   */
   void SetIntColInRow(int32_t col_num, int32_t value) {
-    auto row_pair = table_->InitializerForProjectedRow(col_oids_);
-    byte *col_p = insert_->AccessForceNotNull(row_pair.second[col_oids_[col_num]]);
+    byte *col_p = insert_->AccessForceNotNull(pr_map_->at(col_oids_[col_num]));
     (*reinterpret_cast<uint32_t *>(col_p)) = value;
   }
 
+  /**
+   * Read a string from a row
+   * @param col_num column number in the schema
+   * @param slot - tuple to read from
+   * @return malloc'ed C string (with null terminator). Caller must
+   *   free.
+   */
   char *GetVarcharColInRow(int32_t col_num, storage::TupleSlot slot) {
     auto txn = txn_manager_.BeginTransaction();
-    auto row_pair = table_->InitializerForProjectedRow(col_oids_);
-    auto read_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-    storage::ProjectedRow *read = row_pair.first.InitializeRow(read_buffer);
+    auto read_buffer = common::AllocationUtil::AllocateAligned(pri_->ProjectedRowSize());
+    storage::ProjectedRow *read = pri_->InitializeRow(read_buffer);
     table_->Select(txn, slot, read);
-    byte *col_p = read->AccessForceNotNull(row_pair.second[col_oids_[col_num]]);
+    byte *col_p = read->AccessForceNotNull(pr_map_->at(col_oids_[col_num]));
 
     auto *entry = reinterpret_cast<storage::VarlenEntry *>(col_p);
     // stored string has no null terminator, add space for it
@@ -87,9 +124,13 @@ class SqlTableRW {
     return ret_st;
   }
 
+  /**
+   * Save a string, for insertion by EndRowAndInsert
+   * @param col_num column number in the schema
+   * @param st C string to save.
+   */
   void SetVarcharColInRow(int32_t col_num, const char *st) {
-    auto row_pair = table_->InitializerForProjectedRow(col_oids_);
-    byte *col_p = insert_->AccessForceNotNull(row_pair.second[col_oids_[col_num]]);
+    byte *col_p = insert_->AccessForceNotNull(pr_map_->at(col_oids_[col_num]));
     // string size, without null terminator
     size_t size = strlen(st);
     byte *varlen = common::AllocationUtil::AllocateAligned(size);
@@ -109,7 +150,8 @@ class SqlTableRW {
   std::vector<catalog::Schema::Column> cols_;
   std::vector<catalog::col_oid_t> col_oids_;
 
-  // std::pair<terrier::storage::ProjectedRowInitializer, terrier::storage::ProjectionMap> *row_pair_ = nullptr;
+  storage::ProjectedRowInitializer *pri_ = nullptr;
+  storage::ProjectionMap *pr_map_ = nullptr;
 
   byte *insert_buffer_ = nullptr;
   storage::ProjectedRow *insert_ = nullptr;
@@ -118,67 +160,15 @@ class SqlTableRW {
 struct SqlTableTests : public TerrierTest {
   void SetUp() override {
     TerrierTest::SetUp();
-    txn_manager_ = new transaction::TransactionManager(&buffer_pool_, true, LOGGING_DISABLED);
   }
 
   void TearDown() override {
     TerrierTest::TearDown();
-    delete txn_manager_;
   }
-
-  storage::RecordBufferSegmentPool buffer_pool_{100, 100};
-  transaction::TransactionManager *txn_manager_;
 };
 
 // NOLINTNEXTLINE
-TEST_F(SqlTableTests, DISABLED_SelectInsertTest) {
-  auto txn1 = txn_manager_->BeginTransaction();
-
-  // create a sql table
-  storage::BlockStore block_store{100, 100};
-  std::vector<catalog::Schema::Column> cols;
-  cols.emplace_back("id", type::TypeId::INTEGER, false, catalog::col_oid_t(0));
-  cols.emplace_back("datname", type::TypeId::INTEGER, false, catalog::col_oid_t(1));
-  catalog::Schema schema(cols);
-  storage::SqlTable test(&block_store, schema, catalog::table_oid_t(2));
-
-  std::vector<catalog::col_oid_t> col_oids;
-  col_oids.emplace_back(cols[0].GetOid());
-  col_oids.emplace_back(cols[1].GetOid());
-  auto row_pair = test.InitializerForProjectedRow(col_oids);
-
-  // Insert a row
-  byte *insert_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-  storage::ProjectedRow *insert = row_pair.first.InitializeRow(insert_buffer);
-  // set the first value to be 100
-  byte *first = insert->AccessForceNotNull(row_pair.second[col_oids[0]]);
-  (*reinterpret_cast<uint32_t *>(first)) = 100;
-  // set the second attribute to be 15271
-  byte *second = insert->AccessForceNotNull(row_pair.second[col_oids[1]]);
-  (*reinterpret_cast<uint32_t *>(second)) = 15721;
-  auto slot = test.Insert(txn1, *insert);
-
-  txn_manager_->Commit(txn1, TestCallbacks::EmptyCallback, nullptr);
-  delete txn1;
-
-  auto txn2 = txn_manager_->BeginTransaction();
-  // read that row
-  auto read_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-  storage::ProjectedRow *read = row_pair.first.InitializeRow(read_buffer);
-
-  bool select_result = test.Select(txn2, slot, read);
-  txn_manager_->Commit(txn2, TestCallbacks::EmptyCallback, nullptr);
-
-  delete txn2;
-  EXPECT_TRUE(select_result);
-  delete[] insert_buffer;
-  delete[] read_buffer;
-  // TODO(yangjuns): check actual value
-}
-
-// NOLINTNEXTLINE
-TEST_F(SqlTableTests, NewSelectInsertTest) {
-  // SqlTableRW table = {catalog::table_oid_t(2)};
+TEST_F(SqlTableTests, SelectInsertTest) {
   SqlTableRW table(catalog::table_oid_t(2));
 
   table.DefineColumn("id", type::TypeId::INTEGER, false, catalog::col_oid_t(0));
@@ -206,54 +196,7 @@ TEST_F(SqlTableTests, NewSelectInsertTest) {
 }
 
 // NOLINTNEXTLINE
-TEST_F(SqlTableTests, DISABLED_VarlenInsertTest) {
-  auto txn1 = txn_manager_->BeginTransaction();
-
-  // create a sql table
-  std::vector<catalog::Schema::Column> cols;
-  cols.emplace_back("id", type::TypeId::INTEGER, false, catalog::col_oid_t(0));
-  cols.emplace_back("datname", type::TypeId::VARCHAR, false, catalog::col_oid_t(1));
-  catalog::Schema schema(cols);
-
-  storage::BlockStore block_store{100, 100};
-  storage::SqlTable test(&block_store, schema, catalog::table_oid_t(2));
-
-  std::vector<catalog::col_oid_t> col_oids;
-  col_oids.emplace_back(cols[0].GetOid());
-  col_oids.emplace_back(cols[1].GetOid());
-  auto row_pair = test.InitializerForProjectedRow(col_oids);
-
-  // Insert a row
-  byte *insert_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-
-  storage::ProjectedRow *insert = row_pair.first.InitializeRow(insert_buffer);
-  // set the first value to be 100
-  byte *first = insert->AccessForceNotNull(row_pair.second[col_oids[0]]);
-  (*reinterpret_cast<uint32_t *>(first)) = 100;
-
-  byte *second = insert->AccessForceNotNull(row_pair.second[col_oids[1]]);
-  const char *db_name = "name";
-  memcpy(second, db_name, strlen(db_name) - 1);
-  auto slot = test.Insert(txn1, *insert);
-
-  txn_manager_->Commit(txn1, TestCallbacks::EmptyCallback, nullptr);
-  delete txn1;
-
-  auto txn2 = txn_manager_->BeginTransaction();
-  // read that row
-  auto read_buffer = common::AllocationUtil::AllocateAligned(row_pair.first.ProjectedRowSize());
-  storage::ProjectedRow *read = row_pair.first.InitializeRow(read_buffer);
-
-  test.Select(txn2, slot, read);
-  txn_manager_->Commit(txn2, TestCallbacks::EmptyCallback, nullptr);
-  delete txn2;
-  EXPECT_TRUE(true);
-  delete[] insert_buffer;
-  delete[] read_buffer;
-}
-
-// NOLINTNEXTLINE
-TEST_F(SqlTableTests, NewVarlenInsertTest) {
+TEST_F(SqlTableTests, VarlenInsertTest) {
   SqlTableRW table(catalog::table_oid_t(2));
 
   table.DefineColumn("id", type::TypeId::INTEGER, false, catalog::col_oid_t(0));
