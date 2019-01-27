2a6088d8b48c7b28b16ff4403efbe1dc89c71579
Tanuj Nayak <tanujnay112@live.com>
2019-01-27 04:14:18 -0500

    removed sudos

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -109,8 +109,8 @@ install_linux() {
    apt-get -y install wget
    wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-dev_6.2.4-4_amd64.deb
    wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-6.2_6.2.4-4_amd64.deb
-   sudo dpkg -i libpqxx-6.2_6.2.4-4_amd64.deb
-   sudo dpkg -i libpqxx-dev_6.2.4-4_amd64.deb
+   dpkg -i libpqxx-6.2_6.2.4-4_amd64.deb
+   dpkg -i libpqxx-dev_6.2.4-4_amd64.deb
 }
 
 main "$@"
