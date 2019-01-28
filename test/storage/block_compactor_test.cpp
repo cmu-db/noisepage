51a8ff2c74ba6ca94d702719e8cca12f70d69d4c
Tianyu Li <tli2@andrew.cmu.edu>
2019-01-27 21:54:02 -0500

    arrow experiment

--- test/storage/block_compactor_test.cpp
+++ test/storage/block_compactor_test.cpp
@@ -3,6 +3,8 @@
 #include "storage/tuple_access_strategy.h"
 #include "util/storage_test_util.h"
 #include "util/test_harness.h"
+#include "arrow/api.h"
+
 namespace terrier {
 // NOLINTNEXTLINE
 TEST(BlockCompactorTest, SimpleTest) {
@@ -48,5 +50,42 @@ TEST(BlockCompactorTest, SimpleTest) {
   for (uint32_t i = 4; i < 20; i++) {
     EXPECT_FALSE(table.Select(txn, {block, i}, row));
   }
-}
+
+  storage::ArrowBlockMetadata &arrow_metadata = accessor.GetArrowBlockMetadata(block);
+  auto id_bitmap = std::make_shared<arrow::Buffer>(reinterpret_cast<uint8_t *>(
+                                                       accessor.ColumnNullBitmap(block, storage::col_id_t(2))), 1);
+  auto id_data = std::make_shared<arrow::Buffer>(reinterpret_cast<uint8_t *>(
+                                                     accessor.ColumnStart(block, storage::col_id_t(2))), 32);
+  auto id_array_data = arrow::ArrayData::Make(arrow::int64(),
+                                              arrow_metadata.NumRecords(),
+                                              {id_bitmap, id_data},
+                                              arrow_metadata.NullCount(storage::col_id_t(2)));
+
+  auto id_array = std::make_shared<arrow::Array>();
+  id_array->SetData(id_array_data);
+
+  auto varlen_bitmap =
+      std::make_shared<arrow::Buffer>(reinterpret_cast<uint8_t *>(accessor.ColumnNullBitmap(block,
+                                                                                            storage::col_id_t(1))), 1);
+  storage::ArrowVarlenColumn varlen_col = arrow_metadata.GetVarlenColumn(layout, storage::col_id_t(1));
+  auto varlen_offset = std::make_shared<arrow::Buffer>(reinterpret_cast<uint8_t *>(varlen_col.offsets_), 20);
+
+  auto varlen_values_buffer = std::make_shared<arrow::Buffer>(reinterpret_cast<uint8_t *>(varlen_col.values_), 16);
+  auto varlen_values_array_data = arrow::ArrayData::Make(arrow::uint8(), 16, {NULLPTR, varlen_values_buffer}, 0);
+
+  auto varlen_array_data = arrow::ArrayData::Make(arrow::utf8(),
+                                                  arrow_metadata.NumRecords(),
+                                                  {varlen_bitmap, varlen_offset},
+                                                  {varlen_values_array_data},
+                                                  arrow_metadata.NullCount(storage::col_id_t(1)));
+  auto varlen_array = std::make_shared<arrow::Array>();
+  varlen_array->SetData(id_array_data);
+
+  std::vector<std::shared_ptr<arrow::Field>> schema_vector = {
+      arrow::field("id", arrow::int64()), arrow::field("varlen", arrow::utf8())};
+  auto schema = std::make_shared<arrow::Schema>(schema_vector);
+  auto arrow_table = arrow::Table::Make(schema, {id_array, varlen_array});
+  (void) arrow_table;
+};
+
 }  // namespace terrier
