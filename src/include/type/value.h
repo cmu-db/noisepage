4d089370f774bdc7ff08afbeae302006d502842a
Matt Butrovich <mbutrovich@gmail.com>
2019-02-05 19:13:36 -0500

    Refactor.

--- src/include/type/value.h
+++ src/include/type/value.h
@@ -10,8 +10,6 @@ namespace terrier::type {
 class ValueFactory;
 class ValuePeeker;
 
-constexpr uint8_t VALUE_SIZE = 8;
-
 class TransientValue {
   friend class ValueFactory;
   friend class ValuePeeker;
@@ -23,26 +21,12 @@ class TransientValue {
 
   TransientValue(const TransientValue &other) {
     // clear internal buffer
-    std::memset(data_, 0, VALUE_SIZE);
+    data_ = 0;
     type_ = other.type_;
     if (Type() != TypeId::VARCHAR) {
-      // copy buffer
-      std::memcpy(data_, other.data_, VALUE_SIZE);
+      data_ = other.data_;
     } else {
-      // allocate a VARCHAR buffer
-      const char *const other_varchar = *reinterpret_cast<const char *const *const>(other.data_);
-      const uint32_t length = *reinterpret_cast<const uint32_t *const>(other_varchar);
-      char *const varchar = new char[length + sizeof(uint32_t)];
-
-      // copy the length field into the VARCHAR buffer
-      *(reinterpret_cast<uint32_t *const>(varchar)) = length;
-
-      // copy the VARCHAR contents into the VARCHAR buffer
-      char *const varchar_contents = varchar + sizeof(uint32_t);
-      const char *const other_varchar_contents = other_varchar + sizeof(uint32_t);
-      std::memcpy(varchar_contents, other_varchar_contents, length);
-      // copy the pointer to the VARCHAR buffer into the internal buffer
-      std::memcpy(data_, &varchar, VALUE_SIZE);
+      CopyVarChar(reinterpret_cast<const char *const>(other.data_));
     }
   }
 
@@ -50,29 +34,15 @@ class TransientValue {
     if (this != &other) {  // self-assignment check expected
       if (Type() == TypeId::VARCHAR) {
         // free VARCHAR buffer
-        delete[] * reinterpret_cast<char *const *const>(data_);
+        delete[] reinterpret_cast<char *const>(data_);
       }
       // clear internal buffer
-      std::memset(data_, 0, VALUE_SIZE);
+      data_ = 0;
       type_ = other.type_;
       if (Type() != TypeId::VARCHAR) {
-        // copy buffer
-        std::memcpy(data_, other.data_, VALUE_SIZE);
+        data_ = other.data_;
       } else {
-        // allocate a VARCHAR buffer
-        const char *const other_varchar = *reinterpret_cast<const char *const *const>(other.data_);
-        const uint32_t length = *reinterpret_cast<const uint32_t *const>(other_varchar);
-        char *const varchar = new char[length + sizeof(uint32_t)];
-
-        // copy the length field into the VARCHAR buffer
-        *(reinterpret_cast<uint32_t *const>(varchar)) = length;
-
-        // copy the VARCHAR contents into the VARCHAR buffer
-        char *const varchar_contents = varchar + sizeof(uint32_t);
-        const char *const other_varchar_contents = other_varchar + sizeof(uint32_t);
-        std::memcpy(varchar_contents, other_varchar_contents, length);
-        // copy the pointer to the VARCHAR buffer into the internal buffer
-        std::memcpy(data_, &varchar, VALUE_SIZE);
+        CopyVarChar(reinterpret_cast<const char *const>(other.data_));
       }
     }
     return *this;
@@ -90,18 +60,18 @@ class TransientValue {
 
   ~TransientValue() {
     if (Type() == TypeId::VARCHAR) {
-      delete[] * reinterpret_cast<char *const *const>(data_);
+      delete[] reinterpret_cast<char *const>(data_);
     }
   }
 
   bool operator==(const TransientValue &rhs) const {
     if (type_ != rhs.type_) return false;
-    if (type_ != TypeId::VARCHAR) return std::memcmp(data_, rhs.data_, VALUE_SIZE) == 0;
+    if (type_ != TypeId::VARCHAR) return data_ == rhs.data_;
 
-    const char *const varchar = *reinterpret_cast<const char *const *const>(data_);
+    const char *const varchar = reinterpret_cast<const char *const>(data_);
     const uint32_t length = *reinterpret_cast<const uint32_t *const>(varchar);
 
-    const char *const rhs_varchar = *reinterpret_cast<const char *const *const>(rhs.data_);
+    const char *const rhs_varchar = reinterpret_cast<const char *const>(rhs.data_);
     const uint32_t rhs_length = *reinterpret_cast<const uint32_t *const>(rhs_varchar);
 
     if (length != rhs_length) return false;
@@ -115,30 +85,48 @@ class TransientValue {
   bool operator!=(const TransientValue &rhs) const { return !(operator==(rhs)); }
 
   common::hash_t Hash() const {
-    if (type_ != TypeId::VARCHAR) return common::HashUtil::HashBytes(data_, VALUE_SIZE);
+    if (type_ != TypeId::VARCHAR)
+      return common::HashUtil::HashBytes(reinterpret_cast<const byte *const>(&data_), sizeof(uintptr_t));
 
     const uint32_t length = *reinterpret_cast<const uint32_t *const>(data_);
-    return common::HashUtil::HashBytes(data_, length + sizeof(uint32_t));
+    return common::HashUtil::HashBytes(reinterpret_cast<const byte *const>(data_), length + sizeof(uint32_t));
   }
 
  private:
   template <typename T>
   TransientValue(const TypeId type, T data) {
     // clear internal buffer
-    std::memset(data_, 0, VALUE_SIZE);
+    data_ = 0;
     type_ = type;
-    const auto num_bytes =
-        std::min(static_cast<uint8_t>(static_cast<uint8_t>(TypeUtil::GetTypeSize(type)) & 0x7F), VALUE_SIZE);
-    std::memcpy(data_, &data, num_bytes);
+    const auto num_bytes = std::min(static_cast<uint8_t>(static_cast<uint8_t>(TypeUtil::GetTypeSize(type)) & 0x7F),
+                                    static_cast<uint8_t>(sizeof(uintptr_t)));
+    std::memcpy(&data_, &data, num_bytes);
   }
 
   template <typename T>
   T GetAs() const {
-    return *reinterpret_cast<const T *const>(data_);
+    return *reinterpret_cast<const T *const>(&data_);
+  }
+
+  void CopyVarChar(const char *const other) {
+    // allocate a VARCHAR buffer
+    const char *const other_varchar = reinterpret_cast<const char *const>(other);
+    const uint32_t length = *reinterpret_cast<const uint32_t *const>(other_varchar);
+    char *const varchar = new char[length + sizeof(uint32_t)];
+
+    // copy the length field into the VARCHAR buffer
+    *(reinterpret_cast<uint32_t *const>(varchar)) = length;
+
+    // copy the VARCHAR contents into the VARCHAR buffer
+    char *const varchar_contents = varchar + sizeof(uint32_t);
+    const char *const other_varchar_contents = other_varchar + sizeof(uint32_t);
+    std::memcpy(varchar_contents, other_varchar_contents, length);
+    // copy the pointer to the VARCHAR buffer into the internal buffer
+    data_ = reinterpret_cast<uintptr_t>(varchar);
   }
 
   TypeId type_ = TypeId::INVALID;
-  byte data_[VALUE_SIZE];
+  uintptr_t data_;
 };
 
 }  // namespace terrier::type
