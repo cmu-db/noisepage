cae0c77c601bc6dcd3610f28b2b8839d766e7f53
Matt Butrovich <mbutrovich@users.noreply.github.com>
2019-02-11 13:55:28 -0500

    type::TransientValue (#290)

--- test/parser/expression_test.cpp
+++ test/parser/expression_test.cpp
@@ -19,22 +19,17 @@
 #include "parser/expression/tuple_value_expression.h"
 #include "parser/parameter.h"
 
-#include "type/value.h"
-#include "type/value_factory.h"
+#include "type/transient_value.h"
+#include "type/transient_value_factory.h"
 
 namespace terrier::parser::expression {
 
 // NOLINTNEXTLINE
 TEST(ExpressionTests, BasicTest) {
-  // NULL
-  auto expr_null = new ConstantValueExpression(type::ValueFactory::GetNullValue());
-  EXPECT_TRUE(expr_null->GetValue().GetType() == terrier::type::TypeId::NULL_TYPE);
-  delete expr_null;
-
   // constant Booleans
-  auto expr_b_1 = new ConstantValueExpression(type::ValueFactory::GetBooleanValue(true));
-  auto expr_b_2 = new ConstantValueExpression(type::ValueFactory::GetBooleanValue(false));
-  auto expr_b_3 = new ConstantValueExpression(type::ValueFactory::GetBooleanValue(true));
+  auto expr_b_1 = new ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
+  auto expr_b_2 = new ConstantValueExpression(type::TransientValueFactory::GetBoolean(false));
+  auto expr_b_3 = new ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
 
   EXPECT_FALSE(*expr_b_1 == *expr_b_2);
   EXPECT_TRUE(*expr_b_1 == *expr_b_3);
@@ -47,9 +42,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_b_3;
 
   // constant tinyints
-  auto expr_ti_1 = new ConstantValueExpression(type::ValueFactory::GetTinyIntValue(1));
-  auto expr_ti_2 = new ConstantValueExpression(type::ValueFactory::GetTinyIntValue(1));
-  auto expr_ti_3 = new ConstantValueExpression(type::ValueFactory::GetTinyIntValue(127));
+  auto expr_ti_1 = new ConstantValueExpression(type::TransientValueFactory::GetTinyInt(1));
+  auto expr_ti_2 = new ConstantValueExpression(type::TransientValueFactory::GetTinyInt(1));
+  auto expr_ti_3 = new ConstantValueExpression(type::TransientValueFactory::GetTinyInt(127));
 
   EXPECT_TRUE(*expr_ti_1 == *expr_ti_2);
   EXPECT_FALSE(*expr_ti_1 == *expr_ti_3);
@@ -59,9 +54,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_ti_3;
 
   // constant smallints
-  auto expr_si_1 = new ConstantValueExpression(type::ValueFactory::GetSmallIntValue(1));
-  auto expr_si_2 = new ConstantValueExpression(type::ValueFactory::GetSmallIntValue(1));
-  auto expr_si_3 = new ConstantValueExpression(type::ValueFactory::GetSmallIntValue(32767));
+  auto expr_si_1 = new ConstantValueExpression(type::TransientValueFactory::GetSmallInt(1));
+  auto expr_si_2 = new ConstantValueExpression(type::TransientValueFactory::GetSmallInt(1));
+  auto expr_si_3 = new ConstantValueExpression(type::TransientValueFactory::GetSmallInt(32767));
 
   EXPECT_TRUE(*expr_si_1 == *expr_si_2);
   EXPECT_FALSE(*expr_si_1 == *expr_si_3);
@@ -71,9 +66,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_si_3;
 
   // constant ints
-  auto expr_i_1 = new ConstantValueExpression(type::ValueFactory::GetIntegerValue(1));
-  auto expr_i_2 = new ConstantValueExpression(type::ValueFactory::GetIntegerValue(1));
-  auto expr_i_3 = new ConstantValueExpression(type::ValueFactory::GetIntegerValue(32768));
+  auto expr_i_1 = new ConstantValueExpression(type::TransientValueFactory::GetInteger(1));
+  auto expr_i_2 = new ConstantValueExpression(type::TransientValueFactory::GetInteger(1));
+  auto expr_i_3 = new ConstantValueExpression(type::TransientValueFactory::GetInteger(32768));
 
   EXPECT_TRUE(*expr_i_1 == *expr_i_2);
   EXPECT_FALSE(*expr_i_1 == *expr_i_3);
@@ -83,9 +78,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_i_3;
 
   // constant bigints
-  auto expr_bi_1 = new ConstantValueExpression(type::ValueFactory::GetBigIntValue(1));
-  auto expr_bi_2 = new ConstantValueExpression(type::ValueFactory::GetBigIntValue(1));
-  auto expr_bi_3 = new ConstantValueExpression(type::ValueFactory::GetBigIntValue(32768));
+  auto expr_bi_1 = new ConstantValueExpression(type::TransientValueFactory::GetBigInt(1));
+  auto expr_bi_2 = new ConstantValueExpression(type::TransientValueFactory::GetBigInt(1));
+  auto expr_bi_3 = new ConstantValueExpression(type::TransientValueFactory::GetBigInt(32768));
 
   EXPECT_TRUE(*expr_bi_1 == *expr_bi_2);
   EXPECT_FALSE(*expr_bi_1 == *expr_bi_3);
@@ -95,9 +90,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_bi_3;
 
   // constant double/decimal
-  auto expr_d_1 = new ConstantValueExpression(type::ValueFactory::GetDecimalValue(1));
-  auto expr_d_2 = new ConstantValueExpression(type::ValueFactory::GetDecimalValue(1));
-  auto expr_d_3 = new ConstantValueExpression(type::ValueFactory::GetDecimalValue(32768));
+  auto expr_d_1 = new ConstantValueExpression(type::TransientValueFactory::GetDecimal(1));
+  auto expr_d_2 = new ConstantValueExpression(type::TransientValueFactory::GetDecimal(1));
+  auto expr_d_3 = new ConstantValueExpression(type::TransientValueFactory::GetDecimal(32768));
 
   EXPECT_TRUE(*expr_d_1 == *expr_d_2);
   EXPECT_FALSE(*expr_d_1 == *expr_d_3);
@@ -107,9 +102,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_d_3;
 
   // constant timestamp
-  auto expr_ts_1 = new ConstantValueExpression(type::ValueFactory::GetTimeStampValue(type::timestamp_t(1)));
-  auto expr_ts_2 = new ConstantValueExpression(type::ValueFactory::GetTimeStampValue(type::timestamp_t(1)));
-  auto expr_ts_3 = new ConstantValueExpression(type::ValueFactory::GetTimeStampValue(type::timestamp_t(32768)));
+  auto expr_ts_1 = new ConstantValueExpression(type::TransientValueFactory::GetTimestamp(type::timestamp_t(1)));
+  auto expr_ts_2 = new ConstantValueExpression(type::TransientValueFactory::GetTimestamp(type::timestamp_t(1)));
+  auto expr_ts_3 = new ConstantValueExpression(type::TransientValueFactory::GetTimestamp(type::timestamp_t(32768)));
 
   EXPECT_TRUE(*expr_ts_1 == *expr_ts_2);
   EXPECT_FALSE(*expr_ts_1 == *expr_ts_3);
@@ -119,9 +114,9 @@ TEST(ExpressionTests, BasicTest) {
   delete expr_ts_3;
 
   // constant date
-  auto expr_date_1 = new ConstantValueExpression(type::ValueFactory::GetDateValue(type::date_t(1)));
-  auto expr_date_2 = new ConstantValueExpression(type::ValueFactory::GetDateValue(type::date_t(1)));
-  auto expr_date_3 = new ConstantValueExpression(type::ValueFactory::GetDateValue(type::date_t(32768)));
+  auto expr_date_1 = new ConstantValueExpression(type::TransientValueFactory::GetDate(type::date_t(1)));
+  auto expr_date_2 = new ConstantValueExpression(type::TransientValueFactory::GetDate(type::date_t(1)));
+  auto expr_date_3 = new ConstantValueExpression(type::TransientValueFactory::GetDate(type::date_t(32768)));
 
   EXPECT_TRUE(*expr_date_1 == *expr_date_2);
   EXPECT_FALSE(*expr_date_1 == *expr_date_3);
@@ -139,18 +134,18 @@ TEST(ExpressionTests, BasicTest) {
 // NOLINTNEXTLINE
 TEST(ExpressionTests, ConjunctionTest) {
   std::vector<std::shared_ptr<AbstractExpression>> children1;
-  children1.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(true)));
-  children1.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(false)));
+  children1.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(true)));
+  children1.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(false)));
   auto c_expr_1 = new ConjunctionExpression(ExpressionType::CONJUNCTION_AND, std::move(children1));
 
   std::vector<std::shared_ptr<AbstractExpression>> children2;
-  children2.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(true)));
-  children2.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(false)));
+  children2.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(true)));
+  children2.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(false)));
   auto c_expr_2 = new ConjunctionExpression(ExpressionType::CONJUNCTION_AND, std::move(children2));
 
   std::vector<std::shared_ptr<AbstractExpression>> children3;
-  children3.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(true)));
-  children3.emplace_back(std::make_shared<ConstantValueExpression>(type::ValueFactory::GetBooleanValue(true)));
+  children3.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(true)));
+  children3.emplace_back(std::make_shared<ConstantValueExpression>(type::TransientValueFactory::GetBoolean(true)));
   auto c_expr_3 = new ConjunctionExpression(ExpressionType::CONJUNCTION_AND, std::move(children3));
 
   EXPECT_TRUE(*c_expr_1 == *c_expr_2);
