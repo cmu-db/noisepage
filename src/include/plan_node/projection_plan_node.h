ef58baaf808bc8cedfc00f1995c2df1c9ef60900
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-03-02 17:34:25 -0500

    Formatting

--- src/include/plan_node/projection_plan_node.h
+++ src/include/plan_node/projection_plan_node.h
@@ -5,23 +5,17 @@
 namespace terrier::plan_node {
 
 class ProjectionPlanNode : public AbstractPlanNode {
-
  public:
-
   ProjectionPlanNode(std::shared_ptr<catalog::Schema> output_schema) : AbstractPlanNode(output_schema) {}
 
-  PlanNodeType GetPlanNodeType() const override {
-    return PlanNodeType::PROJECTION;
-  }
+  PlanNodeType GetPlanNodeType() const override { return PlanNodeType::PROJECTION; }
 
   std::unique_ptr<AbstractPlanNode> Copy() const override;
 
   common::hash_t Hash() const override;
 
   bool operator==(const AbstractPlanNode &rhs) const override;
-  bool operator!=(const AbstractPlanNode &rhs) const override {
-    return !(*this == rhs);
-  }
+  bool operator!=(const AbstractPlanNode &rhs) const override { return !(*this == rhs); }
 
  private:
   DISALLOW_COPY_AND_MOVE(ProjectionPlanNode);
