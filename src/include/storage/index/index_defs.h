8306d9d0e78cd6cb922a7dea8aa85e3f91ba6f7d
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2019-03-23 00:16:13 -0400

    Checkpoint. IndexKeyColumn now packs its attributes into a uint32_t.

--- src/include/storage/index/index_defs.h
+++ src/include/storage/index/index_defs.h
@@ -17,7 +17,7 @@ class IndexKeyColumn {
    * @param nullable whether the column is nullable
    * @param type_id the non-varlen type of the column
    */
-  IndexKeyColumn(catalog::indexkeycol_oid_t oid, type::TypeId type_id, bool nullable) : oid_(oid) {
+  IndexKeyColumn(catalog::indexkeycol_oid_t oid, type::TypeId type_id, bool nullable) : oid_(oid), packed_type_(0) {
     TERRIER_ASSERT(!(type_id == type::TypeId::VARCHAR || type_id == type::TypeId::VARBINARY),
                    "Non-varlen constructor.");
     SetTypeId(type_id);
@@ -32,7 +32,7 @@ class IndexKeyColumn {
    * @param max_varlen_size the maximum varlen size
    */
   IndexKeyColumn(catalog::indexkeycol_oid_t oid, type::TypeId type_id, bool nullable, uint16_t max_varlen_size)
-      : oid_(oid) {
+      : oid_(oid), packed_type_(0) {
     TERRIER_ASSERT(type_id == type::TypeId::VARCHAR || type_id == type::TypeId::VARBINARY, "Varlen constructor.");
     SetTypeId(type_id);
     SetNullable(nullable);
@@ -48,27 +48,42 @@ class IndexKeyColumn {
    * @warning only defined for varlen types
    * @return maximum varlen size of this varlen column
    */
-  uint16_t GetMaxVarlenSize() const { return max_varlen_size_; }
+  uint16_t GetMaxVarlenSize() const { return static_cast<uint16_t>((packed_type_ & MASK_VARLEN) >> OFFSET_VARLEN); }
 
   /**
    * @return type of this key column
    */
-  type::TypeId GetType() const { return type_id_; }
+  type::TypeId GetType() const { return static_cast<type::TypeId>(packed_type_ & MASK_TYPE); }
 
   /**
    * @return true if this column is nullable
    */
-  bool IsNullable() const { return is_nullable_; }
+  bool IsNullable() const { return static_cast<bool>(packed_type_ & MASK_NULLABLE); }
 
  private:
-  catalog::indexkeycol_oid_t oid_;
-  uint16_t max_varlen_size_;
-  type::TypeId type_id_;
-  bool is_nullable_;
+  static constexpr uint32_t MASK_VARLEN = 0x00FFFF00;
+  static constexpr uint32_t MASK_NULLABLE = 0x00000080;
+  static constexpr uint32_t MASK_TYPE = 0x0000007F;
+  static constexpr uint32_t OFFSET_VARLEN = 8;
 
-  void SetMaxVarlenSize(uint16_t max_varlen_size) { max_varlen_size_ = max_varlen_size; }
-  void SetTypeId(type::TypeId type_id) { type_id_ = type_id; }
-  void SetNullable(bool nullable) { is_nullable_ = nullable; }
+  catalog::indexkeycol_oid_t oid_;
+  uint32_t packed_type_;
+
+  void SetMaxVarlenSize(uint16_t max_varlen_size) {
+    TERRIER_ASSERT((packed_type_ & MASK_VARLEN) == 0, "Should only set max varlen size once.");
+    const auto varlen_bits = (max_varlen_size << OFFSET_VARLEN) & MASK_VARLEN;
+    packed_type_ = packed_type_ | varlen_bits;
+  }
+
+  void SetTypeId(type::TypeId type_id) {
+    TERRIER_ASSERT((packed_type_ & MASK_TYPE) == 0, "Should only set type once.");
+    packed_type_ = packed_type_ | (static_cast<uint32_t>(type_id) & MASK_TYPE);
+  }
+
+  void SetNullable(bool nullable) {
+    TERRIER_ASSERT((packed_type_ & MASK_NULLABLE) == 0, "Should only set nullability once.");
+    packed_type_ = nullable ? packed_type_ | MASK_NULLABLE : packed_type_;
+  }
 };
 
 /**
