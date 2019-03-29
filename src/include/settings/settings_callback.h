8e5a1faca8e5235d928a46b42b2e80940b661b1e
Wenhao-H <wenhao.huang.work@gmail.com>
2019-03-29 13:02:10 -0400

    callback class

---
+++ src/include/settings/settings_callback.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <common/macros.h>
+namespace terrier::settings {
+
+using callback_fn = void (*)(void *, void *);
+
+/*
+ * SettingCallback:
+ * Stores singletons and settings callbacks.
+ * All settings callbacks must be implemented in this class.
+ */
+
+class SettingsCallback {
+ public:
+  static void EmptyCallback(void *old_value UNUSED_ATTRIBUTE,
+                            void *new_value UNUSED_ATTRIBUTE);
+
+ private:
+};
+
+}  // namespace terrier::settings
---
+++ src/settings/settings_callback.cpp
@@ -0,0 +1,8 @@
+#include "settings/settings_callback.h"
+
+namespace terrier::settings {
+
+void SettingsCallback::EmptyCallback(void *old_value UNUSED_ATTRIBUTE,
+                                     void *new_value UNUSED_ATTRIBUTE) { }
+
+} // namespace terrier::settings
