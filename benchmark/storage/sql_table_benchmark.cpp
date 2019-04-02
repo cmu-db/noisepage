dd497df52d1569f7c5cf97eccdaad1baf462b538
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2019-04-01 22:47:10 -0400

    Add MultiversionMismatchUpdate

--- benchmark/storage/sql_table_benchmark.cpp
+++ benchmark/storage/sql_table_benchmark.cpp
@@ -520,6 +520,42 @@ BENCHMARK_DEFINE_F(SqlTableBenchmark, MultiVersionMatchUpdate)(benchmark::State 
   state.SetItemsProcessed(state.iterations() * num_updates_);
 }
 
+// Update a tuple in a SqlTable num_updates_ times in a single thread
+// The SqlTable has multiple schema versions and the redo cannot be updated in place
+// NOLINTNEXTLINE
+BENCHMARK_DEFINE_F(SqlTableBenchmark, MultiVersionMismatchUpdate)(benchmark::State &state) {
+  // Populate read_table_ by inserting tuples
+  // We can use dummy timestamps here since we're not invoking concurrency control
+  transaction::TransactionContext txn(transaction::timestamp_t(0), transaction::timestamp_t(0), &buffer_pool_,
+                                      LOGGING_DISABLED);
+  // insert a tuple
+  storage::TupleSlot slot = table_->Insert(&txn, *redo_, storage::layout_version_t(0));
+
+  // create new schema
+  catalog::col_oid_t col_oid(column_num_);
+  std::vector<catalog::Schema::Column> new_columns(columns_.begin(), columns_.end() - 1);
+  new_columns.emplace_back("", type::TypeId::BIGINT, false, col_oid);
+  catalog::Schema new_schema(new_columns, storage::layout_version_t(1));
+  table_->UpdateSchema(new_schema);
+
+  // create a update buffer for the first tuple
+  std::vector<catalog::col_oid_t> all_col_oids(new_columns.size());
+  for (size_t i = 0; i < new_columns.size(); i++) all_col_oids[i] = new_columns[i].GetOid();
+  auto pair = table_->InitializerForProjectedRow(all_col_oids, storage::layout_version_t(1));
+  byte *update_buffer = common::AllocationUtil::AllocateAligned(pair.first.ProjectedRowSize());
+  storage::ProjectedRow *update_pr = pair.first.InitializeRow(update_buffer);
+  CatalogTestUtil::PopulateRandomRow(update_pr, new_schema, pair.second, &generator_);
+
+  // NOLINTNEXTLINE
+  for (auto _ : state) {
+    for (uint32_t i = 0; i < num_updates_; ++i) {
+      table_->Update(&txn, slot, *update_pr, pair.second, storage::layout_version_t(1));
+    }
+  }
+  delete[] update_buffer;
+  state.SetItemsProcessed(state.iterations() * num_updates_);
+}
+
 BENCHMARK_REGISTER_F(SqlTableBenchmark, SimpleInsert)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_REGISTER_F(SqlTableBenchmark, ConcurrentInsert)->Unit(benchmark::kMillisecond)->UseRealTime();
@@ -543,5 +579,7 @@ BENCHMARK_REGISTER_F(SqlTableBenchmark, ConcurrentMultiVersionRead)->Unit(benchm
 BENCHMARK_REGISTER_F(SqlTableBenchmark, SingleVersionUpdate)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_REGISTER_F(SqlTableBenchmark, MultiVersionMatchUpdate)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_REGISTER_F(SqlTableBenchmark, MultiVersionMismatchUpdate)->Unit(benchmark::kMillisecond);
 
 }  // namespace terrier
