2a53ab219b92edfbcbdddc987b5bfb54acab37c2
Matt Butrovich <mbutrovich@gmail.com>
2019-04-04 15:48:00 -0400

    Basic file structure and CMake additions.

---
+++ benchmark/tpcc/tpcc_benchmark.cpp
@@ -0,0 +1,65 @@
+#include <random>
+#include <vector>
+#include "benchmark/benchmark.h"
+#include "common/scoped_timer.h"
+#include "storage/garbage_collector.h"
+#include "transaction/transaction_manager.h"
+
+namespace terrier {
+
+class TPCCBenchmark : public benchmark::Fixture {
+ public:
+  void StartGC(transaction::TransactionManager *const txn_manager) {
+    gc_ = new storage::GarbageCollector(txn_manager);
+    run_gc_ = true;
+    gc_thread_ = std::thread([this] { GCThreadLoop(); });
+  }
+
+  void EndGC() {
+    run_gc_ = false;
+    gc_thread_.join();
+    // Make sure all garbage is collected. This take 2 runs for unlink and deallocate
+    gc_->PerformGarbageCollection();
+    gc_->PerformGarbageCollection();
+    delete gc_;
+  }
+  const uint64_t blockstore_size_limit_ = 1000;
+  const uint64_t blockstore_reuse_limit_ = 1000;
+  const uint64_t buffersegment_size_limit_ = 1000000;
+  const uint64_t buffersegment_reuse_limit_ = 1000000;
+  storage::BlockStore block_store_{blockstore_size_limit_, blockstore_reuse_limit_};
+  storage::RecordBufferSegmentPool buffer_pool_{buffersegment_size_limit_, buffersegment_reuse_limit_};
+  std::default_random_engine generator_;
+  const uint32_t num_concurrent_txns_ = 4;
+
+ private:
+  std::thread gc_thread_;
+  storage::GarbageCollector *gc_ = nullptr;
+  volatile bool run_gc_ = false;
+  const std::chrono::milliseconds gc_period_{10};
+
+  void GCThreadLoop() {
+    while (run_gc_) {
+      std::this_thread::sleep_for(gc_period_);
+      gc_->PerformGarbageCollection();
+    }
+  }
+};
+
+// NOLINTNEXTLINE
+BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
+  transaction::TransactionManager txn_manager(&buffer_pool_, true, LOGGING_DISABLED);
+  // NOLINTNEXTLINE
+  for (auto _ : state) {
+    StartGC(&txn_manager);
+    //    uint64_t elapsed_ms;
+    //    { common::ScopedTimer timer(&elapsed_ms); }
+    //    state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
+    EndGC();
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+// BENCHMARK_REGISTER_F(TPCCBenchmark, Basic)->Unit(benchmark::kMillisecond)->UseManualTime();
+BENCHMARK_REGISTER_F(TPCCBenchmark, Basic)->Unit(benchmark::kMillisecond);
+}  // namespace terrier
