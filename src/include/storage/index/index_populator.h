4715a134f64073f32efdfa42656c04ab40c80681
Calculuser <alvin.zuo0923@gmail.com>
2019-04-08 19:06:19 -0400

    Document IndexPopulator class.

--- src/include/storage/index/index_populator.h
+++ src/include/storage/index/index_populator.h
@@ -4,8 +4,24 @@
 #include "storage/sql_table.h"
 #include "transaction/transaction_context.h"
 namespace terrier::storage::index {
+
+/**
+ * An index populator is a helper class that populate the latest version of all tuples in the table
+ * visible to the current transaction, pack target columns into ProjectedRow's and insert into
+ * the given index.
+ *
+ * This class can only be used when creating an index.
+ */
 class IndexPopulator {
  public:
+  /**
+   * The method populates all tuples in the table visible to current transaction with latest version,
+   * then pack target columns into ProjectedRow's and insert into the given index.
+   *
+   * @param txn current transaction context
+   * @param sql_table the target sql table
+   * @param index target index inserted into
+   */
   static void PopulateIndex(transaction::TransactionContext *txn, const SqlTable &sql_table, const Index &index) {
     const IndexMetadata &metadata = index.metadata_;
     const auto &pr_initializer = metadata.GetProjectedRowInitializer();
