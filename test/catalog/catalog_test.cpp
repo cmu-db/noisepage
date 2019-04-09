332741456f72e46db50f220a2656be8c6abfa94f
Yesheng Ma <kimi.ysma@gmail.com>
2019-04-09 02:30:06 -0400

    add test commit

--- test/catalog/catalog_test.cpp
+++ test/catalog/catalog_test.cpp
@@ -40,7 +40,7 @@ TEST_F(CatalogTests, CreateDatabaseTest) {
   catalog_->CreateDatabase(txn_, "test_database");
   auto db_handle = catalog_->GetDatabaseHandle();
   auto entry = db_handle.GetDatabaseEntry(txn_, "test_database");
-  auto oid = entry->GetDatabaseOid();
+  auto oid = entry->GetOid();
   catalog_->Dump(txn_, oid);
 }
 
@@ -65,7 +65,7 @@ TEST_F(CatalogTests, DeleteDatabaseTest) {
   catalog_->CreateDatabase(txn_, "test_database");
   auto db_handle = catalog_->GetDatabaseHandle();
   auto entry = db_handle.GetDatabaseEntry(txn_, "test_database");
-  auto oid = entry->GetDatabaseOid();
+  auto oid = entry->GetOid();
   catalog_->Dump(txn_, oid);
 
   // delete it
