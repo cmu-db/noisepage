659712876534fee52487f1d982d6f5147398610f
Tianyu Li <tli2@andrew.cmu.edu>
2019-04-11 17:23:34 -0400

    Go away GCC, nobody likes you

--- src/include/storage/block_access_controller.h
+++ src/include/storage/block_access_controller.h
@@ -8,7 +8,10 @@ namespace terrier::storage {
 // TODO(Tianyu): Placeholder for the actual implementation that has the same number of bytes. We use this to make sure
 //               the new offset calculations and everything works.
 class BlockAccessController {
- private:
-  uint64_t UNUSED_ATTRIBUTE bytes_;
+ public:
+  /**
+   * TODO(Tianyu): Make GCC happy
+   */
+  uint64_t bytes_;
 };
 }  // namespace terrier::storage
