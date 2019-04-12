135386ae581d2e05c0c28184caf30984d58ab1af
Matt Butrovich <mbutrovich@gmail.com>
2019-04-12 12:22:48 -0400

    Checkpoint for Payment.

--- benchmark/include/tpcc/payment.h
+++ benchmark/include/tpcc/payment.h
@@ -32,6 +32,27 @@ class Payment {
   const uint8_t w_ytd_select_pr_offset;
   const storage::ProjectedRowInitializer warehouse_update_pr_initializer;
 
+  const uint8_t d_id_key_pr_offset;
+  const uint8_t d_w_id_key_pr_offset;
+
+  const catalog::col_oid_t d_name_oid;
+  const catalog::col_oid_t d_street_1_oid;
+  const catalog::col_oid_t d_street_2_oid;
+  const catalog::col_oid_t d_city_oid;
+  const catalog::col_oid_t d_state_oid;
+  const catalog::col_oid_t d_zip_oid;
+  const catalog::col_oid_t d_ytd_oid;
+  const storage::ProjectedRowInitializer district_select_pr_initializer;
+  const storage::ProjectionMap district_select_pr_map;
+  const uint8_t d_name_select_pr_offset;
+  const uint8_t d_street_1_select_pr_offset;
+  const uint8_t d_street_2_select_pr_offset;
+  const uint8_t d_city_select_pr_offset;
+  const uint8_t d_state_select_pr_offset;
+  const uint8_t d_zip_select_pr_offset;
+  const uint8_t d_ytd_select_pr_offset;
+  const storage::ProjectedRowInitializer district_update_pr_initializer;
+
  public:
   explicit Payment(const Database *const db)
 
@@ -59,7 +80,35 @@ class Payment {
         w_state_select_pr_offset(warehouse_select_pr_map.at(w_state_oid)),
         w_zip_select_pr_offset(warehouse_select_pr_map.at(w_zip_oid)),
         w_ytd_select_pr_offset(warehouse_select_pr_map.at(w_ytd_oid)),
-        warehouse_update_pr_initializer(db->warehouse_table_->InitializerForProjectedRow({w_ytd_oid}).first)
+        warehouse_update_pr_initializer(db->warehouse_table_->InitializerForProjectedRow({w_ytd_oid}).first),
+
+        // District metadata
+        d_id_key_pr_offset(db->district_index_->GetKeyOidToOffsetMap().at(db->district_key_schema_.at(1).GetOid())),
+        d_w_id_key_pr_offset(db->district_index_->GetKeyOidToOffsetMap().at(db->district_key_schema_.at(0).GetOid())),
+
+        d_name_oid(db->district_schema_.GetColumn(2).GetOid()),
+        d_street_1_oid(db->district_schema_.GetColumn(3).GetOid()),
+        d_street_2_oid(db->district_schema_.GetColumn(4).GetOid()),
+        d_city_oid(db->district_schema_.GetColumn(5).GetOid()),
+        d_state_oid(db->district_schema_.GetColumn(6).GetOid()),
+        d_zip_oid(db->district_schema_.GetColumn(7).GetOid()),
+        d_ytd_oid(db->district_schema_.GetColumn(9).GetOid()),
+        district_select_pr_initializer(db->district_table_
+                                           ->InitializerForProjectedRow({d_name_oid, d_street_1_oid, d_street_2_oid,
+                                                                         d_city_oid, d_state_oid, d_zip_oid, d_ytd_oid})
+                                           .first),
+        district_select_pr_map(db->district_table_
+                                   ->InitializerForProjectedRow({d_name_oid, d_street_1_oid, d_street_2_oid, d_city_oid,
+                                                                 d_state_oid, d_zip_oid, d_ytd_oid})
+                                   .second),
+        d_name_select_pr_offset(district_select_pr_map.at(d_name_oid)),
+        d_street_1_select_pr_offset(district_select_pr_map.at(d_street_1_oid)),
+        d_street_2_select_pr_offset(district_select_pr_map.at(d_street_2_oid)),
+        d_city_select_pr_offset(district_select_pr_map.at(d_city_oid)),
+        d_state_select_pr_offset(district_select_pr_map.at(d_state_oid)),
+        d_zip_select_pr_offset(district_select_pr_map.at(d_zip_oid)),
+        d_ytd_select_pr_offset(district_select_pr_map.at(d_ytd_oid)),
+        district_update_pr_initializer(db->district_table_->InitializerForProjectedRow({d_ytd_oid}).first)
 
   {}
 
@@ -105,6 +154,41 @@ class Payment {
     bool UNUSED_ATTRIBUTE result = db->warehouse_table_->Update(txn, index_scan_results[0], *warehouse_update_tuple);
     TERRIER_ASSERT(result,
                    "Warehouse update failed. This assertion assumes 1:1 mapping between warehouse and workers.");
+
+    // Look up D_ID, W_ID in index
+    const auto district_key_pr_initializer = db->district_index_->GetProjectedRowInitializer();
+    auto *const district_key = district_key_pr_initializer.InitializeRow(worker->district_key_buffer);
+
+    *reinterpret_cast<int32_t *>(district_key->AccessForceNotNull(d_id_key_pr_offset)) = args.d_id;
+    *reinterpret_cast<int32_t *>(district_key->AccessForceNotNull(d_w_id_key_pr_offset)) = args.w_id;
+
+    index_scan_results.clear();
+    db->district_index_->ScanKey(*district_key, &index_scan_results);
+    TERRIER_ASSERT(index_scan_results.size() == 1, "District index lookup failed.");
+
+    // Select D_NAME, D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP, D_YTD in table
+    auto *const district_select_tuple = district_select_pr_initializer.InitializeRow(worker->district_tuple_buffer);
+    db->district_table_->Select(txn, index_scan_results[0], district_select_tuple);
+    const auto UNUSED_ATTRIBUTE d_name =
+        *reinterpret_cast<storage::VarlenEntry *>(district_select_tuple->AccessWithNullCheck(d_name_select_pr_offset));
+    const auto UNUSED_ATTRIBUTE d_street_1 = *reinterpret_cast<storage::VarlenEntry *>(
+        district_select_tuple->AccessWithNullCheck(d_street_1_select_pr_offset));
+    const auto UNUSED_ATTRIBUTE d_street_2 = *reinterpret_cast<storage::VarlenEntry *>(
+        district_select_tuple->AccessWithNullCheck(d_street_2_select_pr_offset));
+    const auto UNUSED_ATTRIBUTE d_city =
+        *reinterpret_cast<storage::VarlenEntry *>(district_select_tuple->AccessWithNullCheck(d_city_select_pr_offset));
+    const auto UNUSED_ATTRIBUTE d_state =
+        *reinterpret_cast<storage::VarlenEntry *>(district_select_tuple->AccessWithNullCheck(d_state_select_pr_offset));
+    const auto UNUSED_ATTRIBUTE d_zip =
+        *reinterpret_cast<storage::VarlenEntry *>(district_select_tuple->AccessWithNullCheck(d_zip_select_pr_offset));
+    const auto d_ytd = *reinterpret_cast<double *>(district_select_tuple->AccessWithNullCheck(d_ytd_select_pr_offset));
+
+    // Increase D_YTD by H_AMOUNT in table
+    auto *const district_update_tuple = district_update_pr_initializer.InitializeRow(worker->district_tuple_buffer);
+    *reinterpret_cast<double *>(district_update_tuple->AccessForceNotNull(0)) = d_ytd + args.h_amount;
+
+    result = db->district_table_->Update(txn, index_scan_results[0], *district_update_tuple);
+    TERRIER_ASSERT(result, "District update failed. This assertion assumes 1:1 mapping between warehouse and workers.");
 
     txn_manager->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
 
