341d9bbf277c2523cdb0c9c05f307d3551dbf59e
Matt Butrovich <mbutrovich@gmail.com>
2019-04-22 16:40:17 -0400

    Add multithreaded tests to verify Insert() and InsertUnique() behaviors.

--- test/storage/bwtree_index_test.cpp
+++ test/storage/bwtree_index_test.cpp
@@ -6,6 +6,7 @@
 #include <random>
 #include <vector>
 #include "portable_endian/portable_endian.h"
+#include "storage/garbage_collector.h"
 #include "storage/index/compact_ints_key.h"
 #include "storage/index/index_builder.h"
 #include "storage/projected_row.h"
@@ -23,14 +24,42 @@ namespace terrier::storage::index {
 
 class BwTreeIndexTests : public TerrierTest {
  private:
-  storage::BlockStore block_store_{100, 100};
-  storage::RecordBufferSegmentPool buffer_pool_{10000, 10000};
+  void StartGC(transaction::TransactionManager *const txn_manager) {
+    gc_ = new storage::GarbageCollector(txn_manager);
+    run_gc_ = true;
+    gc_thread_ = std::thread([this] { GCThreadLoop(); });
+  }
+
+  void EndGC() {
+    run_gc_ = false;
+    gc_thread_.join();
+    // Make sure all garbage is collected. This take 2 runs for unlink and deallocate
+    gc_->PerformGarbageCollection();
+    gc_->PerformGarbageCollection();
+    delete gc_;
+  }
+
+  std::thread gc_thread_;
+  storage::GarbageCollector *gc_ = nullptr;
+  volatile bool run_gc_ = false;
+  const std::chrono::milliseconds gc_period_{10};
+
+  void GCThreadLoop() {
+    while (run_gc_) {
+      std::this_thread::sleep_for(gc_period_);
+      gc_->PerformGarbageCollection();
+    }
+  }
+
+  storage::BlockStore block_store_{1000, 1000};
+  storage::RecordBufferSegmentPool buffer_pool_{100000, 100000};
   const catalog::Schema table_schema_{
       catalog::Schema({{"attribute", type::TypeId::INTEGER, false, catalog::col_oid_t(0)}})};
   const IndexKeySchema key_schema_{{catalog::indexkeycol_oid_t(1), type::TypeId::INTEGER, false}};
 
  public:
   std::default_random_engine generator_;
+  const uint32_t num_threads_ = MultiThreadTestUtil::HardwareConcurrency();
 
   // SqlTable
   storage::SqlTable *const sql_table_{new storage::SqlTable(&block_store_, table_schema_, catalog::table_oid_t(1))};
@@ -39,14 +68,18 @@ class BwTreeIndexTests : public TerrierTest {
 
   // BwTreeIndex
   Index *default_index_, *unique_index_;
-  transaction::TransactionManager txn_manager_{&buffer_pool_, false, LOGGING_DISABLED};
+  transaction::TransactionManager txn_manager_{&buffer_pool_, true, LOGGING_DISABLED};
 
   byte *insert_buffer_, *key_buffer_1_, *key_buffer_2_;
-  std::vector<transaction::TransactionContext *> loose_txns_;
+
+  common::WorkerPool thread_pool_{num_threads_, {}};
 
  protected:
   void SetUp() override {
     TerrierTest::SetUp();
+
+    StartGC(&txn_manager_);
+
     unique_index_ = (IndexBuilder()
                          .SetConstraintType(ConstraintType::UNIQUE)
                          .SetKeySchema(key_schema_)
@@ -65,13 +98,151 @@ class BwTreeIndexTests : public TerrierTest {
         common::AllocationUtil::AllocateAligned(default_index_->GetProjectedRowInitializer().ProjectedRowSize());
   }
   void TearDown() override {
-    for (auto ptr : loose_txns_) delete ptr;
+    EndGC();
     delete sql_table_;
     delete default_index_;
     delete unique_index_;
     TerrierTest::TearDown();
   }
 };
+
+/**
+ * This test creates multiple worker threads that all try to insert [0,num_inserts) as tuples in the table and into the
+ * primary key index. At completion of the workload, only num_inserts_ txns should have committed with visible versions
+ * in the index and table.
+ */
+// NOLINTNEXTLINE
+TEST_F(BwTreeIndexTests, UniqueInsert) {
+  const uint32_t num_inserts_ = 100000;  // number of tuples/primary keys for each worker to attempt to insert
+  auto workload = [&](uint32_t worker_id) {
+    auto *const insert_buffer =
+        common::AllocationUtil::AllocateAligned(unique_index_->GetProjectedRowInitializer().ProjectedRowSize());
+    auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer);
+    auto *const key_buffer =
+        common::AllocationUtil::AllocateAligned(unique_index_->GetProjectedRowInitializer().ProjectedRowSize());
+    auto *const insert_key = unique_index_->GetProjectedRowInitializer().InitializeRow(key_buffer);
+
+    // some threads count up, others count down. This is to mix whether threads abort for write-write conflict or
+    // previously committed versions
+    if (worker_id % 2 == 0) {
+      for (uint32_t i = 0; i < num_inserts_; i++) {
+        auto *const insert_txn = txn_manager_.BeginTransaction();
+        *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+        const auto tuple_slot = sql_table_->Insert(insert_txn, *insert_tuple);
+
+        *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+        if (unique_index_->InsertUnique(*insert_txn, *insert_key, tuple_slot)) {
+          txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
+        } else {
+          txn_manager_.Abort(insert_txn);
+        }
+      }
+
+    } else {
+      for (uint32_t i = num_inserts_ - 1; i < num_inserts_; i--) {
+        auto *const insert_txn = txn_manager_.BeginTransaction();
+        *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+        const auto tuple_slot = sql_table_->Insert(insert_txn, *insert_tuple);
+
+        *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+        if (unique_index_->InsertUnique(*insert_txn, *insert_key, tuple_slot)) {
+          txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
+        } else {
+          txn_manager_.Abort(insert_txn);
+        }
+      }
+    }
+
+    delete[] insert_buffer;
+  };
+
+  // run the workload
+  for (uint32_t i = 0; i < num_threads_; i++) {
+    thread_pool_.SubmitTask([i, &workload] { workload(i); });
+  }
+  thread_pool_.WaitUntilAllFinished();
+
+  // scan the results
+  auto *const scan_txn = txn_manager_.BeginTransaction();
+
+  std::vector<storage::TupleSlot> results;
+
+  auto *const low_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+  auto *const high_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_2_);
+
+  // scan[0,num_inserts_) should hit num_inserts_ keys (no duplicates)
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 0;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = num_inserts_ - 1;
+  unique_index_->ScanAscending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), num_inserts_);
+
+  txn_manager_.Commit(scan_txn, TestCallbacks::EmptyCallback, nullptr);
+}
+
+/**
+ * This test creates multiple worker threads that all try to insert [0,num_inserts) as tuples in the table and into the
+ * primary key index. At completion of the workload, all num_inserts_ txns * num_threads_ should have committed with
+ * visible versions in the index and table.
+ */
+// NOLINTNEXTLINE
+TEST_F(BwTreeIndexTests, DefaultInsert) {
+  const uint32_t num_inserts_ = 100000;  // number of tuples/primary keys for each worker to attempt to insert
+  auto workload = [&](uint32_t worker_id) {
+    auto *const insert_buffer =
+        common::AllocationUtil::AllocateAligned(default_index_->GetProjectedRowInitializer().ProjectedRowSize());
+    auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer);
+    auto *const key_buffer =
+        common::AllocationUtil::AllocateAligned(default_index_->GetProjectedRowInitializer().ProjectedRowSize());
+    auto *const insert_key = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer);
+
+    // some threads count up, others count down. Threads shouldn't abort each other
+    if (worker_id % 2 == 0) {
+      for (uint32_t i = 0; i < num_inserts_; i++) {
+        auto *const insert_txn = txn_manager_.BeginTransaction();
+        *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+        const auto tuple_slot = sql_table_->Insert(insert_txn, *insert_tuple);
+
+        *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+        EXPECT_TRUE(default_index_->Insert(*insert_key, tuple_slot));
+        txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
+      }
+    } else {
+      for (uint32_t i = num_inserts_ - 1; i < num_inserts_; i--) {
+        auto *const insert_txn = txn_manager_.BeginTransaction();
+        *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+        const auto tuple_slot = sql_table_->Insert(insert_txn, *insert_tuple);
+
+        *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+        EXPECT_TRUE(default_index_->Insert(*insert_key, tuple_slot));
+        txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
+      }
+    }
+
+    delete[] insert_buffer;
+  };
+
+  // run the workload
+  for (uint32_t i = 0; i < num_threads_; i++) {
+    thread_pool_.SubmitTask([i, &workload] { workload(i); });
+  }
+  thread_pool_.WaitUntilAllFinished();
+
+  // scan the results
+  auto *const scan_txn = txn_manager_.BeginTransaction();
+
+  std::vector<storage::TupleSlot> results;
+
+  auto *const low_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+  auto *const high_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_2_);
+
+  // scan[0,num_inserts_) should hit num_inserts_ * num_threads_ keys
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 0;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = num_inserts_ - 1;
+  default_index_->ScanAscending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), num_inserts_ * num_threads_);
+
+  txn_manager_.Commit(scan_txn, TestCallbacks::EmptyCallback, nullptr);
+}
 
 /**
  * Tests basic scan behavior using various windows to scan over (some out of of bounds of keyspace, some matching
@@ -82,7 +253,6 @@ TEST_F(BwTreeIndexTests, ScanAscending) {
   // populate index with [0..20] even keys
   std::map<int32_t, storage::TupleSlot> reference;
   auto *const insert_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(insert_txn);
   for (int32_t i = 0; i <= 20; i += 2) {
     auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer_);
     *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
@@ -96,7 +266,6 @@ TEST_F(BwTreeIndexTests, ScanAscending) {
   txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
 
   auto *const scan_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(scan_txn);
 
   std::vector<storage::TupleSlot> results;
 
@@ -155,7 +324,6 @@ TEST_F(BwTreeIndexTests, ScanDescending) {
   // populate index with [0..20] even keys
   std::map<int32_t, storage::TupleSlot> reference;
   auto *const insert_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(insert_txn);
   for (int32_t i = 0; i <= 20; i += 2) {
     auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer_);
     *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
@@ -169,7 +337,6 @@ TEST_F(BwTreeIndexTests, ScanDescending) {
   txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
 
   auto *const scan_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(scan_txn);
 
   std::vector<storage::TupleSlot> results;
 
@@ -228,7 +395,6 @@ TEST_F(BwTreeIndexTests, ScanLimitAscending) {
   // populate index with [0..20] even keys
   std::map<int32_t, storage::TupleSlot> reference;
   auto *const insert_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(insert_txn);
   for (int32_t i = 0; i <= 20; i += 2) {
     auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer_);
     *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
@@ -242,7 +408,6 @@ TEST_F(BwTreeIndexTests, ScanLimitAscending) {
   txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
 
   auto *const scan_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(scan_txn);
 
   std::vector<storage::TupleSlot> results;
 
@@ -297,7 +462,6 @@ TEST_F(BwTreeIndexTests, ScanLimitDescending) {
   // populate index with [0..20] even keys
   std::map<int32_t, storage::TupleSlot> reference;
   auto *const insert_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(insert_txn);
   for (int32_t i = 0; i <= 20; i += 2) {
     auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer_);
     *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
@@ -311,7 +475,6 @@ TEST_F(BwTreeIndexTests, ScanLimitDescending) {
   txn_manager_.Commit(insert_txn, TestCallbacks::EmptyCallback, nullptr);
 
   auto *const scan_txn = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(scan_txn);
 
   std::vector<storage::TupleSlot> results;
 
@@ -379,7 +542,6 @@ TEST_F(BwTreeIndexTests, ScanLimitDescending) {
 // NOLINTNEXTLINE
 TEST_F(BwTreeIndexTests, CommitInsert1) {
   auto *txn0 = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(txn0);
 
   auto *const insert_tuple = tuple_initializer_.InitializeRow(insert_buffer_);
   *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = 15721;
@@ -400,7 +562,6 @@ TEST_F(BwTreeIndexTests, CommitInsert1) {
   results.clear();
 
   auto *txn1 = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(txn1);
 
   default_index_->ScanKey(*txn1, *scan_key_pr, &results);
   EXPECT_EQ(results.size(), 0);
@@ -415,7 +576,6 @@ TEST_F(BwTreeIndexTests, CommitInsert1) {
   txn_manager_.Commit(txn1, TestCallbacks::EmptyCallback, nullptr);
 
   auto *txn2 = txn_manager_.BeginTransaction();
-  loose_txns_.push_back(txn2);
 
   default_index_->ScanKey(*txn2, *scan_key_pr, &results);
   EXPECT_EQ(results.size(), 1);
