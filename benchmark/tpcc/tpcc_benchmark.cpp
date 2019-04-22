7466e456a9c85d007143d55aef6f04423a8ba8a8
Matt Butrovich <mbutrovich@gmail.com>
2019-04-22 15:50:04 -0400

    Docs.

--- benchmark/tpcc/tpcc_benchmark.cpp
+++ benchmark/tpcc/tpcc_benchmark.cpp
@@ -50,7 +50,9 @@ class TPCCBenchmark : public benchmark::Fixture {
     delete gc_;
   }
 
-  const uint64_t blockstore_size_limit_ = 1000;
+  const uint64_t blockstore_size_limit_ =
+      1000;  // May need to increase this if num_threads_ or num_precomputed_txns_per_worker_ are greatly increased
+             // (table sizes grow with a bigger workload)
   const uint64_t blockstore_reuse_limit_ = 1000;
   const uint64_t buffersegment_size_limit_ = 1000000;
   const uint64_t buffersegment_reuse_limit_ = 1000000;
@@ -59,14 +61,19 @@ class TPCCBenchmark : public benchmark::Fixture {
   std::default_random_engine generator_;
   storage::LogManager *log_manager_ = nullptr;
 
-  const bool only_count_new_order_ = false;
-  const int8_t num_threads_ = 4;
-  const uint32_t num_precomputed_txns_per_worker_ = 100000;
+  const bool only_count_new_order_ = false;  // TPC-C specification is to only measure throughput for New Order in final
+                                             // result, but most academic papers use all txn types
+  const int8_t num_threads_ = 4;  // defines the number of terminals (workers running txns) and warehouses for the
+                                  // benchmark. Sometimes called scale factor
+  const uint32_t num_precomputed_txns_per_worker_ = 100000;  // Number of txns to run per terminal (worker thread)
+
+  // Txn distribution. New Order is not provided because it's the implicit difference of the txns below from 100
   const uint32_t w_payment = 43;
   const uint32_t w_delivery = 4;
   const uint32_t w_order_status = 4;
   const uint32_t w_stock_level = 4;
 
+  // Toggle WAL on or off for the benchmark
   const bool logging_enabled_ = false;
 
   common::WorkerPool thread_pool_{static_cast<uint32_t>(num_threads_), {}};
@@ -102,6 +109,7 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
   std::vector<tpcc::Worker> workers;
   workers.reserve(num_threads_);
 
+  // Reset the worker pool
   thread_pool_.Shutdown();
   thread_pool_.SetNumWorkers(num_threads_);
   thread_pool_.Startup();
@@ -111,7 +119,7 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
   transaction::TransactionManager txn_manager(&buffer_pool_, true, log_manager_);
   auto tpcc_builder = tpcc::Builder(&block_store_);
 
-  // random number generation is slow, so we precompute the args
+  // Precompute all of the input arguments for every txn to be run. We want to avoid the overhead at benchmark time
   std::vector<std::vector<tpcc::TransactionArgs>> precomputed_args;
   precomputed_args.reserve(workers.size());
 
@@ -155,6 +163,7 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
       workers.emplace_back(tpcc_db);
     }
 
+    // populate the tables and indexes
     tpcc::Loader::PopulateDatabase(&txn_manager, &generator_, tpcc_db, workers);
     if (logging_enabled_) log_manager_->Process();  // log all of the Inserts from table creation
     StartGC(&txn_manager);
