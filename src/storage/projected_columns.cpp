9f46aae4118ef89a1c51b719995d120508bf9a9a
jrolli <j.e.rollinson@gmail.com>
2019-04-23 18:02:28 -0400

    Embed attribute sizes into ProjectedColumns (#348)

--- src/storage/projected_columns.cpp
+++ src/storage/projected_columns.cpp
@@ -5,6 +5,17 @@
 #include <utility>
 #include <vector>
 namespace terrier::storage {
+const uint32_t ProjectedColumns::AttrSizeForColumn(uint16_t col_id) {
+  TERRIER_ASSERT(col_id < num_cols_, "Cannot get size for out-of-bounds column");
+  uint8_t shift;
+  for (shift = 0; shift < NUM_ATTR_BOUNDARIES; shift++) {
+    if (col_id < attr_ends_[shift]) break;
+  }
+  TERRIER_ASSERT(shift <= NUM_ATTR_BOUNDARIES, "Out-of-bounds attribute size");
+  TERRIER_ASSERT(shift >= 0, "Out-of-bounds attribute size");
+  return 16u >> shift;
+}
+
 ProjectedColumnsInitializer::ProjectedColumnsInitializer(const BlockLayout &layout, std::vector<col_id_t> col_ids,
                                                          const uint32_t max_tuples)
     : max_tuples_(max_tuples), col_ids_(std::move(col_ids)), offsets_(col_ids_.size()) {
@@ -13,12 +24,12 @@ ProjectedColumnsInitializer::ProjectedColumnsInitializer(const BlockLayout &layo
                  "ProjectedColumns should have fewer columns than the table (can't read version vector)");
   TERRIER_ASSERT((std::set<col_id_t>(col_ids_.cbegin(), col_ids_.cend())).size() == col_ids_.size(),
                  "There should not be any duplicated in the col_ids!");
-  // TODO(Tianyu): We should really assert that it has a subset of columns, but that is a bit more complicated.
 
   // Sort the projection list for optimal space utilization and delta application performance
   // If the col ids are valid ones laid out by BlockLayout, ascending order of id guarantees
   // descending order in attribute size.
   std::sort(col_ids_.begin(), col_ids_.end(), std::less<>());
+
   size_ = sizeof(ProjectedColumns);
   // space needed to store col_ids, must be padded up so that the following offsets are aligned
   size_ = StorageUtil::PadUpToSize(sizeof(uint32_t), size_ + static_cast<uint32_t>(col_ids_.size() * sizeof(uint16_t)));
@@ -28,13 +39,31 @@ ProjectedColumnsInitializer::ProjectedColumnsInitializer(const BlockLayout &layo
   // Space needed to store tuple slots, no need to pad bitmaps
   size_ += static_cast<uint32_t>(sizeof(TupleSlot) * max_tuples_);
 
+  int attr_size_index = 0;
+  for (int attr : {0, NUM_ATTR_BOUNDARIES - 1}) attr_ends_[attr] = 0;
+
   for (uint32_t i = 0; i < col_ids_.size(); i++) {
+    TERRIER_ASSERT(i < (1 << 15), "Out-of-bounds index");
     offsets_[i] = size_;
+
+    // Build out the array that stores the boundaries for attribute sizes
+    int attr_size = layout.AttrSize(col_ids_[i]);
+    TERRIER_ASSERT(attr_size <= (16 >> attr_size_index), "Out-of-order columns");
+    TERRIER_ASSERT(attr_size <= 16 && attr_size > 0, "Unexpected attribute size");
+    while (attr_size < (16 >> attr_size_index)) {
+      if (attr_size_index < (NUM_ATTR_BOUNDARIES - 1)) attr_ends_[attr_size_index + 1] = attr_ends_[attr_size_index];
+      attr_size_index++;
+    }
+    TERRIER_ASSERT(attr_size == (16 >> attr_size_index), "Non-power of two attribute size");
+    if (attr_size_index < NUM_ATTR_BOUNDARIES) attr_ends_[attr_size_index]++;
+    TERRIER_ASSERT(attr_size_index == NUM_ATTR_BOUNDARIES || attr_ends_[attr_size_index] == i + 1,
+                   "Inconsistent state on attribute bounds");
+
     // space needed to store the bitmap, padded up to 8 bytes
     size_ = StorageUtil::PadUpToSize(sizeof(uint64_t),
                                      size_ + common::RawBitmap::SizeInBytes(static_cast<uint32_t>(max_tuples_)));
     // Pad up to always 8 bytes across columns.
-    size_ = StorageUtil::PadUpToSize(sizeof(uint64_t), size_ + layout.AttrSize(col_ids_[i]) * max_tuples_);
+    size_ = StorageUtil::PadUpToSize(sizeof(uint64_t), size_ + attr_size * max_tuples_);
   }
 }
 
@@ -46,6 +75,7 @@ ProjectedColumns *ProjectedColumnsInitializer::Initialize(void *const head) cons
   result->size_ = size_;
   result->max_tuples_ = max_tuples_;
   result->num_tuples_ = 0;
+  for (int i = 0; i < 4; i++) result->attr_ends_[i] = attr_ends_[i];
   result->num_cols_ = static_cast<uint16_t>(col_ids_.size());
   for (uint32_t i = 0; i < col_ids_.size(); i++) result->ColumnIds()[i] = col_ids_[i];
   for (uint32_t i = 0; i < col_ids_.size(); i++) result->AttrValueOffsets()[i] = offsets_[i];
