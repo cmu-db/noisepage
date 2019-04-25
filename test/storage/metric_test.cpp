9cf0bff44a2c1083c0b2c8bc8e6b262dea6c4a23
Ubuntu <ubuntu@ip-172-31-5-114.us-east-2.compute.internal>
2019-04-25 23:17:09 +0000

    add storage test

--- test/storage/metric_test.cpp
+++ test/storage/metric_test.cpp
@@ -45,7 +45,7 @@ class MetricTests : public TerrierTest {
  * Basic test for testing database metric registration and stats collection, single thread
  */
 // NOLINTNEXTLINE
-TEST_F(MetricTests, DatabaseMetricTest) {
+TEST_F(MetricTests, DatabaseMetricBasicTest) {
   const uint32_t num_threads = MultiThreadTestUtil::HardwareConcurrency();
   common::WorkerPool thread_pool(num_threads, {});
   for (uint8_t i = 0; i < num_iterations_; i++) {
@@ -92,4 +92,58 @@ TEST_F(MetricTests, DatabaseMetricTest) {
     }
   }
 }
+
+TEST_F(MetricTests, DatabaseMetricStorageTest) {
+  for (uint8_t i = 0; i < num_iterations_; i++) {
+    auto stats_collector = storage::metric::ThreadLevelStatsCollector();
+
+    // transaction::TransactionManager txn_manager(&buffer_pool_, true, LOGGING_DISABLED);
+    // transaction::TransactionManager txn_manager(nullptr, false, LOGGING_DISABLED);
+    std::unordered_map<uint8_t, int32_t> commit_map;
+    std::unordered_map<uint8_t, int32_t> abort_map;
+    for (uint8_t j = 0; j < num_databases_; j++) {
+      commit_map[j] = 0;
+      abort_map[j] = 0;
+      auto num_txns_ = static_cast<uint8_t>(std::uniform_int_distribution<uint8_t>(0, UINT8_MAX)(generator_));
+      for (uint8_t k = 0; k < num_txns_; k++) {
+        auto *txn = txn_manager_->BeginTransaction();
+        storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTransactionBegin(txn);
+        auto txn_res = static_cast<bool>(std::uniform_int_distribution<uint8_t>(0, 1)(generator_));
+        if (txn_res) {
+          txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTransactionCommit(
+              txn, static_cast<catalog::db_oid_t>(j));
+          commit_map[j]++;
+        } else {
+          txn_manager_->Abort(txn);
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTransactionAbort(
+              txn, static_cast<catalog::db_oid_t>(j));
+          abort_map[j]++;
+        }
+      }
+    }
+
+    storage::metric::StatsAggregator aggregator(txn_manager_, catalog_);
+    aggregator.Aggregate();
+
+    auto txn = txn_manager_->BeginTransaction();
+    const catalog::db_oid_t terrier_oid(catalog::DEFAULT_DATABASE_OID);
+    auto db_handle = catalog_->GetDatabaseHandle();
+    auto table_handle = db_handle.GetNamespaceHandle(txn, terrier_oid).GetTableHandle(txn, "public");
+    auto table = table_handle.GetTable(txn, "database_metric_table");
+
+    for (uint8_t j = 0; j < num_databases_; j++) {
+      std::vector<type::TransientValue> search_vec;
+      search_vec.emplace_back(type::TransientValueFactory::GetInteger(static_cast<int32_t>(j)));
+      auto row = table->FindRow(txn, search_vec);
+      auto commit_cnt = type::TransientValuePeeker::PeekInteger(row[1]);
+      auto abort_cnt = type::TransientValuePeeker::PeekInteger(row[2]);
+      EXPECT_EQ(commit_cnt, commit_map[j]);
+      EXPECT_EQ(abort_cnt, abort_map[j]);
+    }
+    txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+
+  }
+}
+
 }  // namespace terrier
