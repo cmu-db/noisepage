2e66b3c76a4f2f5795b201aa3223ad9c38258391
Yuning Zhang <codeworm96@outlook.com>
2019-04-25 20:44:16 +0000

    Update benchmark

--- benchmark/storage/checkpoint_benchmark.cpp
+++ benchmark/storage/checkpoint_benchmark.cpp
@@ -10,26 +10,6 @@ namespace terrier {
 
 class CheckpointBenchmark : public benchmark::Fixture {
  public:
-  void UnlinkCheckpointFiles() {
-    // TODO(zhaozhes) : checkpoint directory is currently hard-coded here
-    char const *path = ".";
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir(path)) != nullptr) {
-      /* print all the files and directories within directory */
-      while ((ent = readdir(dir)) != nullptr) {
-        std::string checkpoint_file(ent->d_name);
-        if (checkpoint_file.find(CHECKPOINT_FILE_PREFIX) == 0) {
-          unlink(checkpoint_file.c_str());
-        }
-      }
-      closedir(dir);
-    } else {
-      /* could not open directory */
-      throw std::runtime_error("cannot open checkpoint directory");
-    }
-  }
-
   storage::CheckpointManager checkpoint_manager_{CHECKPOINT_FILE_PREFIX};
 };
 
@@ -63,7 +43,7 @@ BENCHMARK_DEFINE_F(CheckpointBenchmark, SingleTable)(benchmark::State &state) {
     }
     state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
     txn_manager->Commit(txn, StorageTestUtil::EmptyCallback, nullptr);
-    UnlinkCheckpointFiles();
+    checkpoint_manager_.UnlinkCheckpointFiles();
     delete txn;
   }
   state.SetItemsProcessed(state.iterations() * num_rows);
