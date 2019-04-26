910ca0eb37c2b9ff56931da67093619e5e9513d3
qdHe <qiduh@andrew.cmu.edu>
2019-04-26 20:06:23 +0000

    add txn metric test

--- test/storage/metric_test.cpp
+++ test/storage/metric_test.cpp
@@ -1,8 +1,11 @@
 #include <random>
 #include <unordered_map>
 #include "storage/metric/database_metric.h"
+#include "storage/metric/transaction_metric.h"
 #include "storage/metric/stats_aggregator.h"
 #include "storage/metric/thread_level_stats_collector.h"
+#include "transaction/transaction_manager.h"
+#include "transaction/transaction_defs.h"
 #include "util/test_harness.h"
 #include "util/testing_stats_util.h"
 #include "util/transaction_test_util.h"
@@ -39,6 +42,8 @@ class MetricTests : public TerrierTest {
   std::default_random_engine generator_;
   const uint8_t num_iterations_ = 2;
   const uint8_t num_databases_ = 2;
+  const uint8_t num_txns_ = 2;
+  const int64_t acc_err = 5;
 };
 
 /**
@@ -142,6 +147,88 @@ TEST_F(MetricTests, DatabaseMetricStorageTest) {
       auto abort_cnt = type::TransientValuePeeker::PeekInteger(row[2]);
       EXPECT_EQ(commit_cnt, commit_map[j]);
       EXPECT_EQ(abort_cnt, abort_map[j]);
+    }
+  }
+}
+
+TEST_F(MetricTests, TransactionMetricBasicTest) {
+  auto stats_collector = storage::metric::ThreadLevelStatsCollector();
+  storage::metric::StatsAggregator aggregator(txn_manager_, catalog_);
+
+  catalog::table_oid_t table_oid = static_cast<catalog::table_oid_t>(2); // any value
+  const catalog::db_oid_t database_oid(catalog::DEFAULT_DATABASE_OID);
+
+
+  for (uint8_t i = 0; i < num_iterations_; i++) {
+    std::unordered_map<uint8_t, transaction::timestamp_t> id_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> read_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> update_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> insert_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> delete_map;
+    std::unordered_map<transaction::timestamp_t, int64_t> latency_map;
+    for (uint8_t j = 0; j < num_txns_; j++) {
+      auto start = std::chrono::high_resolution_clock::now();
+      auto *txn = txn_manager_->BeginTransaction();
+      auto txn_id = txn->TxnId().load();
+      id_map[j] = txn_id;
+      read_map[txn_id] = 0;
+      update_map[txn_id] = 0;
+      insert_map[txn_id] = 0;
+      delete_map[txn_id] = 0;
+
+      auto num_ops_ = static_cast<uint8_t>(std::uniform_int_distribution<uint8_t>(1, UINT8_MAX)(generator_));
+      for (uint8_t k = 0; k < num_ops_; k++) {
+        auto op_type = std::uniform_int_distribution<uint8_t>(0, 3)(generator_);
+        if (op_type == 0) { // Read
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleRead(
+            txn, database_oid, table_oid);
+          read_map[txn_id]++;
+        } else if (op_type == 1) { // Update
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleUpdate(
+            txn, database_oid, table_oid);
+          update_map[txn_id]++;
+        } else if (op_type == 2) { // Insert
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleInsert(
+            txn, database_oid, table_oid);
+          insert_map[txn_id]++;
+        } else { // Delete
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleDelete(
+            txn, database_oid, table_oid);
+          delete_map[txn_id]++;
+        }
+      }
+      txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+      auto latency = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
+              std::chrono::high_resolution_clock::now() - start).count());
+      latency_map[txn_id] = latency;
+    }
+
+    auto result = aggregator.AggregateRawData();
+    EXPECT_FALSE(result.empty());
+
+    for (auto &raw_data : result) {
+      if (raw_data->GetMetricType() == storage::metric::MetricType::TRANSACTION) {
+        for (uint8_t j = 0; j < num_txns_; j++) {
+          auto txn_id = id_map[j];
+          auto read_cnt = dynamic_cast<storage::metric::TransactionMetricRawData *>(raw_data.get())
+                                ->GetTupleRead(txn_id);
+          auto update_cnt = dynamic_cast<storage::metric::TransactionMetricRawData *>(raw_data.get())
+                               ->GetTupleUpdate(txn_id);
+          auto insert_cnt = dynamic_cast<storage::metric::TransactionMetricRawData *>(raw_data.get())
+                                ->GetTupleInsert(txn_id);
+          auto delete_cnt = dynamic_cast<storage::metric::TransactionMetricRawData *>(raw_data.get())
+                               ->GetTupleDelete(txn_id);
+          auto latency = dynamic_cast<storage::metric::TransactionMetricRawData *>(raw_data.get())
+                               ->GetLatency(txn_id);
+
+	  EXPECT_EQ(read_cnt, read_map[txn_id]);
+          EXPECT_EQ(update_cnt, update_map[txn_id]);
+          EXPECT_EQ(insert_cnt, insert_map[txn_id]);
+          EXPECT_EQ(delete_cnt, delete_map[txn_id]);
+	  EXPECT_GE(latency_map[txn_id], latency);
+	  EXPECT_LT(latency_map[txn_id], latency+acc_err);
+        }
+      }
     }
   }
 }
