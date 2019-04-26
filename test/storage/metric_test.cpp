d1565d44c7e4f14a2c7cc9d5e5233108ddf8185e
qdHe <qiduh@andrew.cmu.edu>
2019-04-26 20:24:20 +0000

    add txn metric test

--- test/storage/metric_test.cpp
+++ test/storage/metric_test.cpp
@@ -233,4 +233,84 @@ TEST_F(MetricTests, TransactionMetricBasicTest) {
   }
 }
 
+
+TEST_F(MetricTests, TransactionMetricStorageTest) {
+  auto stats_collector = storage::metric::ThreadLevelStatsCollector();
+  storage::metric::StatsAggregator aggregator(txn_manager_, catalog_);
+
+  catalog::table_oid_t table_oid = static_cast<catalog::table_oid_t>(2);  // any value
+  const catalog::db_oid_t database_oid(catalog::DEFAULT_DATABASE_OID);
+
+  for (uint8_t i = 0; i < num_iterations_; i++) {
+    std::unordered_map<uint8_t, transaction::timestamp_t> id_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> read_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> update_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> insert_map;
+    std::unordered_map<transaction::timestamp_t, int32_t> delete_map;
+    std::unordered_map<transaction::timestamp_t, int64_t> latency_map;
+    for (uint8_t j = 0; j < num_txns_; j++) {
+      auto start = std::chrono::high_resolution_clock::now();
+      auto *txn = txn_manager_->BeginTransaction();
+      auto txn_id = txn->TxnId().load();
+      id_map[j] = txn_id;
+      read_map[txn_id] = 0;
+      update_map[txn_id] = 0;
+      insert_map[txn_id] = 0;
+      delete_map[txn_id] = 0;
+
+      auto num_ops_ = static_cast<uint8_t>(std::uniform_int_distribution<uint8_t>(1, UINT8_MAX)(generator_));
+      for (uint8_t k = 0; k < num_ops_; k++) {
+        auto op_type = std::uniform_int_distribution<uint8_t>(0, 3)(generator_);
+        if (op_type == 0) {  // Read
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleRead(txn, database_oid,
+                                                                                                table_oid);
+          read_map[txn_id]++;
+        } else if (op_type == 1) {  // Update
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleUpdate(txn, database_oid,
+                                                                                                  table_oid);
+          update_map[txn_id]++;
+        } else if (op_type == 2) {  // Insert
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleInsert(txn, database_oid,
+                                                                                                  table_oid);
+          insert_map[txn_id]++;
+        } else {  // Delete
+          storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleDelete(txn, database_oid,
+                                                                                                  table_oid);
+          delete_map[txn_id]++;
+        }
+      }
+      txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+      auto latency = static_cast<uint64_t>(
+          std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+              .count());
+      latency_map[txn_id] = latency;
+    }
+
+    aggregator.Aggregate(txn_);
+
+    auto db_handle = catalog_->GetDatabaseHandle();
+    auto table_handle = db_handle.GetNamespaceHandle(txn_, database_oid).GetTableHandle(txn_, "public");
+    auto table = table_handle.GetTable(txn_, "txn_metric_table");
+
+    for (uint8_t j = 0; j < num_txns_; j++) {
+      auto txn_id = id_map[j];
+      std::vector<type::TransientValue> search_vec;
+      search_vec.emplace_back(type::TransientValueFactory::GetBigInt(static_cast<uint64_t>(txn_id)));
+      auto row = table->FindRow(txn_, search_vec);
+      auto latency = type::TransientValuePeeker::PeekBigInt(row[1]);
+      auto read_cnt = type::TransientValuePeeker::PeekInteger(row[2]);
+      auto insert_cnt = type::TransientValuePeeker::PeekInteger(row[3]);
+      auto delete_cnt = type::TransientValuePeeker::PeekInteger(row[4]);
+      auto update_cnt = type::TransientValuePeeker::PeekInteger(row[5]);
+      EXPECT_EQ(read_cnt, read_map[txn_id]);
+      EXPECT_EQ(update_cnt, update_map[txn_id]);
+      EXPECT_EQ(insert_cnt, insert_map[txn_id]);
+      EXPECT_EQ(delete_cnt, delete_map[txn_id]);
+      EXPECT_GE(latency_map[txn_id], latency);
+      EXPECT_LT(latency_map[txn_id], latency + acc_err);
+    }
+  }
+}
+
+
 }  // namespace terrier
