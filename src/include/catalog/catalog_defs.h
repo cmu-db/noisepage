e599b050e7a802d89af6bc3ec032db582052bad6
Pervaze Akhtar <pakhtar@andrew.cmu.edu>
2019-05-02 09:51:11 -0400

    Fix merge issue + sort strong typedefs by name

--- src/include/catalog/catalog_defs.h
+++ src/include/catalog/catalog_defs.h
@@ -7,16 +7,16 @@ namespace terrier::catalog {
 #define START_OID 1001
 #define DEFAULT_DATABASE_OID db_oid_t(1)
 
+// in name order
 STRONG_TYPEDEF(col_oid_t, uint32_t);
 STRONG_TYPEDEF(db_oid_t, uint32_t);
-STRONG_TYPEDEF(namespace_oid_t, uint32_t);
 STRONG_TYPEDEF(index_oid_t, uint32_t);
 STRONG_TYPEDEF(indexkeycol_oid_t, uint32_t);
-STRONG_TYPEDEF(table_oid_t, uint32_t);
 STRONG_TYPEDEF(namespace_oid_t, uint32_t);
 STRONG_TYPEDEF(settings_oid_t, uint32_t);
+STRONG_TYPEDEF(table_oid_t, uint32_t);
 STRONG_TYPEDEF(tablespace_oid_t, uint32_t);
-STRONG_TYPEDEF(type_oid_t, uint32_t);
 STRONG_TYPEDEF(trigger_oid_t, uint32_t);
+STRONG_TYPEDEF(type_oid_t, uint32_t);
 STRONG_TYPEDEF(view_oid_t, uint32_t);
 }  // namespace terrier::catalog
