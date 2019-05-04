3a7ac689c8a6b2504488982ce79991ef2d3af423
John E. Rollinson <j.e.rollinson@gmail.com>
2019-05-04 17:17:33 -0400

    Remove const to make serialization framework happy

--- src/include/catalog/schema.h
+++ src/include/catalog/schema.h
@@ -211,7 +211,7 @@ class Schema {
   }
 
  private:
-  const storage::layout_version_t version_;
+  storage::layout_version_t version_;
   const std::vector<Column> columns_;
   std::unordered_map<col_oid_t, uint32_t> col_oid_to_offset;
 };
