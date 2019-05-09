385d3c90ec2ac50508afa487413b6f8d82fa8baa
songzz <songzhaozhe@126.com>
2019-05-09 11:12:10 -0400

    why

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -17,6 +17,7 @@ DataTable::DataTable(BlockStore *const store, const BlockLayout &layout, const l
 }
 
 DataTable::~DataTable() {
+  STORAGE_LOG_ERROR("destucting data table")
   common::SpinLatch::ScopedSpinLatch guard(&blocks_latch_);
   for (RawBlock *block : blocks_) {
     DeallocateVarlensOnShutdown(block);
