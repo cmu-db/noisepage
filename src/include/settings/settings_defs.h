0823395d69b939af8e81471da23b9ef6e408d398
Yuze Liao <yuzeliao@gmail.com>
2019-05-09 21:01:22 -0400

    fix bugs

--- src/include/settings/settings_defs.h
+++ src/include/settings/settings_defs.h
@@ -7,23 +7,23 @@
 //===----------------------------------------------------------------------===//
 
 // Terrier port
-SETTING_int(port, "Terrier port (default: 15721)", 15721, 1024, 65535, false, DBMain::EmptyCallback)
+SETTING_int(port, "Terrier port (default: 15721)", 15721, 1024, 65535, false, terrier::DBMain::EmptyCallback)
 
 // Maximum number of connections
-SETTING_int(max_connections, "Maximum number of connections (default: 64)", 64, 1, 512, true, DBMain::EmptyCallback)
+SETTING_int(max_connections, "Maximum number of connections (default: 64)", 64, 1, 512, true, terrier::DBMain::EmptyCallback)
 
 // Buffer pool size in transaction manager
 SETTING_int(buffer_pool_size, "Buffer pool size in transaction manager (default : 100000)", 100000, 1, 1000000,
-            true, DBMain::BufferPoolSizeCallback)
+            true, terrier::DBMain::BufferPoolSizeCallback)
 // RPC port
-SETTING_int(rpc_port, "Terrier rpc port (default: 15445)", 15445, 1024, 65535, false, DBMain::EmptyCallback)
+SETTING_int(rpc_port, "Terrier rpc port (default: 15445)", 15445, 1024, 65535, false, terrier::DBMain::EmptyCallback)
 
 // TODO(tianyu): Remove when we change to a different rpc framework
 // This is here only because capnp cannot exit gracefully and thus causes
 // test failure. This is an issue with the capnp implementation and has
 // been such way for a while, so it's unlikely it gets fixed.
 // See: https://groups.google.com/forum/#!topic/capnproto/bgxCdqGD6oE
-SETTING_bool(rpc_enabled, "Enable rpc, this should be turned off when testing", false, false, DBMain::EmptyCallback)
+SETTING_bool(rpc_enabled, "Enable rpc, this should be turned off when testing", false, false, terrier::DBMain::EmptyCallback)
 
 /*
 // Socket family
@@ -64,33 +64,33 @@ false, false)
 //===----------------------------------------------------------------------===//
 
 SETTING_double(bnlj_buffer_size, "The default buffer size to use for blockwise nested loop joins (default: 1 MB)",
-               1.0 * 1024.0 * 1024.0, 1.0 * 1024, 1.0 * 1024.0 * 1024.0 * 1024, true, DBMain::EmptyCallback)
+               1.0 * 1024.0 * 1024.0, 1.0 * 1024, 1.0 * 1024.0 * 1024.0 * 1024, true, terrier::DBMain::EmptyCallback)
 
 // Size of the MonoQueue task queue
 SETTING_int(monoqueue_task_queue_size, "MonoQueue Task Queue Size (default: 32)", 32, 8, 128, false,
-            DBMain::EmptyCallback)
+            terrier::DBMain::EmptyCallback)
 
 // Size of the MonoQueue worker pool
 SETTING_int(monoqueue_worker_pool_size, "MonoQueue Worker Pool Size (default: 4)", 4, 1, 32, false,
-            DBMain::EmptyCallback)
+            terrier::DBMain::EmptyCallback)
 
 // Number of connection threads used by terrier
 SETTING_int(connection_thread_count, "Number of connection threads (default: std::hardware_concurrency())",
-            std::thread::hardware_concurrency(), 1, 64, false, DBMain::EmptyCallback)
+            std::thread::hardware_concurrency(), 1, 64, false, terrier::DBMain::EmptyCallback)
 
 // Number of gc_threads
 SETTING_int(gc_num_threads, "The number of Garbage collection threads to run", 1, 1, 128, true,
-            DBMain::EmptyCallback)
+            terrier::DBMain::EmptyCallback)
 
 // If parallel execution is enabled
 SETTING_bool(parallel_execution, "Enable parallel execution of queries (default: true)", true, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // Minimum number of tuples a table must have
 SETTING_int(min_parallel_table_scan_size,                                                               // NOLINT
             "Minimum number of tuples a table must have before we consider performing parallel scans "  // NOLINT
             "(default: 10K)",                                                                           // NOLINT
-            10 * 1000, 1, std::numeric_limits<int32_t>::max(), true, DBMain::EmptyCallback)             // NOLINT
+            10 * 1000, 1, std::numeric_limits<int32_t>::max(), true, terrier::DBMain::EmptyCallback)             // NOLINT
 
 //===----------------------------------------------------------------------===//
 // WRITE AHEAD LOG
@@ -118,17 +118,17 @@ true, true)
 //===----------------------------------------------------------------------===//
 
 // Enable or disable index tuner
-SETTING_bool(index_tuner, "Enable index tuner (default: false)", false, true, DBMain::EmptyCallback)
+SETTING_bool(index_tuner, "Enable index tuner (default: false)", false, true, terrier::DBMain::EmptyCallback)
 
 // Enable or disable layout tuner
-SETTING_bool(layout_tuner, "Enable layout tuner (default: false)", false, true, DBMain::EmptyCallback)
+SETTING_bool(layout_tuner, "Enable layout tuner (default: false)", false, true, terrier::DBMain::EmptyCallback)
 
 //===----------------------------------------------------------------------===//
 // BRAIN
 //===----------------------------------------------------------------------===//
 
 // Enable or disable brain
-SETTING_bool(brain, "Enable brain (default: false)", false, true, DBMain::EmptyCallback)
+SETTING_bool(brain, "Enable brain (default: false)", false, true, terrier::DBMain::EmptyCallback)
 
 /*
 SETTING_string(terrier_address,
@@ -138,10 +138,10 @@ false, false)
 */
 
 // Size of the brain task queue
-SETTING_int(brain_task_queue_size, "Brain Task Queue Size (default: 32)", 32, 1, 128, false, DBMain::EmptyCallback)
+SETTING_int(brain_task_queue_size, "Brain Task Queue Size (default: 32)", 32, 1, 128, false, terrier::DBMain::EmptyCallback)
 
 // Size of the brain worker pool
-SETTING_int(brain_worker_pool_size, "Brain Worker Pool Size (default: 1)", 1, 1, 16, false, DBMain::EmptyCallback)
+SETTING_int(brain_worker_pool_size, "Brain Worker Pool Size (default: 1)", 1, 1, 16, false, terrier::DBMain::EmptyCallback)
 
 //===----------------------------------------------------------------------===//
 // CODEGEN
@@ -149,35 +149,35 @@ SETTING_int(brain_worker_pool_size, "Brain Worker Pool Size (default: 1)", 1, 1,
 
 // If code-generation is enabled
 SETTING_bool(codegen, "Enable code-generation for query execution (default: true)", true, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // If llvm code is interpreted
 SETTING_bool(codegen_interpreter, "Force interpretation of generated llvm code (default: false)", false, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // If statics will be printed on generated IR
 SETTING_bool(print_ir_stats, "Print statistics on generated IR (default: false)", false, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // If logging of all generated IR is enabled
-SETTING_bool(dump_ir, "Enable logging of all generated IR (default: false)", false, true, DBMain::EmptyCallback)
+SETTING_bool(dump_ir, "Enable logging of all generated IR (default: false)", false, true, terrier::DBMain::EmptyCallback)
 
 //===----------------------------------------------------------------------===//
 // Optimizer
 //===----------------------------------------------------------------------===//
 SETTING_bool(predicate_push_down, "Enable predicate push-down optimization (default: true)", true, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // If bloom filter for hash join in codegen is enabled
 SETTING_bool(hash_join_bloom_filter, "Enable bloom filter for hash join in codegen (default: false)", false, true,
-             DBMain::EmptyCallback)
+             terrier::DBMain::EmptyCallback)
 
 // Timeout for task execution
 SETTING_int(task_execution_timeout,                             // NOLINT
             "Maximum allowed length of time (in ms) for task "  // NOLINT
             "execution step of optimizer, "                     // NOLINT
             "assuming one plan has been found (default 5000)",  // NOLINT
-            5000, 1000, 60000, true, DBMain::EmptyCallback)     // NOLINT
+            5000, 1000, 60000, true, terrier::DBMain::EmptyCallback)     // NOLINT
 // NOLINT
 
 //===----------------------------------------------------------------------===//
@@ -185,14 +185,14 @@ SETTING_int(task_execution_timeout,                             // NOLINT
 //===----------------------------------------------------------------------===//
 
 // Display configuration
-SETTING_bool(display_settings, "Display settings (default: false)", false, true, DBMain::EmptyCallback)
+SETTING_bool(display_settings, "Display settings (default: false)", false, true, terrier::DBMain::EmptyCallback)
 
 //===----------------------------------------------------------------------===//
 // Used only in Tests
 //===----------------------------------------------------------------------===//
 
 // Test string knob
-SETTING_string(db_name, "The name for this database", "Terrier", true, DBMain::EmptyCallback)
+SETTING_string(db_name, "The name for this database", "Terrier", true, terrier::DBMain::EmptyCallback)
 
 // Test decimal knob
-SETTING_double(pi, "The value of pi", 3.14159, 3.0, 4.0, true, DBMain::EmptyCallback)
+SETTING_double(pi, "The value of pi", 3.14159, 3.0, 4.0, true, terrier::DBMain::EmptyCallback)
