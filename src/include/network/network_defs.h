e7288d549b258748774a649b209d540591b4b078
Weichen Ke <laserae86@126.com>
2019-05-14 10:20:33 -0400

    Change number of libevent threads from 1 to 4

--- src/include/network/network_defs.h
+++ src/include/network/network_defs.h
@@ -34,7 +34,7 @@ class PostgresPacketWriter;
 // static const size_t EPOCH_LENGTH = 40;
 
 // For threads
-#define CONNECTION_THREAD_COUNT 1
+#define CONNECTION_THREAD_COUNT 4
 
 // Number of seconds to timeout on a client read
 #define READ_TIMEOUT (20 * 60)
