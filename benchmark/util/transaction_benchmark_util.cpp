40d30cd6479fbba2059daf51393000fdfec96e74
Matt Butrovich <mbutrovich@gmail.com>
2019-05-30 19:58:51 -0400

    Fix benchmark compilation.

--- benchmark/util/transaction_benchmark_util.cpp
+++ benchmark/util/transaction_benchmark_util.cpp
@@ -36,7 +36,8 @@ void RandomWorkloadTransaction::RandomUpdate(Random *generator) {
   StorageTestUtil::PopulateRandomRow(update, test_object_->layout_, 0.0, generator);
   // TODO(Tianyu): Hardly efficient, but will do for testing.
   if (test_object_->wal_on_) {
-    auto *record = txn_->StageWrite(&test_object_->table_, updated, initializer);
+    auto *record = txn_->StageWrite(catalog::db_oid_t(0), catalog::table_oid_t(0), initializer);
+    record->SetTupleSlot(updated);
     std::memcpy(record->Delta(), update, update->Size());
   }
   auto result = test_object_->table_.Update(txn_, updated, *update);
@@ -56,7 +57,8 @@ void RandomWorkloadTransaction::RandomInsert(Random *generator) {
   storage::TupleSlot inserted = test_object_->table_.Insert(txn_, *insert);
   // TODO(Tianyu): Hardly efficient, but will do for testing.
   if (test_object_->wal_on_) {
-    auto *record = txn_->StageWrite(&test_object_->table_, inserted, initializer);
+    auto *record = txn_->StageWrite(catalog::db_oid_t(0), catalog::table_oid_t(0), initializer);
+    record->SetTupleSlot(inserted);
     std::memcpy(record->Delta(), insert, insert->Size());
   }
 }
@@ -164,7 +166,8 @@ void LargeTransactionBenchmarkObject::PopulateInitialTable(uint32_t num_tuples, 
     storage::TupleSlot inserted = table_.Insert(initial_txn_, *redo);
     // TODO(Tianyu): Hardly efficient, but will do for testing.
     if (wal_on_) {
-      auto *record = initial_txn_->StageWrite(nullptr, inserted, row_initializer_);
+      auto *record = initial_txn_->StageWrite(catalog::db_oid_t(0), catalog::table_oid_t(0), row_initializer_);
+      record->SetTupleSlot(inserted);
       std::memcpy(record->Delta(), redo, redo->Size());
     }
     last_checked_version_.emplace_back(inserted, nullptr);
