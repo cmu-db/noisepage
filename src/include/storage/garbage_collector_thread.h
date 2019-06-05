9f3fe634da70350103ca0a4709136eec7b9a1758
Matt Butrovich <mbutrovich@gmail.com>
2019-06-05 11:22:34 -0400

    Move index GC from thread to GarbageCollector.

--- src/include/storage/garbage_collector_thread.h
+++ src/include/storage/garbage_collector_thread.h
@@ -2,10 +2,7 @@
 
 #include <chrono>  //NOLINT
 #include <thread>  //NOLINT
-#include <unordered_set>
-#include "common/shared_latch.h"
 #include "storage/garbage_collector.h"
-#include "storage/index/index.h"
 
 namespace terrier::storage {
 
@@ -20,9 +17,9 @@ class GarbageCollectorThread {
    * @param gc_period sleep time between GC invocations
    */
   GarbageCollectorThread(transaction::TransactionManager *const txn_manager, const std::chrono::milliseconds gc_period)
-      : run_gc_(true),
+      : gc_(txn_manager),
+        run_gc_(true),
         gc_paused_(false),
-        gc_(txn_manager),
         gc_period_(gc_period),
         gc_thread_(std::thread([this] { GCThreadLoop(); })) {}
 
@@ -52,43 +49,20 @@ class GarbageCollectorThread {
   }
 
   /**
-   * Register an index to be periodically garbage collected
-   * @param index pointer to the index to register
+   * Access the underlying GC object, mostly to register indexes currently.
    */
-  void RegisterIndexForGC(index::Index *const index) {
-    TERRIER_ASSERT(index != nullptr, "Index cannot be nullptr.");
-    common::SharedLatch::ScopedExclusiveLatch guard(&indexes_latch_);
-    TERRIER_ASSERT(indexes_.count(index) == 0, "Trying to register an index that has already been registered.");
-    indexes_.insert(index);
-  }
-
-  /**
-   * Unregister an index to be periodically garbage collected
-   * @param index pointer to the index to unregister
-   */
-  void UnregisterIndexForGC(index::Index *const index) {
-    TERRIER_ASSERT(index != nullptr, "Index cannot be nullptr.");
-    common::SharedLatch::ScopedExclusiveLatch guard(&indexes_latch_);
-    TERRIER_ASSERT(indexes_.count(index) == 1, "Trying to unregister an index that has not been registered.");
-    indexes_.erase(index);
-  }
+  storage::GarbageCollector gc_;
 
  private:
   volatile bool run_gc_;
   volatile bool gc_paused_;
-  storage::GarbageCollector gc_;
   std::chrono::milliseconds gc_period_;
   std::thread gc_thread_;
-
-  std::unordered_set<index::Index *> indexes_;
-  common::SharedLatch indexes_latch_;
 
   void GCThreadLoop() {
     while (run_gc_) {
       std::this_thread::sleep_for(gc_period_);
       if (!gc_paused_) gc_.PerformGarbageCollection();
-      common::SharedLatch::ScopedSharedLatch guard(&indexes_latch_);
-      for (const auto &index : indexes_) index->PerformGarbageCollection();
     }
   }
 };
