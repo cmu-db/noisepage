895b8011ae0931f4dcbf2d029d14e9bdbfc0a8e5
Matt Butrovich <mbutrovich@gmail.com>
2019-06-06 11:52:12 -0400

    Fix metric benchmark compilation.

--- benchmark/storage/metric_benchmark.cpp
+++ benchmark/storage/metric_benchmark.cpp
@@ -46,31 +46,19 @@ class MetricBenchmark : public benchmark::Fixture {
     txn_manager_ = new transaction::TransactionManager(&buffer_pool_, true, LOGGING_DISABLED);
 
     txn_ = txn_manager_->BeginTransaction();
-    catalog_ = new catalog::Catalog(txn_manager_, txn_);
     settings_manager_ = nullptr;
     StartGC(txn_manager_);
   }
 
   void TearDown(const benchmark::State &state) final {
-    txn_manager_->Commit(txn_, TestCallbacks::EmptyCallback, nullptr);
+    txn_manager_->Commit(txn_, transaction::TransactionUtil::EmptyCallback, nullptr);
 
     EndGC();
-    const catalog::db_oid_t terrier_oid(catalog::DEFAULT_DATABASE_OID);
-    auto db_handle = catalog_->GetDatabaseHandle();
-    auto table_handle = db_handle.GetNamespaceTable(txn_, terrier_oid).GetTableHandle(txn_, "public");
-    auto table = table_handle.GetTable(txn_, "database_metric_table");
-    delete table;
-    db_handle = catalog_->GetDatabaseHandle();
-    table_handle = db_handle.GetNamespaceTable(txn_, terrier_oid).GetTableHandle(txn_, "public");
-    table = table_handle.GetTable(txn_, "txn_metric_table");
-    delete table;
-    delete catalog_;  // need to delete catalog_ first
     delete txn_manager_;
   }
 
   settings::SettingsManager *settings_manager_;
 
-  catalog::Catalog *catalog_;
   storage::RecordBufferSegmentPool buffer_pool_{100, 100};
 
   transaction::TransactionContext *txn_ = nullptr;
@@ -91,21 +79,16 @@ class MetricBenchmark : public benchmark::Fixture {
 // NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(MetricBenchmark, AggregateMetric)(benchmark::State &state) {
   auto stats_collector = storage::metric::ThreadLevelStatsCollector();
-  storage::metric::StatsAggregator aggregator(txn_manager_, catalog_, nullptr);
+  storage::metric::StatsAggregator aggregator(txn_manager_, nullptr);
 
-  catalog::table_oid_t table_oid = static_cast<catalog::table_oid_t>(2);  // any value
-  const catalog::db_oid_t database_oid(catalog::DEFAULT_DATABASE_OID);
-  auto db_handle = catalog_->GetDatabaseHandle();
-  const catalog::namespace_oid_t namespace_oid =
-      db_handle.GetNamespaceTable(txn_, database_oid).NameToOid(txn_, default_namespace_);
   // NOLINTNEXTLINE
   for (auto _ : state) {
     for (uint8_t j = 0; j < num_txns_; j++) {
       auto *txn = txn_manager_->BeginTransaction();
       // Simulation of reading a tuple
-      storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleRead(txn, database_oid,
-                                                                                            namespace_oid, table_oid);
-      txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+      storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleRead(
+          txn, catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3));
+      txn_manager_->Commit(txn, transaction::TransactionUtil::EmptyCallback, nullptr);
 
       // The Aggregate() function accounts for more than 99.5% of the total running time for this benchmark.
       // So the throughput of this benchmark can be viewed as the throughput of the Aggeragate() function.
@@ -119,13 +102,8 @@ BENCHMARK_DEFINE_F(MetricBenchmark, AggregateMetric)(benchmark::State &state) {
 // NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(MetricBenchmark, CollectMetric)(benchmark::State &state) {
   auto stats_collector = storage::metric::ThreadLevelStatsCollector();
-  storage::metric::StatsAggregator aggregator(txn_manager_, catalog_, nullptr);
+  storage::metric::StatsAggregator aggregator(txn_manager_, nullptr);
 
-  catalog::table_oid_t table_oid = static_cast<catalog::table_oid_t>(2);  // any value
-  const catalog::db_oid_t database_oid(catalog::DEFAULT_DATABASE_OID);
-  auto db_handle = catalog_->GetDatabaseHandle();
-  const catalog::namespace_oid_t namespace_oid =
-      db_handle.GetNamespaceTable(txn_, database_oid).NameToOid(txn_, default_namespace_);
   // NOLINTNEXTLINE
   for (auto _ : state) {
     for (uint8_t j = 0; j < num_txns_; j++) {
@@ -135,19 +113,19 @@ BENCHMARK_DEFINE_F(MetricBenchmark, CollectMetric)(benchmark::State &state) {
         auto op_type = std::uniform_int_distribution<uint8_t>(0, 3)(generator_);
         if (op_type == 0) {  // Read
           storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleRead(
-              txn, database_oid, namespace_oid, table_oid);
+              txn, catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3));
         } else if (op_type == 1) {  // Update
           storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleUpdate(
-              txn, database_oid, namespace_oid, table_oid);
+              txn, catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3));
         } else if (op_type == 2) {  // Insert
           storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleInsert(
-              txn, database_oid, namespace_oid, table_oid);
+              txn, catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3));
         } else {  // Delete
           storage::metric::ThreadLevelStatsCollector::GetCollectorForThread()->CollectTupleDelete(
-              txn, database_oid, namespace_oid, table_oid);
+              txn, catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3));
         }
       }
-      txn_manager_->Commit(txn, TestCallbacks::EmptyCallback, nullptr);
+      txn_manager_->Commit(txn, transaction::TransactionUtil::EmptyCallback, nullptr);
     }
 
     aggregator.Aggregate(txn_);
