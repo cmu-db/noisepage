23369441a8a727c802aa8179e227109681488a21
Andy Pavlo <pavlo@cs.brown.edu>
2019-06-19 11:33:38 -0400

    Optimizer Stats: Histogram (#438)

---
+++ src/include/loggers/optimizer_logger.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <memory>
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
+
+namespace terrier::optimizer {
+extern std::shared_ptr<spdlog::logger> optimizer_logger;
+
+void init_optimizer_logger();
+}  // namespace terrier::optimizer
+
+#define OPTIMIZER_LOG_TRACE(...) ::terrier::optimizer::optimizer_logger->trace(__VA_ARGS__);
+
+#define OPTIMIZER_LOG_DEBUG(...) ::terrier::optimizer::optimizer_logger->debug(__VA_ARGS__);
+
+#define OPTIMIZER_LOG_INFO(...) ::terrier::optimizer::optimizer_logger->info(__VA_ARGS__);
+
+#define OPTIMIZER_LOG_WARN(...) ::terrier::optimizer::optimizer_logger->warn(__VA_ARGS__);
+
+#define OPTIMIZER_LOG_ERROR(...) ::terrier::optimizer::optimizer_logger->error(__VA_ARGS__);
