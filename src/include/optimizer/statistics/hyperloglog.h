a9101ffe3d963f5a7d2bea697609c074404e51d7
Andy Pavlo <pavlo@cs.brown.edu>
2019-06-23 17:09:04 -0400

    Fixed formatting + doxygen + lint stuff. clang-tidy does not like my overloaded Update methods for strings...

--- src/include/optimizer/statistics/hyperloglog.h
+++ src/include/optimizer/statistics/hyperloglog.h
@@ -1,31 +1,35 @@
 #pragma once
 
-#include <common/hash_util.h>
-#include <inttypes.h>
-#include <libcount/hll.h>
-#include <murmur3/MurmurHash3.h>
-#include <stdint.h>
 #include <cmath>
+#include <string>
 
 #include "common/macros.h"
+#include "libcount/hll.h"
 #include "loggers/optimizer_logger.h"
+#include "murmur3/MurmurHash3.h"
 
 namespace terrier::optimizer {
 
 /**
+ * HyperLogLog (HLL) is an approximate data structure that generates cardinality estimates.
+ * You give it a bunch of keys and then it estimates the number of unique keys
+ * that it has seen over time.
+ * The underlying implementation of the HLL is libcount.
  *
- * @tparam KeyType
+ * @see https://github.com/dialtr/libcount
+ * @tparam KeyType the type of keys that this HLL will track.
  */
 template <typename KeyType>
 class HyperLogLog {
  public:
   /**
-   *
-   * @param precision
+   * Constructor
+   * Using a larger precision means that the estimated cardinalities will be more accurate
+   * in exchange for higher computational and storage overhead.
+   * The default precision in libcount is 9. That's probably good enough.
+   * @param precision what precision level the HLL should record
    */
-  HyperLogLog(const int precision) : precision_{precision}, register_count_{1 << precision} {
-    hll_ = libcount::HLL::Create(precision_);
-  }
+  explicit HyperLogLog(const int precision) : precision_{precision} { hll_ = libcount::HLL::Create(precision_); }
 
   /**
    * Deconstructor.
@@ -33,25 +37,26 @@ class HyperLogLog {
   ~HyperLogLog() { delete hll_; }
 
   /**
-   *
-   * @param key
+   * Update the existence of the given key in the HLL. Note that we only
+   * need to keep track that we saw it and not the number of times that we saw it.
+   * @param key the key to apply to the HLL.
    */
-  void Update(const KeyType &key) {
-    // Throw the given key at murmur3 and get back a 128-bit hash.
-    // We then update the HLL using the first 64-bits of the hash.
-    // Andy tried using the second 64-bits and found that it produced
-    // slightly less accurate estimations. He did not perform
-    // a rigorous test of this though...
-    uint64_t hash[2];
-    murmur3::MurmurHash3_x64_128(reinterpret_cast<const void *>(&key),
-                                 sizeof(key),
-                                 0,
-                                 reinterpret_cast<void *>(&hash));
-    hll_->Update(hash[0]);
-  }
+  void Update(const KeyType &key) { UpdateKey(reinterpret_cast<const void *>(&key), sizeof(key)); }
 
   /**
-   *
+   * Specialized Update method for strings.
+   * @param str the key to apply to the HLL.
+   */
+  void Update(const std::string &str) { UpdateKey(reinterpret_cast<const void *>(str.data()), sizeof(str.size())); }
+
+  /**
+   * Specialized Update method for const chars.
+   * @param str the key to apply to the HLL.
+   */
+  void Update(const char *str) { Update(std::basic_string_view(str)); }
+
+  /**
+   * Compute the bias-corrected estimate using the HyperLogLog++ algorithm.
    * @return
    */
   uint64_t EstimateCardinality() const { return hll_->Estimate(); }
@@ -61,20 +66,33 @@ class HyperLogLog {
    * @return
    */
   double RelativeError() const {
-    TERRIER_ASSERT(register_count_ > 0, "Invalid register count");
-    return 1.04 / std::sqrt(register_count_);
+    // This comes from the original HLL++ algorithm.
+    auto register_count = 1 << precision_;
+    TERRIER_ASSERT(register_count > 0, "Invalid register count");
+    return 1.04 / std::sqrt(register_count);
   }
 
  private:
   /**
-   *
+   * The actual Update method.
+   * @param key a pointer to the underlying storage of the key
+   * @param length the length of the key.
    */
-  const int precision_;
+  void UpdateKey(const void *key, size_t length) {
+    // Throw the given key at murmur3 and get back a 128-bit hash.
+    // We then update the HLL using the first 64-bits of the hash.
+    // Andy tried using the second 64-bits and found that it produced
+    // slightly less accurate estimations. He did not perform
+    // a rigorous test of this though...
+    uint64_t hash[2];
+    murmur3::MurmurHash3_x64_128(key, static_cast<int>(length), 0, reinterpret_cast<void *>(&hash));
+    hll_->Update(hash[0]);
+  }
 
   /**
-   *
+   * The provided precision of the HLL instance.
    */
-  const int register_count_;
+  const int precision_;
 
   /**
    * Libcount's HyperLogLog implementation
--- test/optimizer/hyperloglog_test.cpp
+++ test/optimizer/hyperloglog_test.cpp
@@ -1,4 +1,5 @@
 #include <random>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "loggers/optimizer_logger.h"
@@ -8,158 +9,178 @@
 
 namespace terrier::optimizer {
 
-class HyperLogLogTests : public TerrierTest {};
+class HyperLogLogTests : public TerrierTest {
+ public:
+  /**
+   * Check that the estimate is within our error bounds
+   * @param count the number of keys that the HLL saw
+   * @param actual the groundtruth cardinality
+   * @param estimate the estimated cardinality from the HLL
+   * @param error the allowed error bounds
+   */
+  static void CheckErrorBounds(const int64_t count, const int64_t actual, const int64_t estimate, const double error) {
+    EXPECT_LE(estimate, static_cast<double>(actual) * (1 + error)) << "Error[" << error << "]: "
+                                                                   << "Estimated[" << estimate << "] <-> "
+                                                                   << "Actual[" << actual << "]";
+    EXPECT_GE(estimate, static_cast<double>(actual) * (1 - error)) << "Error[" << error << "]: "
+                                                                   << "Estimated[" << estimate << "] <-> "
+                                                                   << "Actual[" << actual << "]";
+
+    OPTIMIZER_LOG_TRACE("Estimated[{0}] <-> Actual[{1}]", estimate, actual);
+
+    // FIXME: I don't think the true error rate calculation below is accurate.
+    // I am going to leave it commented out for now. If somebody really cares
+    // about validating the HLL, then you should look into this further.
+    // I have enough to do as it is. The basic tests are working.
+    // Just deal with this. I'll see you in hell.
+    // ------------------------------------------
+    // UNUSED_ATTRIBUTE double true_error =
+    //    (static_cast<double>(count) * 1.0 - static_cast<int>(estimate)) / static_cast<double>(estimate);
+    // OPTIMIZER_LOG_TRACE("Estimated cardinality is [{0}] times of real value", true_error);
+    // ------------------------------------------
+  }
+};
 
 // NOLINTNEXTLINE
 TEST_F(HyperLogLogTests, SimpleTest) {
-  HyperLogLog<long> hll{9};
+  // This test case is a simple workload with only 100 unique
+  // values out of a million. This will serve as a canary against
+  // somebody changing our hash function and fucking things up.
+  // If you fail this check, then you did something woefully wrong!
+  HyperLogLog<int64_t> hll{9};
 
+  // Adapted from the libcount example code:
+  // https://github.com/dialtr/libcount/blob/master/examples/cc_example.cc
   const uint64_t kIterations = 1000000;
   const uint64_t kTrueCardinality = 100;
   for (uint64_t i = 0; i < kIterations; ++i) {
-    long key = static_cast<long>(i % kTrueCardinality);
+    auto key = static_cast<int64_t>(i % kTrueCardinality);
     hll.Update(key);
   }
-  const uint64_t estimate = hll.EstimateCardinality();
 
-  std::cout << "actual cardinality:    " << kTrueCardinality << std::endl;
-  std::cout << "estimated cardinality: " << estimate << std::endl;
+  auto actual = kTrueCardinality;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError();
+  HyperLogLogTests::CheckErrorBounds(kIterations, actual, estimate, error);
 }
 
 // 100k values with 10k distinct.
 // NOLINTNEXTLINE
 TEST_F(HyperLogLogTests, Dataset1Test) {
-  HyperLogLog<long> hll{9};
+  HyperLogLog<int64_t> hll{9};
   int threshold = 100000;
   int ratio = 10;
-  double error = hll.RelativeError();
   for (int i = 1; i <= threshold; i++) {
     auto v = i / ratio;
     hll.Update(v);
   }
-  auto actual = threshold / ratio;
-  auto estimated  = hll.EstimateCardinality();
-  OPTIMIZER_LOG_TRACE("Estimated[{0}] <-> Actual[{1}]", estimated, actual);
 
   // Check to make sure that our estimate is in within the expected error range.
-  EXPECT_LE(estimated, threshold / ratio * (1 + error));
-  EXPECT_GE(estimated, threshold / ratio * (1 - error));
-  UNUSED_ATTRIBUTE double true_error =
-      (threshold * 1.0 - static_cast<int>(estimated)) / static_cast<double>(estimated);
-  OPTIMIZER_LOG_TRACE("Estimated cardinality is [{0}] times of real value", true_error);
+  auto actual = threshold / ratio;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError();
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, estimate, error);
 }
 
-//// 100k values with 1k distinct.
-//// This case HLL does not perform very well.
-//// NOLINTNEXTLINE
-// TEST_F(HyperLogLogTests, Dataset2Test) {
-//  HyperLogLog<int> hll{8};
-//  int threshold = 100000;
-//  int ratio = 100;
-//  double error = hll.RelativeError() + 0.05;
-//  for (int i = 1; i <= threshold; i++) {
-//    auto v = i / ratio;
-//    hll.Update(v);
-//  }
-//  uint64_t cardinality = hll.EstimateCardinality();
-//  EXPECT_LE(cardinality, threshold / ratio * (1 + error));
-//  EXPECT_GE(cardinality, threshold / ratio * (1 - error));
-//}
-//
-//// 100k values with 100 distinct.
-//// NOLINTNEXTLINE
-// TEST_F(HyperLogLogTests, Dataset3Test) {
-//  HyperLogLog<std::string> hll{8};
-//  int threshold = 100000;
-//  int ratio = 1000;
-//  double error = hll.RelativeError();
-//  for (int i = 1; i <= threshold; i++) {
-//    auto v = std::to_string(i / ratio);
-//    hll.Update(v);
-//  }
-//  uint64_t cardinality = hll.EstimateCardinality();
-//  EXPECT_LE(cardinality, threshold / ratio * (1 + error));
-//  EXPECT_GE(cardinality, threshold / ratio * (1 - error));
-//}
-//
-//// 100k values with 100k distinct.
-// TEST_F(HyperLogLogTests, Dataset4Test) {
-//  HyperLogLog<double> hll{8};
-//  int threshold = 100000;
-//  int ratio = 1;
-//  double error = hll.RelativeError();
-//  for (int i = 1; i <= threshold; i++) {
-//    auto v = i / ratio;
-//    hll.Update(v);
-//  }
-//  uint64_t cardinality = hll.EstimateCardinality();
-//  EXPECT_LE(cardinality, threshold / ratio * (1 + error));
-//  EXPECT_GE(cardinality, threshold / ratio * (1 - error));
-//}
-//
-//// HLL performance with different precisions.
-//// In general, the higher the precision, the smaller the error.
-// TEST_F(HyperLogLogTests, PrecisionTest) {
-//  int threshold = 100000;
-//  int ratio = 10;
-//  HyperLogLog<int> hll_10{10};
-//  double error_10 = hll_10.RelativeError() + 0.001;
-//  HyperLogLog<int> hll_14{14};
-//  double error_14 = hll_14.RelativeError() + 0.001;
-//  HyperLogLog<int> hll_4{4};
-//  double error_4 = hll_4.RelativeError() + 0.05;  // precision 4 tend to be worse
-//  for (int i = 1; i <= threshold; i++) {
-//    auto v = i / ratio;
-//    hll_4.Update(v);
-//    hll_10.Update(v);
-//    hll_14.Update(v);
-//  }
-//  EXPECT_LE(hll_4.EstimateCardinality(), threshold / ratio * (1 + error_4));
-//  EXPECT_GE(hll_4.EstimateCardinality(), threshold / ratio * (1 - error_4));
-//
-//  EXPECT_LE(hll_10.EstimateCardinality(), threshold / ratio * (1 + error_10));
-//  EXPECT_GE(hll_10.EstimateCardinality(), threshold / ratio * (1 - error_10));
-//
-//  EXPECT_LE(hll_14.EstimateCardinality(), threshold / ratio * (1 + error_14));
-//  EXPECT_GE(hll_14.EstimateCardinality(), threshold / ratio * (1 - error_14));
-//}
+// 100k values with 1k distinct.
+// This case HLL does not perform very well.
+// NOLINTNEXTLINE
+TEST_F(HyperLogLogTests, Dataset2Test) {
+  HyperLogLog<int> hll{8};
+  int threshold = 100000;
+  int ratio = 100;
+  for (int i = 1; i <= threshold; i++) {
+    auto v = i / ratio;
+    hll.Update(v);
+  }
+
+  auto actual = threshold / ratio;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError() + 0.05;  // ???
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, estimate, error);
+}
+
+// 100k values with 100 distinct.
+// NOLINTNEXTLINE
+TEST_F(HyperLogLogTests, Dataset3Test) {
+  HyperLogLog<std::string> hll{8};
+  int threshold = 100000;
+  int ratio = 1000;
+  for (int i = 1; i <= threshold; i++) {
+    auto v = std::to_string(i / ratio);
+    hll.Update(v);
+  }
+
+  auto actual = threshold / ratio;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError();
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, estimate, error);
+}
+
+// 100k values with 100k distinct.
+// NOLINTNEXTLINE
+TEST_F(HyperLogLogTests, Dataset4Test) {
+  HyperLogLog<double> hll{8};
+  int threshold = 100000;
+  int ratio = 1;
+  for (int i = 1; i <= threshold; i++) {
+    auto v = i / ratio;
+    hll.Update(v);
+  }
+
+  auto actual = threshold / ratio;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError();
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, estimate, error);
+}
+
+// HLL performance with different precisions.
+// In general, the higher the precision, the smaller the error.
+// NOLINTNEXTLINE
+TEST_F(HyperLogLogTests, PrecisionTest) {
+  HyperLogLog<int> hll_10{10};
+  HyperLogLog<int> hll_14{14};
+  HyperLogLog<int> hll_4{4};
+
+  int threshold = 100000;
+  int ratio = 10;
+  for (int i = 1; i <= threshold; i++) {
+    auto v = i / ratio;
+    hll_4.Update(v);
+    hll_10.Update(v);
+    hll_14.Update(v);
+  }
+
+  auto actual = threshold / ratio;
+
+  // Precision: 4
+  double error_4 = hll_4.RelativeError() + 0.05;  // precision 4 tend to be worse
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, hll_4.EstimateCardinality(), error_4);
+
+  // Precision: 10
+  double error_10 = hll_10.RelativeError() + 0.001;
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, hll_10.EstimateCardinality(), error_10);
+
+  // Precision: 14
+  double error_14 = hll_14.RelativeError() + 0.001;
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, hll_14.EstimateCardinality(), error_14);
+}
 
 // 100M values with 10M distinct. Comment out due to long running time.
-// TEST_F(HyperLogLogTests, LargeDatasetTest) {
-//   HyperLogLog hll{};
-//   int threshold = 100000000;
-//   int ratio = 10;
-//   double error = hll.RelativeError();
-//   for (int i = 1; i <= 100000000; i++) {
-//     type::Value v = type::ValueFactory::GetIntegerValue(i / 10);
-//     hll.Update(v);
-//   }
-//   uint64_t cardinality = hll.EstimateCardinality();
-//   EXPECT_LE(cardinality, threshold / ratio * (1 + error));
-//   EXPECT_GE(cardinality, threshold / ratio * (1 - error));
-// }
+// NOLINTNEXTLINE
+TEST_F(HyperLogLogTests, LargeDatasetTest) {
+  HyperLogLog<int> hll{10};
+  int threshold = 100000000;
+  int ratio = 10;
+  for (int i = 1; i <= 100000000; i++) {
+    auto v = i / ratio;
+    hll.Update(v);
+  }
 
-// Hyperloglog should be able to handle different value types.
-// TEST_F(HyperLogLogTests, DataTypeTest) {
-//  HyperLogLog<int> hll{};
-//  // integer
-//  type::Value tiny_int = type::ValueFactory::GetTinyIntValue(1);
-//  hll.Update(tiny_int);
-//  type::Value timestamp = type::ValueFactory::GetTimestampValue(1493003492);
-//  hll.Update(timestamp);
-//  // double
-//  type::Value decimal = type::ValueFactory::GetDecimalValue(12.9998435);
-//  hll.Update(decimal);
-//  // string
-//  std::string str{"database"};
-//  type::Value str_val = type::ValueFactory::GetVarcharValue(str);
-//  hll.Update(str_val);
-//  // Null
-//  type::Value null =
-//      type::ValueFactory::GetNullValueByType(type::TypeId::BOOLEAN);
-//  hll.Update(str_val);
-//
-//  hll.EstimateCardinality();
-//}
+  auto actual = threshold / ratio;
+  auto estimate = hll.EstimateCardinality();
+  auto error = hll.RelativeError();
+  HyperLogLogTests::CheckErrorBounds(threshold, actual, estimate, error);
+}
 
 }  // namespace terrier::optimizer
