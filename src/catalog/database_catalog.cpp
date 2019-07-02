9ea75cf9ec5bed119d6ef9e17dd7bb4ad1afc540
John E. Rollinson <j.e.rollinson@gmail.com>
2019-07-02 13:58:05 -0400

    Avoid double defs

--- src/catalog/database_catalog.cpp
+++ src/catalog/database_catalog.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <vector>
 
 #include "catalog/catalog_defs.h"
@@ -14,37 +12,37 @@
 
 namespace terrier::catalog {
 
-namespace_oid_t DatabaseCatalog::CreateNamespace(transaction::TransactionContext *txn, const std::string &name);
+// namespace_oid_t DatabaseCatalog::CreateNamespace(transaction::TransactionContext *txn, const std::string &name);
 
-bool DatabaseCatalog::DeleteNamespace(transaction::TransactionContext *txn, namespace_oid_t ns);
+// bool DatabaseCatalog::DeleteNamespace(transaction::TransactionContext *txn, namespace_oid_t ns);
 
-namespace_oid_t DatabaseCatalog::GetNamespaceOid(transaction::TransactionContext *txn, const std::string &name);
+// namespace_oid_t DatabaseCatalog::GetNamespaceOid(transaction::TransactionContext *txn, const std::string &name);
 
-table_oid_t DatabaseCatalog::CreateTable(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name,
-                          const Schema &schema);
+// table_oid_t DatabaseCatalog::CreateTable(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name,
+//                           const Schema &schema);
 
-bool DatabaseCatalog::DeleteTable(transaction::TransactionContext *txn, table_oid_t table);
+// bool DatabaseCatalog::DeleteTable(transaction::TransactionContext *txn, table_oid_t table);
 
-table_oid_t DatabaseCatalog::GetTableOid(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name);
+// table_oid_t DatabaseCatalog::GetTableOid(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name);
 
-bool DatabaseCatalog::RenameTable(transaction::TransactionContext *txn, table_oid_t table, const std::string &name);
+// bool DatabaseCatalog::RenameTable(transaction::TransactionContext *txn, table_oid_t table, const std::string &name);
 
-bool DatabaseCatalog::UpdateSchema(transaction::TransactionContext *txn, table_oid_t table, Schema *new_schema);
+// bool DatabaseCatalog::UpdateSchema(transaction::TransactionContext *txn, table_oid_t table, Schema *new_schema);
 
-const Schema &DatabaseCatalog::GetSchema(transaction::TransactionContext *txn, table_oid_t table);
+// const Schema &DatabaseCatalog::GetSchema(transaction::TransactionContext *txn, table_oid_t table);
 
-std::vector<constraint_oid_t> DatabaseCatalog::GetConstraints(transaction::TransactionContext *txn, table_oid_t);
+// std::vector<constraint_oid_t> DatabaseCatalog::GetConstraints(transaction::TransactionContext *txn, table_oid_t);
 
-std::vector<index_oid_t> DatabaseCatalog::GetIndexes(transaction::TransactionContext *txn, table_oid_t);
+// std::vector<index_oid_t> DatabaseCatalog::GetIndexes(transaction::TransactionContext *txn, table_oid_t);
 
-index_oid_t DatabaseCatalog::CreateIndex(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name,
-                                         table_oid_t table, IndexSchema *schema);
+// index_oid_t DatabaseCatalog::CreateIndex(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name,
+//                                          table_oid_t table, IndexSchema *schema);
 
-bool DatabaseCatalog::DeleteIndex(transaction::TransactionContext *txn, index_oid_t index);
+// bool DatabaseCatalog::DeleteIndex(transaction::TransactionContext *txn, index_oid_t index);
 
-index_oid_t DatabaseCatalog::GetIndexOid(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name);
+// index_oid_t DatabaseCatalog::GetIndexOid(transaction::TransactionContext *txn, namespace_oid_t ns, const std::string &name);
 
-const IndexSchema &DatabaseCatalog::GetIndexSchema(transaction::TransactionContext *txn, index_oid_t index);
+// const IndexSchema &DatabaseCatalog::GetIndexSchema(transaction::TransactionContext *txn, index_oid_t index);
 
 void DatabaseCatalog::TearDown(transaction::TransactionContext *txn) {
   std::vector<parser::AbstractExpression *> expressions;
