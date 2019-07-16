de3eba4a956553f5012aa656e14939deed968b03
Matt Butrovich <mbutrovich@gmail.com>
2019-07-16 13:32:55 -0400

    Docs.

--- src/include/metrics/metrics_store.h
+++ src/include/metrics/metrics_store.h
@@ -86,6 +86,9 @@ class MetricsStore {
     return enabled_metrics_.test(static_cast<uint8_t>(component));
   }
 
+  /**
+   * MetricsManager pointer that created this MetricsStore
+   */
   const common::ManagedPointer<metrics::MetricsManager> MetricsManager() const { return metrics_manager_; }
 
  private:
