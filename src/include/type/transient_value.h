38b97c093a52c8cb3edd856868d50ab1146d3bfb
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-07-29 15:31:24 -0400

    Fix null transientvalue serialization

--- src/include/type/transient_value.h
+++ src/include/type/transient_value.h
@@ -181,8 +181,7 @@ class TransientValue {
   nlohmann::json ToJson() const {
     nlohmann::json j;
     j["type"] = type_;
-    j["data"] = data_;
-    if (Type() == TypeId::VARCHAR) {
+    if (Type() == TypeId::VARCHAR && !Null()) {
       const uint32_t length = *reinterpret_cast<const uint32_t *const>(data_);
       auto varchar = std::string(reinterpret_cast<const char *const>(data_), length + sizeof(uint32_t));
       j["data"] = varchar;
@@ -199,10 +198,9 @@ class TransientValue {
    */
   void FromJson(const nlohmann::json &j) {
     type_ = j.at("type").get<TypeId>();
-    if (Type() == TypeId::VARCHAR) {
+    if (Type() == TypeId::VARCHAR && !Null()) {
       data_ = 0;
       CopyVarChar(reinterpret_cast<const char *const>(j.at("data").get<std::string>().c_str()));
-
     } else {
       data_ = j.at("data").get<uintptr_t>();
     }
