6689c43773bb85abd4189c12058301db008246f0
John E. Rollinson <j.e.rollinson@gmail.com>
2019-08-01 15:05:59 -0400

    Make it work

--- src/catalog/catalog_accessor.cpp
+++ src/catalog/catalog_accessor.cpp
@@ -5,17 +5,17 @@
 #include "catalog/catalog.h"
 
 namespace terrier::catalog {
-db_oid_t CatalogAccessor::GetDatabaseOid(std::string name) {
+db_oid_t CatalogAccessor::GetDatabaseOid(std::string name) const {
   NormalizeObjectName(&name);
   return catalog_->GetDatabaseOid(txn_, name);
 }
 
-db_oid_t CatalogAccessor::CreateDatabase(std::string name) {
+db_oid_t CatalogAccessor::CreateDatabase(std::string name) const {
   NormalizeObjectName(&name);
   return catalog_->CreateDatabase(txn_, name, true);
 }
 
-bool CatalogAccessor::DropDatabase(db_oid_t db) { return catalog_->DeleteDatabase(txn_, db); }
+bool CatalogAccessor::DropDatabase(db_oid_t db) const { return catalog_->DeleteDatabase(txn_, db); }
 
 void CatalogAccessor::SetSearchPath(std::vector<namespace_oid_t> namespaces) {
   TERRIER_ASSERT(!namespaces.empty(), "search path cannot be empty");
@@ -29,19 +29,19 @@ void CatalogAccessor::SetSearchPath(std::vector<namespace_oid_t> namespaces) {
   search_path_.emplace(search_path_.begin(), NAMESPACE_CATALOG_NAMESPACE_OID);
 }
 
-namespace_oid_t CatalogAccessor::GetNamespaceOid(std::string name) {
+namespace_oid_t CatalogAccessor::GetNamespaceOid(std::string name) const {
   NormalizeObjectName(&name);
   return dbc_->GetNamespaceOid(txn_, name);
 }
 
-namespace_oid_t CatalogAccessor::CreateNamespace(std::string name) {
+namespace_oid_t CatalogAccessor::CreateNamespace(std::string name) const {
   NormalizeObjectName(&name);
   return dbc_->CreateNamespace(txn_, name);
 }
 
-bool CatalogAccessor::DropNamespace(namespace_oid_t ns) { return dbc_->DeleteNamespace(txn_, ns); }
+bool CatalogAccessor::DropNamespace(namespace_oid_t ns) const { return dbc_->DeleteNamespace(txn_, ns); }
 
-table_oid_t CatalogAccessor::GetTableOid(std::string name) {
+table_oid_t CatalogAccessor::GetTableOid(std::string name) const {
   NormalizeObjectName(&name);
   for (auto &path : search_path_) {
     table_oid_t search_result = dbc_->GetTableOid(txn_, path, name);
@@ -50,44 +50,44 @@ table_oid_t CatalogAccessor::GetTableOid(std::string name) {
   return INVALID_TABLE_OID;
 }
 
-table_oid_t CatalogAccessor::GetTableOid(namespace_oid_t ns, std::string name) {
+table_oid_t CatalogAccessor::GetTableOid(namespace_oid_t ns, std::string name) const {
   NormalizeObjectName(&name);
   return dbc_->GetTableOid(txn_, ns, name);
 }
 
-table_oid_t CatalogAccessor::CreateTable(namespace_oid_t ns, std::string name, const Schema &schema) {
+table_oid_t CatalogAccessor::CreateTable(namespace_oid_t ns, std::string name, const Schema &schema) const {
   NormalizeObjectName(&name);
   return dbc_->CreateTable(txn_, ns, name, schema);
 }
 
-bool CatalogAccessor::RenameTable(table_oid_t table, std::string new_table_name) {
+bool CatalogAccessor::RenameTable(table_oid_t table, std::string new_table_name) const {
   NormalizeObjectName(&new_table_name);
   return dbc_->RenameTable(txn_, table, new_table_name);
 }
 
-bool CatalogAccessor::DropTable(table_oid_t table) { return dbc_->DeleteTable(txn_, table); }
+bool CatalogAccessor::DropTable(table_oid_t table) const { return dbc_->DeleteTable(txn_, table); }
 
 bool CatalogAccessor::SetTablePointer(table_oid_t table, storage::SqlTable *table_ptr) {
   return dbc_->SetTablePointer(txn_, table, table_ptr);
 }
 
-common::ManagedPointer<storage::SqlTable> CatalogAccessor::GetTable(table_oid_t table) {
+common::ManagedPointer<storage::SqlTable> CatalogAccessor::GetTable(table_oid_t table) const {
   return dbc_->GetTable(txn_, table);
 }
 
-bool CatalogAccessor::UpdateSchema(table_oid_t table, Schema *new_schema) {
+bool CatalogAccessor::UpdateSchema(table_oid_t table, Schema *new_schema) const {
   return dbc_->UpdateSchema(txn_, table, new_schema);
 }
 
-const Schema &CatalogAccessor::GetSchema(table_oid_t table) { return dbc_->GetSchema(txn_, table); }
+const Schema &CatalogAccessor::GetSchema(table_oid_t table) const { return dbc_->GetSchema(txn_, table); }
 
-std::vector<constraint_oid_t> CatalogAccessor::GetConstraints(table_oid_t table) {
+std::vector<constraint_oid_t> CatalogAccessor::GetConstraints(table_oid_t table) const {
   return dbc_->GetConstraints(txn_, table);
 }
 
-std::vector<index_oid_t> CatalogAccessor::GetIndexes(table_oid_t table) { return dbc_->GetIndexes(txn_, table); }
+std::vector<index_oid_t> CatalogAccessor::GetIndexes(table_oid_t table) const { return dbc_->GetIndexes(txn_, table); }
 
-index_oid_t CatalogAccessor::GetIndexOid(std::string name) {
+index_oid_t CatalogAccessor::GetIndexOid(std::string name) const {
   NormalizeObjectName(&name);
   for (auto &path : search_path_) {
     index_oid_t search_result = dbc_->GetIndexOid(txn_, path, name);
@@ -96,28 +96,28 @@ index_oid_t CatalogAccessor::GetIndexOid(std::string name) {
   return INVALID_INDEX_OID;
 }
 
-index_oid_t CatalogAccessor::GetIndexOid(namespace_oid_t ns, std::string name) {
+index_oid_t CatalogAccessor::GetIndexOid(namespace_oid_t ns, std::string name) const {
   NormalizeObjectName(&name);
   return dbc_->GetIndexOid(txn_, ns, name);
 }
 
-std::vector<index_oid_t> CatalogAccessor::GetIndexOids(table_oid_t table) { return dbc_->GetIndexes(txn_, table); }
+std::vector<index_oid_t> CatalogAccessor::GetIndexOids(table_oid_t table) const { return dbc_->GetIndexes(txn_, table); }
 
 index_oid_t CatalogAccessor::CreateIndex(namespace_oid_t ns, table_oid_t table, std::string name,
-                                         const IndexSchema &schema) {
+                                         const IndexSchema &schema) const {
   NormalizeObjectName(&name);
   return dbc_->CreateIndex(txn_, ns, name, table, schema);
 }
 
-const IndexSchema &CatalogAccessor::GetIndexSchema(index_oid_t index) { return dbc_->GetIndexSchema(txn_, index); }
+const IndexSchema &CatalogAccessor::GetIndexSchema(index_oid_t index) const { return dbc_->GetIndexSchema(txn_, index); }
 
-bool CatalogAccessor::DropIndex(index_oid_t index) { return dbc_->DeleteIndex(txn_, index); }
+bool CatalogAccessor::DropIndex(index_oid_t index) const { return dbc_->DeleteIndex(txn_, index); }
 
-bool CatalogAccessor::SetIndexPointer(index_oid_t index, storage::index::Index *index_ptr) {
+bool CatalogAccessor::SetIndexPointer(index_oid_t index, storage::index::Index *index_ptr) const {
   return dbc_->SetIndexPointer(txn_, index, index_ptr);
 }
 
-common::ManagedPointer<storage::index::Index> CatalogAccessor::GetIndex(index_oid_t index) {
+common::ManagedPointer<storage::index::Index> CatalogAccessor::GetIndex(index_oid_t index) const {
   return dbc_->GetIndex(txn_, index);
 }
 
