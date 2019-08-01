529d926c6c072a9cb191c3f8229aa88276e76bd4
John E. Rollinson <j.e.rollinson@gmail.com>
2019-08-01 15:36:17 -0400

    Lint it up

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
--- src/include/catalog/catalog_accessor.h
+++ src/include/catalog/catalog_accessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
