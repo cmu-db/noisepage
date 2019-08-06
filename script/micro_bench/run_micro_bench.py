c1946ed8246cf01924bfd2387d8876b6a30bc7b1
Matt Butrovich <mbutrovich@users.noreply.github.com>
2019-08-05 20:21:39 -0400

    Add bwtree_benchmark. (#497)

--- script/micro_bench/run_micro_bench.py
+++ script/micro_bench/run_micro_bench.py
@@ -35,7 +35,8 @@ class TestConfig(object):
                                "large_transaction_benchmark",
                                "logging_benchmark",
                                "tuple_access_strategy_benchmark",
-                               "tpcc_benchmark"]
+                               "tpcc_benchmark",
+                               "bwtree_benchmark"]
 
         # how many historical values are "required".
         self.min_ref_values = 10
@@ -716,7 +717,8 @@ class RunMicroBenchmarks(object):
                                "large_transaction_benchmark",
                                "logging_benchmark",
                                "tuple_access_strategy_benchmark",
-                               "tpcc_benchmark"]
+                               "tpcc_benchmark",
+                               "bwtree_benchmark"]
 
         # minimum run time for the benchmark
         self.min_time = 10
