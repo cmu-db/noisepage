51e0ec9edd26738838a2c8d6bc7739eeabeca742
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-08-07 16:22:51 -0400

    Update recovery benchmark

--- benchmark/storage/recovery_benchmark.cpp
+++ benchmark/storage/recovery_benchmark.cpp
@@ -17,7 +17,7 @@ class RecoveryBenchmark : public benchmark::Fixture {
   void SetUp(const benchmark::State &state) final { unlink(LOG_FILE_NAME); }
   void TearDown(const benchmark::State &state) final { unlink(LOG_FILE_NAME); }
 
-  const uint32_t initial_table_size = 1000000;
+  const uint32_t initial_table_size_ = 1000000;
   const uint32_t num_txns_ = 100000;
   storage::BlockStore block_store_{1000, 1000};
   storage::RecordBufferSegmentPool buffer_pool_{1000000, 1000000};
@@ -34,89 +34,81 @@ class RecoveryBenchmark : public benchmark::Fixture {
   const std::chrono::milliseconds log_serialization_interval_{5};
   const std::chrono::milliseconds log_persist_interval_{10};
   const uint64_t log_persist_threshold_ = (1 << 20);  // 1MB
-};
 
-// TODO(Gus): This is duplicate code, can be simplified by passing in the test parameters as function args
+  /**
+   * Runs the recovery benchmark with the provided config
+   * @param state benchmark state
+   * @param config config to use for test object
+   */
+  void RunBenchmark(benchmark::State &state, const LargeSqlTableTestConfiguration &config) {
+    uint32_t recovered_txns = 0;
+
+    // NOLINTNEXTLINE
+    for (auto _ : state) {
+      // Blow away log file after every benchmark iteration
+      unlink(LOG_FILE_NAME);
+      // Initialize table and run workload with logging enabled
+      log_manager_ =
+          new storage::LogManager(LOG_FILE_NAME, num_log_buffers_, log_serialization_interval_, log_persist_interval_,
+                                  log_persist_threshold_, &buffer_pool_, common::ManagedPointer(&thread_registry_));
+      log_manager_->Start();
+      LargeSqlTableTestObject tested = LargeSqlTableTestObject(config, &block_store_, &buffer_pool_, &generator_, log_manager_);
+
+      // Run the test object and log all transactions
+      tested.SimulateOltp(num_txns_, num_concurrent_txns_);
+      log_manager_->PersistAndStop();
+
+      // Start a transaction manager with logging disabled, we don't want to log the log replaying
+      transaction::TransactionManager recovery_txn_manager{&buffer_pool_, true, LOGGING_DISABLED};
+
+      // Create catalog for recovery
+      catalog::Catalog recovered_catalog(&recovery_txn_manager, &block_store_);
+
+      // Instantiate recovery manager, and recover the tables.
+      storage::DiskLogProvider log_provider(LOG_FILE_NAME);
+      recovery_manager_ = new storage::RecoveryManager(&log_provider, common::ManagedPointer(&recovered_catalog), &recovery_txn_manager,
+                                              common::ManagedPointer(&thread_registry_), &block_store_);
+
+      uint64_t elapsed_ms;
+      {
+        common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
+        recovery_manager_->StartRecovery();
+        recovery_manager_->FinishRecovery();
+        recovered_txns += recovery_manager_->GetRecoveredTxnCount();
+      }
+
+      state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
+
+      // Delete test txns
+      gc_thread_ = new storage::GarbageCollectorThread(tested.GetTxnManager(), gc_period_);
+      delete gc_thread_;
+
+      // Delete recovery txns
+      gc_thread_ = new storage::GarbageCollectorThread(&recovery_txn_manager, gc_period_);
+      delete gc_thread_;
+      delete recovery_manager_;
+      delete log_manager_;
+    }
+    state.SetItemsProcessed(recovered_txns);
+  }
+};
 
 /**
  * Run an OLTP-like workload(5 statements per txn, 60% updates, 30% select, 10% deletes).
  */
 // NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(RecoveryBenchmark, OLTPWorkload)(benchmark::State &state) {
-  uint32_t recovered_txns = 0;
-  const uint16_t num_databases = 1;
-  const uint16_t num_tables = 1;
-  const uint16_t num_max_columns = 5;
-  const uint32_t txn_length = 5;
-  const std::vector<double> update_select_delete_ratio = {0.6, 0.3, 0.1};
-  // NOLINTNEXTLINE
-  for (auto _ : state) {
-    // Blow away log file after every benchmark iteration
-    unlink(LOG_FILE_NAME);
-    // Initialize table and run workload with logging enabled
-    log_manager_ =
-        new storage::LogManager(LOG_FILE_NAME, num_log_buffers_, log_serialization_interval_, log_persist_interval_,
-                                log_persist_threshold_, &buffer_pool_, common::ManagedPointer(&thread_registry_));
-    log_manager_->Start();
-    LargeSqlTableTestObject tested = LargeSqlTableTestObject::Builder()
-                                         .SetNumDatabases(num_databases)
-                                         .SetNumTables(num_tables)
-                                         .SetMaxColumns(num_max_columns)
-                                         .SetInitialTableSize(initial_table_size)
-                                         .SetTxnLength(txn_length)
-                                         .SetUpdateSelectDeleteRatio(update_select_delete_ratio)
-                                         .SetBlockStore(&block_store_)
-                                         .SetBufferPool(&buffer_pool_)
-                                         .SetGenerator(&generator_)
-                                         .SetGcOn(true)
-                                         .SetVarlenAllowed(true)
-                                         .SetLogManager(log_manager_)
-                                         .build();
+  LargeSqlTableTestConfiguration config = LargeSqlTableTestConfiguration::Builder()
+      .SetNumDatabases(1)
+      .SetNumTables(1)
+      .SetMaxColumns(5)
+      .SetInitialTableSize(initial_table_size_)
+      .SetTxnLength(5)
+      .SetUpdateSelectDeleteRatio({0.6, 0.3, 0.1})
+      .SetVarlenAllowed(true)
+      .build();
 
-    // Run the test object and log all transactions
-    tested.SimulateOltp(num_txns_, num_concurrent_txns_);
-    log_manager_->PersistAndStop();
-
-    // Create recovery table and recovery catalog
-    EXPECT_EQ(1, tested.GetDatabases().size());
-    auto database_oid = tested.GetDatabases()[0];
-    EXPECT_EQ(1, tested.GetTablesForDatabase(database_oid).size());
-    auto table_oid = tested.GetTablesForDatabase(database_oid)[0];
-    auto *table_schema = tested.GetSchemaForTable(database_oid, table_oid);
-    auto *recovered_sql_table = new storage::SqlTable(&block_store_, *table_schema, table_oid);
-    storage::RecoveryCatalog catalog;
-    catalog[database_oid][table_oid] = recovered_sql_table;
-
-    // Create recovery transaction manager that doesn't log
-    transaction::TransactionManager recovery_txn_manager_{&buffer_pool_, true, LOGGING_DISABLED};
-
-    // Instantiate recovery manager, and recover the tables.
-    storage::DiskLogProvider log_provider(LOG_FILE_NAME);
-    recovery_manager_ = new storage::RecoveryManager(&log_provider, &catalog, &recovery_txn_manager_,
-                                                     common::ManagedPointer(&thread_registry_));
-
-    uint64_t elapsed_ms;
-    {
-      common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
-      recovery_manager_->StartRecovery();
-      recovery_manager_->FinishRecovery();
-      recovered_txns += recovery_manager_->GetRecoveredTxnCount();
-    }
-
-    state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
-
-    // Delete test txns
-    gc_thread_ = new storage::GarbageCollectorThread(tested.GetTxnManager(), gc_period_);
-    delete gc_thread_;
-
-    // Delete recovery txns
-    gc_thread_ = new storage::GarbageCollectorThread(&recovery_txn_manager_, gc_period_);
-    delete gc_thread_;
-    delete recovery_manager_;
-    delete recovered_sql_table;
-    delete log_manager_;
-  }
-  state.SetItemsProcessed(recovered_txns);
+  RunBenchmark(state, config);
 }
 
 /**
@@ -125,80 +117,17 @@ BENCHMARK_DEFINE_F(RecoveryBenchmark, OLTPWorkload)(benchmark::State &state) {
  */
 // NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(RecoveryBenchmark, HighAbortRate)(benchmark::State &state) {
-  uint32_t recovered_txns = 0;
-  const uint16_t num_databases = 1;
-  const uint16_t num_tables = 1;
-  const uint16_t num_max_columns = 5;
-  const uint32_t txn_length = 40;
-  const std::vector<double> update_select_delete_ratio = {0.8, 0.2, 0.0};
-  // NOLINTNEXTLINE
-  for (auto _ : state) {
-    // Blow away log file after every benchmark iteration
-    unlink(LOG_FILE_NAME);
-    // Initialize table and run workload with logging enabled
-    log_manager_ =
-        new storage::LogManager(LOG_FILE_NAME, num_log_buffers_, log_serialization_interval_, log_persist_interval_,
-                                log_persist_threshold_, &buffer_pool_, common::ManagedPointer(&thread_registry_));
-    log_manager_->Start();
-    LargeSqlTableTestObject tested = LargeSqlTableTestObject::Builder()
-                                         .SetNumDatabases(num_databases)
-                                         .SetNumTables(num_tables)
-                                         .SetMaxColumns(num_max_columns)
-                                         .SetInitialTableSize(initial_table_size)
-                                         .SetTxnLength(txn_length)
-                                         .SetUpdateSelectDeleteRatio(update_select_delete_ratio)
-                                         .SetBlockStore(&block_store_)
-                                         .SetBufferPool(&buffer_pool_)
-                                         .SetGenerator(&generator_)
-                                         .SetGcOn(true)
-                                         .SetVarlenAllowed(true)
-                                         .SetLogManager(log_manager_)
-                                         .build();
+  LargeSqlTableTestConfiguration config = LargeSqlTableTestConfiguration::Builder()
+      .SetNumDatabases(1)
+      .SetNumTables(1)
+      .SetMaxColumns(100)
+      .SetInitialTableSize(initial_table_size_)
+      .SetTxnLength(40)
+      .SetUpdateSelectDeleteRatio({0.8, 0.2, 0.0})
+      .SetVarlenAllowed(true)
+      .build();
 
-    // Run the test object and log all transactions
-    tested.SimulateOltp(num_txns_, num_concurrent_txns_);
-    log_manager_->PersistAndStop();
-
-    // Create recovery table and recovery catalog
-    EXPECT_EQ(1, tested.GetDatabases().size());
-    auto database_oid = tested.GetDatabases()[0];
-    EXPECT_EQ(1, tested.GetTablesForDatabase(database_oid).size());
-    auto table_oid = tested.GetTablesForDatabase(database_oid)[0];
-    auto *table_schema = tested.GetSchemaForTable(database_oid, table_oid);
-    auto *recovered_sql_table = new storage::SqlTable(&block_store_, *table_schema, table_oid);
-    storage::RecoveryCatalog catalog;
-    catalog[database_oid][table_oid] = recovered_sql_table;
-
-    // Create recovery transaction manager that doesn't log
-    transaction::TransactionManager recovery_txn_manager_{&buffer_pool_, true, LOGGING_DISABLED};
-
-    // Instantiate recovery manager, and recover the tables.
-    storage::DiskLogProvider log_provider(LOG_FILE_NAME);
-    recovery_manager_ = new storage::RecoveryManager(&log_provider, &catalog, &recovery_txn_manager_,
-                                                     common::ManagedPointer(&thread_registry_));
-
-    uint64_t elapsed_ms;
-    {
-      common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
-      recovery_manager_->StartRecovery();
-      recovery_manager_->FinishRecovery();
-      recovered_txns += recovery_manager_->GetRecoveredTxnCount();
-    }
-
-    state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
-
-    // Delete test txns
-    gc_thread_ = new storage::GarbageCollectorThread(tested.GetTxnManager(), gc_period_);
-    delete gc_thread_;
-
-    // Delete recovery txns
-    gc_thread_ = new storage::GarbageCollectorThread(&recovery_txn_manager_, gc_period_);
-    delete gc_thread_;
-    delete recovery_manager_;
-    delete recovered_sql_table;
-    delete log_manager_;
-  }
-  state.SetItemsProcessed(recovered_txns);
+  RunBenchmark(state, config);
 }
 
 BENCHMARK_REGISTER_F(RecoveryBenchmark, OLTPWorkload)->Unit(benchmark::kMillisecond)->UseManualTime()->MinTime(3);
