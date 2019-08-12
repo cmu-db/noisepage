5b746725ea6df369fb5e563cfa6d872c5e60ee0f
Matt Butrovich <mbutrovich@gmail.com>
2019-08-12 12:12:11 -0400

    AppleClang fixes.

--- src/execution/vm/vm.cpp
+++ src/execution/vm/vm.cpp
@@ -1705,7 +1705,7 @@ const u8 *VM::ExecuteCall(const u8 *ip, VM::Frame *caller) {
   u8 *raw_frame = nullptr;
   if (frame_size > kMaxStackAllocSize) {
     used_heap = true;
-    raw_frame = static_cast<u8 *>(std::aligned_alloc(alignof(u64), frame_size));
+    raw_frame = static_cast<u8 *>(util::MallocAligned(alignof(u64), frame_size));
   } else if (frame_size > kSoftMaxStackAllocSize) {
     // TODO(pmenon): Check stack before allocation
     raw_frame = static_cast<u8 *>(alloca(frame_size));
