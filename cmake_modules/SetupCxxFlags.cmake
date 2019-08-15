96a8728409e3b5fad61f6467fa74530a74da8ca5
Matt Butrovich <mbutrovich@gmail.com>
2019-08-15 17:24:31 -0400

    Checkpoint on CMake.

--- cmake_modules/SetupCxxFlags.cmake
+++ cmake_modules/SetupCxxFlags.cmake
@@ -27,24 +27,6 @@ set(CXX_ONLY_FLAGS "-std=c++17 -fPIC -mcx16 -march=native -fvisibility=hidden")
 set(CLANG_CXX_FLAGS "-Wno-c++98-compat -Wno-c++98-compat-pedantic -Qunused-arguments -Wno-unknown-warning-option -fcolor-diagnostics")
 set(GCC_CXX_FLAGS "-Wconversion -Wno-sign-conversion -fdiagnostics-color=auto")
 
-# compiler flags for different build types (run 'cmake -DCMAKE_BUILD_TYPE=<type> .')
-# For all builds:
-# For CMAKE_BUILD_TYPE=Debug
-#   -ggdb: Enable gdb debugging
-# For CMAKE_BUILD_TYPE=FastDebug
-#   Same as DEBUG, except with some optimizations on.
-# For CMAKE_BUILD_TYPE=Release
-#   -O3: Enable all compiler optimizations
-#   Debug symbols are stripped for reduced binary size. Add
-#   -DTERRIER_CXXFLAGS="-g" to add them
-# For CMAKE_BUILD_TYPE=RelWithDebInfo
-#   -O2: Some compiler optimizations
-#   -ggdb: Enable gdb debugging
-set(CXX_FLAGS_DEBUG "-ggdb -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-set(CXX_FLAGS_FASTDEBUG "-ggdb -O1 -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-set(CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
-set(CXX_FLAGS_RELWITHDEBINFO "-ggdb -O2 -DNDEBUG")
-
 # if no build build type is specified, default to debug builds
 if (NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Debug)
@@ -55,13 +37,13 @@ string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
 # Set compile flags based on the build type.
 message("Configured for ${CMAKE_BUILD_TYPE} build (set with cmake -DCMAKE_BUILD_TYPE={release,debug,fastdebug,relwithdebinfo})")
 if ("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
-    set(CXX_OPTIMIZATION_FLAGS "${CXX_FLAGS_DEBUG}")
+    set(CXX_OPTIMIZATION_FLAGS "-ggdb -O0 -fno-omit-frame-pointer -fno-optimize-sibling-calls")
 elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "FASTDEBUG")
-    set(CXX_OPTIMIZATION_FLAGS "${CXX_FLAGS_FASTDEBUG}")
+    set(CXX_OPTIMIZATION_FLAGS "-ggdb -O1 -fno-omit-frame-pointer -fno-optimize-sibling-calls")
 elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "RELEASE")
-    set(CXX_OPTIMIZATION_FLAGS "${CXX_FLAGS_RELEASE}")
+    set(CXX_OPTIMIZATION_FLAGS "-O3 -DNDEBUG")
 elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "RELWITHDEBINFO")
-    set(CXX_OPTIMIZATION_FLAGS "${CXX_FLAGS_RELWITHDEBINFO}")
+    set(CXX_OPTIMIZATION_FLAGS "-ggdb -O2 -DNDEBUG")
 else ()
     message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
 endif ()
