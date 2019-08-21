2ec7c91a568d48d3d20223a964e4a53053e3ac45
Amadou Latyr Ngom <amlatyr@gmail.com>
2019-08-21 15:43:57 -0400

    Fix visibility on Linux. Remove non deterministic tests

--- cmake_modules/SetupCxxFlags.cmake
+++ cmake_modules/SetupCxxFlags.cmake
@@ -24,6 +24,9 @@ include(CheckCXXCompilerFlag)
 # compiler flags that are common across debug/release builds
 set(CXX_COMMON_FLAGS "-Wall -Werror -Wno-c++98-compat -Wno-c++98-compat-pedantic")
 set(CXX_ONLY_FLAGS "-std=c++17 -fPIC -mcx16 -march=native")
+if (APPLE)
+    set(CXX_ONLY_FLAGS "${CXX_ONLY_FLAGS} -fvisibility=hidden")
+endif ()
 
 # if no build build type is specified, default to debug builds
 if (NOT CMAKE_BUILD_TYPE)
