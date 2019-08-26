e6cd3c7e9a54fc01aae9db74b09ce76ce9c8b77e
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-08-26 16:31:00 -0400

    Change transaction timestamp defs

--- src/include/transaction/transaction_defs.h
+++ src/include/transaction/transaction_defs.h
@@ -9,13 +9,13 @@ namespace terrier::transaction {
 STRONG_TYPEDEF(timestamp_t, uint64_t);
 
 // Invalid txn timestamp. Used for validation.
-static constexpr timestamp_t INVALID_TXN_TIMESTAMP = timestamp_t(0);
+static constexpr timestamp_t INVALID_TXN_TIMESTAMP = timestamp_t(INT64_MIN);
 
 // Used to indicate that no active txns exist
-static constexpr timestamp_t NO_ACTIVE_TXN = timestamp_t(1);
+static constexpr timestamp_t NO_ACTIVE_TXN = timestamp_t(UINT64_MAX);
 
 // First txn timestamp that can be given out by the txn manager
-static constexpr timestamp_t INITIAL_TXN_TIMESTAMP = timestamp_t(2);
+static constexpr timestamp_t INITIAL_TXN_TIMESTAMP = timestamp_t(0);
 
 class TransactionContext;
 // Explicitly define the underlying structure of std::queue as std::list since we believe the default (std::deque) may
