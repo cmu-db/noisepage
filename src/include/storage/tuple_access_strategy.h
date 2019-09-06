12ba297c1fcc8a9b571249137c8eac7163a902b1
Tianyu Li <tli2@andrew.cmu.edu>
2019-09-06 11:37:00 -0400

    remove dead code

--- src/include/storage/tuple_access_strategy.h
+++ src/include/storage/tuple_access_strategy.h
@@ -76,14 +76,6 @@ class TupleAccessStrategy {
       return reinterpret_cast<MiniBlock *>(head);
     }
 
-    // return reference to num_slots. Use as a member.
-    uint32_t &NumSlots() { return *reinterpret_cast<uint32_t *>(block_.content_); }
-
-    // return reference to num_attrs. Use as a member.
-    uint16_t &NumAttrs(const BlockLayout &layout) {
-      return *reinterpret_cast<uint16_t *>(AttrOffsets(layout) + layout.NumColumns());
-    }
-
     RawBlock block_;
   };
 
