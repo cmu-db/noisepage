78af918d51fe53e4c278e89751b5b8322dd01f81
Tanuj Nayak <tanujnay112@live.com>
2019-09-10 12:40:17 -0400

    insert codegen mostly made?

--- src/execution/compiler/operator/insert_translator.cpp
+++ src/execution/compiler/operator/insert_translator.cpp
@@ -1,1 +1,73 @@
-namespace terrier::execution::compiler {}  // namespace terrier::execution::compiler
+
+#include <execution/compiler/operator/insert_translator.h>
+
+#include "execution/compiler/operator/insert_translator.h"
+
+namespace terrier::execution::compiler {
+InsertTranslator::InsertTranslator(const terrier::planner::AbstractPlanNode *op, CodeGen *codegen)
+    : OperatorTranslator(op, codegen), inserter_struct_(codegen->NewIdentifier(inserter_name_)) {}
+
+
+void InsertTranslator::Produce(FunctionBuilder *builder) {
+  // generate the code for the insertion
+  const auto &node = GetOperatorAs<terrier::planner::InsertPlanNode>();
+
+  // var pr : projectedRow
+  auto projected_row = codegen_->InserterGetTablePR(codegen_->PointerTo(inserter_struct_));
+
+  // populate v
+  const auto &node_vals = node.GetValues();
+  auto param_info = node.GetParameterInfo();
+  for (size_t i = 0; i < node_vals.size(); i++) {
+    auto col_id = !param_info[i];
+    auto *src = codegen_->PeekValue(node_vals[i]);
+    auto set_stmt = codegen_->ProjectedRowSet(projected_row, codegen_->IntLiteral(col_id), src)
+    builder->Append(set_stmt);
+  }
+
+//  // @insert(db_oid, table_oid, &v)
+//  util::RegionVector<ast::Expr *> args(pipeline_->GetRegion());
+//  args.emplace_back(codegen_->NewIntLiteral(DUMMY_POS, !node.GetDatabaseOid()));
+//  args.emplace_back(codegen_->NewIntLiteral(DUMMY_POS, !node.GetTableOid()));
+//  args.emplace_back(codegen_->NewUnaryOpExpr(DUMMY_POS, parsing::Token::Type::AMPERSAND, var_ex));
+//  auto call_stmt = codegen_->NewCallExpr(codegen.Binsert(), std::move(args));
+//  codegen_->GetCurrentFunction()->Append(codegen->NewExpressionStmt(call_stmt));
+}
+
+void InsertTranslator::Consume(FunctionBuilder *builder) {
+  // generate the code for insert select
+  const auto &node = GetOperatorAs<terrier::planner::InsertPlanNode>();
+
+  auto var_ex = batch->GetIdentifierExpr();
+
+  // @insert(db_oid, table_oid, &v)
+  util::RegionVector<ast::Expr *> args(codegen_->GetRegion());
+  args.emplace_back(codegen_->NewIntLiteral(DUMMY_POS, !node.GetDatabaseOid()));
+  args.emplace_back(codegen_->NewIntLiteral(DUMMY_POS, !node.GetTableOid()));
+  args.emplace_back(codegen_->NewUnaryOpExpr(DUMMY_POS, parsing::Token::Type::AMPERSAND, var_ex));
+  auto call_stmt = codegen_->NewCallExpr(codegen_->Binsert(), std::move(args));
+  codegen_->GetCurrentFunction()->Append(codegen_->NewExpressionStmt(call_stmt));
+}
+
+void InsertTranslator::InitializeStateFields(util::RegionVector<ast::FieldDecl *> *state_fields) {
+  ast::Expr *inserter_type = codegen_->BuiltinType(ast::BuiltinType::Kind::Inserter);
+  state_fields->emplace_back(codegen_->MakeField(inserter_struct_, inserter_type));
+}
+
+void InsertTranslator::InitializeStructs(util::RegionVector<ast::Decl *> *decls) {
+
+}
+void InsertTranslator::InitializeSetup(util::RegionVector<ast::Stmt *> *setup_stmts) {
+  auto &node = GetOperatorAs<terrier::planner::InsertPlanNode>();
+  ast::Expr *inserter_setup = codegen_->InserterInit(codegen_->PointerTo(inserter_struct_),
+      codegen_->GetExecCtxVar(), codegen_->IntLiteral(!node.GetTableOid()));
+  setup_stmts->emplace_back(codegen_->MakeStmt(inserter_setup));
+}
+
+void InsertTranslator::Produce(FunctionBuilder *builder) {
+
+}
+void InsertTranslator::Consume(FunctionBuilder *builder) {
+
+}
+}  // namespace terrier::execution::compiler
--- src/include/execution/compiler/operator/insert_translator.h
+++ src/include/execution/compiler/operator/insert_translator.h
@@ -1,1 +1,104 @@
 #pragma once
+
+#include "execution/compiler/operator/operator_translator.h"
+#include "planner/plannodes/insert_plan_node.h"
+
+namespace terrier::execution::compiler {
+
+/**
+ * Insert Translator
+ */
+class InsertTranslator : public OperatorTranslator {
+ public:
+
+  InsertTranslator(const terrier::planner::AbstractPlanNode *op, CodeGen *codegen);
+
+  /// Declare the inserter
+  void InitializeStateFields(util::RegionVector<ast::FieldDecl *> *state_fields) override;
+
+  // Declare payload and probe structs
+  void InitializeStructs(util::RegionVector<ast::Decl *> *decls) override;
+
+  // Create the key check function.
+  //void InitializeHelperFunctions(util::RegionVector<ast::Decl *> *decls) override;
+
+  // Call @inserterInit on inserter
+  // Call @inserterInitBind(&inserter)
+  void InitializeSetup(util::RegionVector<ast::Stmt *> *setup_stmts) override;
+
+  // Call @aggHTFree
+  //void InitializeTeardown(util::RegionVector<ast::Stmt *> *teardown_stmts) override;
+
+  // real stuff
+  void Produce(FunctionBuilder *builder) override;
+
+  void Consume(FunctionBuilder *builder) override;
+
+  // This is materializer
+  bool IsMaterializer(bool *is_ptr) override {
+    *is_ptr = true;
+    return true;
+  }
+
+  // Return the payload and its type
+//  std::pair<ast::Identifier *, ast::Identifier *> GetMaterializedTuple() override {
+//    return {&agg_payload_, &payload_struct_};
+//  }
+
+ private:
+  // var tvi : TableVectorIterator
+  void DeclareTVI(FunctionBuilder *builder);
+
+  void SetOids(FunctionBuilder *builder);
+
+  // for (@tableIterInit(&tvi, ...); @tableIterAdvance(&tvi);) {...}
+  void GenTVILoop(FunctionBuilder *builder);
+
+  void DeclarePCI(FunctionBuilder *builder);
+
+  // var pci = @tableIterGetPCI(&tvi)
+  // for (; @pciHasNext(pci); @pciAdvance(pci)) {...}
+  void GenPCILoop(FunctionBuilder *builder);
+
+  // if (cond) {...}
+  void GenScanCondition(FunctionBuilder *builder);
+
+  // @tableIterClose(&tvi)
+  void GenTVIClose(FunctionBuilder *builder);
+
+  // @tableIterReset(&tvi)
+  void GenTVIReset(FunctionBuilder *builder);
+
+  // Whether the seq scan can be vectorized
+  static bool IsVectorizable(const terrier::parser::AbstractExpression *predicate);
+
+  // Generated vectorized filters
+  void GenVectorizedPredicate(FunctionBuilder *builder, const terrier::parser::AbstractExpression *predicate);
+
+ private:
+  static constexpr const char *inserter_name_ = "inserter";
+
+  ast::Identifier inserter_struct_;
+//  const planner::InsertPlanNode *insert_op_;
+//  const catalog::Schema &schema_;
+//  std::vector<catalog::col_oid_t> input_oids_;
+//  storage::ProjectionMap pm_;
+//  bool has_predicate_;
+//  bool is_vectorizable_;
+//
+//  // Structs, functions and locals
+//  static constexpr const char *tvi_name_ = "tvi";
+//  static constexpr const char *col_oids_name_ = "col_oids";
+//  static constexpr const char *pci_name_ = "pci";
+//  static constexpr const char *row_name_ = "row";
+//  static constexpr const char *table_struct_name_ = "TableRow";
+//  static constexpr const char *pci_type_name_ = "ProjectedColumnsIterator";
+//  ast::Identifier tvi_;
+//  ast::Identifier col_oids_;
+//  ast::Identifier pci_;
+//  ast::Identifier row_;
+//  ast::Identifier table_struct_;
+//  ast::Identifier pci_type_;
+};
+
+}  // namespace tpl::compiler
