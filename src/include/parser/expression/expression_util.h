cca0bb51a5bca8bb228a71ac2651ad1a058019db
tpan496 <tianleip@andrew.cmu.edu>
2019-09-10 20:54:36 -0400

    Add helper function to extract ids from AbstractExpression

---
+++ src/include/parser/expression/expression_util.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+#include <set>
+
+#include "catalog/catalog_defs.h"
+#include "common/managed_pointer.h"
+#include "parser/expression/abstract_expression.h"
+#include "parser/expression/column_value_expression.h"
+
+namespace terrier::parser {
+
+/**
+ * An utility class for Expression objects
+ */
+class ExpressionUtil {
+
+ public:
+  // Static utility class
+  ExpressionUtil() = delete;
+
+
+  /**
+   * Populate the given set with all of the column oids referenced
+   * in this expression tree.
+   * @param col_oids
+   * @param expr
+   */
+  static void GetColumnOids(std::set<catalog::col_oid_t> &col_oids, common::ManagedPointer<AbstractExpression> expr) {
+    // Recurse into our children
+    for (const auto &child: expr->GetChildren()) {
+      ExpressionUtil::GetColumnOids(col_oids, child);
+    }
+
+    // If our mofo is a ColumnValueExpression, then pull out our
+    // column ids
+    auto etype = expr->GetExpressionType();
+    if (etype == ExpressionType::COLUMN_VALUE) {
+      auto t_expr = expr.CastManagedPointerTo<ColumnValueExpression>();
+      col_oids.insert(t_expr->GetColumnOid());
+    }
+  }
+
+};
+
+}  // namespace terrier::parser
