64cefada1cd917c6bf870ccf135cb940c04ed17b
Matt Butrovich <mbutrovich@gmail.com>
2019-09-13 12:07:14 -0400

    Fix clang-tidy.

--- src/include/transaction/deferred_action_manager.h
+++ src/include/transaction/deferred_action_manager.h
@@ -52,8 +52,8 @@ class DeferredActionManager {
   uint32_t Process() {
     // Check out a timestamp from the transaction manager to determine the progress of
     // running transactions in the system.
-    timestamp_t oldest_txn = timestamp_manager_->OldestTransactionStartTime();
-    const uint32_t backlog_size = static_cast<uint32_t>(back_log_.size());
+    const timestamp_t oldest_txn = timestamp_manager_->OldestTransactionStartTime();
+    const auto backlog_size = static_cast<uint32_t>(back_log_.size());
     uint32_t processed = ClearBacklog(oldest_txn);
     // There is no point in draining new actions if we haven't cleared the backlog.
     // This leaves some mechanisms for the rest of the system to detect congestion
