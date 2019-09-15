d2e02dde54477093feea398476e3f3d713399ca9
Tanuj Nayak <tanujnay112@live.com>
2019-09-15 03:58:56 -0400

    added missing documentation

--- src/include/planner/plannodes/insert_plan_node.h
+++ src/include/planner/plannodes/insert_plan_node.h
@@ -79,6 +79,11 @@ class InsertPlanNode : public AbstractPlanNode {
       return *this;
     }
 
+
+    /**
+     * @param index_oid oid of insertion index that is to be added to this plan node
+     * @return builder object
+     */
     Builder &AddIndexOid(catalog::index_oid_t index_oid) {
       index_oids_.emplace_back(index_oid);
       return *this;
