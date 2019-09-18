52d82bc3368faa7429d2f14a7dfb34b829e0f64d
Pulkit Agarwal <thepulkitagarwal@gmail.com>
2019-09-17 22:57:38 -0400

    Updater and Deleter Initial Commit

--- src/include/execution/sql/deleter.h
+++ src/include/execution/sql/deleter.h
@@ -1,10 +1,28 @@
 #pragma once
 #include "execution/util/execution_common.h"
+#include "execution/exec/execution_context.h"
 
 namespace terrier::execution::sql {
 
 /**
  * Helper class to perform deletes in SQL Tables.
  */
-class EXPORT Deleter {};
+class EXPORT Deleter {
+ public:
+  Deleter(exec::ExecutionContext *exec_ctx, std::string table_name)
+  : Deleter(exec_ctx, exec_ctx->GetAccessor()->GetTableOid(table_name)) {}
+
+  Deleter(exec::ExecutionContext *exec_ctx, catalog::table_oid_t table_oid)
+  : table_oid_(table_oid), exec_ctx_(exec_ctx) {}
+
+  void TableDelete(storage::TupleSlot slot) {
+    common::ManagedPointer<storage::SqlTable> sql_table = exec_ctx_->GetAccessor()->GetTable(table_oid_);
+    transaction::TransactionContext *txn = exec_ctx_->GetTxn();
+    sql_table->Delete(txn, slot);
+  }
+
+ private:
+  catalog::table_oid_t table_oid_;
+  exec::ExecutionContext *exec_ctx_;
+};
 }  // namespace terrier::execution::sql
--- src/include/execution/sql/updater.h
+++ src/include/execution/sql/updater.h
@@ -6,5 +6,36 @@ namespace terrier::execution::sql {
 /**
  * Helper class to perform updates in SQL Tables.
  */
-class EXPORT Updater {};
+class EXPORT Updater {
+ public:
+  Updater(exec::ExecutionContext *exec_ctx, std::string table_name, std::vector<catalog::col_oid_t> col_oids)
+    : Updater(exec_ctx, exec_ctx->GetAccessor()->GetTableOid(table_name), col_oids) {}
+
+  Updater(exec::ExecutionContext *exec_ctx, catalog::table_oid_t table_oid, std::vector<catalog::col_oid_t> col_oids)
+  : table_oid_(table_oid), col_oids_(col_oids), exec_ctx_(exec_ctx) {
+    common::ManagedPointer<storage::SqlTable> sql_table = exec_ctx_->GetAccessor()->GetTable(table_oid_);
+    transaction::TransactionContext *txn = exec_ctx_->GetTxn();
+    storage::ProjectedRowInitializer initializer = sql_table->InitializerForProjectedRow(col_oids_);
+
+    // TODO: If you StageWrite anything that you didn't succeed in writing into the table or decide you don't
+    //  want to use, the transaction MUST abort
+    redo_record_ = txn->StageWrite(exec_ctx_->DBOid(), table_oid_, initializer);
+  }
+
+  storage::ProjectedRow *GetTablePR() {
+    return redo_record_->Delta();
+  }
+
+  void TableUpdate() {
+    common::ManagedPointer<storage::SqlTable> sql_table = exec_ctx_->GetAccessor()->GetTable(table_oid_);
+    transaction::TransactionContext *txn = exec_ctx_->GetTxn();
+    sql_table->Update(txn, redo_record_);
+  }
+
+ private:
+  catalog::table_oid_t table_oid_;
+  std::vector<catalog::col_oid_t> col_oids_;
+  exec::ExecutionContext *exec_ctx_;
+  storage::RedoRecord *redo_record_;
+};
 }  // namespace terrier::execution::sql
