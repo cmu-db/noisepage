d3d83ed46ea48b172814dcdec24d705af4d26cef
Tanuj Nayak <tanujnay112@live.com>
2019-09-22 01:09:47 -0400

    inserter test

--- test/execution/inserter_test.cpp
+++ test/execution/inserter_test.cpp
@@ -28,18 +28,36 @@ class InserterTest : public SqlBasedTest {
 TEST_F(InserterTest, SimpleInserterTest) {
   //
   auto table_oid = exec_ctx_->GetAccessor()->GetTableOid(NSOid(), "test_1");
-  auto index_oid = exec_ctx_->GetAccessor()->GetIndexOid(NSOid(), "index_1");
-
   Inserter inserter(exec_ctx_.get(), table_oid);
   auto table_pr = inserter.GetTablePR();
   auto schema = exec_ctx_->GetAccessor()->GetSchema(table_oid);
-  auto columns = schema.GetColumns();
 
   *reinterpret_cast<int32_t *>(table_pr->AccessForceNotNull(0)) = 15;
   *reinterpret_cast<int32_t *>(table_pr->AccessForceNotNull(1)) = 721;
   *reinterpret_cast<int32_t *>(table_pr->AccessForceNotNull(2)) = 445;
   *reinterpret_cast<int32_t *>(table_pr->AccessForceNotNull(3)) = 4256;
 
+  inserter.TableInsert();
 
+  auto table = exec_ctx_->GetAccessor()->GetTable(table_oid);
+
+  size_t count = 0;
+  for (auto iter = table->begin(); iter != table->end(); iter++) {
+    count++;
+  }
+  EXPECT_EQ(TEST1_SIZE + 1, count);
+
+  auto index_oid = exec_ctx_->GetAccessor()->GetIndexOid(NSOid(), "index_1");
+  auto index_pr = inserter.GetIndexPR(index_oid);
+  auto index = exec_ctx_->GetAccessor()->GetIndex(index_oid);
+  *reinterpret_cast<int32_t *>(index_pr->AccessForceNotNull(0)) = 15;
+  std::vector<storage::TupleSlot> results1;
+  index->ScanKey(*exec_ctx_->GetTxn(), *index_pr, &results1);
+  EXPECT_TRUE(inserter.IndexInsert());
+  std::vector<storage::TupleSlot> results2;
+  index->ScanKey(*exec_ctx_->GetTxn(), *index_pr, &results2);
+  EXPECT_EQ(results1.size() + 1, results2.size());
 }
+
+
 } // namespace terrier::execution::sql::test
