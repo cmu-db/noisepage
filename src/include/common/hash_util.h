05b007c7c731fa2125cff83cab242a1b73124e91
Andy Pavlo <pavlo@cs.brown.edu>
2019-09-28 18:19:21 -0400

    Add missing documentation for typedefs (#570)
    * Added missing documentation for two typedefs that Doxygen on Travis occassionally complains about #569
    
    * Updated .gitignore to hide libpqxx files in root dir

--- src/include/common/hash_util.h
+++ src/include/common/hash_util.h
@@ -6,6 +6,9 @@
 #include "common/strong_typedef.h"
 namespace terrier::common {
 
+/**
+ * This is our typedef that we use throughout the entire code to represent a hash value.
+ */
 using hash_t = uint64_t;
 
 /**
