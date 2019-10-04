abf812833f859930db550fd1a71ba6defd9d5bc9
tpan496 <tianleip@andrew.cmu.edu>
2019-10-04 02:58:42 -0400

    remove itp

--- src/network/itp/itp_command_factory.cpp
+++
@@ -1,9 +0,0 @@
-#include "network/itp/itp_command_factory.h"
-#include <memory>
-namespace terrier::network {
-
-std::shared_ptr<AbstractNetworkCommand> ITPCommandFactory::PacketToCommand(InputPacket *packet) {
-  throw NETWORK_PROCESS_EXCEPTION("Unexpected Packet Type: ");
-}
-
-}  // namespace terrier::network
