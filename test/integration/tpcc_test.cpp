8d44de163c2295ffad82b6d803158d89d0c537e8
Matt Butrovich <mbutrovich@users.noreply.github.com>
2019-10-04 20:02:10 -0400

    Metrics Manager (#506)

--- test/integration/tpcc_test.cpp
+++ test/integration/tpcc_test.cpp
@@ -1,7 +1,6 @@
 #include <random>
 #include <string>
 #include <vector>
-#include "catalog/catalog.h"
 #include "common/macros.h"
 #include "common/scoped_timer.h"
 #include "common/worker_pool.h"
@@ -62,7 +61,7 @@ class TPCCTests : public TerrierTest {
   const std::chrono::milliseconds log_persist_interval_{20};
   const uint64_t log_persist_threshold_ = (1U << 20U);  // 1MB
 
-  storage::GarbageCollector *gc_ = nullptr;
+  storage::GarbageCollector *gc_;
   storage::GarbageCollectorThread *gc_thread_ = nullptr;
   const std::chrono::milliseconds gc_period_{10};
   const std::chrono::milliseconds metrics_period_{100};
@@ -95,8 +94,7 @@ class TPCCTests : public TerrierTest {
     transaction::DeferredActionManager deferred_action_manager(&timestamp_manager);
     transaction::TransactionManager txn_manager(&timestamp_manager, &deferred_action_manager, &buffer_pool_, true,
                                                 log_manager_);
-    catalog::Catalog catalog(&txn_manager, &block_store_);
-    Builder tpcc_builder(&block_store_, &catalog, &txn_manager);
+    auto tpcc_builder = Builder(&block_store_);
 
     // Precompute all of the input arguments for every txn to be run. We want to avoid the overhead at benchmark time
     const auto precomputed_args =
@@ -111,8 +109,8 @@ class TPCCTests : public TerrierTest {
       workers.emplace_back(tpcc_db);
     }
 
-    // populate the tables and indexes, as well as force log manager to log all changes
-    Loader::PopulateDatabase(&txn_manager, tpcc_db, &workers, &thread_pool_);
+    // populate the tables and indexes
+    Loader::PopulateDatabase(&txn_manager, &generator_, tpcc_db, workers);
     if (logging_enabled) log_manager_->ForceFlush();
 
     gc_ = new storage::GarbageCollector(&timestamp_manager, &deferred_action_manager, &txn_manager, DISABLED);
@@ -129,15 +127,12 @@ class TPCCTests : public TerrierTest {
     thread_pool_.WaitUntilAllFinished();
 
     // cleanup
-    Util::UnregisterIndexesForGC(&(gc_thread_->GetGarbageCollector()), tpcc_db);
-    delete gc_thread_;
-    catalog.TearDown();
-    StorageTestUtil::FullyPerformGC(gc_, log_manager_);
     thread_pool_.Shutdown();
     if (logging_enabled) {
       log_manager_->PersistAndStop();
       delete log_manager_;
     }
+    delete gc_thread_;
     delete gc_;
     delete thread_registry_;
     if (metrics_enabled) {
