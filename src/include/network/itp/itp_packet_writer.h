5b5af944ea35b22dccc77571de9a15a731d78577
tpan496 <tianleip@andrew.cmu.edu>
2019-10-11 15:10:04 -0400

    refactor

--- src/include/network/itp/itp_packet_writer.h
+++ src/include/network/itp/itp_packet_writer.h
@@ -19,19 +19,24 @@ class ITPPacketWriter : public PacketWriter {
   explicit ITPPacketWriter(const std::shared_ptr<WriteQueue> &write_queue) : PacketWriter(write_queue) {}
 
   /**
-   * Writes a Replication command
+   * Create a Replication command
    * --------------------------------------------------------------------------------------------------
    * | message type (char) | message id (uint64_t) | data size (uint64_t) | replication data (varlen) |
    * --------------------------------------------------------------------------------------------------
    * @param message_id message id
    * @param replication_data data for replication
    */
-  void WriteReplicationCommand(uint64_t message_id, const std::string& replication_data) {
-    BeginPacket(NetworkMessageType::ITP_REPLICATION_COMMAND)
-      .AppendValue<uint64_t>(message_id)
-      .AppendValue<uint64_t>(replication_data.length())
-      .AppendString(replication_data)
-      .EndPacket();
+  void BeginReplicationCommand(uint64_t message_id, uint64_t data_size) {
+    BeginPacket(NetworkMessageType::ITP_REPLICATION_COMMAND);
+    AppendValue<uint64_t>(message_id);
+    AppendValue<uint64_t>(data_size);
+  }
+
+  /**
+   * End the Replication command
+   */
+  void EndReplicationCommnad() {
+    EndPacket();
   }
 
 };
