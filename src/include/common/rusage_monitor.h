5b0b8229998d45dd909c8aa7cf8011f0d8ba9b5d
Matt Butrovich <mbutrovich@gmail.com>
2019-10-19 16:02:17 -0400

    Warnings.

--- src/include/common/rusage_monitor.h
+++ src/include/common/rusage_monitor.h
@@ -66,20 +66,20 @@ class RusageMonitor {
   static rusage SubtractRusage(const rusage &lhs, const rusage &rhs) {
     return {SubtractTimeval(lhs.ru_utime, rhs.ru_utime),
             SubtractTimeval(lhs.ru_stime, rhs.ru_stime),
-            lhs.ru_maxrss - rhs.ru_maxrss,
-            lhs.ru_ixrss - rhs.ru_ixrss,
-            lhs.ru_idrss - rhs.ru_idrss,
-            lhs.ru_isrss - rhs.ru_isrss,
-            lhs.ru_minflt - rhs.ru_minflt,
-            lhs.ru_majflt - rhs.ru_majflt,
-            lhs.ru_nswap - rhs.ru_nswap,
-            lhs.ru_inblock - rhs.ru_inblock,
-            lhs.ru_oublock - rhs.ru_oublock,
-            lhs.ru_msgsnd - rhs.ru_msgsnd,
-            lhs.ru_msgrcv - rhs.ru_msgrcv,
-            lhs.ru_nsignals - rhs.ru_nsignals,
-            lhs.ru_nvcsw - rhs.ru_nvcsw,
-            lhs.ru_nivcsw - rhs.ru_nivcsw};
+            {lhs.ru_maxrss - rhs.ru_maxrss},
+            {lhs.ru_ixrss - rhs.ru_ixrss},
+            {lhs.ru_idrss - rhs.ru_idrss},
+            {lhs.ru_isrss - rhs.ru_isrss},
+            {lhs.ru_minflt - rhs.ru_minflt},
+            {lhs.ru_majflt - rhs.ru_majflt},
+            {lhs.ru_nswap - rhs.ru_nswap},
+            {lhs.ru_inblock - rhs.ru_inblock},
+            {lhs.ru_oublock - rhs.ru_oublock},
+            {lhs.ru_msgsnd - rhs.ru_msgsnd},
+            {lhs.ru_msgrcv - rhs.ru_msgrcv},
+            {lhs.ru_nsignals - rhs.ru_nsignals},
+            {lhs.ru_nvcsw - rhs.ru_nvcsw},
+            {lhs.ru_nivcsw - rhs.ru_nivcsw}};
   }
 
   /**
