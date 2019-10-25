13d4b1dbaf867f901dd1a9b46b3bf5ea2ccd166d
tpan496 <tianleip@andrew.cmu.edu>
2019-10-25 16:11:38 -0400

    ITP Pipeline Setup (#602)

---
+++ src/include/storage/recovery/replication_log_provider.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+#include "storage/recovery/abstract_log_provider.h"
+
+namespace terrier::storage {
+
+/**
+ * TODO(Tianlei/Gus): Replace this file with Gus's version
+ */
+class ReplicationLogProvider {
+ public:
+  /**
+   * Passes the content of the buffer to traffic cop
+   * @param buffer content to pass to traffic cop
+   */
+  virtual void HandBufferToReplication(std::unique_ptr<network::ReadBuffer> buffer);
+};
+}  // namespace terrier::storage
