b2f7dc38f2f49579293826fbc2090ac2f6cd887d
Matt Butrovich <mbutrovich@gmail.com>
2019-11-12 15:36:01 -0500

    Make index stuff extern templates.

---
+++ src/storage/index/bwtree_index.cpp
@@ -0,0 +1,16 @@
+#include "storage/index/bwtree_index.h"
+#include "storage/index/compact_ints_key.h"
+#include "storage/index/generic_key.h"
+
+namespace terrier::storage::index {
+
+template class BwTreeIndex<CompactIntsKey<8>>;
+template class BwTreeIndex<CompactIntsKey<16>>;
+template class BwTreeIndex<CompactIntsKey<24>>;
+template class BwTreeIndex<CompactIntsKey<32>>;
+
+template class BwTreeIndex<GenericKey<64>>;
+template class BwTreeIndex<GenericKey<128>>;
+template class BwTreeIndex<GenericKey<256>>;
+
+}  // namespace terrier::storage::index
