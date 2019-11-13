acf6339391b5c80e5ec3b4f3a084fd6540424435
Matt Butrovich <mbutrovich@gmail.com>
2019-11-13 13:47:47 -0500

    Fix compilation.

--- src/include/optimizer/statistics/value_condition.h
+++ src/include/optimizer/statistics/value_condition.h
@@ -5,6 +5,7 @@
 #include <utility>
 
 #include "catalog/catalog_defs.h"
+#include "common/managed_pointer.h"
 #include "loggers/optimizer_logger.h"
 #include "parser/expression_defs.h"
 #include "type/transient_value.h"
