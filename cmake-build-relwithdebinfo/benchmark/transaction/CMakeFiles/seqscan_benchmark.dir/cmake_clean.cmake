693c5dbe452be167102cb76ca767b3bad70ef296
Deepayan Patra <dpatra@andrew.cmu.edu>
2019-11-15 22:07:28 -0500

    setup code written

---
+++ cmake-build-relwithdebinfo/benchmark/transaction/CMakeFiles/seqscan_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../relwithdebinfo/seqscan_benchmark"
+  "../../relwithdebinfo/seqscan_benchmark.pdb"
+  "CMakeFiles/seqscan_benchmark.dir/seqscan_benchmark.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seqscan_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
