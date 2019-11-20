058c0e46515bc5e21beb9b6b462ab47d86e66efc
Yuze Liao <yuzeliao@gmail.com>
2019-11-19 23:00:21 -0500

    fix check-lint

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -380,8 +380,8 @@ void DataTable::ExportTable(const std::string file_name) const {
   for (RawBlock *block : blocks_) {
     std::vector<flatbuf::FieldNode> field_nodes;
     std::vector<flatbuf::Buffer> buffers;
-    while (!block->controller_.TryAcquireInPlaceRead())
-      ;
+    while (!block->controller_.TryAcquireInPlaceRead()) {
+    };
     ArrowBlockMetadata &metadata = accessor_.GetArrowBlockMetadata(block);
     uint32_t num_slots = metadata.NumRecords();
 
