0123351353efdbfb2ed29d929728bc43c84668cc
Matt Butrovich <mbutrovich@gmail.com>
2019-11-22 13:22:18 -0500

    Trigger Homebrew upgrade on Jenkins macOS box.

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -64,6 +64,8 @@ install_mac() {
   fi
   # Update Homebrew.
   brew update
+  brew upgrade
+  brew cleanup
   # Install packages.
   brew ls --versions cmake || brew install cmake
   brew ls --versions coreutils || brew install coreutils
