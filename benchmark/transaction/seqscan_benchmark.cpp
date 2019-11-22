3b21b17b66bdb0b251aec6b5a93e256e06a47522
Deepayan Patra <dpatra@andrew.cmu.edu>
2019-11-22 02:29:51 -0500

    removed SQLTable

--- benchmark/transaction/seqscan_benchmark.cpp
+++ benchmark/transaction/seqscan_benchmark.cpp
@@ -1,50 +1,45 @@
 #include <vector>
 
-#include "execution/sql_test.h"
 #include "execution/sql/table_vector_iterator.h"
+#include "execution/sql_test.h"
 
+#include "../../util/include/execution/table_generator/table_generator.h"
 #include "benchmark/benchmark.h"
 #include "benchmark_util/data_table_benchmark_util.h"
 #include "storage/garbage_collector_thread.h"
-#include "../../util/include/execution/table_generator/table_generator.h"
 
 namespace terrier::execution {
 
-class SeqscanBenchmark : public benchmark::Fixture, public SqlBasedTest {
-  // TODO: Fix disagreement between SetUp function argument types
-  void SetUp(State& st) {
-    // Run setup file
-    SqlBasedTest::SetUp();
-    // Create execution context
-    exec_ctx_ = MakeExecCtx();
-    // Create test tables
-    GenerateTestTables(exec_ctx_.get());
-  }
-
- public:
- parser::ConstantValueExpression DummyExpr() {
-   return parser::ConstantValueExpression(type::TransientValueFactory::GetInteger(0));
- }
-
- protected:
- /**
-  * Execution context to use for the test
-  */
- std::unique_ptr<exec::ExecutionContext> exec_ctx_;
+class SeqscanBenchmark : public benchmark::Fixture {
+  // TODO: Fix disagreement between SetUp function argument types * do I use SqlBasedTest class? *
+//  void SetUp(benchmark::State &st) {
+//
+//  }
+//
+// public:
+//  parser::ConstantValueExpression DummyExpr() {
+//    return parser::ConstantValueExpression(type::TransientValueFactory::GetInteger(0));
+//  }
+//
+// protected:
+//  /**
+//   * Execution context to use for the test
+//   */
+//  std::unique_ptr<exec::ExecutionContext> exec_ctx_;
 };
 
-
-  /** 
-   * Run a sequential scan through the LLVM engine without accessing the SQLTable
-   */
-  // NOLINTNEXTLINE
+/**
+ * Run a sequential scan through the LLVM engine without accessing the SQLTable
+ */
+// NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(SeqscanBenchmark, SequentialScan)(benchmark::State &state) {
   // TODO: Import main function from tpl.cpp and run with command line options
   // TODO: Determine path to tpl.cpp and vec-filter.tpl
 
-  // Pause timer while running compiler
+  // Pause timer while compling file
   state.PauseTiming();
-  system("gcc -o a.out tpl.cpp vec-filter.tpl");
+  const char *cmd = "gcc -std=c++17 -g -ggdb -O0 -Wall -Werror -o a.out ../../util/execution/tpl.cpp ../../sample_tpl/vec-filter.cpp";
+  system(cmd);
   state.ResumeTiming();
 
   // Determine time to run input tpl file
@@ -52,4 +47,4 @@ BENCHMARK_DEFINE_F(SeqscanBenchmark, SequentialScan)(benchmark::State &state) {
 }
 
 BENCHMARK_REGISTER_F(SeqscanBenchmark, SequentialScan)->UseManualTime()->Unit(benchmark::kMillisecond);
-} // namespace terrier
+}  // namespace terrier::execution
