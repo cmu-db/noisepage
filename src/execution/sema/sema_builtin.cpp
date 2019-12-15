a0beba1cd939b8a016044bb4b102e3e37b7abaf3
Matt Butrovich <mbutrovich@gmail.com>
2019-12-15 13:58:38 -0500

    More PR review changes.

--- src/execution/sema/sema_builtin.cpp
+++ src/execution/sema/sema_builtin.cpp
@@ -1,8 +1,7 @@
-#include "execution/sema/sema.h"
-
 #include "execution/ast/ast_node_factory.h"
 #include "execution/ast/context.h"
 #include "execution/ast/type.h"
+#include "execution/sema/sema.h"
 
 namespace terrier::execution::sema {
 
@@ -1506,9 +1505,10 @@ void Sema::CheckBuiltinIndexIteratorScan(execution::ast::CallExpr *call, ast::Bu
   switch (builtin) {
     case ast::Builtin::IndexIteratorScanKey:
     case ast::Builtin::IndexIteratorScanAscending:
-    case ast::Builtin::IndexIteratorScanDescending:
+    case ast::Builtin::IndexIteratorScanDescending: {
       if (!CheckArgCount(call, 1)) return;
       break;
+    }
     case ast::Builtin::IndexIteratorScanLimitAscending:
     case ast::Builtin::IndexIteratorScanLimitDescending: {
       if (!CheckArgCount(call, 2)) return;
@@ -1598,27 +1598,31 @@ void Sema::CheckBuiltinPRCall(ast::CallExpr *call, ast::Builtin builtin) {
     case ast::Builtin::PRSetTinyIntNull:
     case ast::Builtin::PRSetSmallIntNull:
     case ast::Builtin::PRSetIntNull:
-    case ast::Builtin::PRSetBigIntNull:
+    case ast::Builtin::PRSetBigIntNull: {
       is_set_call = true;
       sql_type = ast::BuiltinType::Integer;
       break;
+    }
     case ast::Builtin::PRSetReal:
     case ast::Builtin::PRSetDouble:
     case ast::Builtin::PRSetRealNull:
-    case ast::Builtin::PRSetDoubleNull:
+    case ast::Builtin::PRSetDoubleNull: {
       is_set_call = true;
       sql_type = ast::BuiltinType::Real;
       break;
+    }
     case ast::Builtin::PRSetDate:
-    case ast::Builtin::PRSetDateNull:
+    case ast::Builtin::PRSetDateNull: {
       is_set_call = true;
       sql_type = ast::BuiltinType::Date;
       break;
+    }
     case ast::Builtin::PRSetVarlen:
-    case ast::Builtin::PRSetVarlenNull:
+    case ast::Builtin::PRSetVarlenNull: {
       is_set_call = true;
       sql_type = ast::BuiltinType::StringVal;
       break;
+    }
     case ast::Builtin::PRGetTinyInt:
     case ast::Builtin::PRGetSmallInt:
     case ast::Builtin::PRGetInt:
@@ -1626,23 +1630,27 @@ void Sema::CheckBuiltinPRCall(ast::CallExpr *call, ast::Builtin builtin) {
     case ast::Builtin::PRGetTinyIntNull:
     case ast::Builtin::PRGetSmallIntNull:
     case ast::Builtin::PRGetIntNull:
-    case ast::Builtin::PRGetBigIntNull:
+    case ast::Builtin::PRGetBigIntNull: {
       sql_type = ast::BuiltinType::Integer;
       break;
+    }
     case ast::Builtin::PRGetReal:
     case ast::Builtin::PRGetDouble:
     case ast::Builtin::PRGetRealNull:
-    case ast::Builtin::PRGetDoubleNull:
+    case ast::Builtin::PRGetDoubleNull: {
       sql_type = ast::BuiltinType::Real;
       break;
+    }
     case ast::Builtin::PRGetDate:
-    case ast::Builtin::PRGetDateNull:
+    case ast::Builtin::PRGetDateNull: {
       sql_type = ast::BuiltinType::Date;
       break;
+    }
     case ast::Builtin::PRGetVarlen:
-    case ast::Builtin::PRGetVarlenNull:
+    case ast::Builtin::PRGetVarlenNull: {
       sql_type = ast::BuiltinType::StringVal;
       break;
+    }
     default:
       UNREACHABLE("Undefined projected row call!!");
   }
