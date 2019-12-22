34bd763924495480c1f02a8d68503d638d72b020
Tanuj Nayak <tanujnay112@live.com>
2019-12-21 19:23:02 -0800

    added the rest of the columns in pg_language

--- src/include/catalog/postgres/pg_language.h
+++ src/include/catalog/postgres/pg_language.h
@@ -19,16 +19,28 @@ constexpr index_oid_t LANGUAGE_NAME_INDEX_OID = index_oid_t(73);
 
 
 /*
- * Column names of the form "LANG[name]_COL_OID" are present in the PostgreSQL
+ * Column names of the form "LAN[name]_COL_OID" are present in the PostgreSQL
  * catalog specification and columns of the form "ATT_[name]_COL_OID" are
  * terrier-specific addtions (generally pointers to internal objects).
  */
-constexpr col_oid_t LANGOID_COL_OID = col_oid_t(1);     // INTEGER (pkey) [col_oid_t]
-constexpr col_oid_t LANGNAME_COL_OID = col_oid_t(2);    // VARCHAR (skey) [table_oid_t]
+constexpr col_oid_t LANOID_COL_OID = col_oid_t(1);                // INTEGER (pkey) [col_oid_t]
+constexpr col_oid_t LANNAME_COL_OID = col_oid_t(2);               // VARCHAR (skey)
+constexpr col_oid_t LANISPL_COL_OID = col_oid_t(3);               // BOOLEAN (skey)
+constexpr col_oid_t LANPLTRUSTED_COL_OID = col_oid_t(4);          // BOOLEAN (skey)
 
-constexpr uint8_t NUM_PG_LANGUAGE_COLS = 2;
+// TODO(tanujnay112): Make these foreign keys when we implement pg_proc
+constexpr col_oid_t LANPLCALLFOID_COL_OID = col_oid_t(5);         // INTEGER (skey) [proc_oid_t]
+constexpr col_oid_t LANINLINE_COL_OID = col_oid_t(6);             // INTEGER (skey) [proc_oid_t]
+constexpr col_oid_t LANVALIDATOR_COL_OID = col_oid_t(7);          // INTEGER (skey) [proc_oid_t]
+
+// TODO(tanujnay112): This should be of type aclitem[] but we don't have that yet
+constexpr col_oid_t LANACL_COL_OID = col_oid_t(8);                // BOOLEAN (skey)
+
+
+constexpr uint8_t NUM_PG_LANGUAGE_COLS = 8;
 
 constexpr std::array<col_oid_t, NUM_PG_LANGUAGE_COLS> PG_LANGUAGE_ALL_COL_OIDS = {
-    LANGOID_COL_OID, LANGNAME_COL_OID};
+    LANOID_COL_OID, LANNAME_COL_OID, LANISPL_COL_OID, LANPLTRUSTED_COL_OID,
+    LANPLCALLFOID_COL_OID, LANINLINE_COL_OID, LANVALIDATOR_COL_OID, LANACL_COL_OID};
 
 }  // namespace terrier::catalog::postgres
