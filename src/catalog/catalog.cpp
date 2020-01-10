409401fe34a364ac992569f2aefca6803e7145c7
Matt Butrovich <mbutrovich@users.noreply.github.com>
2020-01-09 22:24:37 -0500

    Add `GetBlockStore()` to `Catalog` and `CatalogAccessor`. (#679)

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -314,4 +314,12 @@ std::function<void()> Catalog::DeallocateDatabaseCatalog(DatabaseCatalog *const 
     delete dbc;
   };
 }
+
+common::ManagedPointer<storage::BlockStore> Catalog::GetBlockStore() const {
+  // TODO(Matt): at some point we may decide the Catalog owns this, but right now it doesn't. Taking ownership may
+  // introduce life cycle issues (i.e. guaranteeing that all tables are freed and Blocks returned before this object
+  // gets deleted)
+  return common::ManagedPointer(catalog_block_store_);
+}
+
 }  // namespace terrier::catalog
