e722a995c600bf24412e5017e9636c7fd3921351
Tanuj Nayak <tanujnay112@live.com>
2020-01-10 18:46:11 -0800

    starting Visit FunctionExpression

--- src/binder/bind_node_visitor.cpp
+++ src/binder/bind_node_visitor.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+#include <binder/bind_node_visitor.h>
 
 #include "catalog/catalog_accessor.h"
 #include "catalog/catalog_defs.h"
@@ -306,6 +307,10 @@ void BindNodeVisitor::Visit(parser::InsertStatement *node, parser::ParseResult *
 
   delete context_;
   context_ = nullptr;
+}
+
+void BindNodeVisitor::Visit(parser::FunctionExpression *expr, parser::ParseResult *parse_result) {
+  SqlNodeVisitor::Visit(expr, parse_result);
 }
 
 void BindNodeVisitor::Visit(parser::DropStatement *node, UNUSED_ATTRIBUTE parser::ParseResult *parse_result) {
