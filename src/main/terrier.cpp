1690722708ea600d2976fa63a795e246f6688f11
Matt Butrovich <mbutrovich@gmail.com>
2020-01-18 10:45:41 -0500

    Did not mean to commit terrier.cpp that bootstraps a test table.

--- src/main/terrier.cpp
+++ src/main/terrier.cpp
@@ -40,41 +40,6 @@ int main(int argc, char *argv[]) {
                      .SetUseNetwork(true)
                      .Build();
 
-  auto *txn = db_main->GetTransactionLayer()->GetTransactionManager()->BeginTransaction();
-  const auto db = db_main->GetCatalogLayer()->GetCatalog()->GetDatabaseOid(terrier::common::ManagedPointer(txn),
-                                                                           terrier::catalog::DEFAULT_DATABASE);
-  db_main->GetTransactionLayer()->GetTransactionManager()->Commit(
-      txn, terrier::transaction::TransactionUtil::EmptyCallback, nullptr);
-
-  auto col1 = terrier::catalog::Schema::Column(
-      "col1", terrier::type::TypeId::INTEGER, true,
-      terrier::parser::ConstantValueExpression(
-          terrier::type::TransientValueFactory::GetNull(terrier::type::TypeId::INTEGER)));
-  auto col2 = terrier::catalog::Schema::Column(
-      "col2", terrier::type::TypeId::VARCHAR, 25, true,
-      terrier::parser::ConstantValueExpression(
-          terrier::type::TransientValueFactory::GetNull(terrier::type::TypeId::VARCHAR)));
-  auto table_schema =
-      std::make_unique<terrier::catalog::Schema>(std::vector<terrier::catalog::Schema::Column>{col1, col2});
-
-  txn = db_main->GetTransactionLayer()->GetTransactionManager()->BeginTransaction();
-  auto accessor = db_main->GetCatalogLayer()->GetCatalog()->GetAccessor(terrier::common::ManagedPointer(txn), db);
-
-  terrier::planner::CreateTablePlanNode::Builder builder;
-  auto create_table_node = builder.SetNamespaceOid(accessor->GetDefaultNamespace())
-                               .SetTableSchema(std::move(table_schema))
-                               .SetTableName("foo")
-                               .SetBlockStore(db_main->GetStorageLayer()->GetBlockStore())
-                               .Build();
-  bool result UNUSED_ATTRIBUTE = terrier::execution::sql::DDLExecutors::CreateTableExecutor(
-      terrier::common::ManagedPointer<terrier::planner::CreateTablePlanNode>(create_table_node),
-      terrier::common::ManagedPointer<terrier::catalog::CatalogAccessor>(accessor), db);
-
-  TERRIER_ASSERT(true, "Failed to create table foo.");
-
-  db_main->GetTransactionLayer()->GetTransactionManager()->Commit(
-      txn, terrier::transaction::TransactionUtil::EmptyCallback, nullptr);
-
   db_main->Run();
 
   terrier::LoggersUtil::ShutDown();
