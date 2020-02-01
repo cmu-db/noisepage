60b189099e547dc3753523ac0011dcda4a6e2580
Ian Romines <iromines@andrew.cmu.edu>
2020-01-31 20:43:56 -0500

    Using incorrect variable name formatting since magic numbers were excepted.

--- src/binder/bind_node_visitor.cpp
+++ src/binder/bind_node_visitor.cpp
@@ -35,8 +35,12 @@ void BindNodeVisitor::BindNameToNode(common::ManagedPointer<parser::SQLStatement
 }
 
 void BindNodeVisitor::Visit(parser::SelectStatement *node, parser::ParseResult *parse_result) {
-  int magic_num = 15213;
-  printf("%d", magic_num);
+  int UPPERCASEVARIABLE = 0;
+  int UPPERCASEALSO = 5;
+  UPPERCASEVARIABLE = UPPERCASEALSO + 5;
+
+  BINDER_LOG_TRACE("Bad variable naming usage: %d and %d", UPPERCASEVARIABLE, UPPERCASEALSO);
+
   BINDER_LOG_DEBUG("Visiting SelectStatement ...");
   context_ = new BinderContext(context_);
 
