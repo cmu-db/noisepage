893eb4eb8e40bb3d2b2b19109862c9348cc47bbf
Matt Butrovich <mbutrovich@users.noreply.github.com>
2020-02-10 16:36:16 -0500

    DatabaseCatalog registers and unregisters indexes for GC (#742)

--- src/catalog/postgres/builder.cpp
+++ src/catalog/postgres/builder.cpp
@@ -1,9 +1,10 @@
+#include "catalog/postgres/builder.h"
+
 #include <utility>
 #include <vector>
 
 #include "catalog/database_catalog.h"
 #include "catalog/index_schema.h"
-#include "catalog/postgres/builder.h"
 #include "catalog/postgres/pg_attribute.h"
 #include "catalog/postgres/pg_class.h"
 #include "catalog/postgres/pg_constraint.h"
@@ -73,8 +74,10 @@ IndexSchema Builder::GetDatabaseNameIndexSchema() {
   return schema;
 }
 
-DatabaseCatalog *Builder::CreateDatabaseCatalog(storage::BlockStore *block_store, db_oid_t oid) {
-  auto dbc = new DatabaseCatalog(oid);
+DatabaseCatalog *Builder::CreateDatabaseCatalog(
+    const common::ManagedPointer<storage::BlockStore> block_store, const db_oid_t oid,
+    const common::ManagedPointer<storage::GarbageCollector> garbage_collector) {
+  auto dbc = new DatabaseCatalog(oid, garbage_collector);
 
   dbc->namespaces_ = new storage::SqlTable(block_store, Builder::GetNamespaceTableSchema());
   dbc->classes_ = new storage::SqlTable(block_store, Builder::GetClassTableSchema());
--- test/include/test_util/tpcc/util.h
+++ test/include/test_util/tpcc/util.h
@@ -18,34 +18,6 @@ namespace terrier::tpcc {
 struct Util {
   Util() = delete;
 
-  static void RegisterIndexesForGC(const common::ManagedPointer<storage::GarbageCollector> gc,
-                                   const common::ManagedPointer<Database> tpcc_db) {
-    gc->RegisterIndexForGC(tpcc_db->item_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->warehouse_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->stock_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->district_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->customer_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->customer_secondary_index_);
-    gc->RegisterIndexForGC(tpcc_db->new_order_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->order_primary_index_);
-    gc->RegisterIndexForGC(tpcc_db->order_secondary_index_);
-    gc->RegisterIndexForGC(tpcc_db->order_line_primary_index_);
-  }
-
-  static void UnregisterIndexesForGC(const common::ManagedPointer<storage::GarbageCollector> gc,
-                                     const common::ManagedPointer<Database> tpcc_db) {
-    gc->UnregisterIndexForGC(tpcc_db->item_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->warehouse_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->stock_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->district_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->customer_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->customer_secondary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->new_order_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->order_primary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->order_secondary_index_);
-    gc->UnregisterIndexForGC(tpcc_db->order_line_primary_index_);
-  }
-
   static std::vector<catalog::col_oid_t> AllColOidsForSchema(const catalog::Schema &schema) {
     const auto &cols = schema.GetColumns();
     std::vector<catalog::col_oid_t> col_oids;
