d8c79ccd0bc9b3a30285bc3f51cbaaa310718a96
Matt Butrovich <mbutrovich@gmail.com>
2020-02-19 09:14:28 -0500

    format.

--- src/include/storage/index/bplustree.h
+++ src/include/storage/index/bplustree.h
@@ -7,7 +7,6 @@ namespace terrier::storage::index {
 template <typename KeyType, typename ValueType, typename KeyComparator = std::less<KeyType>,
           typename KeyEqualityChecker = std::equal_to<KeyType>, typename KeyHashFunc = std::hash<KeyType>,
           typename ValueEqualityChecker = std::equal_to<ValueType>>
-class BPlusTree {
-};
+class BPlusTree {};
 
 }  // namespace terrier::storage::index
