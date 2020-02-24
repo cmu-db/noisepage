e1e19ce6405b2099efd28520970adbad17c17d39
Lin Ma <malin1993ml@gmail.com>
2020-02-24 17:49:52 -0500

    Add back the benchmark compilation flag.

--- benchmark/runner/tpch_runner.cpp
+++ benchmark/runner/tpch_runner.cpp
@@ -10,7 +10,7 @@ class TPCHRunner : public benchmark::Fixture {
  public:
   const int8_t num_threads_ = 1;                        // defines the number of terminals (workers threads)
   const uint32_t num_precomputed_txns_per_worker_ = 4;  // Number of txns to run per terminal (worker thread)
-  const execution::vm::ExecutionMode mode_ = execution::vm::ExecutionMode::Interpret;
+  const execution::vm::ExecutionMode mode_ = execution::vm::ExecutionMode::Compiled;
 
   std::unique_ptr<DBMain> db_main_;
   std::unique_ptr<tpch::Workload> tpch_workload_;
