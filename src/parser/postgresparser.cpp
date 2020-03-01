ec6e9856a009eda44e2939055e529eab04e0f870
tpan496 <tianleip@andrew.cmu.edu>
2020-02-29 21:47:07 -0500

    comments

--- src/parser/postgresparser.cpp
+++ src/parser/postgresparser.cpp
@@ -529,6 +529,8 @@ std::unique_ptr<AbstractExpression> PostgresParser::ColumnRefTransform(ParseResu
       } else {
         auto next_node = reinterpret_cast<Node *>(fields->head->next->data.ptr_value);
         if (next_node->type == T_A_Star) {
+          // Can't directly return StarExpression in this case, because there is a table qualifier. We only want all the
+          // columns for the given table. Consider SELECT x.* FROM y.
           col_name = "*";
         } else {
           col_name = reinterpret_cast<value *>(next_node)->val_.str_;
