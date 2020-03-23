6ec256b3c8a336ec154d48dca39c36d0434d8dd2
Daniel Biales <bialesdaniel@gmail.com>
2020-03-23 13:21:56 -0400

    changing the way we specify the test output file

--- script/testing/junit/constants.py
+++ script/testing/junit/constants.py
@@ -6,3 +6,4 @@ from util.constants import DIR_TESTING
 JUNIT_TEST_DIR = os.path.join(DIR_TESTING, "junit")
 JUNIT_TEST_COMMAND = "ant testconsole"
 JUNIT_TEST_ERROR_MSG = "Error: failed to complete junit test"
+JUNIT_OUTPUT_FILE = "/tmp/junit_log.txt"
