d324cbd1803c960850d23ca431c7a9143b8ec412
Deepayan Patra <dpatra@andrew.cmu.edu>
2020-03-27 15:58:33 -0400

    linked numa?

--- third_party/numactl/clearcache.h
+++
@@ -1,1 +0,0 @@
-void clearcache(unsigned char *mem, unsigned size);
--- third_party/numactl/stream_lib.h
+++
@@ -1,8 +0,0 @@
-long stream_memsize(void);
-long stream_init(void *mem);
-#define STREAM_NRESULTS 4
-void stream_test(double *res);
-void stream_check(void);
-void stream_setmem(unsigned long size);
-extern int stream_verbose;
-extern char *stream_names[];
