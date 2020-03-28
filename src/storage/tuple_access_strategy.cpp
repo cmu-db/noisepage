bcf2ba95d930f7775b26fdf0258a883324fc95ec
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-03-28 13:48:51 -0400

    working on linux

--- src/storage/tuple_access_strategy.cpp
+++ src/storage/tuple_access_strategy.cpp
@@ -29,7 +29,7 @@ void TupleAccessStrategy::InitializeRawBlock(storage::DataTable *const data_tabl
   // Intentional unsafe cast
   raw->data_table_ = data_table;
 #ifndef __APPLE__
-  if (move_pages(0, 1, &raw, NULL, static_cast<void **>(&raw->numa_region_), 0) == -1)
+  if (move_pages(0, 1, &raw, NULL, &raw->numa_region_, 0) == -1)
     raw->numa_region_ = UNSUPPORTED_NUMA_REGION;
 #else
   raw->numa_region_ = UNSUPPORTED_NUMA_REGION;
