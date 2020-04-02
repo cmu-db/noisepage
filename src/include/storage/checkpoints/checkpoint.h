a14ef44428d0aa5f76ea7683095dd4d0258d7f56
Xuanxuan Ge <xuanxuag@andrew.cmu.edu>
2020-04-02 14:56:52 -0400

    Write varlen data to disk

--- src/include/storage/checkpoints/checkpoint.h
+++ src/include/storage/checkpoints/checkpoint.h
@@ -30,13 +30,23 @@ class Checkpoint {
     catalog_table_schemas_[catalog::postgres::TYPE_TABLE_OID] = catalog::postgres::Builder::GetTypeTableSchema();
   }
 
-
+  /**
+   * Take checkpoint of a database
+   * @tparam path the path on disk to save the checkpoint
+   * @param db the database to take the checkpoint of
+   * @return True if succuessully take the checkpoint, False otherwise
+   */
   bool TakeCheckpoint(const std::string &path, catalog::db_oid_t db);
 
+  /**
+   * Generate a file name for a table
+   * @tparam db_oid the oid of the database the table belongs to
+   * @param tb_oid the oid of the table
+   * @return a file name in the format db_oid-tb_oid.txt
+   */
   static std::string GenFileName(catalog::db_oid_t db_oid, catalog::table_oid_t tb_oid){
     return std::to_string((uint32_t)db_oid) + "-" + std::to_string((uint32_t)tb_oid);
   }
-
 
  private:
   // Catalog to fetch table pointers
@@ -47,6 +57,14 @@ class Checkpoint {
   std::vector<catalog::table_oid_t> queue; // for multithreading
   std::mutex queue_latch;
 
+
+  /**
+   * Write the data of a database to disk in parallel, called by TakeCheckpoint()
+   * @tparam path the path on disk to save the checkpoint
+   * @param accessor catalog accessor of the given database
+   * @param db_oid the databse to be checkpointed
+   * @return None
+   */
   void WriteToDisk(const std::string &path, const std::unique_ptr<catalog::CatalogAccessor> &accessor, catalog::db_oid_t db_oid);
 
 
--- src/storage/checkpoints/checkpoint.cpp
+++ src/storage/checkpoints/checkpoint.cpp
@@ -5,8 +5,9 @@
 #include <fstream>
 namespace terrier::storage{
 
+
 bool Checkpoint::TakeCheckpoint(const std::string &path, catalog::db_oid_t db) {
-  // TODO (Xuanxuan): fake code for build purpose, change later
+  // get db catalog accessor
   auto accessor = catalog_->GetAccessor(txn_, db);
   std::unordered_set<catalog::table_oid_t> table_oids = accessor->GetAllTableOids();
 
@@ -49,16 +50,22 @@ void Checkpoint::WriteToDisk(const std::string &path, const std::unique_ptr<cata
     common::ManagedPointer<storage::SqlTable> curr_table = accessor->GetTable(curr_table_oid);
     std::string out_file = GenFileName(db_oid, curr_table_oid);
     std::ofstream f;
+
+    auto curr_data_table = curr_table->table_.data_table_;
+    const BlockLayout &layout = curr_data_table->GetBlockLayout();
+    const std::vector<col_id_t> varlens = layout.Varlens();
+    auto column_ids = layout.AllColumns();
+    size_t col_num = layout.NumColumns();
+
+    curr_data_table->blocks_latch_.Lock();
+    std::list<RawBlock *> blocks = curr_data_table->blocks_;
+    curr_data_table->blocks_latch_.Unlock();
     f.open(path + out_file, std::ios::binary);
 
-    // write block contents (in bytes) to the file
+    // write # of varlen col in the table
     if (f.is_open()){
-
-      for (auto &block : curr_table->table_.data_table_->blocks_){
-        f.write(reinterpret_cast<const char *>(block->content_), sizeof(block->content_));
-      }
-      f.close();
-
+      unsigned long var_col_num = varlens.size();
+      f.write(reinterpret_cast<const char *>(&var_col_num), sizeof(unsigned long));
     } else {
       // failed to copy to disk, add the table_oid back to queue
       queue_latch.lock();
@@ -67,7 +74,44 @@ void Checkpoint::WriteToDisk(const std::string &path, const std::unique_ptr<cata
       return;
     }
 
+    // traverse each block in the block list
+    for (RawBlock *block : blocks){
+      ArrowBlockMetadata &metadata = curr_data_table->accessor_.GetArrowBlockMetadata(block);
+
+      // traverse all cols
+      for (auto i = 0u; i < col_num; i++){
+        col_id_t col_id = column_ids[i];
+        ArrowColumnInfo &col_info = metadata.GetColumnInfo(layout, col_id);
+
+        // if varlength or dictionary
+        if (layout.IsVarlen(col_id) && !(col_info.Type() == ArrowColumnType::FIXED_LENGTH)){
+          switch (col_info.Type()){
+            case ArrowColumnType::GATHERED_VARLEN:{
+              ArrowVarlenColumn &varlen_col = col_info.VarlenColumn();
+              uint32_t value_len = varlen_col.ValuesLength();
+              uint32_t offset_len = varlen_col.OffsetsLength();
+              f.write(reinterpret_cast<const char *>(&col_id), sizeof(col_id_t));
+              f.write(reinterpret_cast<const char *>(&value_len), sizeof(uint32_t));
+              f.write(reinterpret_cast<const char *>(&offset_len), sizeof(uint32_t));
+              f.write(reinterpret_cast<const char *>(varlen_col.Offsets()), offset_len*sizeof(uint64_t));
+              f.write(reinterpret_cast<const char *>(varlen_col.Values()), value_len);
+            }
+            case ArrowColumnType::DICTIONARY_COMPRESSED:{
+              continue;
+            }
+            default:
+              throw std::runtime_error("unexpected control flow");
+          }
+
+        }
+
+      }
+      // write block contents
+      f.write(reinterpret_cast<const char *>(block->content_), sizeof(block->content_));
+    }
+
   }
 }
+
 
 } //namespace terrier::storage
