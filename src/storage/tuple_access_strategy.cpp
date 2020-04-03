8055d12bbaa75ff443e564842fba2f6981c48890
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-04-03 16:03:01 -0400

    working on linux

--- src/storage/tuple_access_strategy.cpp
+++ src/storage/tuple_access_strategy.cpp
@@ -32,7 +32,7 @@ void TupleAccessStrategy::InitializeRawBlock(storage::DataTable *const data_tabl
 #ifndef __APPLE__
   auto page = static_cast<void*>(raw);
   int status;
-  raw->numa_region_ = move_pages(0, 1, &page, NULL, &status, 0) == -1 ? UNSUPPORTED_NUMA_REGION :
+  raw->numa_region_ = numa_supported() == -1 || move_pages(0, 1, &page, NULL, &status, 0) == -1 ? UNSUPPORTED_NUMA_REGION :
                                                               static_cast<numa_region_t>(static_cast<int16_t>(status));
 #else
   raw->numa_region_ = UNSUPPORTED_NUMA_REGION;
