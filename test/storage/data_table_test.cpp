8552b7750651895705b96af6e3fd99222924f922
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-04-03 20:55:54 -0400

    working on linux

--- test/storage/data_table_test.cpp
+++ test/storage/data_table_test.cpp
@@ -376,7 +376,7 @@ TEST_F(DataTableTests, SimpleNumaTest) {
 #else
     for (uint64_t i = 0; i < numa_regions.size(); i++) {
       if (numa_available() != -1) {
-        std::cout << numa_available() << std::endl;
+        std::cout << numa_available() << ", " << move_pages(0, 1, &page, NULL, &status, 0) <<  std::endl;
         EXPECT_TRUE(numa_regions[i] != storage::UNSUPPORTED_NUMA_REGION);
       }
     }
