c6a45020ddc6a36dd0d3e3564e3ea3d4939d22fe
Deepayan Patra <dpatra@andrew.cmu.edu>
2020-04-06 16:43:36 -0400

    stronger condition

--- src/include/common/execution_thread_pool.h
+++ src/include/common/execution_thread_pool.h
@@ -99,7 +99,7 @@ class ExecutionThreadPool : DedicatedThreadOwner {
       CPU_SET(cpu_id_, &mask);
       int result = sched_setaffinity(0, sizeof(cpu_set_t), &mask);
       TERRIER_ASSERT(result == 0, "sched_setaffinity should succeed");
-      if (numa_available() >= 0) {
+      if (numa_available() >= 0 && numa_max_node() >= 0) {
         numa_region_ = static_cast<storage::numa_region_t>(numa_node_of_cpu(cpu_id));
         TERRIER_ASSERT(static_cast<int16_t>(numa_region_) >= 0, "cpu_id must be valid");
       } else {
