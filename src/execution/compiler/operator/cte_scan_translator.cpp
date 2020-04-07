6015a11f8ea5a94ded9adfad2dad52db2158e9e0
rohanaggarwal <aggarwal.rohan@yahoo.co.in>
2020-04-07 16:43:11 -0400

    fStarting to add optimizer support

--- src/execution/compiler/operator/cte_scan_translator.cpp
+++ src/execution/compiler/operator/cte_scan_translator.cpp
@@ -8,6 +8,11 @@ void CteScanTranslator::Produce(FunctionBuilder *builder) {
 }
 
 void CteScanTranslator::Consume(FunctionBuilder *builder) {
+
+  // TODO(Gautam, Preetansh, Rohan)
+  // Right now we are only generating @CteScanNext(@slot_bottom_query) in the tpl
+  // What we need is @slot4 = @CteScanInsert(@slot_bottom_query)
+  //builder->Append(codegen_->MakeStmt(codegen_->OneArgCall(ast::Builtin::CteScanNext, child_translator_->GetSlot())));
   parent_translator_->Consume(builder);
 }
 }  // namespace terrier::execution::compiler
