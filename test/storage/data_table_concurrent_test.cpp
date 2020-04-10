3cd69ad29ae8c17af9049576cbbf43a45a75ddae
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-04-10 10:54:05 -0400

    fix compile on linux

--- test/storage/data_table_concurrent_test.cpp
+++ test/storage/data_table_concurrent_test.cpp
@@ -97,7 +97,8 @@ TEST_F(DataTableConcurrentTests, ConcurrentInsert) {
 
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     storage::BlockLayout layout = StorageTestUtil::RandomLayoutNoVarlen(max_columns, &generator_);
-    storage::DataTable tested(common::ManagedPointer<storage::BlockStore>(&block_store_), layout, storage::layout_version_t(0));
+    storage::DataTable tested(common::ManagedPointer<storage::BlockStore>(&block_store_), layout,
+                              storage::layout_version_t(0));
     std::vector<std::unique_ptr<FakeTransaction>> fake_txns;
     for (uint32_t thread = 0; thread < num_threads; thread++)
       // timestamps are irrelevant for inserts
@@ -136,7 +137,8 @@ TEST_F(DataTableConcurrentTests, ConcurrentUpdateOneWriterWins) {
 
   for (uint32_t iteration = 0; iteration < num_iterations; iteration++) {
     storage::BlockLayout layout = StorageTestUtil::RandomLayoutNoVarlen(max_columns, &generator_);
-    storage::DataTable tested(common::ManagedPointer<storage::BlockStore>(&block_store_), layout, storage::layout_version_t(0));
+    storage::DataTable tested(common::ManagedPointer<storage::BlockStore>(&block_store_), layout,
+                              storage::layout_version_t(0));
     FakeTransaction insert_txn(layout, &tested, null_ratio_(generator_), transaction::timestamp_t(0),
                                transaction::timestamp_t(1), &buffer_pool_);
     // Insert one tuple, the timestamp needs to show committed
