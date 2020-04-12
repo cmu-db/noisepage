e25669831834de460bd3a003d3cabeedf5a6b83e
gautam20197 <gautam20197@gmail.com>
2020-04-11 20:43:11 -0400

    Changes to the cte scan h file

--- src/include/execution/sql/cte_scan_iterator.h
+++ src/include/execution/sql/cte_scan_iterator.h
@@ -3,7 +3,8 @@
 #include <memory>
 #include <vector>
 #include "storage/sql_table.h"
-#include "parser/expression/constant_value_expression.h"
+#include "execution/exec/execution_context.h"
+#include "execution/sql/table_vector_iterator.h"
 
 namespace terrier::execution::sql {
 
@@ -12,33 +13,36 @@ class CteScanIterator {
   /**
    * Constructor for the CTEScanIterator
    */
-  parser::ConstantValueExpression DummyCVE() {
-    return parser::ConstantValueExpression(type::TransientValueFactory::GetInteger(0));
-  }
 
   CteScanIterator(terrier::execution::exec::ExecutionContext *exec_ctx,
-                  uint32_t *schema_cols_type, uint32_t num_schema_cols) : cte_table_oid_(static_cast<catalog::table_oid_t>(999))
-  {
-    // Create column metadata for every column.
-    std::vector<catalog::Schema::Column> all_columns;
-    for (uint32_t i = 0; i < num_schema_cols; i++) {
-      catalog::Schema::Column col("col" + std::to_string(i+1),
-                                  static_cast<type::TypeId>(schema_cols_type[i]), false,
-                                  DummyCVE(), static_cast<catalog::col_oid_t>(i+1));
-      all_columns.push_back(col);
-    }
-
-    // Create the table in the catalog.
-    catalog::Schema cte_table_schema(all_columns);
-    cte_table_ = new storage::SqlTable(exec_ctx->GetAccessor()->GetBlockStore(), cte_table_schema);
-  }
+                  uint32_t *schema_cols_type, uint32_t num_schema_cols);
 
   /**
    * Returns the temporary table that the cte has made
    */
-  storage::SqlTable* GetTable() {
-    return cte_table_;
-  }
+  storage::SqlTable* GetTable();
+
+  /**
+   * Returns the oid of the temporary table that the cte has made
+   */
+  catalog::table_oid_t GetTableOid();
+
+  /**
+   * Returns a projected row of the table for insertion
+   */
+  storage::ProjectedRow *GetInsertTempTablePR();
+
+  /**
+   * Returns the slot which was inserted in the table using the projected row
+   */
+
+  storage::TupleSlot TableInsert();
+
+  /**
+   * Return the iterator that will scan the temp table
+   */
+
+
 
   /**
    * Destructor
@@ -61,8 +65,11 @@ class CteScanIterator {
 
 
  private:
+  terrier::execution::exec::ExecutionContext *exec_ctx_;
   storage::SqlTable* cte_table_;
   catalog::table_oid_t cte_table_oid_;
+  std::vector<catalog::col_oid_t> col_oids_;
+  storage::RedoRecord *table_redo_;
 };
 
 }  // namespace terrier::execution::sql
