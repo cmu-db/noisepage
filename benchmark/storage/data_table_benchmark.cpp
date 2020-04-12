89b09624472c556e1e52a7979f4f701f420c57a5
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-04-12 16:27:21 -0400

    allow for os to move blocks during tests

--- benchmark/storage/data_table_benchmark.cpp
+++ benchmark/storage/data_table_benchmark.cpp
@@ -317,7 +317,7 @@ BENCHMARK_DEFINE_F(DataTableBenchmark, NUMASingleThreadedIteration)(benchmark::S
   std::vector<storage::TupleSlot> read_order;
 
   // inserted the table with 10 million rows
-  for (uint32_t i = 0; i < 10; ++i) {
+  for (uint32_t i = 0; i < num_reads_; ++i) {
     read_order.emplace_back(read_table.Insert(common::ManagedPointer(&txn), *redo_));
   }
 
