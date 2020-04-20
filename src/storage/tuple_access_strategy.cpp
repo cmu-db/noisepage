88816c040f36d2a213792216c042c53d70d71044
Ricky Zhou <zhou0ricky@gmail.com>
2020-04-20 19:18:02 -0400

    added commmon namespace to numa variables

--- src/storage/tuple_access_strategy.cpp
+++ src/storage/tuple_access_strategy.cpp
@@ -33,8 +33,8 @@ void TupleAccessStrategy::InitializeRawBlock(storage::DataTable *const data_tabl
   auto page = static_cast<void *>(raw);
   int status;
   raw->numa_region_ = numa_available() == -1 || move_pages(0, 1, &page, nullptr, &status, 0) == -1
-                          ? UNSUPPORTED_NUMA_REGION
-                          : static_cast<numa_region_t>(static_cast<int16_t>(status));
+                          ? common::UNSUPPORTED_NUMA_REGION
+                          : static_cast<common::numa_region_t>(static_cast<int16_t>(status));
 #else
   raw->numa_region_ = common::UNSUPPORTED_NUMA_REGION;
 #endif
