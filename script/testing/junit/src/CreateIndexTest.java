79dc742ad44488289aea6b69d94887f863daaf72
Cal Lavicka <cal.lavicka@gmail.com>
2020-05-04 16:36:33 -0400

    Upgraded java test

--- script/testing/junit/src/CreateIndexTest.java
+++ script/testing/junit/src/CreateIndexTest.java
@@ -13,8 +13,10 @@ import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("ALL")
 public class CreateIndexTest extends TestUtility {
-    private Connection conn1;
-    private Connection conn2;
+    private static final int NUM_EXTRA_THREADS = 10;
+
+    private Connection conn;
+    private Connection[] thread_conn = new Connection[NUM_EXTRA_THREADS];
 
     private static final String SQL_DROP_TABLE =
             "DROP TABLE IF EXISTS tbl;";
@@ -29,7 +31,7 @@ public class CreateIndexTest extends TestUtility {
      * Initialize the database and table for testing
      */
     private void initDatabase() throws SQLException {
-        Statement stmt = conn1.createStatement();
+        Statement stmt = conn.createStatement();
         stmt.execute(SQL_DROP_TABLE);
         stmt.execute(SQL_CREATE_TABLE);
     }
@@ -41,10 +43,12 @@ public class CreateIndexTest extends TestUtility {
     @Before
     public void setup() throws SQLException {
         try {
-            conn1 = makeDefaultConnection();
-            conn1.setAutoCommit(true);
-            conn2 = makeDefaultConnection();
-            conn2.setAutoCommit(true);
+            conn = makeDefaultConnection();
+            conn.setAutoCommit(true);
+            for (int i = 0; i < NUM_EXTRA_THREADS; i++) {
+                thread_conn[i] = makeDefaultConnection();
+                thread_conn[i].setAutoCommit(true);
+            }
             initDatabase();
         } catch (SQLException e) {
             DumpSQLException(e);
@@ -58,17 +62,17 @@ public class CreateIndexTest extends TestUtility {
     @After
     public void teardown() throws SQLException {
         try {
-            Statement stmt = conn1.createStatement();
+            Statement stmt = conn.createStatement();
             stmt.execute(SQL_DROP_TABLE);
         } catch (SQLException e) {
             DumpSQLException(e);
         } finally {
             try {
-                if (conn1 != null) {
-                    conn1.close();
+                if (conn != null) {
+                    conn.close();
                 }
-                if (conn2 != null) {
-                    conn2.close();
+                for (int i = 0; i < NUM_EXTRA_THREADS; i++) {
+                    thread_conn[i].close();
                 }
             } catch (SQLException e) {
                 DumpSQLException(e);
@@ -87,7 +91,7 @@ public class CreateIndexTest extends TestUtility {
     @Test
     public void testSimpleCreate() throws SQLException {
         String sql = "INSERT INTO tbl VALUES (1, 2, 100), (5, 6, 100), (3, 4, 100);";
-        Statement stmt = conn1.createStatement();
+        Statement stmt = conn.createStatement();
         stmt.execute(sql);
         stmt.execute("CREATE INDEX tbl_ind ON tbl (c2)");
         ResultSet rs = stmt.executeQuery("SELECT * FROM tbl WHERE c2 > 0 ORDER BY c2 ASC");
@@ -106,35 +110,37 @@ public class CreateIndexTest extends TestUtility {
     @Test
     public void testWriteBlocking() throws SQLException, InterruptedException {
         String sql = "INSERT INTO tbl VALUES (1, 2, 100), (5, 6, 100);";
-        Statement stmt = conn1.createStatement();
-        int num_rows = 100000;
+        Statement stmt = conn.createStatement();
+        int num_rows = 1000;
         for (int i = 0; i < num_rows; i++) {
-            try {
-                stmt.execute(sql);
-            } catch (SQLException e) {
-                throw e;
-            }
+            stmt.execute(sql);
         }
-        Thread t2 = new Thread(() -> {
-            try {
-                Statement stmt2 = conn2.createStatement();
-                for (int i = 0; i < num_rows; i++) {
-                    stmt2.execute("INSERT INTO tbl VALUES (3, 4, 200);");
+        Thread[] threads = new Thread[NUM_EXTRA_THREADS];
+        for (int i = 0; i < NUM_EXTRA_THREADS; i++) {
+            final Connection conn2 = thread_conn[i];
+            threads[i] = new Thread(() -> {
+                try {
+                    Statement stmt2 = conn2.createStatement();
+                    for (int j = 0; j < num_rows; j++) {
+                        stmt2.execute("INSERT INTO tbl VALUES (3, 4, 200);");
+                    }
+                } catch(SQLException e) {
+                    DumpSQLException(e);
+                    Assert.fail();
                 }
-            } catch(SQLException e) {
-                DumpSQLException(e);
-                Assert.fail();
-            }
-        });
-        t2.start();
+            });
+            threads[i].start();
+        }
         stmt.execute("CREATE INDEX tbl_ind ON tbl (c2)");
-        t2.join();
+        for (Thread t : threads) {
+            t.join();
+        }
         ResultSet rs = stmt.executeQuery("SELECT * FROM tbl WHERE c2 > 0 ORDER BY c2 ASC");
         for (int i = 0; i < num_rows; i++) {
             rs.next();
             checkIntRow(rs, new String [] {"c1", "c2", "c3"}, new int [] {1, 2, 100});
         }
-        for (int i = 0; i < num_rows; i++) {
+        for (int i = 0; i < num_rows * NUM_EXTRA_THREADS; i++) {
             rs.next();
             checkIntRow(rs, new String [] {"c1", "c2", "c3"}, new int [] {3, 4, 200});
         }
