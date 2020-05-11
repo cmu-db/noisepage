dda52fc3fe0d728dbb5a97ea2724ba463640378a
orankarl <895168349@qq.com>
2020-05-11 15:58:04 -0400

    Add MarkJoinGetToInnerJoinTest & PullFilterThroughMarkJoinAndMarkJoinToInnerJoinTest & PullFilterThroughAggregationTest

--- test/optimizer/optimizer_rewrite_test.cpp
+++ test/optimizer/optimizer_rewrite_test.cpp
@@ -33,7 +33,7 @@ TEST_F(OptimizerRewriteTest, PushImplicitFilterThroughJoinAndEmbedFilterIntoGetT
   auto task_stack = new OptimizerTaskStack();
   optimizer_context->SetTaskPool(task_stack);
 
-  // Create OperatorNode of (GET A) INNERJOIN (GET B), INNERJOIN has two predicates
+  // Create OperatorNode of (GET A) INNER_JOIN (GET B), INNER_JOIN has two predicates
   std::vector<std::unique_ptr<OperatorNode>> lg_children;
   auto left_get = std::make_unique<OperatorNode>(
     LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
@@ -77,7 +77,7 @@ TEST_F(OptimizerRewriteTest, PushImplicitFilterThroughJoinAndEmbedFilterIntoGetT
     delete task;
   }
 
-  // Expected OperatorNode: (GET A) INNERJOIN (GET B), while pred1 is in GET A and pred2 is in GET B
+  // Expected OperatorNode: (GET A) INNER_JOIN (GET B), while pred1 is in GET A and pred2 is in GET B
   auto root_gexpr = optimizer_context->GetMemo().GetGroupByID(root_id)->GetLogicalExpression();
   EXPECT_EQ(root_gexpr->Op().GetType(), OpType::LOGICALINNERJOIN);
   EXPECT_EQ(root_gexpr->GetChildrenGroupsSize(), 2);
@@ -110,7 +110,7 @@ TEST_F(OptimizerRewriteTest, PushExplicitFilterThroughJoinAndEmbedFilterIntoGetT
   auto task_stack = new OptimizerTaskStack();
   optimizer_context->SetTaskPool(task_stack);
 
-  // Create OperatorNode of FILTER((GET A) INNERJOIN (GET B)), FILTER has two predicates
+  // Create OperatorNode of FILTER((GET A) INNER_JOIN (GET B)), FILTER has two predicates
   std::vector<std::unique_ptr<OperatorNode>> lg_children;
   auto left_get = std::make_unique<OperatorNode>(
     LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
@@ -159,7 +159,7 @@ TEST_F(OptimizerRewriteTest, PushExplicitFilterThroughJoinAndEmbedFilterIntoGetT
     delete task;
   }
 
-  // Expected OperatorNode: (GET A) INNERJOIN (GET B), while pred1 is in GET A and pred2 is in GET B
+  // Expected OperatorNode: (GET A) INNER_JOIN (GET B), while pred1 is in GET A and pred2 is in GET B
   auto root_gexpr = optimizer_context->GetMemo().GetGroupByID(root_id)->GetLogicalExpression();
   EXPECT_EQ(root_gexpr->Op().GetType(), OpType::LOGICALINNERJOIN);
   EXPECT_EQ(root_gexpr->GetChildrenGroupsSize(), 2);
@@ -192,7 +192,7 @@ TEST_F(OptimizerRewriteTest, InnerJoinToSemiJoinTest) {
   auto task_stack = new OptimizerTaskStack();
   optimizer_context->SetTaskPool(task_stack);
 
-  // Create OperatorNode of FILTER((GET A) INNERJOIN (GET B)), FILTER has three predicates and one of them is COMPARE_IN
+  // Create OperatorNode of FILTER((GET A) INNER_JOIN (GET B)), FILTER has three predicates and one of them is COMPARE_IN
   std::vector<std::unique_ptr<OperatorNode>> lg_children;
   auto left_get = std::make_unique<OperatorNode>(
     LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
@@ -475,6 +475,63 @@ TEST_F(OptimizerRewriteTest, EmbedFilterIntoGetTest) {
 }
 
 // NOLINTNEXTLINE
+TEST_F(OptimizerRewriteTest, MarkJoinGetToInnerJoinTest) {
+  auto optimizer_context = new OptimizerContext(nullptr);
+  auto optimization_context = new OptimizationContext(optimizer_context, nullptr);
+  optimizer_context->AddOptimizationContext(optimization_context);
+
+  // Setup the task stack
+  auto task_stack = new OptimizerTaskStack();
+  optimizer_context->SetTaskPool(task_stack);
+
+  // Create OperatorNode of (GET A) MARK_JOIN (GET A)
+  std::vector<std::unique_ptr<OperatorNode>> children;
+  auto left_get = std::make_unique<OperatorNode>(
+    LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl", false),
+    std::move(children));
+
+  auto right_get = left_get->Copy();
+  EXPECT_EQ(*left_get, *right_get);
+
+  std::vector<std::unique_ptr<OperatorNode>> join_children;
+  join_children.emplace_back(std::move(left_get));
+  join_children.emplace_back(std::move(right_get));
+  auto join = std::make_unique<OperatorNode>(LogicalMarkJoin::Make(), std::move(join_children));
+
+  // RecordOperatorNodeIntoGroup
+  GroupExpression *join_gexpr;
+  EXPECT_TRUE(optimizer_context->RecordOperatorNodeIntoGroup(common::ManagedPointer(join), &join_gexpr));
+  EXPECT_TRUE(join_gexpr != nullptr);
+
+  // Add rewrite tasks
+  group_id_t root_id = join_gexpr->GetGroupID();
+  task_stack->Push(new BottomUpRewrite(root_id, optimization_context, RuleSetName::UNNEST_SUBQUERY, false));
+
+  // Execute the tasks in stack
+  // MARK_JOIN_GET_TO_INNER_JOIN will be applied
+  while (!task_stack->Empty()) {
+    auto task = task_stack->Pop();
+    task->Execute();
+    delete task;
+  }
+
+  // Expected OperatorNode: (GET A) INNER_JOIN (GET A)
+  auto root_gexpr = optimizer_context->GetMemo().GetGroupByID(root_id)->GetLogicalExpression();
+  EXPECT_EQ(root_gexpr->Op().GetType(), OpType::LOGICALINNERJOIN);
+  EXPECT_EQ(root_gexpr->GetChildrenGroupsSize(), 2);
+
+  auto left_get_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(root_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(left_get_gexpr->Op().GetType(), OpType::LOGICALGET);
+
+  auto right_get_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(root_gexpr->GetChildGroupId(1) )->GetLogicalExpression();
+  EXPECT_EQ(right_get_gexpr->Op().GetType(), OpType::LOGICALGET);
+
+  delete(optimizer_context);
+}
+
+// NOLINTNEXTLINE
 TEST_F(OptimizerRewriteTest, SingleJoinGetToInnerJoinTest) {
   auto optimizer_context = new OptimizerContext(nullptr);
   auto optimization_context = new OptimizationContext(optimizer_context, nullptr);
@@ -542,7 +599,7 @@ TEST_F(OptimizerRewriteTest, SingleJoinGetToInnerJoinTest) {
 }
 
 // NOLINTNEXTLINE
-TEST_F(OptimizerRewriteTest, DependentJoinGetToInnerJoin) {
+TEST_F(OptimizerRewriteTest, DependentJoinGetToInnerJoinTest) {
   auto optimizer_context = new OptimizerContext(nullptr);
   auto optimization_context = new OptimizationContext(optimizer_context, nullptr);
   optimizer_context->AddOptimizationContext(optimization_context);
@@ -552,16 +609,16 @@ TEST_F(OptimizerRewriteTest, DependentJoinGetToInnerJoin) {
   optimizer_context->SetTaskPool(task_stack);
 
   // Create OperatorNode of (GET A) SINGLE_JOIN (FILTER (AGGREGATE (GET B)))
-  std::vector<std::unique_ptr<OperatorNode>> left_children;
+  std::vector<std::unique_ptr<OperatorNode>> lg_children;
   auto left_get = std::make_unique<OperatorNode>(
     LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
-    std::move(left_children));
+    std::move(lg_children));
   auto lg_copy = left_get->Copy();
 
-  std::vector<std::unique_ptr<OperatorNode>> right_children;
+  std::vector<std::unique_ptr<OperatorNode>> rg_children;
   auto right_get = std::make_unique<OperatorNode>(
     LogicalGet::Make(catalog::db_oid_t(4), catalog::namespace_oid_t(5), catalog::table_oid_t(6), {}, "tbl2", false),
-    std::move(right_children));
+    std::move(rg_children));
 
   std::vector<std::unique_ptr<OperatorNode>> agg_children;
   agg_children.emplace_back(std::move(right_get));
@@ -655,4 +712,179 @@ TEST_F(OptimizerRewriteTest, DependentJoinGetToInnerJoin) {
   delete(optimizer_context);
 }
 
+// NOLINTNEXTLINE
+TEST_F(OptimizerRewriteTest, PullFilterThroughMarkJoinAndMarkJoinToInnerJoinTest) {
+  auto optimizer_context = new OptimizerContext(nullptr);
+  auto optimization_context = new OptimizationContext(optimizer_context, nullptr);
+  optimizer_context->AddOptimizationContext(optimization_context);
+
+  // Setup the task stack
+  auto task_stack = new OptimizerTaskStack();
+  optimizer_context->SetTaskPool(task_stack);
+
+  // Create OperatorNode of (GET A) MARK_JOIN (FILTER (GET B))
+  std::vector<std::unique_ptr<OperatorNode>> lg_children;
+  auto left_get = std::make_unique<OperatorNode>(
+    LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
+    std::move(lg_children));
+  auto lg_copy = left_get->Copy();
+
+  std::vector<std::unique_ptr<OperatorNode>> rg_children;
+  auto right_get = std::make_unique<OperatorNode>(
+    LogicalGet::Make(catalog::db_oid_t(4), catalog::namespace_oid_t(5), catalog::table_oid_t(6), {}, "tbl2", false),
+    std::move(rg_children));
+
+  // Build a COMPARE_EQUAL expression for filter, which contains two children
+  parser::AbstractExpression *expr_b_1 =
+    new parser::ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
+  parser::AbstractExpression *expr_b_2 =
+    new parser::ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
+
+  auto x_1 = common::ManagedPointer<parser::AbstractExpression>(expr_b_1);
+  auto x_2 = common::ManagedPointer<parser::AbstractExpression>(expr_b_2);
+
+  // Two predicates refer to different tables
+  auto expression_1 = AnnotatedExpression(x_1, std::unordered_set<std::string>{"tbl1"});
+  auto expression_2 = AnnotatedExpression(x_2, std::unordered_set<std::string>{"tbl2"});
+  std::vector<AnnotatedExpression> predicates {expression_1, expression_2};
+  std::vector<std::unique_ptr<OperatorNode>> filter_children;
+  filter_children.emplace_back(std::move(right_get));
+  auto filter = std::make_unique<OperatorNode>(LogicalFilter::Make(std::move(predicates)), std::move(filter_children));
+
+  std::vector<std::unique_ptr<OperatorNode>> join_children;
+  join_children.emplace_back(std::move(left_get));
+  join_children.emplace_back(std::move(filter));
+  auto join = std::make_unique<OperatorNode>(LogicalMarkJoin::Make(), std::move(join_children));
+
+  // RecordOperatorNodeIntoGroup
+  GroupExpression *original_gexpr;
+  EXPECT_TRUE(optimizer_context->RecordOperatorNodeIntoGroup(common::ManagedPointer(join), &original_gexpr));
+  EXPECT_TRUE(original_gexpr != nullptr);
+
+  // Add rewrite tasks
+  group_id_t root_id = original_gexpr->GetGroupID();
+  task_stack->Push(new BottomUpRewrite(root_id, optimization_context, RuleSetName::UNNEST_SUBQUERY, false));
+
+  // Execute the tasks in stack
+  // Two rules will be applied: PULL_FILTER_THROUGH_MARK_JOIN and MARK_JOIN_GET_TO_INNER_JOIN
+  while (!task_stack->Empty()) {
+    auto task = task_stack->Pop();
+    task->Execute();
+    delete task;
+  }
+
+  // Expected OperatorNode: FILTER((GET A) INNER_JOIN (GET B))
+  auto root_gexpr = optimizer_context->GetMemo().GetGroupByID(root_id)->GetLogicalExpression();
+  EXPECT_EQ(root_gexpr->Op().GetType(), OpType::LOGICALFILTER);
+  EXPECT_EQ(root_gexpr->GetChildrenGroupsSize(), 1);
+  std::vector<AnnotatedExpression> preds {expression_1, expression_2};
+  EXPECT_EQ(root_gexpr->Op().As<LogicalFilter>()->GetPredicates(), preds);
+
+  auto join_gexpr = optimizer_context->GetMemo().GetGroupByID(root_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(join_gexpr->Op().GetType(), OpType::LOGICALINNERJOIN);
+  EXPECT_EQ(join_gexpr->GetChildrenGroupsSize(), 2);
+
+  auto left_get_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(join_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(left_get_gexpr->Op().GetType(), OpType::LOGICALGET);
+
+  auto right_get_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(join_gexpr->GetChildGroupId(0) )->GetLogicalExpression();
+  EXPECT_EQ(right_get_gexpr->Op().GetType(), OpType::LOGICALGET);
+
+  delete(expr_b_1);
+  delete(expr_b_2);
+  delete(optimizer_context);
+}
+
+// NOLINTNEXTLINE
+TEST_F(OptimizerRewriteTest, PullFilterThroughAggregationTest) {
+  auto optimizer_context = new OptimizerContext(nullptr);
+  auto optimization_context = new OptimizationContext(optimizer_context, nullptr);
+  optimizer_context->AddOptimizationContext(optimization_context);
+
+  // Setup the task stack
+  auto task_stack = new OptimizerTaskStack();
+  optimizer_context->SetTaskPool(task_stack);
+
+  // Create OperatorNode of AGGREGATE(FILTER (GET A))
+  // The filter contains two predicates.
+  // One of them(inner one) refers to the same table as GET while the other(outer one) doesn't.
+  std::vector<std::unique_ptr<OperatorNode>> get_children;
+  auto get = std::make_unique<OperatorNode>(
+    LogicalGet::Make(catalog::db_oid_t(1), catalog::namespace_oid_t(2), catalog::table_oid_t(3), {}, "tbl1", false),
+    std::move(get_children));
+
+  // Build a COMPARE_EQUAL expression for filter, which contains two children
+  parser::AbstractExpression *expr_b_1 =
+    new parser::ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
+  parser::AbstractExpression *expr_b_2 =
+    new parser::ConstantValueExpression(type::TransientValueFactory::GetBoolean(true));
+
+  std::vector<std::unique_ptr<parser::AbstractExpression>> expr_cmp_children;
+  expr_cmp_children.emplace_back(expr_b_1);
+  expr_cmp_children.emplace_back(expr_b_2);
+  parser::AbstractExpression *expr_cmp =
+    new parser::ComparisonExpression(parser::ExpressionType::COMPARE_EQUAL, std::move(expr_cmp_children));
+
+  auto x_1 = common::ManagedPointer<parser::AbstractExpression>(expr_cmp);
+  auto x_2 = common::ManagedPointer<parser::AbstractExpression>(expr_cmp);
+
+  // Two predicates refer to different tables
+  auto outer_expression = AnnotatedExpression(x_1, std::unordered_set<std::string>{"tbl1"});
+  auto inner_expression = AnnotatedExpression(x_2, std::unordered_set<std::string>{"tbl2"});
+  std::vector<AnnotatedExpression> predicates {outer_expression, inner_expression};
+  std::vector<std::unique_ptr<OperatorNode>> filter_children;
+  filter_children.emplace_back(std::move(get));
+  auto filter = std::make_unique<OperatorNode>(LogicalFilter::Make(std::move(predicates)), std::move(filter_children));
+
+  std::vector<std::unique_ptr<OperatorNode>> agg_children;
+  agg_children.emplace_back(std::move(filter));
+  auto agg = std::make_unique<OperatorNode>(LogicalAggregateAndGroupBy::Make(), std::move(agg_children));
+
+  // RecordOperatorNodeIntoGroup
+  GroupExpression *original_gexpr;
+  EXPECT_TRUE(optimizer_context->RecordOperatorNodeIntoGroup(common::ManagedPointer(agg), &original_gexpr));
+  EXPECT_TRUE(original_gexpr != nullptr);
+
+  // Add rewrite tasks
+  group_id_t root_id = original_gexpr->GetGroupID();
+  task_stack->Push(new BottomUpRewrite(root_id, optimization_context, RuleSetName::UNNEST_SUBQUERY, false));
+
+  // Execute the tasks in stack
+  // PULL_FILTER_THROUGH_AGGREGATION will be applied
+  while (!task_stack->Empty()) {
+    auto task = task_stack->Pop();
+    task->Execute();
+    delete task;
+  }
+
+  // Expected OperatorNode: FILTER (AGGREGATE (FILTER (GET A))
+  // The upper filter should have the outer_expression as its predicate.
+  // The lower filter should have the inner_expression as its predicate.
+  auto root_gexpr = optimizer_context->GetMemo().GetGroupByID(root_id)->GetLogicalExpression();
+  EXPECT_EQ(root_gexpr->Op().GetType(), OpType::LOGICALFILTER);
+  EXPECT_EQ(root_gexpr->GetChildrenGroupsSize(), 1);
+  EXPECT_EQ(root_gexpr->Op().As<LogicalFilter>()->GetPredicates().size(), 1);
+  EXPECT_EQ(root_gexpr->Op().As<LogicalFilter>()->GetPredicates()[0], outer_expression);
+
+  auto agg_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(root_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(agg_gexpr->Op().GetType(), OpType::LOGICALAGGREGATEANDGROUPBY);
+  EXPECT_EQ(agg_gexpr->GetChildrenGroupsSize(), 1);
+
+  auto filter_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(agg_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(filter_gexpr->Op().GetType(), OpType::LOGICALFILTER);
+  EXPECT_EQ(filter_gexpr->GetChildrenGroupsSize(), 1);
+  EXPECT_EQ(filter_gexpr->Op().As<LogicalFilter>()->GetPredicates().size(), 1);
+  EXPECT_EQ(filter_gexpr->Op().As<LogicalFilter>()->GetPredicates()[0], inner_expression);
+
+  auto get_gexpr =
+    optimizer_context->GetMemo().GetGroupByID(filter_gexpr->GetChildGroupId(0))->GetLogicalExpression();
+  EXPECT_EQ(get_gexpr->Op().GetType(), OpType::LOGICALGET);
+
+  delete(expr_cmp);
+  delete(optimizer_context);
+}
 }
