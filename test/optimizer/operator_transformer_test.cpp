4c3dfd07d33739e9fed8eee372a72df0e131af89
gautam20197 <gautam20197@gmail.com>
2020-05-12 16:51:32 -0400

    Default value for select constructor and clang fixes

--- test/optimizer/operator_transformer_test.cpp
+++ test/optimizer/operator_transformer_test.cpp
@@ -1947,7 +1947,7 @@ TEST_F(OperatorTransformerTest, SelectWithTest) {
 
   auto parse_tree = parser::PostgresParser::BuildParseTree(select_sql);
   auto statement = parse_tree->GetStatements()[0];
-  binder_->BindNameToNode(common::ManagedPointer(parse_tree));
+  binder_->BindNameToNode(common::ManagedPointer(parse_tree), nullptr);
   operator_transformer_ =
       std::make_unique<optimizer::QueryToOperatorTransformer>(common::ManagedPointer(accessor_), db_oid_);
   operator_tree_ = operator_transformer_->ConvertToOpExpression(statement, common::ManagedPointer(parse_tree));
