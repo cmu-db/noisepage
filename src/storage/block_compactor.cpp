391d7e111f66a5bb232f127917e1a944560dd09f
vilas897 <vilasbhat97@gmail.com>
2020-05-14 18:20:42 -0400

    Minor check-format issue fixed

--- src/storage/block_compactor.cpp
+++ src/storage/block_compactor.cpp
@@ -78,7 +78,7 @@ bool BlockCompactor::EliminateGaps(CompactionGroup *cg) {
   for (auto &entry : cg->blocks_to_compact_) {
     RawBlock *block = entry.first;
     std::vector<uint32_t> &empty_slots = entry.second;
-     TERRIER_ASSERT(block->GetInsertHead() == layout.NumSlots(),
+    TERRIER_ASSERT(block->GetInsertHead() == layout.NumSlots(),
                    "The block should be full to stop inserts from coming in");
 
     // We will loop through each block and figure out if we are safe to proceed with compaction and identify
