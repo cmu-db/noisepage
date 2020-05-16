77fec5bacc6c2d9db02ec885b599073611675829
tianhanhu <adrianhu96@gmail.com>
2020-05-16 12:29:09 -0400

    add more tests
    (cherry picked from commit 9b5481706f204e90d6b438baf1caa3b8cb4e4b7f)

--- test/execution/sql_functions_arithmetic_functions_test.cpp
+++ test/execution/sql_functions_arithmetic_functions_test.cpp
@@ -246,9 +246,11 @@ TEST_F(ArithmeticFunctionsTests, MathFuncs) {
 
   CHECK_SQL_FUNC(Sqrt, std::sqrt, 4.0);
   CHECK_SQL_FUNC(Sqrt, std::sqrt, 1.0);
+  CHECK_SQL_FUNC(Sqrt, std::sqrt, 213.5);
 
   CHECK_SQL_FUNC(Cbrt, std::cbrt, 4.0);
   CHECK_SQL_FUNC(Cbrt, std::cbrt, 1.0);
+  CHECK_SQL_FUNC(Cbrt, std::cbrt, 82.3);
 
   CHECK_SQL_FUNC(Exp, std::exp, 4.0);
   CHECK_SQL_FUNC(Exp, std::exp, 1.0);
