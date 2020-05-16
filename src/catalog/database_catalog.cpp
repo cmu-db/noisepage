7206f656f95bf71bfd4896f528e6127a4fb7dda9
YuhongZhang98 <danielzhangyh@gmail.com>
2020-05-16 16:51:49 -0400

    InitCap()
    (cherry picked from commit bc9fe2b5a03ee7c978a45d24a9d083b5f93bbd5e)

--- src/catalog/database_catalog.cpp
+++ src/catalog/database_catalog.cpp
@@ -2004,6 +2004,12 @@ void DatabaseCatalog::BootstrapProcContexts(const common::ManagedPointer<transac
   SetProcCtxPtr(txn, postgres::MD5_PRO_OID, func_context);
   txn->RegisterAbortAction([=]() { delete func_context; });
 
+  // initcap
+  func_context = new execution::functions::FunctionContext("initcap", type::TypeId::VARCHAR, {type::TypeId::VARCHAR},
+                                                           execution::ast::Builtin::InitCap, true);
+  SetProcCtxPtr(txn, postgres::INITCAP_PRO_OID, func_context);
+  txn->RegisterAbortAction([=]() { delete func_context; });
+
   // pow
   func_context = new execution::functions::FunctionContext("pow", type::TypeId::DECIMAL, {type::TypeId::DECIMAL},
                                                                 execution::ast::Builtin::Pow);
