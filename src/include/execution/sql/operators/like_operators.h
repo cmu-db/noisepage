603e7a9aa7132b96dc69b9c126f9e1aebd45e9d1
Wan Shen Lim <wanshen.lim@gmail.com>
2020-05-27 12:43:07 -0400

    Try to port values and runtime types? I think this is enough to build on, but I'm not sure that I got everything.

--- src/include/execution/sql/operators/like_operators.h
+++ src/include/execution/sql/operators/like_operators.h
@@ -12,15 +12,9 @@ static constexpr const char DEFAULT_ESCAPE = '\\';
  * Functor implementing the SQL LIKE() operator
  */
 struct Like {
-  /**
-   * Check if @p str is like @p pattern.
-   */
   static bool Apply(const char *str, std::size_t str_len, const char *pattern, std::size_t pattern_len,
                     char escape = DEFAULT_ESCAPE);
 
-  /**
-   * Check if @p str is like @p pattern.
-   */
   static bool Apply(const storage::VarlenEntry &str, const storage::VarlenEntry &pattern,
                     char escape = DEFAULT_ESCAPE) {
     return Apply(reinterpret_cast<const char *>(str.Content()), str.Size(),
@@ -32,17 +26,11 @@ struct Like {
  * Functor implementing the SQL NOT LIKE() operator
  */
 struct NotLike {
-  /**
-   * Check if @p str is not like @p pattern.
-   */
   static bool Apply(const char *str, std::size_t str_len, const char *pattern, std::size_t pattern_len,
                     char escape = DEFAULT_ESCAPE) {
     return !Like::Apply(str, str_len, pattern, pattern_len, escape);
   }
 
-  /**
-   * Check if @p str is not like @p pattern.
-   */
   static bool Apply(const storage::VarlenEntry &str, const storage::VarlenEntry &pattern,
                     char escape = DEFAULT_ESCAPE) {
     return !Like::Apply(reinterpret_cast<const char *>(str.Content()), str.Size(),
