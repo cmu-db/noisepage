985060ad9984679a12713fe07ce27ec8e1340469
Matt Butrovich <mbutrovich@gmail.com>
2020-06-01 16:22:02 -0400

    Better documentation of the test scenario.

--- script/testing/junit/src/TrafficCopTest.java
+++ script/testing/junit/src/TrafficCopTest.java
@@ -37,25 +37,32 @@ public class TrafficCopTest extends TestUtility {
   @Test
   public void test_DisconnectAbort() throws SQLException {
 
+   // create another connection that will take the write lock on a tuple, forcing an about on the default connection's
+   // createStatement
    Connection second_conn = makeDefaultConnection();
    second_conn.setAutoCommit(true);
 
    Statement stmt = second_conn.createStatement();
    stmt.execute("CREATE TABLE FOO (ID INT);");
    stmt.execute("INSERT INTO FOO VALUES (1),(2),(3);");
+
+   // begin explicit txn and take the write lock on a tuple
    stmt.execute("BEGIN;");
    stmt.execute("UPDATE FOO SET ID = 4 WHERE ID = 3;");
    assertEquals(stmt.getUpdateCount(), 1);
 
    Statement second_stmt = conn.createStatement();
    try {
+      // another statement tries to update the same tuple and will fail
       second_stmt.execute("UPDATE FOO SET ID = 5 WHERE ID = 3;");
       fail();
      } catch (SQLException ex) {
       assertEquals(ex.getMessage(), "Query failed.");
      }
+   // close the second connection, forcing the explicit txn that has the write lock to abort
    second_conn.close();
 
+   // another statement can now acquire the write lock on the tuple
    Statement third_stmt = conn.createStatement();
    third_stmt.execute("UPDATE FOO SET ID = 5 WHERE ID = 3;");
    assertEquals(third_stmt.getUpdateCount(), 1);
