4f317fc66b5ecc5fb1a9e11482553be398f03309
Thanh Doan <tcdoan@gmail.com>
2020-06-06 05:37:14 +0000

    Fix package versions.

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -106,12 +106,12 @@ install_linux() {
       ant
    #install libpqxx-6.2 manually
    apt-get -y install wget
-   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-dev_6.2.4-4_amd64.deb
-   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-6.2_6.2.4-4_amd64.deb
-   dpkg -i libpqxx-6.2_6.2.4-4_amd64.deb
-   dpkg -i libpqxx-dev_6.2.4-4_amd64.deb
-   rm libpqxx-6.2_6.2.4-4_amd64.deb
-   rm libpqxx-dev_6.2.4-4_amd64.deb
+   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-dev_6.2.5-1_amd64.deb
+   wget http://mirrors.kernel.org/ubuntu/pool/universe/libp/libpqxx/libpqxx-6.2_6.2.5-1_amd64.deb
+   dpkg -i libpqxx-dev_6.2.5-1_amd64.deb
+   dpkg -i libpqxx-6.2_6.2.5-1_amd64.deb
+   rm libpqxx-dev_6.2.5-1_amd64.deb
+   rm libpqxx-6.2_6.2.5-1_amd64.deb
 }
 
 main "$@"
