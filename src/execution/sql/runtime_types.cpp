0867d5621263c95a23977862bff3cc153907f98e
Deepayan Patra <dpatra@andrew.cmu.edu>
2020-06-17 14:29:31 -0400

    timestamp suffix

--- src/execution/sql/runtime_types.cpp
+++ src/execution/sql/runtime_types.cpp
@@ -122,6 +122,13 @@ void SplitTime(int64_t jd, int32_t *hour, int32_t *min, int32_t *sec, int32_t *f
   *fsec = time - (*sec * K_MICRO_SECONDS_PER_SECOND);
 }
 
+// Check if a string value ends with string ending
+inline bool ends_with(std::string const & value, std::string const & ending)
+{
+  if (ending.size() > value.size()) return false;
+  return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
 }  // namespace
 
 //===----------------------------------------------------------------------===//
@@ -376,6 +383,10 @@ std::pair<bool, Timestamp> Timestamp::FromString(const char *str, std::size_t le
   // Trim leading and trailing whitespace
   while (ptr != limit && static_cast<bool>(std::isspace(*ptr))) ptr++;
   while (ptr != limit && static_cast<bool>(std::isspace(*(limit - 1)))) limit--;
+
+  std::string suffix_str("::timestamp");
+  std::string ts_str(str, len);
+  if (ends_with(ts_str, suffix_str)) limit -= suffix_str.length();
 
   uint32_t year = 0, month = 0, day = 0, hour = 0, min = 0, sec = 0, milli = 0, micro = 0;
 
