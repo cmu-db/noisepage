74390e9090fdbbef2e189a4d5e37d70e2c15608d
Matt Butrovich <mbutrovich@gmail.com>
2020-06-18 13:40:50 -0400

    Address PR feedback.

--- test/binder/binder_util_test.cpp
+++ test/binder/binder_util_test.cpp
@@ -12,51 +12,51 @@ class BinderUtilTest : public TerrierTest {};
 
 // NOLINTNEXTLINE
 TEST_F(BinderUtilTest, VarcharToTinyInt) {
-  constexpr std::string_view val = "100";
+  constexpr std::string_view val = "15";
   auto string_val = execution::sql::ValueUtil::CreateStringVal(val);
   const auto cve = std::make_unique<parser::ConstantValueExpression>(type::TypeId::VARCHAR, string_val.first,
                                                                      std::move(string_val.second));
   binder::BinderUtil::CheckAndTryPromoteType(common::ManagedPointer(cve), type::TypeId::TINYINT);
-  EXPECT_EQ(cve->Peek<int8_t>(), 100);
+  EXPECT_EQ(cve->Peek<int8_t>(), 15);
 }
 
 // NOLINTNEXTLINE
 TEST_F(BinderUtilTest, VarcharToSmallInt) {
-  constexpr std::string_view val = "100";
+  constexpr std::string_view val = "15721";
   auto string_val = execution::sql::ValueUtil::CreateStringVal(val);
   const auto cve = std::make_unique<parser::ConstantValueExpression>(type::TypeId::VARCHAR, string_val.first,
                                                                      std::move(string_val.second));
   binder::BinderUtil::CheckAndTryPromoteType(common::ManagedPointer(cve), type::TypeId::SMALLINT);
-  EXPECT_EQ(cve->Peek<int16_t>(), 100);
+  EXPECT_EQ(cve->Peek<int16_t>(), 15721);
 }
 
 // NOLINTNEXTLINE
 TEST_F(BinderUtilTest, VarcharToInteger) {
-  constexpr std::string_view val = "100";
+  constexpr std::string_view val = "1572115445";
   auto string_val = execution::sql::ValueUtil::CreateStringVal(val);
   const auto cve = std::make_unique<parser::ConstantValueExpression>(type::TypeId::VARCHAR, string_val.first,
                                                                      std::move(string_val.second));
   binder::BinderUtil::CheckAndTryPromoteType(common::ManagedPointer(cve), type::TypeId::INTEGER);
-  EXPECT_EQ(cve->Peek<int32_t>(), 100);
+  EXPECT_EQ(cve->Peek<int32_t>(), 1572115445);
 }
 
 // NOLINTNEXTLINE
 TEST_F(BinderUtilTest, VarcharToBigInt) {
-  constexpr std::string_view val = "100";
+  constexpr std::string_view val = "1572115445";
   auto string_val = execution::sql::ValueUtil::CreateStringVal(val);
   const auto cve = std::make_unique<parser::ConstantValueExpression>(type::TypeId::VARCHAR, string_val.first,
                                                                      std::move(string_val.second));
   binder::BinderUtil::CheckAndTryPromoteType(common::ManagedPointer(cve), type::TypeId::BIGINT);
-  EXPECT_EQ(cve->Peek<int64_t>(), 100);
+  EXPECT_EQ(cve->Peek<int64_t>(), 1572115445);
 }
 
 // NOLINTNEXTLINE
 TEST_F(BinderUtilTest, VarcharToDecimal) {
-  constexpr std::string_view val = "100.0";
+  constexpr std::string_view val = "15721.15445";
   auto string_val = execution::sql::ValueUtil::CreateStringVal(val);
   const auto cve = std::make_unique<parser::ConstantValueExpression>(type::TypeId::VARCHAR, string_val.first,
                                                                      std::move(string_val.second));
   binder::BinderUtil::CheckAndTryPromoteType(common::ManagedPointer(cve), type::TypeId::DECIMAL);
-  EXPECT_EQ(cve->Peek<double>(), 100.0);
+  EXPECT_EQ(cve->Peek<double>(), 15721.15445);
 }
 }  // namespace terrier
