5e58d7dea61a962881f89195a851195f23223762
Ambarish Balakumar <abalakum@andrew.cmu.edu>
2020-06-22 13:52:49 -0500

    Removed headers from binder sherpa

--- src/include/binder/binder_sherpa.h
+++ src/include/binder/binder_sherpa.h
@@ -5,9 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
-#include "execution/sql/value.h"
 #include "parser/expression/abstract_expression.h"
-#include "parser/expression/constant_value_expression.h"
 #include "util/time_util.h"
 
 namespace terrier::binder {
