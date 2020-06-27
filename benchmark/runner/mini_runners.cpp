b1097db185b4c78a34cdca98cb6bb5a06ec7b2a8
Matt Butrovich <mbutrovich@gmail.com>
2020-06-27 13:54:47 -0400

    Fix benchmark compilation.

--- benchmark/runner/mini_runners.cpp
+++ benchmark/runner/mini_runners.cpp
@@ -1,9 +1,9 @@
 #include <common/macros.h>
 #include <gflags/gflags.h>
-#include <pqxx/pqxx>
 
 #include <cstdio>
 #include <functional>
+#include <pqxx/pqxx>
 #include <random>
 #include <utility>
 
@@ -27,6 +27,7 @@
 #include "optimizer/query_to_operator_transformer.h"
 #include "planner/plannodes/index_scan_plan_node.h"
 #include "planner/plannodes/seq_scan_plan_node.h"
+#include "storage/sql_table.h"
 #include "traffic_cop/traffic_cop_util.h"
 
 namespace terrier::runner {
