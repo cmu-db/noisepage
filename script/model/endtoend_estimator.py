Small calculation fix.

script/model/endtoend_estimator.py
@@ -97,7 +97,7 @@ class EndtoendEstimator:
 
         # Calculate the accumulated ratio error
         mini_model_y_pred = np.array(mini_model_y_pred)
-        raw_y_pred = mini_model_y_pred * (y_pred + 1)
+        raw_y_pred = (mini_model_y_pred + 1) * y_pred
         accumulated_raw_y = np.sum(raw_y, axis=0)
         accumulated_raw_y_pred = np.sum(raw_y_pred, axis=0)
         original_ratio_error = np.average(np.abs(raw_y - mini_model_y_pred) / (raw_y + 1), axis=0)

script/model/global_trainer.py
@@ -172,7 +172,7 @@ class GlobalTrainer:
         # Calculate the accumulated ratio error
         mini_model_y_pred = np.array(mini_model_y_pred)[test_indices]
         y_pred = trained_model.predict(x)[test_indices]
-        raw_y_pred = mini_model_y_pred * (y_pred + 1)
+        raw_y_pred = (mini_model_y_pred + 1) * y_pred
         raw_y = np.array(raw_y)[test_indices]
         accumulated_raw_y = np.sum(raw_y, axis=0)
         accumulated_raw_y_pred = np.sum(raw_y_pred, axis=0)