8576cc627cd30901f6112f290a218a5b946d57c6
Tanuj Nayak <tanujnay112@live.com>
2020-07-10 02:57:33 -0700

    working on recursive not segfaulting

--- src/include/execution/compiler/codegen.h
+++ src/include/execution/compiler/codegen.h
@@ -1398,7 +1398,7 @@ class CodeGen {
    * Return the identifier for a cte scan iterator in tpl generated
    * @return identifier for a cte scan iterator in tpl generated
    */
-  ast::Identifier GetCteScanIdentifier() { return cte_scan_iterator_; }
+//  ast::Identifier GetCteScanIdentifier() { return cte_scan_iterator_; }
 
  private:
   // Enter a new lexical scope.
