dd60a11523de6db2e9fa828e6b3d4a621c8afc35
Wan Shen Lim <wanshenl@cs.cmu.edu>
2020-08-04 06:48:44 -0400

    Fix parallel scan.

--- src/execution/sql/table_vector_iterator.cpp
+++ src/execution/sql/table_vector_iterator.cpp
@@ -106,7 +106,7 @@ class ScanTask {
     byte *const thread_state = thread_state_container_->AccessCurrentThreadState();
 
     // Call scanning function
-    scanner_(query_state_, thread_state, &iter, exec_ctx_);
+    scanner_(query_state_, thread_state, &iter);
   }
 
  private:
