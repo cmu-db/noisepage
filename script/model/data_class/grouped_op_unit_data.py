e2fcd3cfe0b89e9df80a7f4c7793d08a5be58a63
William Zhang <17zhangw@gmail.com>
2020-08-06 07:36:15 -0400

    new TPCC ids

--- script/model/data_class/grouped_op_unit_data.py
+++ script/model/data_class/grouped_op_unit_data.py
@@ -108,7 +108,10 @@ def _pipeline_get_grouped_op_unit_data(filename, warmup_period):
                 p_id = int(line[1])
 
                 # Hack to tweak the feature based on the query
-                if q_id == 29 and p_id == 0:
+                if q_id == 33 and p_id == 0:
+                    # SELECT NO_O_ID FROM NEW_ORDER WHERE NO_D_ID = $1    AND NO_W_ID = $2
+                    # ORDER BY NO_O_ID ASC  LIMIT 1
+                    #
                     # q31 returns ~850 tuples from IDX_SCAN to SORT_BUILD
                     if feature == 'SORT_BUILD':
                         # Set # input rows to SORT_BUILD as 850
@@ -118,7 +121,10 @@ def _pipeline_get_grouped_op_unit_data(filename, warmup_period):
                     elif feature == 'IDX_SCAN':
                         # Set # output rows of IDX_SCAN as 850
                         x_loc[3] = 850
-                elif q_id == 37 and p_id == 0:
+                elif q_id == 30 and p_id == 0:
+                    # SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT  FROM ORDER_LINE, STOCK WHERE
+                    # OL_W_ID = $1 AND OL_D_ID = $2 AND OL_O_ID < $3 AND OL_O_ID >= $4 AND S_W_ID = $5
+                    # AND S_I_ID = OL_I_ID AND S_QUANTITY < $6
                     if feature == 'AGG_BUILD':
                         # Set agg_build input rows to 200, assume output unchanged
                         # Since there's distinct, set the correct key size/input key
