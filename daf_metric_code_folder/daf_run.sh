b9bfbef8ea3a30545e9446e0613f8b42b4bcabf0
Ling <ling.zhang.mlz@gmail.com>
2020-08-07 00:31:45 -0400

    temp change for metric

--- daf_metric_code_folder/daf_run.sh
+++ daf_metric_code_folder/daf_run.sh
@@ -3,7 +3,7 @@
 cd ../build
 rm daf_count_agg_woLogging_1thread.csv daf_time_agg_woLogging_1thread.csv daf_count_agg_woLogging_4thread.csv daf_time_agg_woLogging_4thread.csv daf_count_agg_woLogging_8thread.csv daf_time_agg_woLogging_8thread.csv 2> /dev/null
 rm num_txn_processed_1thread.csv num_txn_processed_4thread.csv num_txn_processed_8thread.csv  2> /dev/null
-cmake -DTERRIER_USE_ASAN=OFF -DCMAKE_BUILD_TYPE=Release -DTERRIER_USE_JEMALLOC=ON .. && make tpcc_benchmark -j4
+cmake -DTERRIER_USE_ASAN=OFF -DCMAKE_BUILD_TYPE=Release -DTERRIER_USE_JEMALLOC=ON .. && make tpcc_benchmark -j12
 export TERRIER_BENCHMARK_THREADS=1
 ./release/tpcc_benchmark 
 mv daf_count_agg.csv daf_count_agg_woLogging_1thread.csv
