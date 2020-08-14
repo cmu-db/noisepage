638cd53e3430a861932eafc8c901cc8b5b1ed788
Ling <ling.zhang.mlz@gmail.com>
2020-08-14 11:12:01 -0400

    metric code update

--- daf_metric_code_folder/daf_run.sh
+++ daf_metric_code_folder/daf_run.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-cd ../build
-rm daf_count_agg_woLogging_1thread.csv daf_time_agg_woLogging_1thread.csv daf_count_agg_woLogging_4thread.csv daf_time_agg_woLogging_4thread.csv daf_count_agg_woLogging_8thread.csv daf_time_agg_woLogging_8thread.csv 2> /dev/null
-rm num_txn_processed_1thread.csv num_txn_processed_4thread.csv num_txn_processed_8thread.csv  2> /dev/null
-cmake -DTERRIER_USE_ASAN=OFF -DCMAKE_BUILD_TYPE=Release -DTERRIER_USE_JEMALLOC=ON .. && make tpcc_benchmark -j12
-export TERRIER_BENCHMARK_THREADS=1
-./release/tpcc_benchmark 
-mv daf_count_agg.csv daf_count_agg_woLogging_1thread.csv
-mv daf_time_agg.csv daf_time_agg_woLogging_1thread.csv
-# mv num_txn_processed.csv num_txn_processed_1thread.csv
-export TERRIER_BENCHMARK_THREADS=4
-./release/tpcc_benchmark 
-mv daf_count_agg.csv daf_count_agg_woLogging_4thread.csv
-mv daf_time_agg.csv daf_time_agg_woLogging_4thread.csv
-# mv num_txn_processed.csv num_txn_processed_4thread.csv
-# sudo su
-export TERRIER_BENCHMARK_THREADS=8
-./release/tpcc_benchmark 
-# perf timechart ./release/tpcc_benchmark
-# perf report
-mv daf_count_agg.csv daf_count_agg_woLogging_8thread.csv
-mv daf_time_agg.csv daf_time_agg_woLogging_8thread.csv
-# mv num_txn_processed.csv num_txn_processed_8thread.csv
-cd ..
+cd build
+rm *.csv  2> /dev/null
+NOW=$(date +%m%d%Y)
+cmake -DTERRIER_USE_ASAN=OFF -DCMAKE_BUILD_TYPE=Release -DTERRIER_USE_JEMALLOC=ON .. && make tpcc_benchmark -j30
+
+for num_daf_threads in 1 2 4 8
+do
+	export TERRIER_BENCHMARK_DAF_THREADS=$num_daf_threads
+	for num_worker_threads in 1 4 8 12 16 20 24
+	# for num_worker_threads in 1 4
+	do
+		export TERRIER_BENCHMARK_THREADS=$num_worker_threads
+		./release/tpcc_benchmark --benchmark_out_format=json --benchmark_out=${num_daf_threads}daf_${num_worker_threads}worker_result.json
+		mv daf_count_agg.csv ${num_daf_threads}daf_count_agg_woLogging_${num_worker_threads}thread.csv
+		mv daf_time_agg.csv ${num_daf_threads}daf_time_agg_woLogging_${num_worker_threads}thread.csv
+		cat expr_results.csv >> agg_expr_results.txt
+	done
+	mkdir ${num_daf_threads}DAF_TPCC_${NOW}
+	mv *.csv *.json ${num_daf_threads}DAF_TPCC_${NOW}
+	mv ${num_daf_threads}DAF_TPCC_${NOW} ../../../daf_metric_results_folder/
+done
+mv agg_expr_results.txt agg_expr_results.csv
+mv agg_expr_results.csv ../../../daf_metric_results_folder/
