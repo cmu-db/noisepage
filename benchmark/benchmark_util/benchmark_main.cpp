60b0a186e55c89694279c993245be0826a70cd1f
Ling <ling.zhang.mlz@gmail.com>
2020-08-13 22:17:46 -0400

    fix daf thread env

--- benchmark/benchmark_util/benchmark_main.cpp
+++ benchmark/benchmark_util/benchmark_main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
   if (env_num_threads != nullptr) terrier::BenchmarkConfig::num_threads = atoi(env_num_threads);
 
   const char *env_num_daf_threads = std::getenv(terrier::ENV_NUM_DAF_THREADS);
-  if (env_num_daf_threads != nullptr) terrier::BenchmarkConfig::num_threads = atoi(env_num_daf_threads);
+  if (env_num_daf_threads != nullptr) terrier::BenchmarkConfig::num_daf_threads = atoi(env_num_daf_threads);
 
   const char *env_logfile_path = std::getenv(terrier::ENV_LOGFILE_PATH);
   if (env_logfile_path != nullptr) terrier::BenchmarkConfig::logfile_path = std::string_view(env_logfile_path);
