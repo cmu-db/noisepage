7a49e233ea74078e92a0bc3c9a9912bee30a6d85
Tanuj Nayak <tanujnay112@live.com>
2020-08-14 16:33:55 -0700

    broke down trace files

--- script/testing/junit/traces/cte.sql
+++ script/testing/junit/traces/cte.sql
@@ -7,4 +7,3 @@ WITH cte(x) AS (SELECT 1), cte2(y) AS (SELECT 2) SELECT * FROM cte INNER JOIN ct
 CREATE TABLE tree (node INTEGER, parent INTEGER);
 INSERT INTO tree VALUES (1,NULL), (10, 1), (11, 1), (100, 10), (101, 10), (110, 11), (111, 11);
 WITH RECURSIVE cte(x) AS (SELECT 1 UNION ALL SELECT tree.node FROM tree INNER JOIN cte ON tree.parent=cte.x) SELECT * FROM cte;
-WITH ITERATIVE cte(x) AS (SELECT 1 UNION ALL SELECT tree.node FROM tree INNER JOIN cte ON tree.parent=cte.x) SELECT * FROM cte;
