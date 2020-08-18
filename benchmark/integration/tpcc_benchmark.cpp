b4aa64e82c5482e03cdca5ae00a14109fba9976a
Ling <ling.zhang.mlz@gmail.com>
2020-08-17 23:51:13 -0400

    remove daf_thread

--- benchmark/integration/tpcc_benchmark.cpp
+++ benchmark/integration/tpcc_benchmark.cpp
@@ -444,13 +444,7 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, ScaleFactor4WithGCMetrics)(benchmark::State &s
   std::vector<Worker> workers;
   workers.reserve(terrier::BenchmarkConfig::num_threads);
 
-  auto curr_num_precomputed_txns = 2 * ((terrier::BenchmarkConfig::num_daf_threads + 2 - 1) / 2) * num_precomputed_txns_per_worker_ / terrier::BenchmarkConfig::num_threads;
-  if (terrier::BenchmarkConfig::num_daf_threads == 8 && terrier::BenchmarkConfig::num_threads == 1)
-    curr_num_precomputed_txns = curr_num_precomputed_txns / 4;
-  if (terrier::BenchmarkConfig::num_daf_threads == 4 && terrier::BenchmarkConfig::num_threads == 1)
-    curr_num_precomputed_txns = curr_num_precomputed_txns / 4;
-  if (terrier::BenchmarkConfig::num_daf_threads == 2 && terrier::BenchmarkConfig::num_threads == 8)
-    curr_num_precomputed_txns = curr_num_precomputed_txns * 2;
+  auto curr_num_precomputed_txns = 2 * num_precomputed_txns_per_worker_ / terrier::BenchmarkConfig::num_threads;
 
   std::cout << "num daf thread:" << terrier::BenchmarkConfig::num_daf_threads << " num worker threads:" << terrier::BenchmarkConfig::num_threads << " txns per thread:" << curr_num_precomputed_txns << std::endl;
   // Precompute all of the input arguments for every txn to be run. We want to avoid the overhead at benchmark time
@@ -514,7 +508,7 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, ScaleFactor4WithGCMetrics)(benchmark::State &s
 
     std::ofstream expr_result_file(expr_result_file_name.data());
     // num_daf_threads, num_worker_thread, time_elapsed
-    expr_result_file << BenchmarkConfig::num_daf_threads << ", " << BenchmarkConfig::num_threads << ", " << static_cast<double>(elapsed_ms) / 1000.0 << std::endl;
+    expr_result_file << BenchmarkConfig::num_threads << ", " << static_cast<double>(elapsed_ms) / 1000.0 << std::endl;
     expr_result_file.close();
     // cleanup
     catalog.TearDown();
