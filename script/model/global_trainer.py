81e000f6e73e85bd141f6b95d4d6359b64b35df1
Lin Ma <malin1993ml@gmail.com>
2020-08-19 15:36:22 -0400

    Small fix.

--- script/model/global_trainer.py
+++ script/model/global_trainer.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     aparser.add_argument('--ml_models', nargs='*', type=str, default=["nn"],
                          help='ML models for the mini trainer to evaluate')
     aparser.add_argument('--test_ratio', type=float, default=0.2, help='Test data split ratio')
-    aparser.add_argument('--impact_model_ratio', type=float, default=1,
+    aparser.add_argument('--impact_model_ratio', type=float, default=0.1,
                          help='Sample ratio to train the global impact model')
     aparser.add_argument('--warmup_period', type=float, default=1, help='OLTPBench warmup period')
     aparser.add_argument('--simulate_cache', default=False, help='Should simulate cache at 0.4')
