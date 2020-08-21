29e1cc516e8b1d7dd1bb6930e79167715693b50c
Joseph Koshakow <jkosh44@users.noreply.github.com>
2020-08-21 10:54:08 -0400

    May 2020 Hackathon SQL String Functions  (#1091)
    * Hackathon: Position() string function added (#7)
    
    * Add Position logic in string_functions
    
    * Added function declaration and TPL bytecode for Postion
    
    * Added semantic checking/typing for Position
    
    * Changes to VM and bytecode generator for Position function
    
    * Added position function to catalog
    
    * Changes to get Position() working - with Junit test
    
    * Missing Junit test changes
    
    * Fixed format errors
    
    * Use custom search function to avoid copying the strings
    
    Co-authored-by: Abhijith Anilkumar <abhijithanilkumar@live.com>
    Co-authored-by: Arvind Sai Krishnan <arvindsk@andrew.cmu.edu>
    (cherry picked from commit 5da6124bb769f401cc71423868780aeeaa8aa4e0)
    
    * Fix StringFunctions::Position
    
    (cherry picked from commit 55a2a10f05d581a5a51934dcac2682b966b90859)
    
    * Update position bytecode generator
    
    * Fix broken tests
    
    * Add position test traces
    
    * The ASCII function returns the ascii value of the first character in the string that is passed into it. This commit includes implementation (in string_functions and built-in specification files), integration (in database_catalog), a C++ test (in string_functions_test), and a Junit test (in FunctionsTest).
    
    * Recommended fixes to implementation
    
    * Update emitter and str length functions for ascii
    
    * chr
    
    * builtin
    
    * fixed chr
    
    * fixed bytes
    
    * exp
    
    * charlength
    
    * c++ test
    
    * Update emitter func and remove dup vars
    
    * Fix order of OpCharLength call
    
    * Set return type of builtin string functions
    
    * Format code and fix tests
    
    * Add test traces for exp, ascii, chr, char_length
    
    * Fix formatting and ordering
    
    * Remove comment
    
    * Refactor string function signatures
    
    * Organize pg_proc numbers
    
    * Fix typo
    
    * Organize pg_proc ids
    
    * Fix format
    
    * Fix pg_proc ids
    
    * Change type of Builtin enum from uint8_t to uint16_t
    
    * Remove uneccessary calls to Resolve
    
    * Format code blocks
    
    * Remove redundant null checks
    
    * Fix comments
    
    Co-authored-by: Andy Pavlo <pavlo@cs.brown.edu>
    Co-authored-by: Paulina Davison <pldaviso@andrew.cmu.edu>
    Co-authored-by: YingjingLu <yingjinglu2019us@gmail.com>
    Co-authored-by: wuwenw <wuwenw@andrew.cmu.edu>
    Co-authored-by: Matt Butrovich <mbutrovich@users.noreply.github.com>

--- src/execution/vm/bytecode_generator.cpp
+++ src/execution/vm/bytecode_generator.cpp
@@ -1689,6 +1689,11 @@ void BytecodeGenerator::VisitBuiltinTrigCall(ast::CallExpr *call, ast::Builtin b
       GetEmitter()->Emit(Bytecode::Log2, dest, src);
       break;
     }
+    case ast::Builtin::Exp: {
+      src = VisitExpressionForRValue(call->Arguments()[1]);
+      GetEmitter()->Emit(Bytecode::Exp, dest, src);
+      break;
+    }
     default: {
       UNREACHABLE("Impossible trigonometric bytecode");
     }
@@ -2128,6 +2133,22 @@ void BytecodeGenerator::VisitBuiltinStringCall(ast::CallExpr *call, ast::Builtin
   LocalVar exec_ctx = VisitExpressionForRValue(call->Arguments()[0]);
   LocalVar ret = GetExecutionResult()->GetOrCreateDestination(call->GetType());
   switch (builtin) {
+    case ast::Builtin::Chr: {
+      // input_string here is a integer type number
+      LocalVar input_string = VisitExpressionForRValue(call->Arguments()[1]);
+      GetEmitter()->Emit(Bytecode::Chr, ret, exec_ctx, input_string);
+      break;
+    }
+    case ast::Builtin::CharLength: {
+      LocalVar input_string = VisitExpressionForRValue(call->Arguments()[1]);
+      GetEmitter()->Emit(Bytecode::CharLength, ret, exec_ctx, input_string);
+      break;
+    }
+    case ast::Builtin::ASCII: {
+      LocalVar input_string = VisitExpressionForRValue(call->Arguments()[1]);
+      GetEmitter()->Emit(Bytecode::ASCII, ret, exec_ctx, input_string);
+      break;
+    }
     case ast::Builtin::Lower: {
       LocalVar input_string = VisitExpressionForRValue(call->Arguments()[1]);
       GetEmitter()->Emit(Bytecode::Lower, ret, exec_ctx, input_string);
@@ -2135,6 +2156,12 @@ void BytecodeGenerator::VisitBuiltinStringCall(ast::CallExpr *call, ast::Builtin
     }
     case ast::Builtin::Version: {
       GetEmitter()->Emit(Bytecode::Version, ret, exec_ctx);
+      break;
+    }
+    case ast::Builtin::Position: {
+      LocalVar input_string = VisitExpressionForRValue(call->Arguments()[1]);
+      LocalVar sub_string = VisitExpressionForRValue(call->Arguments()[2]);
+      GetEmitter()->Emit(Bytecode::Position, ret, exec_ctx, input_string, sub_string);
       break;
     }
     default:
@@ -2385,6 +2412,7 @@ void BytecodeGenerator::VisitBuiltinCallExpr(ast::CallExpr *call) {
     case ast::Builtin::IndexIteratorGetSlot:
       VisitBuiltinIndexIteratorCall(call, builtin);
       break;
+    case ast::Builtin::Exp:
     case ast::Builtin::ACos:
     case ast::Builtin::ASin:
     case ast::Builtin::ATan:
@@ -2489,8 +2517,12 @@ void BytecodeGenerator::VisitBuiltinCallExpr(ast::CallExpr *call) {
       VisitBuiltinParamCall(call, builtin);
       break;
     }
+    case ast::Builtin::Chr:
+    case ast::Builtin::CharLength:
+    case ast::Builtin::ASCII:
     case ast::Builtin::Lower:
-    case ast::Builtin::Version: {
+    case ast::Builtin::Version:
+    case ast::Builtin::Position: {
       VisitBuiltinStringCall(call, builtin);
       break;
     }
