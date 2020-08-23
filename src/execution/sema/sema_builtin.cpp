zechengh09/terrier

Adding POW builtin function

src/execution/sema/sema_builtin.cpp
@@ -1310,7 +1310,8 @@ void Sema::CheckMathTrigCall(ast::CallExpr *call, ast::Builtin builtin) {
 
   const auto &call_args = call->Arguments();
   switch (builtin) {
-    case ast::Builtin::ATan2: {
+    case ast::Builtin::ATan2:
+    case ast::Builtin::Pow: {
       // check to make sure we have the right number of arguments
       if (!CheckArgCount(call, 2)) {
         return;
@@ -2526,7 +2527,8 @@ void Sema::CheckBuiltinCall(ast::CallExpr *call) {
     case ast::Builtin::Cos:
     case ast::Builtin::Cot:
     case ast::Builtin::Sin:
-    case ast::Builtin::Tan: {
+    case ast::Builtin::Tan:
+    case ast::Builtin::Pow: {
       CheckMathTrigCall(call, builtin);
       break;
     }

src/execution/vm/bytecode_generator.cpp
@@ -1511,6 +1511,10 @@ void BytecodeGenerator::VisitBuiltinTrigCall(ast::CallExpr *call, ast::Builtin b
       Emitter()->Emit(Bytecode::Tan, dest, src);
       break;
     }
+    case ast::Builtin::Pow: {
+      Emitter()->Emit(Bytecode::Pow, dest, src);
+      break;
+    }
     default: {
       UNREACHABLE("Impossible trigonometric bytecode");
     }
@@ -2234,7 +2238,8 @@ void BytecodeGenerator::VisitBuiltinCallExpr(ast::CallExpr *call) {
     case ast::Builtin::Cos:
     case ast::Builtin::Cot:
     case ast::Builtin::Sin:
-    case ast::Builtin::Tan: {
+    case ast::Builtin::Tan:
+    case ast::Builtin::Pow: {
       VisitBuiltinTrigCall(call, builtin);
       break;
     }

cmu-db/noisepage

(cherry picked from commit 90bba23c49dc5f000feb5aa75759e896dbaab5f3)

src/execution/sema/sema_builtin.cpp
@@ -1458,7 +1458,8 @@ void Sema::CheckMathTrigCall(ast::CallExpr *call, ast::Builtin builtin) {
 
   const auto &call_args = call->Arguments();
   switch (builtin) {
-    case ast::Builtin::ATan2: {
+    case ast::Builtin::ATan2:
+    case ast::Builtin::Pow: {
       if (!CheckArgCount(call, 2)) {
         return;
       }
@@ -3049,7 +3050,8 @@ void Sema::CheckBuiltinCall(ast::CallExpr *call) {
     case ast::Builtin::Sqrt:
     case ast::Builtin::Cbrt:
     case ast::Builtin::Round:
-    case ast::Builtin::Round2: {
+    case ast::Builtin::Round2:
+    case ast::Builtin::Pow: {
       CheckMathTrigCall(call, builtin);
       break;
     }

src/execution/vm/bytecode_generator.cpp
@@ -1710,6 +1710,10 @@ void BytecodeGenerator::VisitBuiltinTrigCall(ast::CallExpr *call, ast::Builtin b
       GetEmitter()->Emit(Bytecode::Round2, dest, src, VisitExpressionForRValue(call->Arguments()[1]));
       break;
     }
+    case ast::Builtin::Pow: {
+      Emitter()->Emit(Bytecode::Pow, dest, src);
+      break;
+    }
     default: {
       UNREACHABLE("Impossible trigonometric bytecode");
     }
@@ -2478,7 +2482,8 @@ void BytecodeGenerator::VisitBuiltinCallExpr(ast::CallExpr *call) {
     case ast::Builtin::Sqrt:
     case ast::Builtin::Cbrt:
     case ast::Builtin::Round:
-    case ast::Builtin::Round2: {
+    case ast::Builtin::Round2:
+    case ast::Builtin::Pow: {
       VisitBuiltinTrigCall(call, builtin);
       break;
     }