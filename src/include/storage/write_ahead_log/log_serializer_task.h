d513829ec63e16676378084daf4076997806bb61
gonzalezjo <gonzalezjo@users.noreply.github.com>
2020-08-28 14:24:22 -0400

    Port general header fixes from #1067
    Fixes build error that resulted from removing some headers.

--- src/include/storage/write_ahead_log/log_serializer_task.h
+++ src/include/storage/write_ahead_log/log_serializer_task.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <condition_variable>  // NOLINT
 #include <queue>
+#include <thread>  // NOLINT
 #include <tuple>
 #include <unordered_map>
 #include <utility>
--- src/storage/write_ahead_log/disk_log_consumer_task.cpp
+++ src/storage/write_ahead_log/disk_log_consumer_task.cpp
@@ -1,5 +1,7 @@
 #include "storage/write_ahead_log/disk_log_consumer_task.h"
 
+#include <thread>  // NOLINT
+
 #include "common/resource_tracker.h"
 #include "common/scoped_timer.h"
 #include "common/thread_context.h"
