3f51ea37d552446813ea478ee2bc73c22f6681fd
Ling <ling.zhang.mlz@gmail.com>
2020-09-01 10:43:34 -0400

    use default 1 deferred action thread

--- benchmark/benchmark_util/benchmark_config.cpp
+++ benchmark/benchmark_util/benchmark_config.cpp
@@ -4,7 +4,7 @@ namespace terrier {
 
 // Instantiating the static variables in BenchmarkConfig here so that we don't have linker errors
 uint32_t BenchmarkConfig::num_threads = 1;
-uint32_t BenchmarkConfig::num_daf_threads = 2;
+uint32_t BenchmarkConfig::num_daf_threads = 1;
 std::string_view BenchmarkConfig::logfile_path = "/tmp/benchmark.log";
 
 }  // namespace terrier
