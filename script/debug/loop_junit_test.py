a55c71fe465970cb72862dd40e11d260a0eb14c8
Ricthofen <jazzplato@gmail.com>
2020-09-01 16:01:48 -0400

    add debug script for loop junit tests

---
+++ script/debug/loop_junit_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shlex
+import subprocess
+
+DIR_CURR = os.path.dirname(os.path.realpath(__file__))
+DIR_SCRIPT = os.path.dirname(DIR_CURR)
+DIR_TESTING = os.path.join(DIR_SCRIPT, "testing")
+DIR_JUNIT = os.path.join(DIR_TESTING, "junit")
+FILE_RUN_JUNIT = os.path.join(DIR_JUNIT, "run_junit.py")
+LOG_FILE = os.path.join(DIR_CURR, "log_loop_junit.log")
+CMD = "python3 {} --build-type=debug --query-mode=simple".format(
+    FILE_RUN_JUNIT)
+
+if __name__ == "__main__":
+    count = 1
+    while True:
+        print("\r{}".format(count))
+        p = subprocess.Popen(shlex.split(CMD),
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            with open(LOG_FILE, "wb+") as f:
+                f.write(stdout)
+                f.write(stderr)
+            exit(1)
+        count += 1
