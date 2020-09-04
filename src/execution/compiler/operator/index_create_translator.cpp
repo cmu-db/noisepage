4f67809dde58376ee630c53dad2ab3b730419975
wuwenw <wuwenw@andrew.cmu.edu>
2020-09-04 16:03:54 -0400

    parallel works

--- src/execution/compiler/operator/index_create_translator.cpp
+++ src/execution/compiler/operator/index_create_translator.cpp
@@ -38,7 +38,7 @@ IndexCreateTranslator::IndexCreateTranslator(const planner::CreateIndexPlanNode 
   for (const auto &index_col : index_schema.GetColumns()) {
     compilation_context->Prepare(*index_col.StoredExpression());
   }
-  pipeline->RegisterSource(this, Pipeline::Parallelism::Serial);
+  pipeline->RegisterSource(this, Pipeline::Parallelism::Parallel);
 }
 
 util::RegionVector<ast::FieldDecl *> IndexCreateTranslator::GetWorkerParams() const {
