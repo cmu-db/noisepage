bedf4b39fd2f98c7cf0d34d73d2c285c5286b397
Matt Butrovich <mbutrovich@gmail.com>
2020-09-15 17:48:35 -0400

    Regex refactor.

--- src/include/catalog/database_catalog.h
+++ src/include/catalog/database_catalog.h
@@ -13,6 +13,7 @@
 #include "catalog/postgres/pg_type.h"
 #include "catalog/schema.h"
 #include "common/managed_pointer.h"
+#include "execution/ast/builtins.h"
 #include "storage/projected_row.h"
 #include "transaction/transaction_defs.h"
 
@@ -614,6 +615,10 @@ class DatabaseCatalog {
    * @param txn transaction to insert into catalog with
    */
   void BootstrapProcContexts(common::ManagedPointer<transaction::TransactionContext> txn);
+
+  void BootstrapProcContext(common::ManagedPointer<transaction::TransactionContext> txn, proc_oid_t proc_oid,
+                            std::string &&func_name, type::TypeId func_ret_type, std::vector<type::TypeId> &&args_type,
+                            execution::ast::Builtin builtin, bool is_exec_ctx_required);
 
   /**
    * Creates all of the ProjectedRowInitializers and ProjectionMaps for the catalog. These can be stashed because the
