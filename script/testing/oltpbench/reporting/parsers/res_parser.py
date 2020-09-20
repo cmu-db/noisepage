5fb5dd940287f89c4b9eff6a6c5cec04687b1820
Daniel Biales <bialesdaniel@gmail.com>
2020-09-20 13:47:22 -0400

    Refactor res_parser to simplify the code and for more reuse

--- script/testing/oltpbench/reporting/parsers/res_parser.py
+++ script/testing/oltpbench/reporting/parsers/res_parser.py
@@ -14,8 +14,6 @@ def parse_res_file(path):
         incremental_metrics (list, json array): The throughput at different time.
 
     """
-    # time, throughput, min_lat, lat_25th, median_lat, avg_lat, lat_75th, lat_90th, lat_95th, lat_99th, max_lat = [
-    # ], [], [], [], [], [], [], [], [], [], []
     with open(path) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         incremental_metrics = []
@@ -24,23 +22,10 @@ def parse_res_file(path):
                 "time": float(get_value_by_pattern(row,'time',None)),
                 "throughput": float(get_value_by_pattern(row, 'throughput', None))
             }
-            # time.append(float(row['time(sec)']))
-            # throughput.append(float(row[' throughput(req/sec)']))
             latency = {}
             for key, pattern in LATENCY_ATTRIBUTE_MAPPING:
                 value = get_value_by_pattern(row, pattern, None)
-                latency[key] = float(value) if value else value
+                latency[key] = float("{:.4}".format(value)) if value else value
             metrics_instance['latency'] = latency
             incremental_metrics.append(metrics_instance)
-            # min_lat.append(float(row[' min_lat(ms)']))
-            # lat_25th.append(float(row[' 25th_lat(ms)']))
-            # median_lat.append(float(row[' median_lat(ms)']))
-            # avg_lat.append(float(row[' avg_lat(ms)']))
-            # lat_75th.append(float(row[' 75th_lat(ms)']))
-            # lat_90th.append(float(row[' 90th_lat(ms)']))
-            # lat_95th.append(float(row[' 95th_lat(ms)']))
-            # lat_99th.append(float(row[' 99th_lat(ms)']))
-            # max_lat.append(float(row[' max_lat(ms)']))
-        # incremental_metrics = [{"time": t, "throughput": tp, "latency":latency}
-        #                        for t, tp, ml, l25, mel, al, l75, l90, l95, l99, mal in zip(time, throughput, min_lat, lat_25th, median_lat, avg_lat, lat_75th, lat_90th, lat_95th, lat_99th, max_lat)]
         return incremental_metrics
