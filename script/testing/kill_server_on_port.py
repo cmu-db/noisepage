ce41b88ba1aeaf474992d940db52fd1c3734d8b8
Ricthofen <jazzplato@gmail.com>
2020-09-24 18:51:21 -0400

    add back argparse

--- script/testing/kill_server_on_port.py
+++ script/testing/kill_server_on_port.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python3
 import sys
-from util.constants import LOG, ErrorCode
+import argparse
+from util.constants import LOG
 from util.common import kill_pids_on_port
-
-
-def usage():
-    LOG.error("python3 kill_server_on_port.py [PORT] ([PORT_2] [PORT_3]...])")
-    exit(ErrorCode.ERROR)
-
-
-def sanitize_args():
-    if len(sys.argv) < 2:
-        LOG.error("You need to provide at least 1 port number")
-        usage()
-
-    ports = []
-    for arg in sys.argv[1:]:
-        try:
-            ports.append(int(arg))
-        except ValueError:
-            LOG.error("Invalid port value '{}'".format(arg))
-            usage()
-    return ports
 
 
 def main():
@@ -30,7 +11,7 @@ def main():
     aparser.add_argument("ports", type=int, nargs="+", help="Ports to check")
     args = vars(aparser.parse_args())
 
-    for port in args["ports"]:
+    for port in args.get("ports", []):
         LOG.info(
             "****** start killing processes on port {} ******".format(port))
         kill_pids_on_port(port)
