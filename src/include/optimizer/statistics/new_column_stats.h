84353993027edfc32eb8eff9a658a97ab2f2887c
Arvind Sai Krishnan <arvindsk@andrew.cmu.edu>
2020-10-05 23:05:54 -0400

    Made NewColumnStats which is templated. Updated test to check for Integer and Floating point (DECIMAL).

---
+++ src/include/optimizer/statistics/new_column_stats.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "catalog/catalog_defs.h"
+#include "common/json_header.h"
+#include "common/macros.h"
+#include "execution/sql/value.h"
+
+namespace terrier::optimizer {
+/**
+ * Represents the statistics of a given column. Stores relevant oids,
+ * important trends (most common values/their frequencies in the column), and other
+ * useful information. The template denotes the type of the column for which
+ * statistics are being stored.
+ */
+template <typename T>
+class NewColumnStats {
+  using CppType = decltype(T::val_);
+
+ public:
+  /**
+   * Constructor
+   * @param database_id - database oid of column
+   * @param table_id - table oid of column
+   * @param column_id - column oid of column
+   * @param num_rows - number of rows in column
+   * @param cardinality - cardinality of column
+   * @param frac_null - fraction of null values out of total values in column
+   * @param most_common_vals - list of most common values in the column
+   * @param most_common_freqs - list of the frequencies of the most common values in the column
+   * @param histogram_bounds - the bounds of the histogram of the column e.g. (1.0 - 4.0)
+   * @param is_base_table - indicates whether the column is from a base table
+   */
+  NewColumnStats(catalog::db_oid_t database_id, catalog::table_oid_t table_id, catalog::col_oid_t column_id,
+                 size_t num_rows, double cardinality, double frac_null, std::vector<CppType> most_common_vals,
+                 std::vector<double> most_common_freqs, std::vector<CppType> histogram_bounds, bool is_base_table)
+      : database_id_(database_id),
+        table_id_(table_id),
+        column_id_(column_id),
+        num_rows_(num_rows),
+        cardinality_(cardinality),
+        frac_null_(frac_null),
+        most_common_vals_(std::move(most_common_vals)),
+        most_common_freqs_(std::move(most_common_freqs)),
+        histogram_bounds_(std::move(histogram_bounds)),
+        is_base_table_{is_base_table} {}
+
+  /**
+   * Default constructor for deserialization
+   */
+  NewColumnStats() = default;
+
+  /**
+   * Gets the column oid of the column
+   * @return the column oid
+   */
+  catalog::col_oid_t GetColumnID() const { return column_id_; }
+
+  /**
+   * Gets the number of rows in the column
+   * @return the number of rows
+   */
+  size_t &GetNumRows() { return this->num_rows_; }
+
+  /**
+   * Sets the number of rows int he column
+   * @param num_rows number of rows
+   */
+  void SetNumRows(size_t num_rows) { num_rows_ = num_rows; }
+
+  /**
+   * Gets the cardinality of the column
+   * @return the cardinality
+   */
+  double &GetCardinality() { return this->cardinality_; }
+
+  /**
+   * Gets the histogram bounds
+   * @return histogram bounds
+   */
+  const std::vector<CppType> &GetHistogramBounds() const { return histogram_bounds_; }
+
+  /**
+   * Gets the Common Vals
+   * @return column vals
+   */
+  const std::vector<CppType> &GetCommonVals() const { return most_common_vals_; }
+
+  /**
+   * Gets the Common Freqs
+   * @return common freqs
+   */
+  const std::vector<double> &GetCommonFreqs() const { return most_common_freqs_; }
+
+  /**
+   * Serializes a column stats object
+   * @return column stats object serialized to json
+   */
+  nlohmann::json ToJson() const;
+
+  /**
+   * Deserializes a column stats object
+   * @param j - serialized column stats object
+   */
+  void FromJson(const nlohmann::json &j);
+
+ private:
+  /**
+   * database oid
+   */
+  catalog::db_oid_t database_id_;
+
+  /**
+   * table oid
+   */
+  catalog::table_oid_t table_id_;
+
+  /**
+   * column oid
+   */
+  catalog::col_oid_t column_id_;
+
+  /**
+   * number of rows in column
+   */
+  size_t num_rows_;
+
+  /**
+   * cardinality of column
+   */
+  double cardinality_;
+
+  /**
+   * fraction of null values/total values in column
+   */
+  double frac_null_;
+
+  /**
+   * list of most common values in column
+   */
+  std::vector<CppType> most_common_vals_;
+
+  /**
+   * list of frequencies for most common values in column
+   */
+  std::vector<double> most_common_freqs_;
+
+  /**
+   * bounds for the histogram of the column
+   */
+  std::vector<CppType> histogram_bounds_;
+
+  /**
+   * tells whether column is from a base table
+   */
+  bool is_base_table_;
+};
+//
+// nlohmann::json NewColumnStats::ToJson() const {
+//  nlohmann::json j;
+//  j["database_id"] = database_id_;
+//  j["table_id"] = table_id_;
+//  j["column_id"] = column_id_;
+//  j["num_rows"] = num_rows_;
+//  j["cardinality"] = cardinality_;
+//  j["frac_null"] = frac_null_;
+//  j["most_common_vals"] = most_common_vals_;
+//  j["most_common_freqs"] = most_common_freqs_;
+//  j["histogram_bounds"] = histogram_bounds_;
+//  j["is_basetable"] = is_base_table_;
+//  return j;
+//}
+//
+// void NewColumnStats::FromJson(const nlohmann::json &j) {
+//  database_id_ = j.at("database_id").get<catalog::db_oid_t>();
+//  table_id_ = j.at("table_id").get<catalog::table_oid_t>();
+//  column_id_ = j.at("column_id").get<catalog::col_oid_t>();
+//  num_rows_ = j.at("num_rows").get<size_t>();
+//  cardinality_ = j.at("cardinality").get<double>();
+//  frac_null_ = j.at("frac_null").get<double>();
+//  most_common_vals_ = j.at("most_common_vals").get<std::vector<CppType>>();
+//  most_common_freqs_ = j.at("most_common_freqs").get<std::vector<double>>();
+//  histogram_bounds_ = j.at("histogram_bounds").get<std::vector<CppType>>();
+//  is_base_table_ = j.at("is_basetable").get<bool>();
+//}
+
+// DEFINE_JSON_HEADER_DECLARATIONS(NewColumnStats<CppType>);
+// DEFINE_JSON_BODY_DECLARATIONS(NewColumnStats<CppType>);
+}  // namespace terrier::optimizer
--- test/optimizer/selectivity_util_test.cpp
+++ test/optimizer/selectivity_util_test.cpp
@@ -2,63 +2,87 @@
 
 #include <memory>
 
+#include "execution/sql/value.h"
 #include "gtest/gtest.h"
 #include "test_util/test_harness.h"
 
 namespace terrier::optimizer {
 class SelectivityUtilTests : public TerrierTest {
  protected:
-  ColumnStats column_stats_obj_1_;
+  NewColumnStats<execution::sql::Real> column_stats_obj_1_;
+  NewColumnStats<execution::sql::Integer> column_stats_obj_2_;
   SelectivityUtil selectivity_util;
 
   void SetUp() override {
-    column_stats_obj_1_ = ColumnStats(catalog::db_oid_t(1), catalog::table_oid_t(1),
+    // Floating point type column.
+    column_stats_obj_1_ = NewColumnStats<execution::sql::Real>(catalog::db_oid_t(1), catalog::table_oid_t(1),
                                       catalog::col_oid_t(1), 5, 4, 0.2,
                                       {3, 4, 5}, {2, 2, 2}, {1.0, 5.0},
                                       true);
+    // Integer type column.
+    column_stats_obj_2_ = NewColumnStats<execution::sql::Integer>(catalog::db_oid_t(1), catalog::table_oid_t(1),
+                                                               catalog::col_oid_t(3), 5, 4, 0.2,
+                                                               {3, 4, 5}, {2, 2, 2}, {1, 5},
+                                                               true);
   }
 };
 
 // NOLINTNEXTLINE
-TEST_F(SelectivityUtilTests, TestDecimal1) {
+TEST_F(SelectivityUtilTests, TestFloat1) {
   // Create a constant value expression to pass to ValueCondition.
+  // Floating point numbers have DECIMAL type and are represented as Reals in execution layer.
   std::unique_ptr<parser::ConstantValueExpression> const_value_expr_ptr =
       std::make_unique<parser::ConstantValueExpression>(type::TypeId::DECIMAL, execution::sql::Real(6.f));
   // Create a value condition to pass to SelectivityUtil.
   ValueCondition value_condition(catalog::col_oid_t(1), parser::ExpressionType::COMPARE_LESS_THAN,
                                  std::move(const_value_expr_ptr));
-  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<ColumnStats>(&column_stats_obj_1_),
+  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<NewColumnStats<execution::sql::Real>>(&column_stats_obj_1_),
                                                    value_condition);
   // The value 6 goes past the last bucket in the histogram and so selectivity must be predicted to be 1.
   ASSERT_EQ(res, 1.f);
 }
 
-TEST_F(SelectivityUtilTests, TestDecimal2) {
+TEST_F(SelectivityUtilTests, TestFloat2) {
   // Create a constant value expression to pass to ValueCondition.
+  // Floating point numbers have DECIMAL type and are represented as Reals in execution layer.
   std::unique_ptr<parser::ConstantValueExpression> const_value_expr_ptr =
           std::make_unique<parser::ConstantValueExpression>(type::TypeId::DECIMAL, execution::sql::Real(3.f));
   // Create a value condition to pass to SelectivityUtil.
   ValueCondition value_condition(catalog::col_oid_t(1), parser::ExpressionType::COMPARE_LESS_THAN,
                                  std::move(const_value_expr_ptr));
-  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<ColumnStats>(&column_stats_obj_1_),
+  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<NewColumnStats<execution::sql::Real>>(&column_stats_obj_1_),
                                                    value_condition);
   // The value 3 falls in the last bucket of the histogram (with 2 buckets) and so selectivity is predicted to be 0.5.
   ASSERT_EQ(res, 0.5f);
 }
 
-TEST_F(SelectivityUtilTests, TestDecimal3) {
+TEST_F(SelectivityUtilTests, TestFloat3) {
   // Create a constant value expression to pass to ValueCondition.
+  // Floating point numbers have DECIMAL type and are represented as Reals in execution layer.
   std::unique_ptr<parser::ConstantValueExpression> const_value_expr_ptr =
           std::make_unique<parser::ConstantValueExpression>(type::TypeId::DECIMAL, execution::sql::Real(0.f));
   // Create a value condition to pass to SelectivityUtil.
   ValueCondition value_condition(catalog::col_oid_t(1), parser::ExpressionType::COMPARE_LESS_THAN,
                                  std::move(const_value_expr_ptr));
-  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<ColumnStats>(&column_stats_obj_1_),
+  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<NewColumnStats<execution::sql::Real>>(&column_stats_obj_1_),
                                                    value_condition);
   // The value 0 falls in the first bucket of the histogram (with 2 buckets) and so selectivity is predicted to be 0.
   ASSERT_EQ(res, 0.f);
 }
 
+// NOLINTNEXTLINE
+TEST_F(SelectivityUtilTests, TestInteger1) {
+  // Create a constant value expression to pass to ValueCondition.
+  std::unique_ptr<parser::ConstantValueExpression> const_value_expr_ptr =
+      std::make_unique<parser::ConstantValueExpression>(type::TypeId::INTEGER, execution::sql::Integer(6));
+  // Create a value condition to pass to SelectivityUtil.
+  ValueCondition value_condition(catalog::col_oid_t(3), parser::ExpressionType::COMPARE_LESS_THAN,
+                                 std::move(const_value_expr_ptr));
+  double res = selectivity_util.ComputeSelectivity(common::ManagedPointer<NewColumnStats<execution::sql::Integer>>(&column_stats_obj_2_),
+                                                   value_condition);
+  // The value 6 goes past the last bucket in the histogram and so selectivity must be predicted to be 1.
+  ASSERT_EQ(res, 1);
+}
 }  // namespace terrier::optimizer
 
 
