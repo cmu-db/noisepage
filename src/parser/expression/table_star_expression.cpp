09c1e3730f919bc875a5bbde9ec1c644bf69737a
William Zhang <17zhangw@gmail.com>
2020-10-20 02:07:48 -0400

    Copy

--- src/parser/expression/table_star_expression.cpp
+++ src/parser/expression/table_star_expression.cpp
@@ -7,6 +7,8 @@ namespace terrier::parser {
 std::unique_ptr<AbstractExpression> TableStarExpression::Copy() const {
   auto expr = std::make_unique<TableStarExpression>();
   expr->SetMutableStateForCopy(*this);
+  expr->target_table_specified_ = target_table_specified_;
+  expr->target_table_ = target_table_;
   return expr;
 }
 
