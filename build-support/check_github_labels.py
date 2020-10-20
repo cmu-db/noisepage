15edcd5c7a49a153dfa264ce44e3ea4917e53722
Wan Shen Lim <wanshen.lim@gmail.com>
2020-10-20 01:14:02 -0400

    Switch from requests to urllib.

--- build-support/check_github_labels.py
+++ build-support/check_github_labels.py
@@ -5,11 +5,11 @@ If a PR number cannot be detected from the current working directory,
 or if the tags are present, then the script exits with error code 0.
 Otherwise the script exits with error code 1.
 '''
-import re
+import json
 import os
+import re
 import sys
-
-import requests
+import urllib.request
 
 
 def get_pr_num():
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         sys.exit(0)
     # Otherwise, get the labels for the PR.
     api_url = r'https://api.github.com/repos/cmu-db/noisepage/issues/{}/labels'
-    req = requests.get(api_url.format(pr_num))
+    req = json.loads(urllib.request.urlopen(api_url.format(pr_num)).read().decode('utf-8'))
     labels = [label['name'] for label in req.json()]
 
     if 'ready-for-review' in labels or 'ready-to-merge' in labels:
