8a4c173ca7586a264998d8d8e3b5bb3f1b0343cc
Wan Shen Lim <wanshen.lim@gmail.com>
2020-10-27 14:05:23 -0400

    potato

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -25,6 +25,7 @@ OSX_BUILD_PACKAGES=(\
   "pkg-config" \
   "ninja" \
   "tbb" \
+  "zeromq" \
 )
 OSX_TEST_PACKAGES=(\
   "ant" \
@@ -45,6 +46,7 @@ LINUX_BUILD_PACKAGES=(\
   "libpq-dev" \
   "libpqxx-dev" \
   "libtbb-dev" \
+  "libzmq3-dev" \
   "lld" \
   "llvm-8" \
   "pkg-config" \
