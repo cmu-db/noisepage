eda5239b3e7fcd9c045c9654ffbfc1adfa5b03c4
rabbit721 <001207jiaojie@gmail.com>
2020-11-08 00:21:22 -0500

    restored metrics_test

--- test/metrics/metrics_test.cpp
+++ test/metrics/metrics_test.cpp
@@ -5,14 +5,10 @@
 #include <thread>  //NOLINT
 #include <unordered_map>
 #include <utility>
-#include <iostream>
-#include <fstream>
 
-#include "execution/exec_defs.h"
 #include "main/db_main.h"
 #include "metrics/metrics_manager.h"
 #include "metrics/metrics_store.h"
-#include "parser/expression/constant_value_expression.h"
 #include "settings/settings_callbacks.h"
 #include "settings/settings_manager.h"
 #include "storage/sql_table.h"
@@ -40,17 +36,17 @@ class MetricsTests : public TerrierTest {
     std::unordered_map<settings::Param, settings::ParamInfo> param_map;
     settings::SettingsManager::ConstructParamMap(param_map);
     db_main_ = noisepage::DBMain::Builder()
-                   .SetSettingsParameterMap(std::move(param_map))
-                   .SetUseSettingsManager(true)
-                   .SetUseGC(true)
-                   .SetUseCatalog(true)
-                   .SetUseGCThread(true)
-                   .SetUseTrafficCop(true)
-                   .SetUseStatsStorage(true)
-                   .SetUseLogging(true)
-                   .SetUseNetwork(true)
-                   .SetUseExecution(true)
-                   .Build();
+        .SetSettingsParameterMap(std::move(param_map))
+        .SetUseSettingsManager(true)
+        .SetUseGC(true)
+        .SetUseCatalog(true)
+        .SetUseGCThread(true)
+        .SetUseTrafficCop(true)
+        .SetUseStatsStorage(true)
+        .SetUseLogging(true)
+        .SetUseNetwork(true)
+        .SetUseExecution(true)
+        .Build();
 
     settings_manager_ = db_main_->GetSettingsManager();
     metrics_manager_ = db_main_->GetMetricsManager();
@@ -355,123 +351,6 @@ TEST_F(MetricsTests, QueryCSVTest) {
   action_context = std::make_unique<common::ActionContext>(common::action_id_t(2));
   settings_manager_->SetBool(settings::Param::query_trace_metrics_enable, false, common::ManagedPointer(action_context),
                              setter_callback);
-}
-
-TEST_F(MetricsTests, QueryLoadTest) {
-  uint8_t NUM_COLS = 4;
-  std::vector<parser::ConstantValueExpression> params;
-  // Create an input filestream
-  std::ifstream myFile("./query_trace.csv");
-  // Make sure the file is open
-  if(!myFile.is_open()) throw std::runtime_error("Could not open file");
-  
-  // Helper vars
-  std::string line, val_string, type_string;
-  if(!myFile.good()) throw std::runtime_error("File stream is not good");
-
-  // ignore header
-  std::getline(myFile, line);
-
-  // Read data, line by line
-  execution::query_id_t query_id;
-  size_t pos, pos2, colnum;
-  bool null_detected;
-  std::vector<std::string> val_vec (NUM_COLS, "");
-  
-
-  while(std::getline(myFile, line))
-  {
-    colnum = 0;
-    val_vec.assign(NUM_COLS, "");
-    // std::cout << line << "\n" << std::flush;
-    while ((pos = line.find(",")) != std::string::npos && colnum < NUM_COLS) {
-      if (pos > 0) {
-        val_vec[colnum] = line.substr(0, pos);
-      }
-      line.erase(0, pos + 2);
-      colnum ++;
-    }
-
-    if (val_vec[0] == "") {
-      // query_id not found
-      continue;
-    }
-
-    query_id = static_cast<execution::query_id_t>(std::stoi(val_vec[0]));
-    val_string = val_vec[2];
-    type_string = val_vec[3];
-
-    std::vector<parser::ConstantValueExpression> param_vec;
-    while ((pos = val_string.find(";")) != std::string::npos 
-           && (pos2 = type_string.find(";")) != std::string::npos) {
-      if (pos > 0) {
-        auto cve = parser::ConstantValueExpression::FromString(val_string.substr(0, pos), 
-                                                               std::stoi(type_string.substr(0, pos2)));
-        
-        if (cve.ToString() != val_string.substr(0, pos)) {
-          std::cout << cve.ToString() << " " << val_string.substr(0, pos) << std::endl;
-        }
-        param_vec.push_back(cve);
-        // std::cout << cve.ToString() << "," << std::flush;
-        // std::cout << val_vec.at(counter) << " " << val_string << ",\n" << std::flush;
-      } else {
-        std::cout << "null value detected in query params recorded\n" << std::flush;
-        null_detected = true;
-        break;
-      }
-      val_string.erase(0, pos + 1);
-      type_string.erase(0, pos2 + 1);
-    }
-
-    // std::cout << "\n" << std::flush;
-    // std::cout << "\nnewline\n" << std::endl;
-  }
-  // Close file
-  myFile.close();
-}
-
-TEST_F(MetricsTests, QueryLoadTest2) {
-  uint8_t NUM_COLS = 3;
-  // Create an input filestream
-  std::ifstream myFile("./query_text.csv");
-  // Make sure the file is open
-  if(!myFile.is_open()) throw std::runtime_error("Could not open file");
-  
-  // Helper vars
-  std::string line;
-  if(!myFile.good()) throw std::runtime_error("File stream is not good");
-
-  // ignore header
-  std::getline(myFile, line);
-
-  // Read data, line by line
-  execution::query_id_t query_id;
-  size_t pos, colnum;
-  std::vector<std::string> val_vec (NUM_COLS, "");
-
-  while(std::getline(myFile, line))
-  {
-    colnum = 0;
-    val_vec.assign(NUM_COLS, "");
-    // std::cout << line << "\n" << std::flush;
-    while ((pos = line.find(",")) != std::string::npos && colnum < NUM_COLS) {
-      if (pos > 0) {
-        val_vec[colnum] = line.substr(0, pos);
-      }
-      line.erase(0, pos + 2);
-      colnum ++;
-    }
-
-    if (val_vec[0] == "") {
-      // query_id not found
-      continue;
-    }
-    query_id = static_cast<execution::query_id_t>(std::stoi(val_vec[0]));
-    // std::cout << "\n" << std::flush;
-    // std::cout << "\nnewline\n" << std::endl;
-  }
-  // Close file
-  myFile.close();
 }
 
 /**
