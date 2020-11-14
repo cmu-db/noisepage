b9014956b42b4036ef4ec650d2a03d287dd46389
Daniel Biales <bialesdaniel@gmail.com>
2020-11-14 15:25:36 -0500

    Fix junit bug

--- script/testing/junit/test_junit.py
+++ script/testing/junit/test_junit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-from junit import constants
+from junit.constants import DEFAULT_PREPARE_THRESHOLD
 from util.test_server import TestServer
 
 
@@ -13,14 +13,12 @@ class TestJUnit(TestServer):
         TestServer.__init__(self, args)
 
         self.query_mode = args.get("query_mode", "simple")
-        self.set_env_vars(self.query_mode)
+        self.set_env_vars(self.query_mode, args.get('prepare_threshold'))
 
-    def set_env_vars(self, query_mode):
+    def set_env_vars(self, query_mode, prepare_threshold=DEFAULT_PREPARE_THRESHOLD):
         # set env var for QUERY_MODE
         os.environ["NOISEPAGE_QUERY_MODE"] = query_mode
 
         # set env var for PREPARE_THRESHOLD if the QUERY_MODE is 'extended'
         if query_mode == "extended":
-            prepare_threshold = self.args.get(
-                "prepare_threshold", constants.DEFAULT_PREPARE_THRESHOLD)
             os.environ["NOISEPAGE_PREPARE_THRESHOLD"] = str(prepare_threshold)
