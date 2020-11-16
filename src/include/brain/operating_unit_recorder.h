354f5e9920beea46311b700f5f27865530f87537
Lin Ma <malin1993ml@gmail.com>
2020-11-16 00:12:37 -0500

    Unused field.

--- src/include/brain/operating_unit_recorder.h
+++ src/include/brain/operating_unit_recorder.h
@@ -57,7 +57,7 @@ class OperatingUnitRecorder : planner::PlanVisitor {
                                  common::ManagedPointer<execution::ast::Context> ast_ctx,
                                  common::ManagedPointer<execution::compiler::Pipeline> pipeline,
                                  common::ManagedPointer<const std::string> query_text)
-      : accessor_(accessor), ast_ctx_(ast_ctx), current_pipeline_(pipeline), query_text_(query_text) {}
+      : accessor_(accessor), ast_ctx_(ast_ctx), current_pipeline_(pipeline) {}
 
   /**
    * Extracts features from OperatorTranslators
@@ -228,8 +228,6 @@ class OperatingUnitRecorder : planner::PlanVisitor {
 
   /** Pipeline, used to figure out if current translator is Build or Probe. */
   common::ManagedPointer<execution::compiler::Pipeline> current_pipeline_;
-
-  common::ManagedPointer<const std::string> query_text_;
 };
 
 }  // namespace noisepage::brain
