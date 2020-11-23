3b848a734f7ad378c70de853ec156cab94eae3c6
Tpan496 <tianleip@andrew.cmu.edu>
2020-11-23 00:06:44 -0500

    more messup

--- src/include/messenger/messenger_defs.h
+++ src/include/messenger/messenger_defs.h
@@ -8,18 +8,19 @@
 namespace noisepage::messenger {
 
 class Messenger;
-class ZmqMessage;
 
 /**
  * All messages can take in a callback function to be invoked when a reply is received.
  */
-using CallbackFn = std::function<void(common::ManagedPointer<Messenger> messenger, const ZmqMessage &msg)>;
+using CallbackFn = std::function<void(common::ManagedPointer<Messenger> messenger, std::string_view sender_id,
+                                      std::string_view message, uint64_t recv_cb_id)>;
 
 /** Predefined convenience callbacks. */
 class CallbackFns {
  public:
   /** A noop version of CallbackFn, provided for convenience. */
-  static void Noop(common::ManagedPointer<Messenger> messenger, const ZmqMessage &msg) {}
+  static void Noop(common::ManagedPointer<Messenger> messenger, std::string_view sender_id, std::string_view message,
+                   uint64_t recv_cb_id) {}
 };
 
 }  // namespace noisepage::messenger
