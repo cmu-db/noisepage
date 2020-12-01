98dbf3e10b748055a530cb9578371d925b8e51d1
Daniel Biales <bialesdaniel@gmail.com>
2020-11-30 21:58:30 -0500

    jenkins debug

--- Jenkinsfile-utils.groovy
+++ Jenkinsfile-utils.groovy
@@ -14,36 +14,36 @@ def noisePageBuild(Map args = [:]){
     def String compile_cmd = "cmake -GNinja -DNoisePage_UNITY_BUILD=ON -DCMAKE_BUILD_TYPE=$config.buildType"
     
     if(config.isCodeCoverage){
-        compile_cmd += "-DNOISEPAGE_GENERATE_COVERAGE=ON"
+        compile_cmd += " -DNOISEPAGE_GENERATE_COVERAGE=ON"
     }
 
     if(config.useCache){
-        compile_cmd += "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+        compile_cmd += " -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
     }
 
     if(config.os=='ubuntu' && config.isBuildTests){
-        compile_cmd += "-DNOISEPAGE_TEST_PARALLELISM=\$(nproc)"
+        compile_cmd += " -DNOISEPAGE_TEST_PARALLELISM=\$(nproc)"
     }
 
     if(config.useASAN){
-        compile_cmd += "-DNOISEPAGE_USE_ASAN=ON"
+        compile_cmd += " -DNOISEPAGE_USE_ASAN=ON"
     }
 
     if(!config.isBuildBenchmarks){
-        compile_cmd += "-DNOISEPAGE_BUILD_BENCHMARKS=OFF"
+        compile_cmd += " -DNOISEPAGE_BUILD_BENCHMARKS=OFF"
     }
 
     if(config.buildType=='Release' && !config.isBuildTests){
-        compile_cmd += "-DNOISEPAGE_USE_JEMALLOC=ON"
+        compile_cmd += " -DNOISEPAGE_USE_JEMALLOC=ON"
     }
 
     if(config.isJumboTest){
-        compile_cmd += "-DNOISEPAGE_USE_JUMBOTESTS=ON"
+        compile_cmd += " -DNOISEPAGE_USE_JUMBOTESTS=ON"
     }
 
     def String build_cmd = "ninja"
     if(!config.isBuildBenchmarks && !config.isBuildTests){
-        build_cmd += "noisepage"
+        build_cmd += " noisepage"
     }
 
     sh script: compile_cmd, label: 'Compiling'
