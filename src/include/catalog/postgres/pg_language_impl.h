34c17b080930cc9f6294fc42869daff0ff0d3cc1
Wan Shen Lim <wanshen.lim@gmail.com>
2020-12-02 18:00:16 -0500

    More documentation for pg_language.

--- src/include/catalog/postgres/pg_language_impl.h
+++ src/include/catalog/postgres/pg_language_impl.h
@@ -25,23 +25,35 @@ class Builder;
 
 /** The NoisePage version of pg_language. */
 class PgLanguageImpl {
- public:
+ private:
+  friend class Builder;                   ///< The builder is used to construct pg_language.
+  friend class storage::RecoveryManager;  ///< The RM accesses tables and indexes without going through the catalog.
+  friend class catalog::DatabaseCatalog;  ///< DatabaseCatalog sets up and owns pg_language.
+
   /**
-   * Prepare to create pg_language.
+   * @brief Prepare to create pg_language.
+   *
    * Does NOT create anything until the relevant bootstrap functions are called.
    *
    * @param db_oid          The OID of the database that pg_language should be created in.
    */
   explicit PgLanguageImpl(db_oid_t db_oid);
 
-  /** Bootstrap the projected row initializers for pg_language. */
+  /** @brief Bootstrap the projected row initializers for pg_language. */
   void BootstrapPRIs();
 
   /**
+   * @brief Create pg_language and associated indexes.
+   *
    * Bootstrap:
    *    pg_language
    *    pg_languages_oid_index
    *    pg_languages_name_index
+   *
+   * Dependencies (for bootstrapping):
+   *    pg_core must have been bootstrapped.
+   * Dependencies (for execution):
+   *    No other dependencies.
    *
    * @param txn             The transaction to bootstrap in.
    * @param dbc             The catalog object to bootstrap in.
@@ -50,32 +62,32 @@ class PgLanguageImpl {
                  common::ManagedPointer<DatabaseCatalog> dbc);
 
   /**
-   * Create a language entry in the pg_language table.
+   * @brief Create a language entry in the pg_language table.
    *
    * @param txn             The transaction to use.
    * @param lanname         The name of the language to insert.
    * @param oid             The OID to assign to the language.
-   * @return True if the creation succeeded. False otherwise.
+   * @return                True if the creation succeeded. False otherwise.
    */
   bool CreateLanguage(const common::ManagedPointer<transaction::TransactionContext> txn, const std::string &lanname,
                       language_oid_t oid);
 
   /**
-   * Look up a language entry in the pg_language table.
+   * @brief Look up a language entry in the pg_language table.
    *
    * @param txn             The transaction to use.
    * @param lanname         The name of the language to look up.
-   * @return The OID of the language if found. Else INVALID_LANGUAGE_OID.
+   * @return                The OID of the language if found. Else INVALID_LANGUAGE_OID.
    */
   language_oid_t GetLanguageOid(common::ManagedPointer<transaction::TransactionContext> txn,
                                 const std::string &lanname);
 
   /**
-   * Delete a language entry from the pg_language table.
+   * @brief Delete a language entry from the pg_language table.
    *
    * @param txn             The transaction to use.
    * @param oid             The OID of the language to delete.
-   * @return True if deletion is successful. False otherwise.
+   * @return                True if deletion is successful. False otherwise.
    */
   bool DropLanguage(common::ManagedPointer<transaction::TransactionContext> txn, language_oid_t oid);
 
@@ -83,7 +95,7 @@ class PgLanguageImpl {
   friend class Builder;
   friend class storage::RecoveryManager;
 
-  /** Bootstrap all the builtin languages in pg_languages. */
+  /** @brief Bootstrap all the builtin languages in pg_languages. */
   void BootstrapLanguages(common::ManagedPointer<transaction::TransactionContext> txn,
                           common::ManagedPointer<DatabaseCatalog> dbc);
 
