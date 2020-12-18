94acb530927c381b318a29df7826a4848f88dda7
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-12-17 22:59:58 -0500

    fix memory issue?

--- src/binder/bind_node_visitor.cpp
+++ src/binder/bind_node_visitor.cpp
@@ -472,8 +472,7 @@ void BindNodeVisitor::Visit(common::ManagedPointer<parser::InsertStatement> node
               auto const_ret_type = const_val->GetReturnValueType();
 
               // we need to reallocate the buffer to fit the new truncated size
-              auto data = common::ManagedPointer(const_val->GetStringVal().GetContent());
-              auto resized_str_val = execution::sql::ValueUtil::CreateStringVal(data, true_len);
+              auto resized_str_val = execution::sql::ValueUtil::CreateStringVal(common::ManagedPointer(const_val->GetStringVal().GetContent()), true_len);
               const_val->SetValue(const_ret_type, resized_str_val.first, std::move(resized_str_val.second));
               ins_val = const_val.CastManagedPointerTo<parser::AbstractExpression>();
             }
