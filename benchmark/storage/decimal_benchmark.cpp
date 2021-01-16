59b2fb4b0ae36312abe6396efbd07e84ad55d69c
rohan <rohanagg@andrew.cmu.edu>
2021-01-15 22:23:56 -0500

    cleanup final

--- benchmark/storage/decimal_benchmark.cpp
+++ benchmark/storage/decimal_benchmark.cpp
@@ -12,7 +12,7 @@ class DecimalBenchmark : public benchmark::Fixture {
   void SetUp(const benchmark::State &state) final {
     // generate a random column of decimals and floats
     for (unsigned i = 0; i < 1000000; i++) {
-      execution::sql::Decimal128 d(i);
+      noisepage::execution::sql::Decimal128 d(i);
       decimals_.push_back(d);
       float f = (static_cast<float>(i)) / (static_cast<float>(1000));
       floats_.push_back(f);
@@ -22,19 +22,19 @@ class DecimalBenchmark : public benchmark::Fixture {
   void TearDown(const benchmark::State &state) final {}
 
   // Read buffers pointers for concurrent reads
-  std::vector<execution::sql::Decimal128> decimals_;
-  std::vector<int128_t> floats_;
+  std::vector<noisepage::execution::sql::Decimal128> decimals_;
+  std::vector<float> floats_;
 };
 
 // NOLINTNEXTLINE
 BENCHMARK_DEFINE_F(DecimalBenchmark, AddDecimal)(benchmark::State &state) {
   uint64_t elapsed_ms;
   {
-    execution::sql::Decimal128 result(0);
-    common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
+    noisepage::execution::sql::Decimal128 result(0);
+    noisepage::common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
     for (unsigned j = 0; j < 1000; j++)
-      for (unsigned i = 0; i < decimals_.size(); i++) {
-        result += decimals_[i];
+      for (auto & decimal : decimals_) {
+        result += decimal;
       }
   }
   state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
@@ -45,10 +45,10 @@ BENCHMARK_DEFINE_F(DecimalBenchmark, AddFloat)(benchmark::State &state) {
   uint64_t elapsed_ms;
   {
     float result = 0;
-    common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
+    noisepage::common::ScopedTimer<std::chrono::milliseconds> timer(&elapsed_ms);
     for (unsigned j = 0; j < 1000; j++)
-      for (unsigned i = 0; i < floats_.size(); i++) {
-        result += floats_[i];
+      for (auto & fl : floats_) {
+        result += fl;
       }
   }
   state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
