a4b7458086e85b29bd26d52d08b816243cdac180
Wan Shen Lim <wanshen.lim@gmail.com>
2021-01-18 02:32:08 -0500

    Clean up decimal_multiplication_generator.py.

--- script/fixed_decimal/decimal_multiplication_generator.py
+++ script/fixed_decimal/decimal_multiplication_generator.py
@@ -1,50 +1,36 @@
-import math
+#!/usr/bin/env python3
+import random
 
-from random import seed
-from random import randint
 
-# seed random number generator
+MAX_NUM_BITS = 38
 
-seed(1)
 
-# generate some integers
-
-for i in range(10000000):
-
-    precision = i % 38
-    precision += 1
-    d1 = '0.'
-    d2 = '0.'
-
-    di1 = 0
-    di2 = 0
-
+def generate_random_num(precision):
+    result = 0
     for j in range(precision):
-        random_num = randint(0, 9)
+        random_num = random.randint(0, 9)
         if j == 0 and random_num == 0:
             random_num = random_num + 1
-        di1 += random_num
+        result += random_num
         if j != precision - 1:
-            di1 *= 10
+            result *= 10
+    return result
 
-    for j in range(precision):
-        random_num = randint(0, 9)
-        if j == 0 and random_num == 0:
-            random_num = random_num + 1
-        di2 += random_num
 
-        if j != precision - 1:
-            di2 *= 10
+if __name__ == "__main__":
+    random.seed(1)
+    for i in range(10000000):
 
-    d1 = d1 + str(di1)
-    d2 = d2 + str(di2)
+        precision = (i % MAX_NUM_BITS) + 1          # [1, MAX_NUM_BITS]
 
-    stresult = str(di1 * di2)
+        di1 = generate_random_num(precision)
+        di2 = generate_random_num(precision)
 
-    if len(stresult) == 2 * precision:
-        stresult = '0.' + stresult[0:precision]
-    else:
-        stresult = '0.0' + stresult[0:precision - 1]
+        result = str(di1 * di2)
 
-    print d1 + ' ' + str(precision) + ' ' + d2 + ' ' + str(precision) \
-          + ' ' + stresult + ' ' + str(precision)
+        if len(result) == 2 * precision:
+            result = '0.' + result[0:precision]
+        else:
+            result = '0.0' + result[0:precision - 1]
+
+        print(f"0.{di1} {precision} 0.{di2} {precision} {result} {precision}")
