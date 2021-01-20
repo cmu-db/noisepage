6ddf1a74ad90c6f4798ae4f9fe3e2d5de2bf681f
Wan Shen Lim <wanshen.lim@gmail.com>
2021-01-19 19:51:44 -0500

    Whack messenger_test with a pthread hammer.
    I noticed that a stuck run of messenger_test could have been a
    victim of unfortunate scheduling on a busy CI box.
    The forked processes were sleeping while the original process was
    spinning, because all my coordination previously was based on
    dirty sleeping hacks.
    The scheduler never woke up those sleeping processes.
    
    This brings in the pthread_mutex and pthread_cond hammers.
    By waiting on the condvar, our forked processes should sleep
    if they cannot make useful progress.
    
    The code also looks awful now and if this doesn't fix messenger_test
    getting stuck occasionally, I will probably look at writing a Python
    harness of some kind.

--- test/messenger/messenger_test.cpp
+++ test/messenger/messenger_test.cpp
@@ -1,5 +1,6 @@
 #include "messenger/messenger.h"
 
+#include <pthread.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -112,14 +113,40 @@ TEST_F(MessengerTests, BasicReplicationTest) {
   done[1] = false;
   done[2] = false;
 
-  auto spin_until_init = [init]() {
+  auto *pmutex = static_cast<pthread_mutex_t *>(
+      mmap(nullptr, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  NOISEPAGE_ASSERT(MAP_FAILED != pmutex, "mmap() failed.");
+  pthread_mutexattr_t mutexattr;
+  pthread_mutexattr_init(&mutexattr);
+  pthread_mutexattr_setpshared(&mutexattr, PTHREAD_PROCESS_SHARED);
+  pthread_mutex_init(pmutex, &mutexattr);
+
+  auto *pcond = static_cast<pthread_cond_t *>(
+      mmap(nullptr, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  NOISEPAGE_ASSERT(MAP_FAILED != pcond, "mmap() failed.");
+  pthread_condattr_t condattr;
+  pthread_condattr_init(&condattr);
+  pthread_condattr_setpshared(&condattr, PTHREAD_PROCESS_SHARED);
+  pthread_cond_init(pcond, &condattr);
+
+  auto spin_until_init = [init, pmutex, pcond]() {
     while (!(init[0] && init[1] && init[2])) {
+      pthread_mutex_lock(pmutex);
+      pthread_cond_wait(pcond, pmutex);
+      pthread_mutex_unlock(pmutex);
     }
   };
 
-  auto spin_until_done = [done]() {
+  auto spin_until_done = [done, pmutex, pcond]() {
     while (!(done[0] && done[1] && done[2])) {
+      pthread_mutex_lock(pmutex);
+      pthread_cond_wait(pcond, pmutex);
+      pthread_mutex_unlock(pmutex);
     }
+  };
+
+  auto wake_all = [pmutex, pcond]() {
+    pthread_cond_broadcast(pcond);
   };
 
   VoidFn primary_fn = [=]() {
@@ -127,6 +154,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
     primary->GetNetworkLayer()->GetServer()->RunServer();
 
     init[0] = true;
+    wake_all();
     spin_until_init();
     DirtySleep();
 
@@ -135,6 +163,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
 
     MESSENGER_LOG_TRACE("Primary done.");
     done[0] = true;
+    wake_all();
     spin_until_done();
     MESSENGER_LOG_TRACE("Primary exit.");
     primary->ForceShutdown();
@@ -145,6 +174,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
     replica1->GetNetworkLayer()->GetServer()->RunServer();
 
     init[1] = true;
+    wake_all();
     spin_until_init();
     DirtySleep();
 
@@ -186,6 +216,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
 
     MESSENGER_LOG_TRACE("Replica 1 done.");
     done[1] = true;
+    wake_all();
     spin_until_done();
     MESSENGER_LOG_TRACE("Replica 1 exit.");
     replica1->ForceShutdown();
@@ -196,6 +227,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
     replica2->GetNetworkLayer()->GetServer()->RunServer();
 
     init[2] = true;
+    wake_all();
     spin_until_init();
     DirtySleep();
 
@@ -237,6 +269,7 @@ TEST_F(MessengerTests, BasicReplicationTest) {
 
     MESSENGER_LOG_TRACE("Replica 2 done.");
     done[2] = true;
+    wake_all();
     spin_until_done();
     MESSENGER_LOG_TRACE("Replica 2 exit.");
     replica2->ForceShutdown();
@@ -245,8 +278,12 @@ TEST_F(MessengerTests, BasicReplicationTest) {
   std::vector<pid_t> pids = ForkTests({primary_fn, replica1_fn, replica2_fn});
 
   // Spin until all done.
-  while (!(done[0] && done[1] && done[2])) {
-  }
+  spin_until_done();
+
+  pthread_mutex_destroy(pmutex);
+  pthread_mutexattr_destroy(&mutexattr);
+  pthread_cond_destroy(pcond);
+  pthread_condattr_destroy(&condattr);
 
   DirtySleep();
   {
@@ -255,6 +292,14 @@ TEST_F(MessengerTests, BasicReplicationTest) {
   }
   {
     UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(const_cast<bool *>(done)), 3 * sizeof(bool));
+    NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
+  }
+  {
+    UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(pmutex), sizeof(pthread_mutex_t));
+    NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
+  }
+  {
+    UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(pcond), sizeof(pthread_cond_t));
     NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
   }
 }
@@ -283,6 +328,22 @@ TEST_F(MessengerTests, BasicListenTest) {
   done[0] = false;
   done[1] = false;
 
+  auto *pmutex = static_cast<pthread_mutex_t *>(
+      mmap(nullptr, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  NOISEPAGE_ASSERT(MAP_FAILED != pmutex, "mmap() failed.");
+  pthread_mutexattr_t mutexattr;
+  pthread_mutexattr_init(&mutexattr);
+  pthread_mutexattr_setpshared(&mutexattr, PTHREAD_PROCESS_SHARED);
+  pthread_mutex_init(pmutex, &mutexattr);
+
+  auto *pcond = static_cast<pthread_cond_t *>(
+      mmap(nullptr, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  NOISEPAGE_ASSERT(MAP_FAILED != pcond, "mmap() failed.");
+  pthread_condattr_t condattr;
+  pthread_condattr_init(&condattr);
+  pthread_condattr_setpshared(&condattr, PTHREAD_PROCESS_SHARED);
+  pthread_cond_init(pcond, &condattr);
+
   auto spin_until_init = [init]() {
     while (!(init[0] && init[1])) {
     }
@@ -291,6 +352,10 @@ TEST_F(MessengerTests, BasicListenTest) {
   auto spin_until_done = [done]() {
     while (!(done[0] && done[1])) {
     }
+  };
+
+  auto wake_all = [pmutex, pcond]() {
+    pthread_cond_broadcast(pcond);
   };
 
   VoidFn primary_fn = [=]() {
@@ -311,6 +376,7 @@ TEST_F(MessengerTests, BasicListenTest) {
         });
 
     init[0] = true;
+    wake_all();
     spin_until_init();
     DirtySleep();
 
@@ -319,6 +385,7 @@ TEST_F(MessengerTests, BasicListenTest) {
 
     MESSENGER_LOG_TRACE("Primary done.");
     done[0] = true;
+    wake_all();
     spin_until_done();
     MESSENGER_LOG_TRACE("Primary exit.");
     primary->ForceShutdown();
@@ -328,6 +395,7 @@ TEST_F(MessengerTests, BasicListenTest) {
     auto replica1 = BuildDBMain(port_replica1, port_messenger_replica1, "replica1");
     replica1->GetNetworkLayer()->GetServer()->RunServer();
     init[1] = true;
+    wake_all();
     spin_until_init();
     DirtySleep();
 
@@ -363,6 +431,7 @@ TEST_F(MessengerTests, BasicListenTest) {
 
     MESSENGER_LOG_TRACE("Replica 1 done.");
     done[1] = true;
+    wake_all();
     spin_until_done();
     MESSENGER_LOG_TRACE("Replica 1 exit.");
     replica1->ForceShutdown();
@@ -371,8 +440,12 @@ TEST_F(MessengerTests, BasicListenTest) {
   std::vector<pid_t> pids = ForkTests({primary_fn, replica1_fn});
 
   // Spin until all done.
-  while (!(done[0] && done[1])) {
-  }
+  spin_until_done();
+
+  pthread_mutex_destroy(pmutex);
+  pthread_mutexattr_destroy(&mutexattr);
+  pthread_cond_destroy(pcond);
+  pthread_condattr_destroy(&condattr);
 
   DirtySleep();
   {
@@ -381,6 +454,14 @@ TEST_F(MessengerTests, BasicListenTest) {
   }
   {
     UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(const_cast<bool *>(done)), 2 * sizeof(bool));
+    NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
+  }
+  {
+    UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(pmutex), sizeof(pthread_mutex_t));
+    NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
+  }
+  {
+    UNUSED_ATTRIBUTE int munmap_retval = munmap(static_cast<void *>(pcond), sizeof(pthread_cond_t));
     NOISEPAGE_ASSERT(-1 != munmap_retval, "munmap() failed.");
   }
 }
