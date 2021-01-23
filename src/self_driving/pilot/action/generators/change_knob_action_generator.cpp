7193f088feb6a3d6ea16c291f5ed1c968938533b
Lin Ma <malin1993ml@gmail.com>
2021-01-22 21:51:11 -0500

    Restart the build...

--- src/self_driving/pilot/action/generators/change_knob_action_generator.cpp
+++ src/self_driving/pilot/action/generators/change_knob_action_generator.cpp
@@ -62,7 +62,7 @@ void ChangeKnobActionGenerator::GenerateActionForType(
       // Populate the reverse actions
       action_map->at(first_action_id)->AddReverseAction(second_action_id);
 
-      // Note that change knob actions do not have any enabled/invalidated actions since they're based on deltas and
+      // NOTE: change knob actions do not have any enabled/invalidated actions since they're based on deltas and
       // always valid (unless exceeds the knob setting limit, which is handled by `ChangeKnobAction::IsValid()`).
     }
   }
