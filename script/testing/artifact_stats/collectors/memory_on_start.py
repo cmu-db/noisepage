d87aac48cebf840ac6c7a23380533075abff38af
Wan Shen Lim <wanshen.lim@gmail.com>
2021-02-10 01:34:26 -0500

    Refactor scripts/testing. (#1429)

--- script/testing/artifact_stats/collectors/memory_on_start.py
+++ script/testing/artifact_stats/collectors/memory_on_start.py
@@ -1,15 +1,21 @@
-import os
-import time
-import subprocess
 import psutil
 
-from artifact_stats.base_artifact_stats_collector import BaseArtifactStatsCollector
-from util.db_server import NoisePageServer
+from ...util.db_server import NoisePageServer
+from ..base_artifact_stats_collector import BaseArtifactStatsCollector
 
 
 class MemoryOnStartCollector(BaseArtifactStatsCollector):
-    def __init__(self, is_debug):
-        super().__init__(is_debug)
+    """
+    Measure the memory consumption of the NoisePage DBMS in release mode
+    on startup.
+
+    Notes
+    -----
+    The NoisePage DBMS is assumed to have been started by the same user as the
+    user running this script, otherwise sudo permission may be required.
+
+    It is assumed that no other instance of the NoisePage server is running.
+    """
 
     def setup(self):
         super().setup()
@@ -17,6 +23,9 @@ class MemoryOnStartCollector(BaseArtifactStatsCollector):
         self.db_instance.run_db()
 
     def run_collector(self):
+        """
+        Measure the memory consumption of the DBMS process.
+        """
         process = psutil.Process(self.db_instance.db_process.pid)
         memory_data = process.memory_info()
         self.metrics['rss_on_start'] = memory_data.rss
--- script/testing/collect_mem_info.py
+++ script/testing/collect_mem_info.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import os
-import sys
 import argparse
+
 from util.common import collect_mem_info
+from util.constants import MEM_INFO_SPLITTER
 
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(
         description="Collect the memory info of the given process IDs.")
-    aparser.add_argument("pids", type=int, nargs="+", help="Pids to collect")
+    aparser.add_argument("pids", type=int, nargs="+", help="PIDs to collect.")
     args = vars(aparser.parse_args())
 
     for pid in args.get("pids", []):
         mem_info = collect_mem_info(pid)
         rss = mem_info.rss if mem_info else ""
         vms = mem_info.vms if mem_info else ""
-        res = "{RSS},{VMS}".format(RSS=rss, VMS=vms)
+        res = f"{rss}{MEM_INFO_SPLITTER}{vms}"
         print(res)
