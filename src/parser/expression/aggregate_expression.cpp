ca630b619937ac0811723172de6f85164d6c5f7e
Joseph Koshakow <koshy44@gmail.com>
2021-02-16 14:08:42 -0500

    Add aggregator cleanup info for topk and histogram

--- src/parser/expression/aggregate_expression.cpp
+++ src/parser/expression/aggregate_expression.cpp
@@ -80,6 +80,9 @@ bool AggregateExpression::RequiresCleanup() const {
     case ExpressionType::AGGREGATE_SUM:
     case ExpressionType::AGGREGATE_AVG:
       return false;
+    case ExpressionType::AGGREGATE_TOP_K:
+    case ExpressionType::AGGREGATE_HISTOGRAM:
+      return true;
     default:
       throw PARSER_EXCEPTION(fmt::format("Not a valid aggregation expression type: %d", static_cast<int>(expr_type)));
   }
