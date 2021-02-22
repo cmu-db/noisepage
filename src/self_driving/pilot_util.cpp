fa9baf25fece47cc6f2c5fb6a067a7910f25df1f
Lin Ma <malin1993ml@gmail.com>
2021-02-21 21:50:00 -0500

    Remove the BUILD_ABS_PATH from pilot.

--- src/self_driving/pilot_util.cpp
+++ src/self_driving/pilot_util.cpp
@@ -261,12 +261,11 @@ void PilotUtil::InferenceWithFeatures(const std::string &model_save_path,
 
   PilotUtil::GroupFeaturesByOU(&pipeline_to_ou_position, pipeline_qids, pipeline_data, &ou_to_features);
   NOISEPAGE_ASSERT(model_server_manager->ModelServerStarted(), "Model Server should have been started");
-  std::string project_build_path = getenv(Pilot::BUILD_ABS_PATH);
   std::unordered_map<ExecutionOperatingUnitType, std::vector<std::vector<double>>> inference_result;
   for (auto &ou_map_it : ou_to_features) {
     auto res = model_server_manager->DoInference(
-        selfdriving::OperatingUnitUtil::ExecutionOperatingUnitTypeToString(ou_map_it.first),
-        project_build_path + model_save_path, ou_map_it.second);
+        selfdriving::OperatingUnitUtil::ExecutionOperatingUnitTypeToString(ou_map_it.first), model_save_path,
+        ou_map_it.second);
     if (!res.second) {
       throw PILOT_EXCEPTION("Inference through model server manager has error", common::ErrorCode::ERRCODE_WARNING);
     }
