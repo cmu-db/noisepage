97a3e3d74cd02b7c910340b2d9eedde47767c93f
turingcompl33t <kdotterrer@gmail.com>
2021-02-21 19:45:49 -0500

    remove dead code, remove one runtime call to string::append()

--- test/include/test_util/fs_util.h
+++ test/include/test_util/fs_util.h
@@ -5,12 +5,6 @@
 
 namespace noisepage::common {
 /**
- * Determine the absolute path to the project root directory (e.g. noisepage/).
- * @return The absolute path to the project root, as a string.
- */
-std::string GetProjectRootPath();
-
-/**
  * Determine the absolute path to the build directory for the current build (e.g. noisepage/build/)
  * @return The absolute path to the build directory, as a string.
  */
@@ -27,15 +21,4 @@ std::string GetBuildRootPath();
  * @return The absolute path to the binary artifact, as a string.
  */
 std::string GetBinaryArtifactPath(std::string_view name);
-
-/**
- * Determine the absolute path to the file with the given name by performing a
- * recursive directory search from the specified root path.
- * @param filename The name of the target file
- * @param root_path The absolute path from which recursive search should begin
- * @return The absolute path to a file with the specified name, as a string.
- * @throw std::runtime_error in the event the file cannot be found.
- */
-std::string FindFileFrom(std::string_view filename, std::string_view root_path);
-
 }  // namespace noisepage::common
