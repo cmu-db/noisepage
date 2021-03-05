2e31cb0f21717210d41177bcca9ae02ca8b9f91f
Wan Shen Lim <wanshen.lim@gmail.com>
2021-03-05 14:31:12 -0500

    Add a script for printing docker image name, creation time, current time.

---
+++ build-support/print_docker_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The hash of the docker image.
+# On a regular system, no clue.
+IMG_NAME=$(cat /proc/self/cgroup | grep memory | cut -d/ -f 3)
+# The time that the docker image was brought up.
+# On a regular system, the time the system started.
+IMG_TIME_CREATION=$(date --date="@$(stat -c '%Y' /proc/1/cmdline)" +'%F %T %z')
+# The current time.
+IMG_TIME_NOW=$(date +'%F %T %z')
+
+echo "Image: ${IMG_NAME}"
+echo "Image creation time : ${IMG_TIME_CREATION}"
+echo "Image current time  : ${IMG_TIME_NOW}"
