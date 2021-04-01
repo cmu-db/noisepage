442c5b8e8e755e4843d45bfd549840147a02a16c
Jie Jiao <001207jiaojie@gmail.com>
2021-04-01 15:06:13 -0400

    switched from unordered_map to map for workload forecast segments

--- src/include/self_driving/forecasting/workload_forecast_segment.h
+++ src/include/self_driving/forecasting/workload_forecast_segment.h
@@ -2,7 +2,6 @@
 
 #include <queue>
 #include <tuple>
-#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 
@@ -19,15 +18,15 @@ class WorkloadForecastSegment {
    * Constructor for WorkloadForecastSegment
    * @param id_to_num_exec Map from qids to number of execution of this query in this interval
    */
-  explicit WorkloadForecastSegment(std::unordered_map<execution::query_id_t, uint64_t> id_to_num_exec);
+  explicit WorkloadForecastSegment(std::map<execution::query_id_t, uint64_t> id_to_num_exec);
 
   /**
    * Return const reference to id_to_num_exec_
    */
-  const std::unordered_map<execution::query_id_t, uint64_t> &GetIdToNumexec() const { return id_to_num_exec_; }
+  const std::map<execution::query_id_t, uint64_t> &GetIdToNumexec() const { return id_to_num_exec_; }
 
  private:
-  std::unordered_map<execution::query_id_t, uint64_t> id_to_num_exec_;
+  std::map<execution::query_id_t, uint64_t> id_to_num_exec_;
 };
 
 }  // namespace noisepage::selfdriving
