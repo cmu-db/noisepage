743874e5656c198fed31bc0dbdf05a93fb55dea8
Tanuj Nayak <tanujnay112@live.com>
2021-04-19 09:11:32 -0700

    memory leak fixes

--- src/include/execution/sql/ind_cte_scan_iterator.h
+++ src/include/execution/sql/ind_cte_scan_iterator.h
@@ -1,88 +1,87 @@
-#pragma once
-
+- #pragma once
 #include <memory>
 #include <vector>
 #include "execution/sql/cte_scan_iterator.h"
 
-namespace noisepage::execution::sql {
-
-/**
- * An iterator over a CTE Temp table's data
- */
-class EXPORT IndCteScanIterator {
- public:
-  /**
-   * Constructor for the CTEScanIterator
-   */
-  IndCteScanIterator(exec::ExecutionContext *exec_ctx, catalog::table_oid_t table_oid, uint32_t *schema_cols_ids,
-                     uint32_t *schema_cols_type, uint32_t num_schema_cols, bool is_recursive);
+    namespace noisepage::execution::sql {
 
   /**
-   * @return Returns the temporary table that the cte has made
+   * An iterator over a CTE Temp table's data
    */
-  CteScanIterator *GetWriteCte();
+  class EXPORT IndCteScanIterator {
+   public:
+    /**
+     * Constructor for the CTEScanIterator
+     */
+    IndCteScanIterator(exec::ExecutionContext *exec_ctx, catalog::table_oid_t table_oid, uint32_t *schema_cols_ids,
+                       uint32_t *schema_cols_type, uint32_t num_schema_cols, bool is_recursive);
 
-  /**
-   * @return Returns the temporary table that the cte has made
-   */
-  CteScanIterator *GetReadCte();
+    /**
+     * @return Returns the temporary table that the cte has made
+     */
+    CteScanIterator *GetWriteCte();
 
-  /**
-   * @return Returns the oid of the temporary table that the cte has made
-   */
-  catalog::table_oid_t GetReadTableOid();
+    /**
+     * @return Returns the temporary table that the cte has made
+     */
+    CteScanIterator *GetReadCte();
 
-  /**
-   * @return Returns the CTE scan that stores the object
-   */
-  CteScanIterator *GetResultCTE();
+    /**
+     * @return Returns the oid of the temporary table that the cte has made
+     */
+    catalog::table_oid_t GetReadTableOid();
 
-  /**
-   * @return Returns a projected row of the table for insertion
-   */
-  storage::ProjectedRow *GetInsertTempTablePR();
+    /**
+     * @return Returns the CTE scan that stores the object
+     */
+    CteScanIterator *GetResultCTE();
 
-  /**
-   * @return Returns the slot which was inserted in the table using the projected row
-   */
-  storage::TupleSlot TableInsert();
+    /**
+     * @return Returns a projected row of the table for insertion
+     */
+    storage::ProjectedRow *GetInsertTempTablePR();
 
-  /**
-   * Accumulates the results of read and write table to get ready for a new
-   * iteration of writing to the write table. In this case, the write and read
-   * tables are swapped if the write table isn't empty.
-   * @return If the swap happened then true is returned, else false is returned.
-   */
-  bool Accumulate();
+    /**
+     * @return Returns the slot which was inserted in the table using the projected row
+     */
+    storage::TupleSlot TableInsert();
 
-  /**
-   * Destructor
-   */
-  ~IndCteScanIterator() = default;
+    /**
+     * Accumulates the results of read and write table to get ready for a new
+     * iteration of writing to the write table. In this case, the write and read
+     * tables are swapped if the write table isn't empty.
+     * @return If the swap happened then true is returned, else false is returned.
+     */
+    bool Accumulate();
 
-  /**
-   * This class cannot be copied or moved
-   */
-  DISALLOW_COPY_AND_MOVE(IndCteScanIterator);
+    /**
+     * Destructor
+     */
+    ~IndCteScanIterator() = default;
 
- private:
-  exec::ExecutionContext *exec_ctx_;
-  // Three CTEScanIterators that we use to store various results
-  // cte_scan_read and cte_scan_write are always pointers to one of these each,
-  // though which iterator they point to is not necessarily constant
-  CteScanIterator cte_scan_1_;
-  CteScanIterator cte_scan_2_;
-  CteScanIterator cte_scan_3_;
+    /**
+     * This class cannot be copied or moved
+     */
+    DISALLOW_COPY_AND_MOVE(IndCteScanIterator);
 
-  // Read table containing the results of the inductive queries so far
-  CteScanIterator *cte_scan_read_;
-  // Write table containing the results of the next inductive iteration
-  // These results are then moved into the read table
-  CteScanIterator *cte_scan_write_;
-  catalog::table_oid_t table_oid_;
-  common::ManagedPointer<transaction::TransactionContext> txn_;
-  bool written_;
-  bool is_recursive_;
-};
+   private:
+    exec::ExecutionContext *exec_ctx_;
+    // Three CTEScanIterators that we use to store various results
+    // cte_scan_read and cte_scan_write are always pointers to one of these each,
+    // though which iterator they point to is not necessarily constant
+    CteScanIterator cte_scan_1_;
+    CteScanIterator cte_scan_2_;
+    CteScanIterator cte_scan_3_;
+
+    // Read table containing the results of the inductive queries so far
+    CteScanIterator *cte_scan_read_;
+    // Write table containing the results of the next inductive iteration
+    // These results are then moved into the read table
+    CteScanIterator *cte_scan_write_;
+    catalog::table_oid_t table_oid_;
+    common::ManagedPointer<transaction::TransactionContext> txn_;
+    bool written_;
+    bool is_recursive_;
+  };
 
 }  // namespace noisepage::execution::sql
