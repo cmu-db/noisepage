4f272dfc2aa35e091df4b0ab8c1539da7e0b7bcc
Wan Shen Lim <wanshen.lim@gmail.com>
2021-05-25 14:55:47 -0400

    You would think that nodiscard should be the default. But nooo. C++. Legacy designs. grumble.

--- src/include/execution/vm/module_metadata.h
+++ src/include/execution/vm/module_metadata.h
@@ -15,9 +15,9 @@ class CompileTimeModuleMetadata {
   void SetTBC(std::string &&tbc) { map_["tbc"] = std::move(tbc); }
 
   /** @return The TPL in the module, if it exists. Otherwise empty string. */
-  std::string GetTPL() const { return map_.find("tpl") == map_.end() ? "" : map_.at("tpl"); }
+  [[nodiscard]] std::string GetTPL() const { return map_.find("tpl") == map_.end() ? "" : map_.at("tpl"); }
   /** @return The TBC in the module, if it exists. Otherwise empty string. */
-  std::string GetTBC() const { return map_.find("tbc") == map_.end() ? "" : map_.at("tbc"); }
+  [[nodiscard]] std::string GetTBC() const { return map_.find("tbc") == map_.end() ? "" : map_.at("tbc"); }
 
  private:
   // TODO(WAN): The value-type may change in the future depending on what tagged dictionaries need.
@@ -35,7 +35,7 @@ class ModuleMetadata {
       : compile_time_metadata_(compile_time_metadata) {}
 
   /** @return The metadata collected at compile-time for this module. */
-  const CompileTimeModuleMetadata &GetCompileTimeMetadata() const { return compile_time_metadata_; }
+  [[nodiscard]] const CompileTimeModuleMetadata &GetCompileTimeMetadata() const { return compile_time_metadata_; }
 
  private:
   CompileTimeModuleMetadata compile_time_metadata_;  ///< Metadata that was gathered at compile-time.
