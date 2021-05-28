c5770a71be13faf9b5c4d1d6dd414abadef54ac4
turingcompl33t <kdotterrer@gmail.com>
2021-05-28 15:10:47 -0400

    update format for cte traces with actual SQL comments

--- script/testing/junit/sql/cte_iterative.sql
+++ script/testing/junit/sql/cte_iterative.sql
@@ -1,7 +1,7 @@
-# Input queries for iterative CTE integration tests.
-#
-# Postgres does not support iterative CTEs. Therefore, we 
-# must manually verify the output from this trace.
+-- Input queries for iterative CTE integration tests.
+--
+-- Postgres does not support iterative CTEs. Therefore, we 
+-- must manually verify the output from this trace.
 
 CREATE TABLE tree (node INTEGER, parent INTEGER);
 INSERT INTO tree VALUES (1,NULL), (10, 1), (11, 1), (100, 10), (101, 10), (110, 11), (111, 11);
--- script/testing/junit/sql/sqlite-with2.sql
+++ script/testing/junit/sql/sqlite-with2.sql
@@ -1,7 +1,7 @@
-# CTE-related integration tests from SQLite trace file `with2.test`
+-- CTE-related integration tests from SQLite trace file `with2.test`
 
-# ---------------------------------------------------------
-# with2.test, section 1
+-----------------------------------------------------------
+-- with2.test, section 1
 
 CREATE TABLE t1(a INTEGER);
 INSERT INTO t1 VALUES(1);
@@ -9,14 +9,14 @@ INSERT INTO t1 VALUES(2);
 
 WITH x1 AS (SELECT * FROM t1) SELECT sum(a) FROM x1;
 
-# We crash on this query; Postgres succeeds
-# WITH x1 AS (SELECT * FROM t1) SELECT (SELECT sum(a) FROM x1);
+-- We crash on this query; Postgres succeeds
+-- WITH x1 AS (SELECT * FROM t1) SELECT (SELECT sum(a) FROM x1);
 
-# We crash on this query; Postgres succeeds
-# WITH x1 AS (SELECT * FROM t1) SELECT (SELECT sum(a) FROM x1), (SELECT max(a) FROM x1);
+-- We crash on this query; Postgres succeeds
+-- WITH x1 AS (SELECT * FROM t1) SELECT (SELECT sum(a) FROM x1), (SELECT max(a) FROM x1);
 
-# This fails in the parser (TargetTransform==null)
-# WITH RECURSIVE t4(x) AS (VALUES(4) UNION ALL SELECT x+1 FROM t4 WHERE x<10) SELECT * FROM t4;
+-- This fails in the parser (TargetTransform==null)
+-- WITH RECURSIVE t4(x) AS (VALUES(4) UNION ALL SELECT x+1 FROM t4 WHERE x<10) SELECT * FROM t4;
 
-# terminator; the testing infrastructure chokes on trailing comments
+-- terminator; the testing infrastructure chokes on trailing comments
 SELECT 1;
