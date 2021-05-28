cbe6e22155b989e930559aecb3061f224f3d5495
turingcompl33t <kdotterrer@gmail.com>
2021-05-28 10:46:37 -0400

    add some queries from postgres regression tests; takeaway is that we are not ready to merge, we have bugs to fix

--- script/testing/junit/sql/cte.sql
+++ script/testing/junit/sql/cte.sql
@@ -1,4 +1,7 @@
-# Input queries for vanilla (simple) CTE integration tests.
+-- Input queries for vanilla (simple) CTE integration tests.
+
+-----------------------------------------------------------
+-- Hand-Rolled Test Cases
 
 WITH cte(x) AS (SELECT 1) SELECT x FROM cte;
 WITH cte AS (SELECT 1) SELECT * FROM cte;
@@ -6,8 +9,8 @@ WITH cte AS (SELECT 1 AS "x") SELECT * FROM cte;
 WITH cte(y) AS (SELECT 1 AS "x") SELECT y FROM cte;
 WITH cte(x) AS (WITH cte2(y) AS (SELECT 1) SELECT y FROM cte2) SELECT x FROM cte;
 
-# TODO: We currently fail on this query because of the arbitrary SELECT * ordering
-# WITH cte(x) AS (SELECT 1), cte2(y) AS (SELECT 2) SELECT * FROM cte INNER JOIN cte2 ON cte.x+1 = cte2.y;
+-- TODO: We currently fail on this query because of the arbitrary SELECT * ordering
+-- WITH cte(x) AS (SELECT 1), cte2(y) AS (SELECT 2) SELECT * FROM cte INNER JOIN cte2 ON cte.x+1 = cte2.y;
 
 WITH cte(x) AS (SELECT 1), cte2(y) AS (SELECT 2) SELECT cte.x, cte2.y FROM cte INNER JOIN cte2 ON cte.x+1 = cte2.y;
 
@@ -15,7 +18,7 @@ WITH cte(x,x) AS (SELECT 1, 2) SELECT * FROM cte;
 WITH cte AS (SELECT 4, 3) SELECT * FROM cte;
 WITH cte(y,y,x) AS (SELECT 5,4,3) SELECT x FROM cte;
 
-# Porting over junit test cases
+-- Port of some JUnit test cases
 CREATE TABLE company (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, age INT NOT NULL, address CHAR(50), salary REAL);
 INSERT INTO company (id,name,age,address,salary) VALUES (1, 'Paul', 32, 'California', 20000.00);
 INSERT INTO company (id,name,age,address,salary) VALUES (2, 'George', 21, 'NY', 10000.00);
@@ -23,15 +26,24 @@ INSERT INTO company (id,name,age,address,salary) VALUES (2, 'George', 21, 'NY', 
 WITH employee AS (SELECT id, name, age FROM company) SELECT name FROM employee;
 WITH employee AS (SELECT age+age AS sumage, name FROM company) SELECT E2.name, E1.sumage FROM employee AS E1, employee AS E2 WHERE E1.name = E2.name;
 
-# Aggregate inside cte query
+-- Aggregate inside CTE query
 WITH employee AS (SELECT SUM(age) AS sumage FROM company) SELECT * FROM employee;
-# Join inside cte query
+
+-- Join inside CTE query
 WITH employee AS (SELECT C1.name AS name, C2.age AS age FROM company AS C1, company AS C2) SELECT * FROM employee;
-# Aggregate with alias inside cte query
+
+-- Aggregate with alias inside CTE query
 WITH employee AS (SELECT MAX(age) AS mxage FROM company) SELECT E2.name, E2.age FROM employee AS E1, company AS E2 WHERE E1.mxage = E2.age;
 
 CREATE TABLE tmp(x INT);
 INSERT INTO tmp VALUES (1), (2);
-# CTE with self-join
+
+-- CTE with self-join
 WITH cte AS (SELECT * FROM tmp) SELECT A.x, B.x, C.x FROM cte A, cte B, tmp C ORDER BY A.x, B.x, C.x;
 WITH cte AS (SELECT * FROM tmp) SELECT A.x, B.x, C.x FROM cte A, cte B, cte C ORDER BY A.x, B.x, C.x;
+
+-----------------------------------------------------------
+-- Adapted from Postgres Regression Test (`with.sql`)
+-- https://github.com/postgres/postgres/blob/master/src/test/regress/sql/with.sql
+
+WITH q1(x,y) AS (SELECT 1,2) SELECT * FROM q1, q1 AS q2;
--- script/testing/junit/sql/cte_recursive.sql
+++ script/testing/junit/sql/cte_recursive.sql
@@ -1,6 +1,60 @@
-# Input queries for recursive CTE integration tests.
+-- Input queries for recursive CTE integration tests.
 
-# Baseline recursive CTE
+-----------------------------------------------------------
+-- Hand-Rolled Test Cases
+
+-- Baseline recursive CTE
 CREATE TABLE tree (node INTEGER, parent INTEGER);
 INSERT INTO tree VALUES (1,NULL), (10, 1), (11, 1), (100, 10), (101, 10), (110, 11), (111, 11);
 WITH RECURSIVE cte(x) AS (SELECT 1 UNION ALL SELECT tree.node FROM tree INNER JOIN cte ON tree.parent=cte.x) SELECT * FROM cte;
+
+-----------------------------------------------------------
+-- Adapted from Postgres Regression Test (`with.sql`)
+-- https://github.com/postgres/postgres/blob/master/src/test/regress/sql/with.sql
+
+-- sum of 1..100
+-- TODO: Fails in parser (TargetTransform root==null)
+-- WITH RECURSIVE t(n) AS (VALUES (1) UNION ALL SELECT n+1 FROM t WHERE n < 100) SELECT sum(n) FROM t;
+
+-- TODO: Fails in parser (TargetTransform root==null)
+-- WITH RECURSIVE t(n) AS (SELECT (VALUES(1)) UNION ALL SELECT n+1 FROM t WHERE n < 5) SELECT * FROM t;
+
+-- This is an infinite loop with UNION ALL, but not with UNION
+-- TODO: We loop infinitely on this
+-- WITH RECURSIVE t(n) AS (SELECT 1 UNION SELECT 10-n FROM t) SELECT * FROM t;
+
+-- This'd be an infinite loop, but outside query reads only as much as needed
+-- TODO: Fails in parser (TargetTransform root==null)
+-- WITH RECURSIVE t(n) AS (VALUES (1) UNION ALL SELECT n+1 FROM t) SELECT * FROM t LIMIT 10;
+
+-- TODO: Fails in parser
+-- WITH RECURSIVE y (id) AS (VALUES (1)), x (id) AS (SELECT * FROM y UNION ALL SELECT id+1 FROM x WHERE id < 5) SELECT * FROM x;
+
+-- TODO: Fails in parser
+-- WITH RECURSIVE x(id) AS (SELECT * FROM y UNION ALL SELECT id+1 FROM x WHERE id < 5), y(id) AS (values (1)) SELECT * FROM x;
+
+-- subquery
+-- TODO: Fails in parser
+-- WITH RECURSIVE x(n) AS (SELECT 1 UNION ALL SELECT n+1 FROM x WHERE n IN (SELECT * FROM x)) SELECT * FROM x;
+
+CREATE TABLE department (id INTEGER PRIMARY KEY, parent_department INTEGER, name TEXT);
+
+INSERT INTO department VALUES (0, NULL, 'ROOT');
+INSERT INTO department VALUES (1, 0, 'A');
+INSERT INTO department VALUES (2, 1, 'B');
+INSERT INTO department VALUES (3, 2, 'C');
+INSERT INTO department VALUES (4, 2, 'D');
+INSERT INTO department VALUES (5, 0, 'E');
+INSERT INTO department VALUES (6, 4, 'F');
+INSERT INTO department VALUES (7, 5, 'G');
+
+-- extract all departments under 'A'. Result should be A, B, C, D and F
+-- TODO: Crashes the DBMS
+-- WITH RECURSIVE subdepartment AS (SELECT name as root_name, * FROM department WHERE name = 'A' UNION ALL SELECT sd.root_name, d.* FROM department AS d, subdepartment AS sd WHERE d.parent_department = sd.id) SELECT * FROM subdepartment ORDER BY name;
+
+-- handle the case were recursive structure is ignored
+-- TODO: Crashes the DBMS
+-- WITH RECURSIVE subdepartment AS (SELECT * FROM department WHERE name = 'A') SELECT * FROM subdepartment ORDER BY name;
+
+-- Terminator; TracefileTest chokes on trailing comments
+SELECT 1;
