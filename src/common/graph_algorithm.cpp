147f47bb20937404714f77df3a13cd9cb3c35e96
turingcompl33t <kdotterrer@gmail.com>
2021-06-01 16:37:37 -0400

    cycle detection and toplogical sort for table reference dependencies

--- src/common/graph_algorithm.cpp
+++ src/common/graph_algorithm.cpp
@@ -1,21 +1,13 @@
 #include "common/graph_algorithm.h"
 
 #include <algorithm>
+#include <functional>
+#include <stack>
 
 #include "common/graph.h"
+#include "common/macros.h"
 
 namespace noisepage::common::graph {
-
-namespace detail {
-/**
- * Encodes the state of individual nodes in graph traversal algorithms.
- */
-enum class NodeState {
-  UNVISITED,
-  IN_PROGRESS,
-  VISITED,
-};
-}  // namespace detail
 
 // ----------------------------------------------------------------------------
 // graph::Isomorphic
@@ -54,70 +46,162 @@ bool Isomorphic(const Graph &lhs, const Graph &rhs) {
 // graph::HasCycle
 // ----------------------------------------------------------------------------
 
-// namespace detail {
+namespace detail {
+/**
+ * Encodes the state of individual nodes in cycle detection algorithm.
+ */
+enum class NodeState {
+  UNVISITED,
+  IN_PROGRESS,
+  VISITED,
+};
 
-// /**
-//  * @brief Determine if the DFS tree rooted at the current top of `to_visit` contains a cycle.
-//  * @param graph The graph in which the search is performed
-//  * @param node_states A map from transaction ID to the current state of the transaction in the search
-//  * @param to_visit A stack of the transaction IDs that remain to be visited
-//  * @param youngest_txn The out-parameter used to return the transaction ID of
-//  * the youngest transaction in the first cycle found in the graph, if present
-//  */
-// bool HasCycleFrom(std::unordered_map<txn_id_t, std::vector<txn_id_t>> *graph,
-//                                   std::unordered_map<txn_id_t, NodeState> *node_states, std::stack<txn_id_t>
-//                                   *to_visit, txn_id_t *youngest_txn) {
-//   auto &adjacent = (*graph)[to_visit->top()];
-//   std::sort(adjacent.begin(), adjacent.end());
-//   for (const auto &txn : adjacent) {
-//     const auto state = (*node_states)[txn];
-//     if (NodeState::IN_PROGRESS == state) {
-//       *youngest_txn = YoungestTransactionInCycle(to_visit, txn);
-//       return true;
-//     }
+/**
+ * @brief Determine if the DFS tree rooted at the current top of `to_visit` contains a cycle.
+ * @param graph The graph in which the search is performed
+ * @param node_states A map from transaction ID to the current state of the transaction in the search
+ * @param to_visit A stack of the transaction IDs that remain to be visited
+ * @param youngest_txn The out-parameter used to return the transaction ID of
+ * the youngest transaction in the first cycle found in the graph, if present
+ */
+bool HasCycleFrom(const Graph &graph, std::unordered_map<std::size_t, NodeState> *node_states,
+                  std::stack<std::size_t> *to_visit) {
+  // TODO(Kyle): This is gross, sorting the vertices WITHIN the graph...
+  const auto id = to_visit->top();
+  std::vector<std::size_t> adjacent{graph.AdjacenciesFor(id).cbegin(), graph.AdjacenciesFor(id).cend()};
+  std::sort(adjacent.begin(), adjacent.end());
 
-//     if (NodeState::UNVISITED == state) {
-//       to_visit->push(txn);
-//       (*node_states)[txn] = NodeState::IN_PROGRESS;
-//       if (DFSTreeHasCycle(graph, node_states, to_visit, youngest_txn)) {
-//         return true;
-//       }
-//     }
-//   }
+  for (const auto &vertex : adjacent) {
+    const auto state = (*node_states)[vertex];
+    if (state == NodeState::IN_PROGRESS) {
+      return true;
+    }
 
-//   (*node_states)[to_visit->top()] = NodeState::VISITED;
-//   to_visit->pop();
+    if (state == NodeState::UNVISITED) {
+      to_visit->push(vertex);
+      (*node_states)[vertex] = NodeState::IN_PROGRESS;
+      if (HasCycleFrom(graph, node_states, to_visit)) {
+        return true;
+      }
+    }
+  }
 
-//   // no cycles found in this DFS tree
-//   return false;
-// }
-// }  // namespace detail
+  (*node_states)[to_visit->top()] = NodeState::VISITED;
+  to_visit->pop();
+
+  // No cycles found rooted at this vertex!
+  return false;
+}
+}  // namespace detail
 
 bool HasCycle(const Graph &graph) {
   // Initialize vertex states
-  // std::unordered_map<std::size_t, detail::NodeState> node_states{};
-  // for (const auto &id : graph.VertexSet()) {
-  //   node_states[id] = detail::NodeState::UNVISITED;
-  // }
+  std::unordered_map<std::size_t, detail::NodeState> node_states{};
+  for (const auto &id : graph.VertexSet()) {
+    node_states[id] = detail::NodeState::UNVISITED;
+  }
 
-  // // ensure that we always search in order of transaction ID (increasing)
-  // std::vector<std::size_t> to_search{};
-  // to_search.reserve();
-  // std::transform(graph->cbegin(), graph->cend(), std::back_inserter(to_search), [](const auto &p) { return p.first;
-  // }); std::sort(to_search.begin(), to_search.end());
+  // Ensure that we always search in order of vertex ID (increasing)
+  // TODO(Kyle): May or may not actually need this functionality
+  const auto vertex_set = graph.VertexSet();
+  std::vector<std::size_t> to_search{vertex_set.cbegin(), vertex_set.cend()};
+  std::sort(to_search.begin(), to_search.end());
 
-  // for (const auto &txn : to_search) {
-  //   if (NodeState::UNVISITED == node_states[txn]) {
-  //     std::stack<txn_id_t> to_visit{};
-  //     to_visit.push(txn);
-  //     if (DFSTreeHasCycle(graph, &node_states, &to_visit, txn_id)) {
-  //       return true;
-  //     }
-  //   }
-  // }
+  for (const auto &vertex : to_search) {
+    if (node_states[vertex] == detail::NodeState::UNVISITED) {
+      std::stack<std::size_t> to_visit{};
+      to_visit.push(vertex);
+      if (HasCycleFrom(graph, &node_states, &to_visit)) {
+        return true;
+      }
+    }
+  }
 
-  // no cycles found
+  // No cycles present!
   return false;
+}
+
+// ----------------------------------------------------------------------------
+// graph::TopologicalSort
+// ----------------------------------------------------------------------------
+
+namespace detail {
+/**
+ * Denotes the visitation state of a node in graph traversal for topological sort.
+ * NOTE: The semantics of node color are adapted from the description in CLRS.
+ */
+enum class NodeColor { WHITE, GRAY, BLACK };
+
+/**
+ * Helper function for recursive depth-first graph traversal.
+ * @param graph The graph in which traversal is performed
+ * @param vertex The vertex that is currently being visited
+ * @param finishing_times The finishing times data structure
+ * @param colors The colors data structure
+ * @param time The time at which visitiation of the current vertex was initiated
+ */
+static void FinishingTimeDFSVisit(const Graph &graph, std::size_t vertex,
+                                  std::unordered_map<std::size_t, std::size_t> *finishing_times,
+                                  std::unordered_map<std::size_t, NodeColor> *colors, const std::size_t time) {
+  (*colors)[vertex] = NodeColor::GRAY;
+  for (const auto &adjacent : graph.AdjacenciesFor(vertex)) {
+    if ((*colors)[adjacent] == NodeColor::WHITE) {
+      FinishingTimeDFSVisit(graph, adjacent, finishing_times, colors, time + 1);
+    }
+  }
+  (*colors)[vertex] = NodeColor::BLACK;
+  (*finishing_times)[vertex] = time + 2;
+}
+
+/**
+ * Perform "finishing-time depth-first search" on graph `graph`.
+ *
+ * NOTE: This is a special-case implementation of the DFS algorithm
+ * from CLRS because we only care about finishing time for the
+ * purposes of computing a topological sort. It might be better practice
+ * to write a general-purpose DFS, but this is a more expedient solution.
+ *
+ * @param graph The input graph
+ * @return A map that denotes finishing time for each vertex in `graph`
+ *  Node ID -> Finishing Time
+ */
+static std::unordered_map<std::size_t, std::size_t> FinishingTimeDFS(const Graph &graph) {
+  // The data structure we will populate
+  std::unordered_map<std::size_t, std::size_t> finishing_times{};
+
+  // Temporary data structure for the traversal
+  std::unordered_map<std::size_t, NodeColor> colors{};
+  for (const auto &vertex : graph.VertexSet()) {
+    colors[vertex] = NodeColor::WHITE;
+  }
+
+  std::size_t time = 0;
+  for (const auto &vertex : graph.VertexSet()) {
+    if (colors[vertex] == NodeColor::WHITE) {
+      FinishingTimeDFSVisit(graph, vertex, &finishing_times, &colors, time);
+    }
+  }
+
+  return finishing_times;
+}
+}  // namespace detail
+
+std::vector<std::size_t> TopologicalSort(const Graph &graph) {
+  NOISEPAGE_ASSERT(!HasCycle(graph), "Graph must be acyclic to compute a topological sort");
+
+  // Compute the finishing time for each node in the graph
+  const auto finishing_times = detail::FinishingTimeDFS(graph);
+  NOISEPAGE_ASSERT(finishing_times.size() == graph.Order(), "Broken Invariant");
+
+  // Compute the topological sort from finishing times;
+  // topological sort is determined by reverse order of finishing time
+  const auto vertex_set = graph.VertexSet();
+  std::vector<std::size_t> topological_sort{vertex_set.cbegin(), vertex_set.cend()};
+  std::sort(topological_sort.begin(), topological_sort.end(),
+            [&finishing_times](const std::size_t &a_id, const std::size_t &b_id) {
+              return finishing_times.at(a_id) > finishing_times.at(b_id);
+            });
+  return topological_sort;
 }
 
 }  // namespace noisepage::common::graph
--- test/common/graph_algorithm_test.cpp
+++ test/common/graph_algorithm_test.cpp
@@ -1,5 +1,7 @@
 #include "common/graph_algorithm.h"
 
+#include <numeric>
+
 #include "common/graph.h"
 #include "test_util/test_harness.h"
 
@@ -47,5 +49,103 @@ TEST_F(GraphAlgorithmTest, Isomorphic2) {
 // ----------------------------------------------------------------------------
 // graph::HasCycle
 // ----------------------------------------------------------------------------
+
+TEST_F(GraphAlgorithmTest, HasCycle0) {
+  Graph g{};
+  EXPECT_FALSE(graph::HasCycle(g));
+}
+
+TEST_F(GraphAlgorithmTest, HasCycle1) {
+  Graph g{};
+  g.AddEdge(0, 1);
+  g.AddEdge(1, 2);
+  EXPECT_FALSE(graph::HasCycle(g));
+}
+
+TEST_F(GraphAlgorithmTest, HasCycle2) {
+  Graph g{};
+  g.AddEdge(0, 1);
+  g.AddEdge(1, 0);
+  EXPECT_TRUE(graph::HasCycle(g));
+}
+
+TEST_F(GraphAlgorithmTest, HasCycle3) {
+  Graph g{};
+  g.AddEdge(0, 1);
+  g.AddEdge(1, 2);
+  g.AddEdge(2, 0);
+  EXPECT_TRUE(graph::HasCycle(g));
+}
+
+TEST_F(GraphAlgorithmTest, HasCycle4) {
+  static constexpr const std::size_t graph_order = 10;
+
+  Graph g{};
+  for (std::size_t i = 0; i < graph_order; ++i) {
+    g.AddEdge(i, i + 1);
+  }
+  EXPECT_FALSE(graph::HasCycle(g));
+}
+
+TEST_F(GraphAlgorithmTest, HasCycle5) {
+  static constexpr const std::size_t graph_order = 10;
+
+  Graph g{};
+  for (std::size_t i = 0; i < graph_order; ++i) {
+    g.AddEdge(i, i + 1);
+  }
+  g.AddEdge(graph_order, 0);
+  EXPECT_TRUE(graph::HasCycle(g));
+}
+
+// ----------------------------------------------------------------------------
+// graph::TopologicalSort
+// ----------------------------------------------------------------------------
+
+TEST_F(GraphAlgorithmTest, TopologicalSort0) {
+  Graph g{};
+  const auto order = graph::TopologicalSort(g);
+  EXPECT_EQ(0UL, order.size());
+}
+
+TEST_F(GraphAlgorithmTest, TopologicalSort1) {
+  Graph g{};
+  g.AddEdge(0, 1);
+
+  const std::vector<std::size_t> expected{1, 0};
+
+  const auto order = graph::TopologicalSort(g);
+  EXPECT_EQ(2UL, order.size());
+  EXPECT_EQ(expected, order);
+}
+
+TEST_F(GraphAlgorithmTest, TopologicalSort2) {
+  Graph g{};
+  g.AddEdge(0, 1);
+  g.AddEdge(1, 2);
+
+  const std::vector<std::size_t> expected{2, 1, 0};
+
+  const auto order = graph::TopologicalSort(g);
+  EXPECT_EQ(3UL, order.size());
+  EXPECT_EQ(expected, order);
+}
+
+TEST_F(GraphAlgorithmTest, TopologicalSort3) {
+  static constexpr const std::size_t graph_order = 10;
+
+  Graph g{};
+  for (std::size_t i = 0; i < graph_order - 1; ++i) {
+    g.AddEdge(i, i + 1);
+  }
+
+  std::vector<std::size_t> expected(graph_order);
+  std::iota(expected.begin(), expected.end(), 0);
+  std::reverse(expected.begin(), expected.end());
+
+  const auto order = graph::TopologicalSort(g);
+  EXPECT_EQ(graph_order, order.size());
+  EXPECT_EQ(expected, order);
+}
 
 }  // namespace noisepage
