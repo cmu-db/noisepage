8e036b4310e2e8910f9d1c6ec254075d153426de
Wan Shen Lim <wanshen.lim@gmail.com>
2021-06-14 12:33:00 -0400

    Only clone oltpbench with depth 1.

--- script/testing/oltpbench/constants.py
+++ script/testing/oltpbench/constants.py
@@ -6,8 +6,8 @@ from ..util.constants import DIR_TMP
 OLTPBENCH_GIT_URL = "https://github.com/oltpbenchmark/oltpbench.git"
 OLTPBENCH_GIT_LOCAL_PATH = os.path.join(DIR_TMP, "oltpbench")
 OLTPBENCH_GIT_CLEAN_COMMAND = "rm -rf {}".format(OLTPBENCH_GIT_LOCAL_PATH)
-OLTPBENCH_GIT_CLONE_COMMAND = "git clone {} {}".format(OLTPBENCH_GIT_URL,
-                                                 OLTPBENCH_GIT_LOCAL_PATH)
+OLTPBENCH_GIT_CLONE_COMMAND = "git clone --depth 1 {} {}".format(OLTPBENCH_GIT_URL,
+                                                                 OLTPBENCH_GIT_LOCAL_PATH)
 
 # OLTPBench default settings.
 OLTPBENCH_DEFAULT_TIME = 30
