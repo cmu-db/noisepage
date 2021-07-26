4ce441189302a175f3daf2d77ee14c2559b9e63a
Wan Shen Lim <wanshen.lim@gmail.com>
2021-07-26 05:56:55 -0400

    Adapt CI framework to use oltpbench_tim.

--- script/testing/oltpbench/constants.py
+++ script/testing/oltpbench/constants.py
@@ -3,11 +3,15 @@ import os
 from ..util.constants import DIR_TMP
 
 # git settings for OLTPBench.
+OLTPBENCH_VERSION = "oltpbench2-20.1.3-SNAPSHOT"
 OLTPBENCH_GIT_URL = "https://github.com/oltpbenchmark/oltpbench.git"
 OLTPBENCH_GIT_LOCAL_PATH = os.path.join(DIR_TMP, "oltpbench")
+OLTPBENCH_GIT_TARGET_PATH = os.path.join(OLTPBENCH_GIT_LOCAL_PATH, "target")
+OLTPBENCH_GIT_FINAL_PATH = os.path.join(OLTPBENCH_GIT_TARGET_PATH, OLTPBENCH_VERSION)
 OLTPBENCH_GIT_CLEAN_COMMAND = "rm -rf {}".format(OLTPBENCH_GIT_LOCAL_PATH)
-OLTPBENCH_GIT_CLONE_COMMAND = "git clone --depth 1 {} {}".format(OLTPBENCH_GIT_URL,
-                                                                 OLTPBENCH_GIT_LOCAL_PATH)
+OLTPBENCH_GIT_CLONE_COMMAND = "git clone --single-branch --branch oltpbench_tim --depth 1 {} {}".format(
+    OLTPBENCH_GIT_URL,
+    OLTPBENCH_GIT_LOCAL_PATH)
 
 # OLTPBench default settings.
 OLTPBENCH_DEFAULT_TIME = 30
@@ -21,7 +25,7 @@ OLTPBENCH_DEFAULT_PASSWORD = "postgres"
 OLTPBENCH_DEFAULT_DBTYPE = "noisepage"
 OLTPBENCH_DEFAULT_DRIVER = "org.postgresql.Driver"
 OLTPBENCH_DEFAULT_RATE = "unlimited"
-OLTPBENCH_DEFAULT_BIN = os.path.join(OLTPBENCH_GIT_LOCAL_PATH, "oltpbenchmark")
+OLTPBENCH_DEFAULT_BIN = "java -jar oltpbench2.jar "
 OLTPBENCH_DEFAULT_DATABASE_RESTART = True
 OLTPBENCH_DEFAULT_DATABASE_CREATE = True
 OLTPBENCH_DEFAULT_DATABASE_LOAD = True
@@ -30,17 +34,8 @@ OLTPBENCH_DEFAULT_REPORT_SERVER = None
 OLTPBENCH_DEFAULT_WAL_ENABLE = True
 OLTPBENCH_DEFAULT_CONTINUE_ON_ERROR = False
 
-OLTPBENCH_DIR_CONFIG = os.path.join(OLTPBENCH_GIT_LOCAL_PATH, "config")
-OLTPBENCH_DIR_TEST_RESULT = os.path.join(OLTPBENCH_GIT_LOCAL_PATH, "results")
-
-# ant commands for invoking OLTPBench.
-OLTPBENCH_ANT_BUILD_FILE = os.path.join(OLTPBENCH_GIT_LOCAL_PATH, "build.xml")
-OLTPBENCH_ANT_COMMANDS = [
-    "ant bootstrap -buildfile {}".format(OLTPBENCH_ANT_BUILD_FILE),
-    "ant resolve -buildfile {}".format(OLTPBENCH_ANT_BUILD_FILE),
-    "ant clean -buildfile {}".format(OLTPBENCH_ANT_BUILD_FILE),
-    "ant build -buildfile {}".format(OLTPBENCH_ANT_BUILD_FILE),
-]
+OLTPBENCH_DIR_CONFIG = os.path.join(OLTPBENCH_GIT_FINAL_PATH, "config", "noisepage")
+OLTPBENCH_DIR_TEST_RESULT = os.path.join(OLTPBENCH_GIT_FINAL_PATH, "results")
 
 # API endpoints for Performance Storage Service
 # Each pair represents different environment. One could choose where the benchmark testing result will be uploaded to
