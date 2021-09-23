9d5a4a40d8432e498963dc29b4d2a1ddc42e2f4b
turingcompl33t <kdotterrer@gmail.com>
2021-09-23 10:04:13 -0400

    update postgresparser

--- src/parser/postgresparser.cpp
+++ src/parser/postgresparser.cpp
@@ -108,6 +108,8 @@ static std::optional<BaseFunctionParameter::DataType> TypeNameToDataType(const c
     data_type = BaseFunctionParameter::DataType::DOUBLE;
   } else if ((strcmp(name, "real") == 0) || (strcmp(name, "float4") == 0)) {
     data_type = BaseFunctionParameter::DataType::FLOAT;
+  } else if ((strcmp(name, "decimal") == 0) || strcmp(name, "numeric") == 0) {
+    return BaseFunctionParameter::DataType::DECIMAL;
   } else if (strcmp(name, "text") == 0) {
     data_type = BaseFunctionParameter::DataType::TEXT;
   } else if (strcmp(name, "bpchar") == 0) {
@@ -1704,6 +1706,7 @@ std::unique_ptr<FuncParameter> PostgresParser::FunctionParameterTransform(ParseR
                                                                           FunctionParameter *root) {
   // TODO(WAN): significant code duplication, refactor out char* -> DataType
   char *name = (reinterpret_cast<value *>(root->arg_type_->names_->tail->data.ptr_value)->val_.str_);
+  std::cout << name << std::endl;
   auto data_type = TypeNameToDataType(name);
   if (!data_type.has_value()) {
     PARSER_LOG_AND_THROW("FunctionParameterTransform", "DataType", name);
